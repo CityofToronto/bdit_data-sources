f46af355201f0ddf21e0709410d22429246b5c08
Raphael Dumas <raphael.a.dumas@gmail.com>
2016-08-30 10:44:30 -0400

    Updating copy-data with a main block

--- inrix/python/copy-data.py
+++ inrix/python/copy-data.py
@@ -26,29 +26,30 @@ def getYYYYMM (filename):
     #Return last 6 digit match in filename
     return res[len(res)-1]
 
-for filename in os.listdir():
-    if filename.endswith('.csv') and bool(regex_yyyymm.search(filename)): 
-        yyyymm = getYYYYMM(filename)
-        table = 'inrix.raw_data'+yyyymm
-        logger.info('Creating table %(table)s', {'table': AsIs(table)})
-        cursor.execute('CREATE TABLE IF NOT EXISTS %(table)s () INHERITS (inrix.raw_data)', {'table': AsIs(table)})
-        #cursor.execute('ALTER TABLE {table} OWNER TO rdumas'.format(table=table))
-        cursor.execute('ALTER TABLE %(table)s SET UNLOGGED', {'table': AsIs(table)})
-        con.commit()
-        datafile = open(filename)
-        logger.info("Copying data from: {filename}".format(filename=datafile.name))
-        #Performance tweaks to speed up copying allegedly.
-        cursor.execute('SET LOCAL synchronous_commit=off;')
-        #Creating a temporary table to COPY data to, and then fix timestamps when inserting into {table}
-        cursor.execute('CREATE TEMP TABLE raw_data_import (LIKE inrix.raw_data);')
-        cursor.copy_expert("COPY raw_data_import FROM STDIN WITH CSV",datafile)
-        cursor.execute('TRUNCATE %(table)s ;', {'table': AsIs(table)})
-        cursor.execute('INSERT INTO %(table)s '
-                       'SELECT (tx AT TIME ZONE \'UTC\') AT TIME ZONE \'America/Toronto\' AS tx, tmc, speed, score '
-                       'FROM raw_data_import;', {'table': AsIs(table)})
-        con.commit()
-        datafile.close()
-        logger.info("Setting table status to logged")
-        cursor.execute('ALTER TABLE %(table)s SET LOGGED', {'table': AsIs(table)})
-        con.commit()
+if __name__ = "__main__":
+    for filename in os.listdir():
+        if filename.endswith('.csv') and bool(regex_yyyymm.search(filename)): 
+            yyyymm = getYYYYMM(filename)
+            table = 'inrix.raw_data'+yyyymm
+            logger.info('Creating table %(table)s', {'table': AsIs(table)})
+            cursor.execute('CREATE TABLE IF NOT EXISTS %(table)s () INHERITS (inrix.raw_data)', {'table': AsIs(table)})
+            #cursor.execute('ALTER TABLE {table} OWNER TO rdumas'.format(table=table))
+            cursor.execute('ALTER TABLE %(table)s SET UNLOGGED', {'table': AsIs(table)})
+            con.commit()
+            datafile = open(filename)
+            logger.info("Copying data from: {filename}".format(filename=datafile.name))
+            #Performance tweaks to speed up copying allegedly.
+            cursor.execute('SET LOCAL synchronous_commit=off;')
+            #Creating a temporary table to COPY data to, and then fix timestamps when inserting into {table}
+            cursor.execute('CREATE TEMP TABLE raw_data_import (LIKE inrix.raw_data);')
+            cursor.copy_expert("COPY raw_data_import FROM STDIN WITH CSV",datafile)
+            cursor.execute('TRUNCATE %(table)s ;', {'table': AsIs(table)})
+            cursor.execute('INSERT INTO %(table)s '
+                           'SELECT (tx AT TIME ZONE \'UTC\') AT TIME ZONE \'America/Toronto\' AS tx, tmc, speed, score '
+                           'FROM raw_data_import;', {'table': AsIs(table)})
+            con.commit()
+            datafile.close()
+            logger.info("Setting table status to logged")
+            cursor.execute('ALTER TABLE %(table)s SET LOGGED', {'table': AsIs(table)})
+            con.commit()
         
