a8aed31b5131f040a071dcc8a17b6585f2d5a834
Raphael Dumas <raphael.a.dumas@gmail.com>
2016-08-31 14:42:05 -0400

    Index creation improvement
    Separating sql functions
    Adding command-line arguments
    Adding testing

---
+++ inrix/sql/raw_indexes/raw_tmc_index.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION inrix.create_raw_tmc_idx (tablename TEXT)
+RETURNS INTEGER
+AS $$
+BEGIN
+    EXECUTE format('CREATE INDEX ON %I (tmc);', tablename);
+    RETURN 1;
+END;
+$$
+LANGUAGE plpgsql;
+GRANT EXECUTE ON FUNCTION inrix.create_raw_tmc_idx (TEXT) TO rdumas_py;
---
+++ inrix/sql/raw_indexes/raw_tx_partial_index.sql
@@ -0,0 +1,13 @@
+/*Reducing timestamp index size by applying it only to rows
+  where the speed record is based on observed data (score=30)*/
+
+CREATE OR REPLACE FUNCTION inrix.create_raw_tx_idx (tablename TEXT)
+RETURNS INTEGER
+AS $$
+BEGIN
+    EXECUTE format('CREATE INDEX ON %I (tx) WHERE score = 30;', tablename);
+    RETURN 1;
+END;
+$$
+LANGUAGE plpgsql;
+GRANT EXECUTE ON FUNCTION inrix.create_raw_score_idx (TEXT) TO rdumas_py;
