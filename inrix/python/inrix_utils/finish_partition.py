f1a23c0ea9ed5ceb41dc90fb05a25f17c6c5d1a1
Raphael Dumas <raphael.a.dumas@gmail.com>
2016-10-25 12:10:50 -0400

    Fixes #10
    Created execute_function in `utils` and then refactored all the
    utilities to use it

--- inrix/python/inrix_utils/finish_partition.py
+++ inrix/python/inrix_utils/finish_partition.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 '''Finish partitioning tables in the Inrix database.'''
 import logging
-from time import sleep
-from psycopg2 import connect, OperationalError
 from psycopg2.extensions import AsIs
-from utils import get_yyyymm, get_yyyymmdd, try_connection
+from utils import get_yyyymm, get_yyyymmdd, try_connection, execute_function
 
-def _partition_table(tableyyyymm, startdate, logger, cursor, timecol):
+def _partition_table(logger, cursor, **kwargs):
     '''Add check constraints on the inrix.raw_data partitioned table ending with yyyymm.'''
-    logger.info('Adding check constraints on table %s', tableyyyymm)
+    logger.info('Adding check constraints on table %s', kwargs['tableyyyymm'])
     cursor.execute("ALTER TABLE %(table)s ADD CHECK (%(timecol)s >= DATE %(startdate)s "
                    "AND %(timecol)s < DATE %(startdate)s + INTERVAL '1 month')"
-                   , {'table':AsIs(tableyyyymm), 'startdate':startdate,
-                   'timecol':AsIs(timecol)})
+                   , {'table':AsIs(kwargs['tableyyyymm']), 'startdate':kwargs['startdate'],
+                   'timecol':AsIs(kwargs['timecol'])})
 
 def partition_tables(years, dbset, logger, table, timecol):
     '''Add check constraints for a series of tables based on the years dictionary \
@@ -22,17 +20,11 @@ def partition_tables(years, dbset, logger, table, timecol):
 
     for year in years:
         for month in years[year]:
-            tableyyyymm = table + get_yyyymm(year, month)
-            startdate = get_yyyymmdd(year, month)
-
-            while True:
-                try:
-                    _partition_table(tableyyyymm, startdate, logger, cursor, timecol)
-                except OperationalError as oe:
-                    logger.error(oe)
-                    con, cursor = try_connection(logger, dbset, autocommit=True)
-                else:
-                    break
+            execute_function(_partition_table, logger, cursor, dbset,
+                             autocommit=True,
+                             timecol=timecol,
+                             tableyyyymm = table + get_yyyymm(year, month),
+                             startdate = get_yyyymmdd(year, month))
 
     logger.info('Partitioning complete, connection to %s database %s closed',
                 dbset['host'],
