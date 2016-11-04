777815f1d93b50de5dff1a5f2d65448b5d26f496
sunnyqywang <sunnywang950529@hotmail.com>
2016-11-04 16:39:44 -0400

    Revert "automatic update to database from TM and OpenData"
    This reverts commit 0f19b4fd7730620c9cdfdcb3e41dff3f5dea20f8.

--- CityOpenDataFeed/parseEventXML.py
+++ CityOpenDataFeed/parseEventXML.py
@@ -7,68 +7,44 @@ Created on Thu Oct 27 10:12:48 2016
 import pandas as pd
 import requests
 import xml.etree.ElementTree as ET
-from pg import DB
-import datetime
 
-db = DB(dbname='bigdata',host='137.15.155.38',port=5432,user='qwang2',passwd='xe3cer4')
 proxies = {'http':'http://137.15.73.132:8080'}
 
 r = requests.get('http://wx.toronto.ca/festevents.nsf/tpaview?readviewentries', proxies = proxies)
 
+event = []
+venue = []
+venueAdd = []
+dateBegin = []
+timeBegin = []
+dateEnd = []
+timeEnd = []
+category = []
 tree = ET.fromstring(r.content)
       
 for entry in tree.findall('viewentry'):
-    row = {}
-    row["id"] = entry.attrib['noteid']
-    row["event_name"] = entry.find("./entrydata[@name='EventName']/text").text
-    
     if entry.find("./entrydata[@name='Location']/text") is not None:
-        row["venue_name"] = entry.find("./entrydata[@name='Location']").find("./text").text
+        venue.append(entry.find("./entrydata[@name='Location']").find("./text").text)
     else:
         loc=''
         for l in entry.findall("./entrydata[@name='CategoryList']/textlist/text"):
             loc = loc+l.text+' '
-        row["venue_name"] = loc
+        venue.append(loc)
         
-    row["address"] = entry.find("./entrydata[@name='Address']/text").text
-    
-    try:
-        t = entry.find("./entrydata[@name='TimeBegin']/text").text
-        if t.find('PM')>0:
-            start_time = 12
-        else:
-            start_time = 0
-        start_time = start_time + int(t[:t.find(':')])
-        row["start_time"] = datetime.time(start_time,0,0)
-    except:
-        row["start_time"] = None
-    
-    try:
-        t = entry.find("./entrydata[@name='TimeEnd']/text").text
-        if t.find('PM')>0:
-            end_time = 12
-        else:
-            end_time = 0
-        end_time = end_time + int(t[:t.find(':')])
-        row["end_time"] = datetime.time(end_time,0,0)
-    except:
-        row["end_time"] = None
-        
-    row["start_date"] = entry.find("./entrydata[@name='DateBeginShow']/text").text
-    row["end_date"] = entry.find("./entrydata[@name='DateEndShow']/text").text
+    event.append(entry.find("./entrydata[@name='EventName']").find("./text").text)
+    venueAdd.append(entry.find("./entrydata[@name='Address']").find("./text").text)
+    dateBegin.append(entry.find("./entrydata[@name='DateBeginShow']").find("./text").text)
+    timeBegin.append(entry.find("./entrydata[@name='TimeBegin']").find("./text").text)
+    dateEnd.append(entry.find("./entrydata[@name='DateEndShow']").find("./text").text)
+    timeEnd.append(entry.find("./entrydata[@name='TimeEnd']").find("./text").text)
     
     if entry.find("./entrydata[@name='CategoryList']/text") is not None:
-        row["classification"] = entry.find("./entrydata[@name='CategoryList']/text").text
+        category.append(entry.find("./entrydata[@name='CategoryList']/text").text)
     else:
         cat=''
         for c in entry.findall("./entrydata[@name='CategoryList']/textlist/text"):
             cat = cat+c.text+','
-        row["classification"] = cat[:len(cat)-1]
-        
-    db.upsert('city.opendata_events',row)
-    
-db.close()
-'''            
+        category.append(cat[:len(cat)-1])
+            
 Events = pd.DataFrame({'Event':event, 'Venue':venue, 'Venue Address':venueAdd, 'Category':category, 'DateBegin':dateBegin, 'TimeBegin':timeBegin, 'DateEnd':dateEnd, 'TimeEnd':timeEnd})
 Events.to_csv('city_open_data_events.csv')
-'''
