f6748c13aa0161a9575bf3aaf1dd5878979e8653
Aakash Harpalani <aharpal@toronto.ca>
2016-11-10 08:15:46 -0500

    Minor changes to SQL equivalency checks

---
+++ bluetooth/check_count-equivalency.sql
@@ -0,0 +1,5 @@
+﻿select startpoint_name, endpoint_name, extract(year from (measured_timestamp - INTERVAL '1 second')) as yr, extract(month from (measured_timestamp - INTERVAL '1 second')) as mth, count(*)
+from bluetooth.raw_store
+where outlier_level = 0
+group by startpoint_name, endpoint_name, extract(year from (measured_timestamp - INTERVAL '1 second')), extract(month from (measured_timestamp - INTERVAL '1 second'))
+order by startpoint_name, endpoint_name, extract(year from (measured_timestamp - INTERVAL '1 second')), extract(month from (measured_timestamp - INTERVAL '1 second'))
