a779548f32833cd58f0d3f2036e1e12c8026816f
Raphael Dumas <raphael.a.dumas@gmail.com>
2016-11-14 18:23:00 -0500

    Inrix score explanation
    Continues #6

---
+++ inrix/sql/descriptive_analysis/score20exploration.sql
@@ -0,0 +1,9 @@
+﻿SELECT mon, score, sum(count)
+FROM rdumas.inrix_score_counts2
+INNER JOIN gis.inrix_tmc_tor USING (tmc)
+WHERE tmc in (SELECT tmc FROM score20props) AND roadname IN ('Don Valley Pky','Gardiner Expy')
+GROUP BY mon, score
+
+SELECT tmc, count/ 3 as num_per_month, 100.0* count/(('2016-03-31'::DATE - '2016-01-01'::DATE) *24 * 60) AS pct_observations, 100.0* count / SUM(count) OVER () AS proportion
+INTO rdumas.score20props
+FROM score20_counts
--- inrix/sql/descriptive_analysis/score_counts_over_time.sql
+++ inrix/sql/descriptive_analysis/score_counts_over_time.sql
@@ -1,5 +1,5 @@
 ﻿SELECT date_trunc('month', tx) as mon, score, COUNT(*)
 FROM inrix.raw_data
-WHERE (tx BETWEEN '2014-01-01'::date AND '2014-03-31'::DATE OR tx BETWEEN '2015-01-01'::date AND '2015-03-31'::DATE OR tx BETWEEN '2016-01-01'::date AND '2016-03-31'::DATE)
+WHERE (tx BETWEEN '2014-04-01'::date AND '2014-12-31'::DATE OR tx BETWEEN '2015-04-01'::date AND '2015-12-31'::DATE OR tx BETWEEN '2016-04-01'::date AND '2016-06-30'::DATE)
 GROUP BY score, date_trunc('month', tx)
 ORDER BY mon, score
