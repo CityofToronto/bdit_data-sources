bba07bbcff865c480947f1dbf9e3b80df68852aa
Aakash Harpalani <aharpal@toronto.ca>
2016-11-24 17:50:21 -0500

    SQL scripts for 5-minute aggregations

---
+++ bluetooth/bt_create-table-aggr_5min.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE IF EXISTS bluetooth.aggr_5min;
+CREATE TABLE bluetooth.aggr_5min(
+	id bigserial not null primary key,
+	analysis_id bigint,
+	datetime_bin timestamp without time zone,
+	tt numeric,
+	obs integer
+);
