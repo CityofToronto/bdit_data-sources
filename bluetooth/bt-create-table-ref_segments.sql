09b5602e81d21e15b8c7bacdfac214378c93b913
Aakash Harpalani <aharpal@toronto.ca>
2017-02-28 10:27:40 -0500

    Bluetooth Schema Changes

--- bluetooth/bt-create-table-ref_segments.sql
+++ bluetooth/bt-create-table-ref_segments.sql
@@ -1,6 +1,9 @@
+﻿DROP TABLE IF EXISTS bluetooth.ref_segments;
+
 CREATE TABLE bluetooth.ref_segments
 (
   segment_id integer NOT NULL,
+  analysis_id integer NOT NULL,
   segment_name character varying(10),
   startpointname character varying(10),
   endpointname character varying(10),
@@ -9,15 +12,13 @@ CREATE TABLE bluetooth.ref_segments
   start_crossstreet character varying(40),
   end_road character varying(20),
   end_crossstreet character varying(40),
-  analysisid integer,
   orig_startpointname character varying(10),
   orig_endpointname character varying(10),
   CONSTRAINT segments_pkey PRIMARY KEY (segment_id)
-)
-WITH (
-  OIDS=FALSE
 );
+
 ALTER TABLE bluetooth.ref_segments
   OWNER TO aharpal;
 GRANT ALL ON TABLE bluetooth.ref_segments TO aharpal;
 GRANT ALL ON TABLE bluetooth.ref_segments TO public;
+
---
+++ bluetooth/bt-delete-raw_store.sql
@@ -0,0 +1,2 @@
+﻿DELETE FROM bluetooth.raw_store
+WHERE startpoint_name = 'B' AND endpoint_name = 'A' AND measured_timestamp > '2014-03-01 00:00:00' AND measured_timestamp <= '2014-04-01 00:00:00'
