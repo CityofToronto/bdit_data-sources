a98d0b12bf2953c9937e490a9bfc33e8faa7c927
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-03-15 16:34:13 -0400

    Creating HERE data folder
    Also an update SRID script for uploaded layers

---
+++ here/gis/update_srid.sql
@@ -0,0 +1,6 @@
+﻿/*Author: Raphael Dumas
+Creates a number of UPDATE queries to execute to update the SRID for the layers uploaded through the PostGIS Shapefile importer. Copy and paste the resulting rows into a new query window.*/
+
+SELECT 'UPDATE TABLE '|| schemaname || '.' || tablename ||' SET geom = TO ST_SETSRID(geom, 4326);'
+FROM pg_tables WHERE NOT schemaname IN ('pg_catalog', 'here_gis')
+ORDER BY schemaname, tablename;
