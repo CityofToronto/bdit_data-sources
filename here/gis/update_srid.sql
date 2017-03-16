ccc416d0d7ca746c7048052532ae56898dccb1ad
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-03-16 16:47:52 -0400

    Specifying SRID in geometry column

--- here/gis/update_srid.sql
+++ here/gis/update_srid.sql
@@ -1,6 +1,19 @@
 ﻿/*Author: Raphael Dumas
 Creates a number of UPDATE queries to execute to update the SRID for the layers uploaded through the PostGIS Shapefile importer. Copy and paste the resulting rows into a new query window.*/
 
-SELECT 'UPDATE TABLE '|| schemaname || '.' || tablename ||' SET geom = TO ST_SETSRID(geom, 4326);'
-FROM pg_tables WHERE NOT schemaname IN ('pg_catalog', 'here_gis')
+--Shapefile import defaults to public schema.
+SELECT 'ALTER TABLE '|| schemaname || '.' || tablename ||' SET SCHEMA here_gis;'
+FROM pg_tables WHERE schemaname IN ('public')
+ORDER BY schemaname, tablename;
+
+--Update SRID since importer doesn't autodetect SRID
+SELECT 'UPDATE '|| schemaname || '.' || tablename ||' SET geom = ST_SETSRID(geom, 4326);'
+FROM pg_tables WHERE schemaname IN ('here_gis')
+ORDER BY schemaname, tablename;
+
+--Specifying SRID in column definition
+SELECT 'ALTER TABLE '|| schemaname || '.' || tablename ||' ALTER geom TYPE geometry('||type||' , 4326);'
+FROM pg_tables 
+INNER JOIN geometry_columns ON f_table_schema = schemaname AND tablename = f_table_name
+WHERE schemaname IN ('here_gis')
 ORDER BY schemaname, tablename;
