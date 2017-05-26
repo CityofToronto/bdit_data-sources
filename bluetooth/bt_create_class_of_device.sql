fb1273dc9377e90dd598bccc6ac344ac071c4a47
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-05-26 17:00:40 -0400

    Add BT Class of Device Table #36

---
+++ bluetooth/bt_create_class_of_device.sql
@@ -0,0 +1,20 @@
+﻿-- Table: bluetooth.class_of_device
+
+-- DROP TABLE bluetooth.class_of_device;
+
+CREATE TABLE bluetooth.class_of_device
+(
+  cod_hex bytea NOT NULL,
+  cod_binary bit varying(24) NOT NULL,
+  device_type character varying(64) NOT NULL,
+  device_example text,
+  confirmed character varying(10),
+  confirmed_example text,
+  major_device_class text,
+  cod bigint
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE bluetooth.class_of_device
+  OWNER TO bt_admins;
