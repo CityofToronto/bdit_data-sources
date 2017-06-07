d77983c956b80d8dcc1e3c69076a34c6939ee5c9
Aakash Harpalani <aharpal@toronto.ca>
2017-06-07 14:55:43 -0400

    minor change

--- volumes/compass/r/download_data.R
+++ volumes/compass/r/download_data.R
@@ -2,6 +2,7 @@ library(downloader)
 
 source("connect/one-its.R")
 
+url = paste0('http://',user,':',pw,url_base,'year=2017&month=4&reportType=min_30&sensorName=400DN0005DSS')
 destfile = 'C:\\Users\\aharpal\\Downloads\\400DN0005DSS.csv'
 method = 'auto'
 
