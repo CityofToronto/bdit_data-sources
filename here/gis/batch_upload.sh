ef5744ff5f6e9d39c92970a96ad968adbc677fbb
Raphael Dumas <radumas@users.noreply.github.com>
2017-08-08 10:48:22 -0400

    Update clipping function to reflect HERE geometry

--- here/gis/batch_upload.sh
+++ here/gis/batch_upload.sh
@@ -4,6 +4,6 @@ do
 	f_lower="$(echo ${f,,})"
 	tablename="$(echo ${f_lower%.*}_16_1)"
 	shp2pgsql -D -I -s 4326 -S $f here_gis.$tablename | psql -h 10.160.12.47 -d bigdata 
-	psql -h 10.160.12.47 -d bigdata -c "ALTER TABLE here_gis.$tablename OWNER TO here_admins; SELECT gis.clip_to('here_gis','$tablename');"
+	psql -h 10.160.12.47 -d bigdata -c "ALTER TABLE here_gis.$tablename OWNER TO here_admins; SELECT here_gis.clip_to('here_gis','$tablename');"
 
 done
