1f3f49e83e2003a3ab01fde4b031652d25276731
Aakash Harpalani <aharpal@toronto.ca>
2017-12-11 10:58:22 -0500

    Aggregation BT Queries - All Devices
    Seperate tables for aggregation of all BT/WiFi devices (for expressway
    reporting)

---
+++ bluetooth/bt-load-aggr_5min_alldevices.sql
@@ -0,0 +1,12 @@
+﻿TRUNCATE bluetooth.aggr_5min_alldevices;
+
+INSERT INTO bluetooth.aggr_5min_alldevices (analysis_id, datetime_bin, tt, obs)
+SELECT	rs.analysis_id,
+	TIMESTAMP WITHOUT TIME ZONE 'epoch' +
+	INTERVAL '1 second' * (floor((extract('epoch' from rs.measured_timestamp)-1) / 300) * 300) as datetime_bin,
+	percentile_cont(0.5) WITHIN GROUP (ORDER BY rs.measured_time) AS travel_time,
+	COUNT(rs.user_id) AS obs
+FROM bluetooth.observations rs
+WHERE rs.outlier_level = 0
+GROUP BY rs.analysis_id, (floor((extract('epoch' from rs.measured_timestamp)-1) / 300) * 300)
+ORDER BY rs.analysis_id, (floor((extract('epoch' from rs.measured_timestamp)-1) / 300) * 300);
---
+++ ttc/sql/create-table-bt_ttc_tt.sql
@@ -0,0 +1,15 @@
+﻿DROP TABLE IF EXISTS king_pilot.bt_ttc_tt;
+
+CREATE TABLE king_pilot.bt_ttc_tt (
+	btt_uid serial,
+	bt_id int,
+	start_dt timestamp without time zone,
+	end_dt timestamp without time zone,
+	tt_sec int);
+
+INSERT INTO king_pilot.bt_ttc_tt(bt_id, start_dt, end_dt, tt_sec)
+SELECT bt_id, B.dep_time AS start_dt, C.dep_time AS end_dt, EXTRACT(EPOCH FROM (C.dep_time - B.dep_time)) AS tt_sec
+FROM ttc.bt_stops A
+INNER JOIN ttc.trip_stops B ON B.stop_uid = A.stop_uid_from
+INNER JOIN ttc.trip_stops C ON C.stop_uid = A.stop_uid_to AND B.trip_uid = C.trip_uid
+ORDER BY B.dep_time;
