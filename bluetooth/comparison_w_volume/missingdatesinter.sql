48a70e00fc4ac6df768357ce25b1658097920d9c
andrewvlouis <andrewvlouis@gmail.com>
2018-01-30 16:20:10 -0500

    summarized three intersections of interest

---
+++ bluetooth/comparison_w_volume/missingdatesinter.sql
@@ -0,0 +1,19 @@
+﻿select distinct(datetime_bin) from 
+(select intersection_uid, datetime_bin, leg, dir, sum(volume) from miovision.volumes_15min
+where (intersection_uid = 18 and leg = 'W' and dir = 'EB' 
+and datetime_bin::date >= '2017-11-01' and datetime_bin::date <= '2017-11-08' 
+and classification_uid in (1,4,5))
+group by intersection_uid, datetime_bin, leg, dir
+order by intersection_uid, datetime_bin) f
+
+
+EXCEPT
+
+select distinct(datetime_bin) from 
+(select intersection_uid, datetime_bin, leg, dir, sum(volume) from miovision.volumes_15min
+where (intersection_uid = 15 and leg = 'E' and dir = 'EB' 
+and datetime_bin::date >= '2017-11-01' and datetime_bin::date <= '2017-11-08' 
+and classification_uid in (1,4,5))
+group by intersection_uid, datetime_bin, leg, dir
+order by intersection_uid, datetime_bin) f2;
+
