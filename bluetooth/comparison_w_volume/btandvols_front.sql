c508b3657d9f45c9fed6125240378178650ca2fd
andrewvlouis <andrewvlouis@gmail.com>
2018-01-31 16:21:17 -0500

    query to grab and join bt and volume for front st

---
+++ bluetooth/comparison_w_volume/btandvols_front.sql
@@ -0,0 +1,13 @@
+﻿select bt.datetime_bin, bt.obs, mio.v from (
+select intersection_uid, datetime_bin, leg, dir, sum(volume) as v from miovision.volumes_15min
+where (intersection_uid = 6  and leg = 'W' and dir = 'EB' 
+and datetime_bin::date >= '2017-11-01' and datetime_bin::date <= '2017-11-08' and classification_uid in (1,4,5))
+
+group by intersection_uid, datetime_bin, leg, dir
+order by intersection_uid, datetime_bin) mio, 
+
+(select datetime_bin, obs from bluetooth.aggr_15min
+where analysis_id = 1454523
+and datetime_bin::date >= '2017-11-01' and datetime_bin::date <= '2017-11-08') bt
+
+where mio.datetime_bin = bt.datetime_bin;
