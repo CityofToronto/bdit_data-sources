ffe25f8e59bec11c60ab6fe0504730e7588fca1d
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-02-02 16:05:10 -0500

    Mega bluetooth folder cleanup #2

--- bluetooth/check_count-equivalency.sql
+++
@@ -1,5 +0,0 @@
-﻿select startpoint_name, endpoint_name, extract(year from (measured_timestamp - INTERVAL '1 second')) as yr, extract(month from (measured_timestamp - INTERVAL '1 second')) as mth, count(*)
-from bluetooth.observations
-where outlier_level = 0
-group by startpoint_name, endpoint_name, extract(year from (measured_timestamp - INTERVAL '1 second')), extract(month from (measured_timestamp - INTERVAL '1 second'))
-order by startpoint_name, endpoint_name, extract(year from (measured_timestamp - INTERVAL '1 second')), extract(month from (measured_timestamp - INTERVAL '1 second'))
