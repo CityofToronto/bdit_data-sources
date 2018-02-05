8f95a7f59951c99319313d743418e25c36f90673
andrewvlouis <andrewvlouis@gmail.com>
2018-02-05 14:51:40 -0500

    queries for percentage analysis
    each query represents segment of interest
    if no 'wb' in filename, then assume eastbound direction

--- bluetooth/comparison_w_volume/adelaide_percentage.sql
+++ bluetooth/comparison_w_volume/adelaide_percentage.sql
@@ -1,14 +1,14 @@
 ﻿select datetime_bin::time, avg(obs) as obs, avg(volume) as volume from(
+
 select bt.hourly as datetime_bin, bt.obs, mio.volume  from (
 select intersection_uid, date_trunc('hour', datetime_bin) as hourly, leg, dir, sum(volume) as volume from miovision.volumes_15min
-where (intersection_uid = 6  and leg = 'W' and dir = 'EB' 
-and datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07') and classification_uid in (1,4,5))
+where (intersection_uid = 2  and leg = 'W' and dir = 'EB' and datetime_bin::date in ('2017-12-04', '2017-12-05', '2017-12-06','2017-12-07','2017-12-08')
+and classification_uid in (1,4,5))
 
 group by intersection_uid, hourly, leg, dir) mio, 
 
 (select date_trunc('hour', datetime_bin) as hourly, sum(obs) as obs from bluetooth.aggr_15min
-where analysis_id = 1454523
-and datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07')
+where analysis_id = 1453959
 group by hourly) bt
 
 where mio.hourly = bt.hourly) final
