f4ff697f1f7da6b7ec026b4115a6e092818202bf
andrewvlouis <andrewvlouis@gmail.com>
2018-02-05 10:44:51 -0500

    fixed hourly average aggregation
    grouped by each hour of the day, took means of obs and volumes

--- bluetooth/comparison_w_volume/front_percentage.sql
+++ bluetooth/comparison_w_volume/front_percentage.sql
@@ -1,13 +1,15 @@
-﻿select bt.thirtymins as datetime_bin, bt.obs, mio.volume  from (
-select intersection_uid, date_trunc('hour', datetime_bin)+ date_part('minute', datetime_bin)::int / 30 * interval '30 min'  as thirtymins, leg, dir, sum(volume) as volume from miovision.volumes_15min
+﻿select datetime_bin::time, avg(obs) as obs, avg(volume) as volume from(
+select bt.hourly as datetime_bin, bt.obs, mio.volume  from (
+select intersection_uid, date_trunc('hour', datetime_bin) as hourly, leg, dir, sum(volume) as volume from miovision.volumes_15min
 where (intersection_uid = 6  and leg = 'W' and dir = 'EB' 
-and datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03', '2017-11-06', '2017-11-07') and classification_uid in (1,4,5))
+and datetime_bin::date >= '2017-11-01' and datetime_bin::date <= '2017-11-08' and classification_uid in (1,4,5))
 
-group by intersection_uid, thirtymins, leg, dir) mio, 
+group by intersection_uid, hourly, leg, dir) mio, 
 
-(select date_trunc('hour', datetime_bin)+ date_part('minute', datetime_bin)::int / 30 * interval '30 min'  as thirtymins, sum(obs) as obs from bluetooth.aggr_15min
+(select date_trunc('hour', datetime_bin) as hourly, sum(obs) as obs from bluetooth.aggr_15min
 where analysis_id = 1454523
 and datetime_bin::date >= '2017-11-01' and datetime_bin::date <= '2017-11-08'
-group by thirtymins) bt
+group by hourly) bt
 
-where mio.thirtymins = bt.thirtymins;
+where mio.hourly = bt.hourly) final
+group by datetime_bin::time;
