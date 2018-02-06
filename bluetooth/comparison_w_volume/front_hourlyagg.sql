a4dd6dfdcd737bbdd741173d68aa19798a6d923e
andrewvlouis <andrewvlouis@gmail.com>
2018-02-06 10:50:13 -0500

    editted for weekends

--- bluetooth/comparison_w_volume/front_hourlyagg.sql
+++ bluetooth/comparison_w_volume/front_hourlyagg.sql
@@ -1,13 +1,13 @@
 ﻿select bt.hourly as datetime_bin, bt.obs, mio.volume  from (
 select intersection_uid, date_trunc('hour', datetime_bin) as hourly, leg, dir, sum(volume) as volume from miovision.volumes_15min
 where (intersection_uid = 6  and leg = 'W' and dir = 'EB' 
-and datetime_bin::date >= '2017-11-01' and datetime_bin::date <= '2017-11-08' and classification_uid in (1,4,5))
+and datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07') and classification_uid in (1,4,5))
 
 group by intersection_uid, hourly, leg, dir) mio, 
 
 (select date_trunc('hour', datetime_bin) as hourly, sum(obs) as obs from bluetooth.aggr_15min
 where analysis_id = 1454523
-and datetime_bin::date >= '2017-11-01' and datetime_bin::date <= '2017-11-08'
+and datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07')
 group by hourly) bt
 
 where mio.hourly = bt.hourly;
