61914793494f86e8339bd95b0b163694582d4fa2
andrewvlouis <andrewvlouis@gmail.com>
2018-02-08 14:10:57 -0500

    fixed timestamp issue

--- bluetooth/od/complexroute.sql
+++ bluetooth/od/complexroute.sql
@@ -1,12 +1,12 @@
-﻿with route as (with ones as (select startend_path.datetime_bin, startend_path.userid, s_du_sp, e_du_ja from alouis2.startend_path
+﻿with route as (with ones as (select startend_path.datetime_bin::date, startend_path.userid, s_du_sp, e_du_ja from alouis2.startend_path
 where s_du_sp = 1 and e_du_ja = 1)
 
-select ones.datetime_bin, ones.userid, du_sp, du_ja, qu_sp, qu_ja, rm_sp, rm_ja, ad_sp, ad_ja, kn_sp, kn_yo, kn_ja, path_total from ones
+select ones.datetime_bin::date, ones.userid, s_du_sp, e_du_ja , du_sp, du_ja, qu_sp, qu_ja, rm_sp, rm_ja, ad_sp, ad_ja, kn_sp, kn_yo, kn_ja, path_total from ones
 
-INNER JOIN others_path on ones.userid = others_path.userid and ones.datetime_bin = others_path.datetime_bin
+INNER JOIN others_path on ones.userid = others_path.userid and ones.datetime_bin::date = others_path.datetime_bin::date
 
 where path_total = 11)
 
-select datetime_bin, userid, du_sp, du_ja, qu_sp, qu_ja, rm_sp, rm_ja, ad_sp, ad_ja, kn_sp, kn_yo, kn_ja, path_total, startpoint_name, endpoint_name from route, bluetooth.observations
+select datetime_bin::date, userid, s_du_sp, e_du_ja, du_sp, du_ja, qu_sp, qu_ja, rm_sp, rm_ja, ad_sp, ad_ja, kn_sp, kn_yo, kn_ja, path_total, startpoint_name, endpoint_name from route, bluetooth.observations
 
-where observations.user_id = route.userid and observations.measured_timestamp = route.datetime_bin;
+where observations.user_id = route.userid and observations.measured_timestamp::date = route.datetime_bin::date;
--- bluetooth/od/simpleroute.sql
+++ bluetooth/od/simpleroute.sql
@@ -1,17 +1,17 @@
-﻿with route as (with ones as (select startend_path.datetime_bin, startend_path.userid, s_co_ba, e_co_un from alouis2.startend_path
+﻿with route as (with ones as (select startend_path.datetime_bin::date, startend_path.userid, s_co_ba, e_co_un from alouis2.startend_path
 where s_co_ba = 1 and e_co_un = 1)
 
 
-select ones.datetime_bin, ones.userid, s_co_ba, e_co_un, du_ba, du_sp, du_un, path_total from ones
+select ones.datetime_bin::date, ones.userid, s_co_ba, e_co_un, du_ba, du_sp, du_un, path_total from ones
 
 
-INNER JOIN others_path ON ones.userid = others_path.userid and ones.datetime_bin = others_path.datetime_bin
+INNER JOIN others_path ON ones.userid = others_path.userid and ones.datetime_bin::date = others_path.datetime_bin::date
 
-where path_total= 5)
+where path_total= 5 and du_ba = 1 and du_sp = 1 and du_un = 1)
 
-select datetime_bin, userid, s_co_ba, e_co_un, du_ba, du_sp, du_un, path_total, startpoint_name, endpoint_name  from route, bluetooth.observations
+select datetime_bin::date, userid, s_co_ba, e_co_un, du_ba, du_sp, du_un, path_total, startpoint_name, endpoint_name  from route, bluetooth.observations
 
-where observations.user_id = route.userid and observations.measured_timestamp = route.datetime_bin;
+where observations.user_id = route.userid and observations.measured_timestamp::date = route.datetime_bin::date;
 
 
 
