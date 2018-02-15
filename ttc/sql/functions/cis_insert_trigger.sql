e882a5d037d30f6d0b300466f891170908cd4828
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-02-15 17:31:48 -0500

    Bugfix on #81

--- ttc/sql/functions/cis_insert_trigger.sql
+++ ttc/sql/functions/cis_insert_trigger.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE FUNCTION ttc.cis_insert_trigger()
   RETURNS trigger AS
 $BODY$
 BEGIN
-IF ( message_datetime >= '2017-01-01' AND message_datetime < '2017-01-01' + INTERVAL '1 year') THEN 
+IF ( NEW.message_datetime >= '2017-01-01' AND NEW.message_datetime < '2017-01-01'::DATE + INTERVAL '1 year') THEN 
     INSERT INTO ttc.cis_2017 VALUES (NEW.*) ON CONFLICT DO NOTHING;
-ELSIF (message_datetime >= '2018-01-01' AND message_datetime < '2018-01-01' + INTERVAL '1 year') THEN
+ELSIF (NEW.message_datetime >= '2018-01-01' AND NEW.message_datetime < '2018-01-01'::DATE + INTERVAL '1 year') THEN
     INSERT INTO ttc.cis_2018 VALUES (NEW.*) ON CONFLICT DO NOTHING;
 ELSE 
     RAISE EXCEPTION 'message_datetime out of range';
