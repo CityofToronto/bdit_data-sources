f2e728676472b9da65523890339ed4e620311f26
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-03-15 17:13:08 -0400

    Update gis batch upload script to clean tables

--- here/gis/batch_upload.sh
+++ here/gis/batch_upload.sh
@@ -4,7 +4,7 @@ for f in *.shp
 do 
 	f_lower="$(echo ${f,,})"
 	tablename="$(echo ${f_lower%.*}_$rev)"
-	shp2pgsql -D -I -s 4326 -S $f here_gis.$tablename | psql -h 10.160.12.47 -d bigdata 
+	shp2pgsql -D -I -d -s 4326 -S $f here_gis.$tablename | psql -h 10.160.12.47 -d bigdata 
 	psql -h 10.160.12.47 -d bigdata -c "ALTER TABLE here_gis.$tablename OWNER TO here_admins; SELECT here_gis.clip_to('$tablename', '$rev');"
 
 done
