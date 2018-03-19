5f27483197ee76ed9a5d8caf3e44c1065713f3e6
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-03-19 16:48:15 -0400

    #95
    CIS data views

---
+++ ttc/sql/create/test_days_views.sql
@@ -0,0 +1,11 @@
+﻿CREATE OR REPLACE VIEW ttc.cis_20171004 AS 
+SELECT * FROM 
+ttc.cis_2017
+WHERE message_datetime >= '2017-10-04' AND message_datetime < '2017-10-04'::DATE + INTERVAL '1 Day';
+GRANT SELECT ON ttc.cis_20171004 TO bdit_humans;
+
+CREATE OR REPLACE VIEW ttc.cis_20171114 AS 
+SELECT * FROM 
+ttc.cis_2017
+WHERE message_datetime >= '2017-11-14' AND message_datetime < '2017-11-14'::DATE + INTERVAL '1 Day';
+GRANT SELECT ON ttc.cis_20171114 TO bdit_humans;
