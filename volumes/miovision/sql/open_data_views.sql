1012bb19b456451cc9c080f0e573732b5bbd27f0
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-03-28 11:41:57 -0400

    Close #97

---
+++ volumes/miovision/sql/open_data_views.sql
@@ -0,0 +1,47 @@
+﻿-- View: open_data.miovision_2017
+
+DROP VIEW open_data.miovision_2017;
+
+CREATE OR REPLACE VIEW open_data.miovision_2017 AS 
+ SELECT int_id,
+    intersec5 AS intersection_name,
+    volumes_15min.datetime_bin,
+    CASE WHEN classification ='Buses' THEN 'Buses and streetcars'
+    ELSE classification END AS classification,
+    volumes_15min.leg,
+    volumes_15min.dir,
+    SUM(volumes_15min.volume)
+   FROM miovision.volumes_15min
+   INNER JOIN miovision.intersections USING (intersection_uid)
+   INNER JOIN gis.centreline_intersection USING (int_id)
+   INNER JOIN miovision.classifications USING(classification_uid)
+      
+  WHERE date_part('year'::text, volumes_15min.datetime_bin) = 2017::double precision
+GROUP BY int_id, intersec5, datetime_bin, classification, leg, dir;
+ALTER TABLE open_data.miovision_2017
+  OWNER TO rdumas;
+GRANT ALL ON TABLE open_data.miovision_2017 TO rdumas;
+GRANT SELECT ON TABLE open_data.miovision_2017 TO od_extract_svc;
+
+DROP VIEW open_data.miovision_2018;
+
+CREATE OR REPLACE VIEW open_data.miovision_2018 AS 
+ SELECT int_id,
+    intersec5 AS intersection_name,
+    volumes_15min.datetime_bin,
+    CASE WHEN classification ='Buses' THEN 'Buses and streetcars'
+    ELSE classification END AS classification,
+    volumes_15min.leg,
+    volumes_15min.dir,
+    SUM(volumes_15min.volume)
+   FROM miovision.volumes_15min
+   INNER JOIN miovision.intersections USING (intersection_uid)
+   INNER JOIN gis.centreline_intersection USING (int_id)
+   INNER JOIN miovision.classifications USING(classification_uid)
+      
+  WHERE date_part('year'::text, volumes_15min.datetime_bin) = 2017::double precision
+GROUP BY int_id, intersec5, datetime_bin, classification, leg, dir;
+ALTER TABLE open_data.miovision_2018
+  OWNER TO rdumas;
+GRANT ALL ON TABLE open_data.miovision_2018 TO rdumas;
+GRANT SELECT ON TABLE open_data.miovision_2018 TO od_extract_svc;
