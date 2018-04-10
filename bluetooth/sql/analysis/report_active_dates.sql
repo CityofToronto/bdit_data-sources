972081d94291ad9c128ace5958df34877c8030ca
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-04-10 16:52:53 -0400

    #62 Update view for route report start/end

--- bluetooth/sql/analysis/report_active_dates.sql
+++ bluetooth/sql/analysis/report_active_dates.sql
@@ -1,6 +1,7 @@
-﻿CREATE VIEW bluetooth.report_active_dates AS
-SELECT analysis_id, report_name, MIN(datetime_bin)::DATE AS start_date, CASE WHEN MAX(datetime_bin)::DATE > '2018-04-01' THEN NULL ELSE MAX(datetime_bin)::DATE END as end_date
+﻿DROP MATERIALIZED VIEW bluetooth.report_active_dates;
+CREATE MATERIALIZED VIEW bluetooth.report_active_dates AS
+SELECT analysis_id, report_name, MIN(datetime_bin)::DATE AS start_date, CASE WHEN MAX(datetime_bin)::DATE < now()::DATE - INTERVAL '2 days' THEN MAX(datetime_bin)::DATE END as end_date
 FROM bluetooth.aggr_5min
 INNER JOIN bluetooth.all_analyses USING (analysis_id)
 GROUP BY analysis_id, report_name
-ORDER BY report_name, start_date
+ORDER BY report_name, start_date; 
