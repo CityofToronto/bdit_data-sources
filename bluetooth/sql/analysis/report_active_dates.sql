1ca2f1043078349731fdecde5ef6766b56774f9c
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-04-13 10:26:22 -0400

    #105 Update report_active_dates permissions

--- bluetooth/sql/analysis/report_active_dates.sql
+++ bluetooth/sql/analysis/report_active_dates.sql
@@ -4,4 +4,8 @@ SELECT analysis_id, report_name, MIN(datetime_bin)::DATE AS start_date, CASE WHE
 FROM bluetooth.aggr_5min
 INNER JOIN bluetooth.all_analyses USING (analysis_id)
 GROUP BY analysis_id, report_name
-ORDER BY report_name, start_date; 
+ORDER BY report_name, start_date;
+
+ALTER TABLE bluetooth.observations
+  OWNER TO bt_admins;
+GRANT SELECT ON TABLE bluetooth.observations TO bdit_humans;
