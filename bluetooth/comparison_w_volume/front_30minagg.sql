d9a43baba994e3ca84b9673143a0d93feaf5092d
andrewvlouis <andrewvlouis@gmail.com>
2018-04-16 14:16:50 -0400

    syntax corrections

--- bluetooth/comparison_w_volume/front_30minagg.sql
+++ bluetooth/comparison_w_volume/front_30minagg.sql
@@ -1,13 +1,16 @@
-﻿select bt.thirtymins as datetime_bin, bt.obs, mio.volume  from (
-select intersection_uid, date_trunc('hour', datetime_bin)+ date_part('minute', datetime_bin)::int / 30 * interval '30 min'  as thirtymins, leg, dir, sum(volume) as volume from miovision.volumes_15min
-where (intersection_uid = 6  and leg = 'W' and dir = 'EB' 
-and datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07') and classification_uid in (1,4,5))
+﻿SELECT bt.thirtymins as datetime_bin, bt.obs, mio.volume  
 
-group by intersection_uid, thirtymins, leg, dir) mio, 
+FROM (
+	SELECT intersection_uid, date_trunc('hour', datetime_bin)+ date_part('minute', datetime_bin)::int / 30 * interval '30 min'  as thirtymins, leg, dir, sum(volume) as volume 
+	FROM miovision.volumes_15min
+	WHERE (intersection_uid = 6  AND leg = 'W'AND dir = 'EB' 
+	AND datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07') AND classification_uid in (1,4,5))
+	GROUP BY intersection_uid, thirtymins, leg, dir) mio, 
 
-(select date_trunc('hour', datetime_bin)+ date_part('minute', datetime_bin)::int / 30 * interval '30 min'  as thirtymins, sum(obs) as obs from bluetooth.aggr_15min
-where analysis_id = 1454523
-and datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07')
-group by thirtymins) bt
+	(SELECT date_trunc('hour', datetime_bin)+ date_part('minute', datetime_bin)::int / 30 * interval '30 min'  as thirtymins, sum(obs) as obs 
+	FROM bluetooth.aggr_15min
+	WHERE analysis_id = 1454523
+	AND datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07')
+	GROUP BY thirtymins) bt
 
-where mio.thirtymins = bt.thirtymins;
+WHERE mio.thirtymins = bt.thirtymins;
--- bluetooth/comparison_w_volume/front_percentage.sql
+++ bluetooth/comparison_w_volume/front_percentage.sql
@@ -1,15 +1,15 @@
-﻿select datetime_bin::time, avg(obs) as obs, avg(volume) as volume from(
-select bt.hourly as datetime_bin, bt.obs, mio.volume  from (
-select intersection_uid, date_trunc('hour', datetime_bin) as hourly, leg, dir, sum(volume) as volume from miovision.volumes_15min
-where (intersection_uid = 6  and leg = 'W' and dir = 'EB' 
-and datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07') and classification_uid in (1,4,5))
+﻿SELECT datetime_bin::TIME, AVG(obs) AS obs, AVG(volume) AS volume FROM(
+	SELECT bt.hourly AS datetime_bin, bt.obs, mio.volume  FROM (
+			SELECT intersection_uid, date_trunc('hour', datetime_bin) as hourly, leg, dir, sum(volume) as volume 
+			FROM miovision.volumes_15min
+			WHERE (intersection_uid = 6  AND leg = 'W' AND dir = 'EB' 
+			AND datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07') and classification_uid in (1,4,5))
+			group by intersection_uid, hourly, leg, dir) mio, 
 
-group by intersection_uid, hourly, leg, dir) mio, 
+			(SELECT date_trunc('hour', datetime_bin) as hourly, sum(obs) as obs FROM bluetooth.aggr_15min
+			WHERE analysis_id = 1454523
+			AND datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07')
+			GROUP BY hourly) bt
 
-(select date_trunc('hour', datetime_bin) as hourly, sum(obs) as obs from bluetooth.aggr_15min
-where analysis_id = 1454523
-and datetime_bin::date in ('2017-11-01', '2017-11-02', '2017-11-03','2017-11-06','2017-11-07')
-group by hourly) bt
-
-where mio.hourly = bt.hourly) final
-group by datetime_bin::time;
+	WHERE mio.hourly = bt.hourly) final
+	GROUP BY datetime_bin::time;
