3158cf3cf9e7ec8667fb602631f649adb7d6b5ab
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-04-17 16:52:29 -0400

    Close #110 Yearly disagg views

--- bluetooth/sql/analysis/open_data_ksp_travel_times.sql
+++ bluetooth/sql/analysis/open_data_ksp_travel_times.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR REPLACE VIEW open_data.ksp_travel_times AS 
+﻿CREATE OR REPLACE VIEW open_data.ksp_travel_times_2017 AS 
 SELECT 
 	segment_name, 	
 		B.datetime_bin,
@@ -10,10 +10,30 @@ SELECT
                 
 	FROM king_pilot.bt_segments A
 	INNER JOIN bluetooth.aggr_5min B USING (analysis_id)
-	JOIN king_pilot.date_lookup c ON dt = datetime_bin
+	JOIN king_pilot.date_lookup c ON dt = datetime_bin::DATE
 	JOIN king_pilot.periods d USING (day_type)
 	JOIN bluetooth.segments USING (analysis_id)
-	WHERE NOT A.bt_id = 33 AND B.datetime_bin >= '2017-09-19 19:00';
-GRANT ALL ON open_data.ksp_travel_times TO rds_superuser WITH GRANT OPTION;
-GRANT ALL ON open_data.ksp_travel_times TO dbadmin;
- GRANT SELECT ON open_data.ksp_travel_times TO bdit_humans;
+	WHERE NOT A.bt_id = 33 AND B.datetime_bin >= '2017-09-19 19:00' AND B.datetime_bin < '2018-01-01';
+GRANT ALL ON open_data.ksp_travel_times_2017 TO rds_superuser WITH GRANT OPTION;
+GRANT ALL ON open_data.ksp_travel_times_2017 TO dbadmin;
+ GRANT SELECT ON open_data.ksp_travel_times_2017 TO bdit_humans;
+
+CREATE OR REPLACE VIEW open_data.ksp_travel_times_2018 AS 
+SELECT 
+	segment_name, 	
+		B.datetime_bin,
+		day_type, 
+		category,
+		tt,
+		obs,
+                period_name
+                
+	FROM king_pilot.bt_segments A
+	INNER JOIN bluetooth.aggr_5min B USING (analysis_id)
+	JOIN king_pilot.date_lookup c ON dt = datetime_bin::DATE
+	JOIN king_pilot.periods d USING (day_type)
+	JOIN bluetooth.segments USING (analysis_id)
+	WHERE NOT A.bt_id = 33 AND B.datetime_bin >= '2018-01-01' AND B.datetime_bin < '2018-01-01'::DATE + INTERVAL '1 year';
+GRANT ALL ON open_data.ksp_travel_times_2018 TO rds_superuser WITH GRANT OPTION;
+GRANT ALL ON open_data.ksp_travel_times_2018 TO dbadmin;
+ GRANT SELECT ON open_data.ksp_travel_times_2018 TO bdit_humans;
