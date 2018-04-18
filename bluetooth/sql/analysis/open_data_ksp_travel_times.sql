37424911bce451f345b9d11cf0ccd8c256881519
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-04-18 15:58:46 -0400

    #110 Bugfix and rename segment_name

--- bluetooth/sql/analysis/open_data_ksp_travel_times.sql
+++ bluetooth/sql/analysis/open_data_ksp_travel_times.sql
@@ -1,8 +1,9 @@
-﻿CREATE OR REPLACE VIEW open_data.ksp_travel_times_2017 AS 
+﻿DROP VIEW IF EXISTS open_data.ksp_travel_times_2017;
+CREATE OR REPLACE VIEW open_data.ksp_travel_times_2017 AS 
 SELECT 
-	segment_name, 	
+	segment_name AS result_id, 	
 		B.datetime_bin,
-		day_type, 
+		c.day_type, 
 		category,
 		tt,
 		obs,
@@ -11,18 +12,19 @@ SELECT
 	FROM king_pilot.bt_segments A
 	INNER JOIN bluetooth.aggr_5min B USING (analysis_id)
 	JOIN king_pilot.date_lookup c ON dt = datetime_bin::DATE
-	JOIN king_pilot.periods d USING (day_type)
+	LEFT OUTER JOIN king_pilot.periods d ON c.day_type = d.day_type AND datetime_bin::TIME <@ d.period_range
 	JOIN bluetooth.segments USING (analysis_id)
-	WHERE NOT A.bt_id = 33 AND B.datetime_bin >= '2017-09-19 19:00' AND B.datetime_bin < '2018-01-01';
+	WHERE NOT A.bt_id = 33  AND B.datetime_bin >= '2017-09-19 19:00' AND B.datetime_bin < '2018-01-01';
 GRANT ALL ON open_data.ksp_travel_times_2017 TO rds_superuser WITH GRANT OPTION;
 GRANT ALL ON open_data.ksp_travel_times_2017 TO dbadmin;
  GRANT SELECT ON open_data.ksp_travel_times_2017 TO bdit_humans;
 
+DROP VIEW IF EXISTS open_data.ksp_travel_times_2018;
 CREATE OR REPLACE VIEW open_data.ksp_travel_times_2018 AS 
 SELECT 
-	segment_name, 	
+	segment_name AS result_id, 	
 		B.datetime_bin,
-		day_type, 
+		c.day_type, 
 		category,
 		tt,
 		obs,
@@ -31,7 +33,7 @@ SELECT
 	FROM king_pilot.bt_segments A
 	INNER JOIN bluetooth.aggr_5min B USING (analysis_id)
 	JOIN king_pilot.date_lookup c ON dt = datetime_bin::DATE
-	JOIN king_pilot.periods d USING (day_type)
+	LEFT OUTER JOIN king_pilot.periods d ON c.day_type = d.day_type AND datetime_bin::TIME <@ d.period_range
 	JOIN bluetooth.segments USING (analysis_id)
 	WHERE NOT A.bt_id = 33 AND B.datetime_bin >= '2018-01-01' AND B.datetime_bin < '2018-01-01'::DATE + INTERVAL '1 year';
 GRANT ALL ON open_data.ksp_travel_times_2018 TO rds_superuser WITH GRANT OPTION;
