20e5a5facee7d0d5ba97a7e30bf9390f1696685b
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-04-24 15:52:56 -0400

    #109 Update
    Change rounding method
    Ensure only complete months included
    Change ordering of rows to be chronological

--- bluetooth/sql/analysis/open_data_ksp_agg_travel_times.sql
+++ bluetooth/sql/analysis/open_data_ksp_agg_travel_times.sql
@@ -1,16 +1,18 @@
 ﻿
+DROP VIEW open_data.ksp_agg_travel_time ;
 CREATE VIEW open_data.ksp_agg_travel_time AS
 
 SELECT to_char(date_trunc('month', dt), 'Mon ''YY') AS "month", street, direction,
 from_intersection, to_intersection,
   day_type,
   period || ' ' || period_range AS time_period,
-to_char(baseline.tt, '999.0')  as baseline_travel_time,
-  to_char(AVG(pilot.tt), '999.0') AS average_travel_time
+  round(baseline.tt, 1)  as baseline_travel_time,
+  round(AVG(pilot.tt), 1) AS average_travel_time
   FROM king_pilot.dash_daily_dev pilot
   INNER JOIN king_pilot.dash_baseline baseline USING (street, direction, day_type, period)
-  WHERE category = 'Pilot'
-  GROUP BY "month", street, from_intersection, to_intersection, direction, day_type, time_period, baseline.tt ;
+  WHERE category = 'Pilot' AND dt < date_trunc('month', current_date)
+  GROUP BY (to_char(date_trunc('month'::text, pilot.dt::timestamp with time zone), 'Mon ''YY'::text)), date_trunc('month', dt), pilot.street, baseline.from_intersection, baseline.to_intersection, pilot.direction, pilot.day_type, ((pilot.period || ' '::text) || baseline.period_range), baseline.tt
+  ORDER BY date_trunc('month', dt);
   
 GRANT SELECT ON TABLE open_data.ksp_agg_travel_time TO od_extract_svc;
 GRANT SELECT ON TABLE open_data.ksp_agg_travel_time TO bdit_humans;
