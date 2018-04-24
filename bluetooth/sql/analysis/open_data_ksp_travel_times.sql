3cf3b99145bf35f898812f7b11088d628b3dcf42
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-04-24 15:53:05 -0400

    #110 Ensure only complete months included

--- bluetooth/sql/analysis/open_data_ksp_travel_times.sql
+++ bluetooth/sql/analysis/open_data_ksp_travel_times.sql
@@ -35,7 +35,7 @@ SELECT
 	JOIN king_pilot.date_lookup c ON dt = datetime_bin::DATE
 	LEFT OUTER JOIN king_pilot.periods d ON c.day_type = d.day_type AND datetime_bin::TIME <@ d.period_range
 	JOIN bluetooth.segments USING (analysis_id)
-	WHERE NOT A.bt_id = 33 AND B.datetime_bin >= '2018-01-01' AND B.datetime_bin < '2018-01-01'::DATE + INTERVAL '1 year';
+	WHERE NOT A.bt_id = 33 AND B.datetime_bin >= '2018-01-01' AND B.datetime_bin < LEAST(date_trunc('month', current_date), '2018-01-01'::DATE + INTERVAL '1 year');
 GRANT ALL ON open_data.ksp_travel_times_2018 TO rds_superuser WITH GRANT OPTION;
 GRANT ALL ON open_data.ksp_travel_times_2018 TO dbadmin;
  GRANT SELECT ON open_data.ksp_travel_times_2018 TO bdit_humans;
