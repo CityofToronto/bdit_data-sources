36b91bd97f89146d5fec56a83ad9dc4bf39baba3
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-05-04 14:30:16 -0400

    #110 Open Data View doesn't depend on "live" data

--- bluetooth/sql/analysis/open_data_ksp_agg_travel_times.sql
+++ bluetooth/sql/analysis/open_data_ksp_agg_travel_times.sql
@@ -1,4 +1,4 @@
-﻿
+
 DROP VIEW open_data.ksp_agg_travel_time ;
 CREATE VIEW open_data.ksp_agg_travel_time AS
 
@@ -8,7 +8,7 @@ from_intersection, to_intersection,
   period || ' ' || period_range AS time_period,
   round(baseline.tt, 1)  as baseline_travel_time,
   round(AVG(pilot.tt), 1) AS average_travel_time
-  FROM king_pilot.dash_daily_dev pilot
+  FROM king_pilot.daily_report_tt pilot
   INNER JOIN king_pilot.dash_baseline baseline USING (street, direction, day_type, period)
   WHERE category = 'Pilot' AND dt < date_trunc('month', current_date)
   GROUP BY (to_char(date_trunc('month'::text, pilot.dt::timestamp with time zone), 'Mon ''YY'::text)), date_trunc('month', dt), pilot.street, baseline.from_intersection, baseline.to_intersection, pilot.direction, pilot.day_type, ((pilot.period || ' '::text) || baseline.period_range), baseline.tt
