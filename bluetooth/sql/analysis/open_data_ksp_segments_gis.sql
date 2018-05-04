16f2753928a054db43eef8b175e00ffd19977266
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-05-04 17:47:04 -0400

    Close #117

---
+++ bluetooth/sql/analysis/open_data_ksp_segments_gis.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW open_data.ksp_bluetooth_gis AS
+SELECT segment_name, street, s.direction, start_crossstreet AS from_intersection, 
+end_crossstreet AS to_intersection, length, geom
+FROM bluetooth.segments s
+INNER JOIN king_pilot.bt_segments USING(analysis_id)
