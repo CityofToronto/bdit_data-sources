7acfca21cbb454da93faf93175c5dc55fe121d38
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-07-30 17:29:50 -0400

    Added additional Where clauses when setting volume_15min_tmc_uid
    #133

--- volumes/miovision/sql/function-aggregate-volumes_15min_tmc_optimizations.sql
+++ volumes/miovision/sql/function-aggregate-volumes_15min_tmc_optimizations.sql
@@ -1,12 +1,13 @@
--- Function: aggregate_15_min_tmc1()
+-- Function: aggregate_15_min_tmc3()
 
--- DROP FUNCTION aggregate_15_min_tmc1();
+-- DROP FUNCTION aggregate_15_min_tmc3();
 
-CREATE OR REPLACE FUNCTION aggregate_15_min_tmc1()
+CREATE OR REPLACE FUNCTION aggregate_15_min_tmc3()
   RETURNS integer AS
 $BODY$
 BEGIN
 
+	RAISE NOTICE '% Function Start', timeofday();
 	DROP TABLE IF EXISTS bins;
 	CREATE TEMPORARY TABLE bins (
 		intersection_uid integer,
@@ -28,11 +29,13 @@ BEGIN
 	FROM rliu.volumes A
 	WHERE volume_15min_tmc_uid IS NULL
 	GROUP BY intersection_uid, TIMESTAMP WITHOUT TIME ZONE 'epoch' + INTERVAL '1 second' * (floor((extract('epoch' from A.datetime_bin)) / 900) * 900)
-
 	-- REMOVE ALL 15-minute bins where 5 or less of the 1-minute bins are populated with volume
 	HAVING COUNT(DISTINCT A.datetime_bin) > 5;
+	RAISE NOTICE '% Insert Finished', timeofday();
+	
 
 	-- IF one of two 1-minute time bins BEFORE and AFTER 15-minute bin are populated, assume no interpolation needed
+
 	UPDATE bins A 
 	SET interpolated = FALSE
 	FROM (SELECT DISTINCT intersection_uid, datetime_bin FROM rliu.volumes WHERE volume_15min_tmc_uid IS NULL) B 
@@ -42,19 +45,24 @@ BEGIN
 	AND (B.datetime_bin BETWEEN (A.datetime_bin + INTERVAL '15 minutes') AND  (A.datetime_bin + INTERVAL '16 minutes')
 	OR B.datetime_bin BETWEEN(A.datetime_bin - INTERVAL '1 minute') AND (A.datetime_bin - (INTERVAL '2 minutes')));
 
+	-- IF # of populated 1-minute bins exceeds difference between start and end time, assume no interpolation needed	
+
+	RAISE NOTICE '% SELECT Distinct Bins', timeofday();
 	UPDATE bins A
 	SET interpolated = CASE
-	-- IF # of populated 1-minute bins exceeds difference between start and end time, assume no interpolation needed										    
 				WHEN 	(EXTRACT(minutes FROM A.end_time - A.start_time)+1) > A.avail_minutes AND A.avail_minutes < 15
 				THEN 	FALSE
 				WHEN 	interpolated IS NULL AND A.avail_minutes < 15	
-				-- ASSUME for all other 15-minute bins with missing data, interpolation needed due to missing video
+	-- ASSUME for all other 15-minute bins with missing data, interpolation needed due to missing video
 				THEN	TRUE
 				END;
-											       
-	-- FOR 15-minute bins with interpolation needed, IF missing data at end of 15-minute period, SET end_time = end_time - 1 minute to account for potential partial count
-	-- FOR 15-minute bins with interpolation needed, IF missing data at start of 15-minute period, SET start_time = start_time + 1 minute to account for potential partial count
 
+	RAISE NOTICE '% Rest of interpolated conditional', timeofday();
+
+	
+
+	-- FOR 15-minute bins with interpolation needed, IF missing data at start of 15-minute period, SET start_time = start_time + 1 minute to account for potential partial count
+	-- FOR 15-minute bins with interpolation needed, IF missing data at end of 15-minute period, SET end_time = end_time - 1 minute to account for potential partial count
 	UPDATE bins 
 	SET end_time = CASE
 		WHEN interpolated = TRUE AND datetime_bin = start_time THEN end_time - INTERVAL '1 minute' ELSE end_time
@@ -62,7 +70,8 @@ BEGIN
 	start_time = CASE
 		WHEN interpolated = TRUE AND datetime_bin + INTERVAL '14 minutes' = end_time THEN start_time + INTERVAL '1 minute' ELSE start_time
 		END;
-
+		RAISE NOTICE '% Adjusting Interpolated Bins', timeofday();
+	
 
 	-- INSERT INTO volumes_15min_tmc, with interpolated volumes
 	WITH aggregate_insert AS(
@@ -75,25 +84,30 @@ BEGIN
 		CASE WHEN B.interpolated = TRUE THEN SUM(A.volume)*15.0/((EXTRACT(minutes FROM B.end_time - B.start_time)+1)*1.0) ELSE SUM(A.volume) END AS volume
 	FROM rliu.volumes A
 	INNER JOIN bins B USING (intersection_uid)
-	WHERE B.start_time <= A.datetime_bin AND B.end_time >= A.datetime_bin 
+	WHERE A.datetime_bin BETWEEN B.start_time AND B.end_time
 	GROUP BY A.intersection_uid, TIMESTAMP WITHOUT TIME ZONE 'epoch' + INTERVAL '1 second' * (floor((extract('epoch' from A.datetime_bin)) / 900) * 900), A.classification_uid, A.leg, A.movement_uid, B.interpolated, (EXTRACT(minutes FROM B.end_time - B.start_time)+1)
-	RETURNING volume_15min_tmc_uid, datetime_bin, classification_uid, leg, movement_uid
+
+	RETURNING volume_15min_tmc_uid, intersection_uid, datetime_bin, classification_uid, leg, movement_uid
 	)
 	UPDATE rliu.volumes a
-	SET a.volume_15min_tmc_uid = b.volume_15min_tmc_uid
+	SET volume_15min_tmc_uid = b.volume_15min_tmc_uid
 	FROM aggregate_insert b
 	WHERE a.datetime_bin >= b.datetime_bin AND a.datetime_bin < b.datetime_bin + INTERVAL '15 minutes'
 	AND a.classification_uid  = b.classification_uid 
 	AND a.leg = b.leg
-	AND a.movement_uid = b.movement_uid;
+	AND a.movement_uid = b.movement_uid
+	AND a.intersection_uid = b.intersection_uid
+	AND a.volume_15min_tmc_uid IS NULL;
+	RAISE NOTICE '% Inserted and Updated Primary Key', timeofday();
     RETURN 1;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION aggregate_15_min_tmc1()
+ALTER FUNCTION aggregate_15_min_tmc3()
   OWNER TO rliu;
-GRANT EXECUTE ON FUNCTION aggregate_15_min_tmc1() TO public;
-GRANT EXECUTE ON FUNCTION aggregate_15_min_tmc1() TO dbadmin WITH GRANT OPTION;
-GRANT EXECUTE ON FUNCTION aggregate_15_min_tmc1() TO bdit_humans WITH GRANT OPTION;
-GRANT EXECUTE ON FUNCTION aggregate_15_min_tmc1() TO rliu;
+GRANT EXECUTE ON FUNCTION aggregate_15_min_tmc3() TO public;
+GRANT EXECUTE ON FUNCTION aggregate_15_min_tmc3() TO dbadmin WITH GRANT OPTION;
+GRANT EXECUTE ON FUNCTION aggregate_15_min_tmc3() TO bdit_humans WITH GRANT OPTION;
+GRANT EXECUTE ON FUNCTION aggregate_15_min_tmc3() TO rliu;
+
