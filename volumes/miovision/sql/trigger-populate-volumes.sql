aa72e8f0e746a1c100e80e4a2e4b4bea07841bd1
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-08-03 14:40:37 -0400

    Fixed errors in SELECT/JOINs in trigger function
    #101
    The previous issue with it hanging was actually errors caused by the
    query trying to select movement from miovision.intersections. This query
    solves the problem by first selecting the data to an intermediate table.
    The function also changes the trigger so that inserts data to volumes
    after it has been inserted to raw_data.

--- volumes/miovision/sql/trigger-populate-volumes.sql
+++ volumes/miovision/sql/trigger-populate-volumes.sql
@@ -1,19 +1,35 @@
-﻿CREATE OR REPLACE FUNCTION miovision.insert_volumes_trigger()
+﻿CREATE OR REPLACE FUNCTION insert_volumes()
   RETURNS trigger AS
 $BODY$
+
 BEGIN
-    INSERT INTO miovision.volumes (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume)
-    SELECT B.intersection_uid, (NEW.datetime_bin AT TIME ZONE 'America/Toronto') AS datetime_bin, C.classification_uid, NEW.entry_dir_name as leg, D.movement_uid, NEW.volume
-    FROM miovision.intersections B 
-    INNER JOIN miovision.movements D USING (movement)
-    INNER JOIN miovision.classifications C USING (classification, location_only)
-    WHERE regexp_replace(NEW.study_name,'Yong\M','Yonge') = B.intersection_name
+
+	WITH new_data AS(
+	   
+
+		SELECT B.intersection_uid, (NEW.datetime_bin AT TIME ZONE 'America/Toronto') AS datetime_bin, NEW.classification, NEW.entry_dir_name as leg, NEW.movement, NEW.volume
+		FROM miovision.intersections B 
+		WHERE regexp_replace(NEW.study_name,'Yong\M','Yonge') = B.intersection_name)
+		INSERT INTO rliu.volumes (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume)
+	 
+	SELECT A.intersection_uid, A.datetime_bin, C.classification_uid, A.leg, D.movement_uid, A.volume 
+	FROM new_data A
+	INNER JOIN miovision.movements D USING (movement)
+	INNER JOIN miovision.classifications C USING (classification, location_only);
+	RETURN NULL;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-CREATE TRIGGER insert_volumes
-  BEFORE INSERT
-  ON miovision.raw_data
+ALTER FUNCTION insert_volumes()
+  OWNER TO rliu;
+GRANT EXECUTE ON FUNCTION insert_volumes() TO public;
+GRANT EXECUTE ON FUNCTION insert_volumes() TO dbadmin WITH GRANT OPTION;
+GRANT EXECUTE ON FUNCTION insert_volumes() TO bdit_humans WITH GRANT OPTION;
+GRANT EXECUTE ON FUNCTION insert_volumes() TO rliu;
+
+CREATE TRIGGER insert_volumes_trigger
+  AFTER INSERT
+  ON raw_data
   FOR EACH ROW
-  EXECUTE PROCEDURE miovision.insert_volumes_trigger();
+  EXECUTE PROCEDURE insert_volumes();
