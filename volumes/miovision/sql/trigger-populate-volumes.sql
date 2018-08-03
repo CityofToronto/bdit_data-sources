78b86176316ef6ab4362f2c128230f2fbb14a808
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-08-03 16:16:24 -0400

    Eliminated CTE in function by eliminating USING
    #99
    Resulted in about 8 second time savings for May data compared to
    previous commit.

--- volumes/miovision/sql/trigger-populate-volumes.sql
+++ volumes/miovision/sql/trigger-populate-volumes.sql
@@ -4,18 +4,24 @@ $BODY$
 
 BEGIN
 
-	WITH new_data AS(
-	   
+	INSERT INTO rliu.volumes (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume)
+	SELECT B.intersection_uid, (NEW.datetime_bin AT TIME ZONE 'America/Toronto') AS datetime_bin, C.classification_uid, NEW.entry_dir_name as leg, D.movement_uid, NEW.volume
+	FROM miovision.intersections B 
+	INNER JOIN miovision.movements D ON (D.movement = NEW.movement)
+	INNER JOIN miovision.classifications C ON C.classification = NEW.classification AND C.location_only = D.location_only
+    WHERE regexp_replace(NEW.study_name,'Yong\M','Yonge') = B.intersection_name;
+    RETURN NULL;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION insert_volumes()
+  OWNER TO rliu;
+GRANT EXECUTE ON FUNCTION insert_volumes() TO public;
+GRANT EXECUTE ON FUNCTION insert_volumes() TO dbadmin WITH GRANT OPTION;
+GRANT EXECUTE ON FUNCTION insert_volumes() TO bdit_humans WITH GRANT OPTION;
+GRANT EXECUTE ON FUNCTION insert_volumes() TO rliu;
 
-		SELECT B.intersection_uid, (NEW.datetime_bin AT TIME ZONE 'America/Toronto') AS datetime_bin, NEW.classification, NEW.entry_dir_name as leg, NEW.movement, NEW.volume
-		FROM miovision.intersections B 
-		WHERE regexp_replace(NEW.study_name,'Yong\M','Yonge') = B.intersection_name)
-		INSERT INTO rliu.volumes (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume)
-	 
-	SELECT A.intersection_uid, A.datetime_bin, C.classification_uid, A.leg, D.movement_uid, A.volume 
-	FROM new_data A
-	INNER JOIN miovision.movements D USING (movement)
-	INNER JOIN miovision.classifications C USING (classification, location_only);
 	RETURN NULL;
 END;
 $BODY$
