28ec291bd7cb86f689e383671bff80e257b60b98
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-08-07 17:13:54 -0400

    Includes functionality to set volume_id of raw data
    #99

--- volumes/miovision/sql/trigger-populate-volumes.sql
+++ volumes/miovision/sql/trigger-populate-volumes.sql
@@ -4,25 +4,15 @@ $BODY$
 
 BEGIN
 
+
 	INSERT INTO rliu.volumes (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume)
 	SELECT B.intersection_uid, (NEW.datetime_bin AT TIME ZONE 'America/Toronto') AS datetime_bin, C.classification_uid, NEW.entry_dir_name as leg, D.movement_uid, NEW.volume
 	FROM miovision.intersections B 
 	INNER JOIN miovision.movements D ON (D.movement = NEW.movement)
 	INNER JOIN miovision.classifications C ON C.classification = NEW.classification AND C.location_only = D.location_only
-    WHERE regexp_replace(NEW.study_name,'Yong\M','Yonge') = B.intersection_name;
-    RETURN NULL;
-END;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-ALTER FUNCTION insert_volumes()
-  OWNER TO rliu;
-GRANT EXECUTE ON FUNCTION insert_volumes() TO public;
-GRANT EXECUTE ON FUNCTION insert_volumes() TO dbadmin WITH GRANT OPTION;
-GRANT EXECUTE ON FUNCTION insert_volumes() TO bdit_humans WITH GRANT OPTION;
-GRANT EXECUTE ON FUNCTION insert_volumes() TO rliu;
-
-	RETURN NULL;
+	WHERE regexp_replace(NEW.study_name,'Yong\M','Yonge') = B.intersection_name
+	RETURNING volume_uid INTO NEW.volume_uid;
+	RETURN NEW;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
@@ -35,7 +25,7 @@ GRANT EXECUTE ON FUNCTION insert_volumes() TO bdit_humans WITH GRANT OPTION;
 GRANT EXECUTE ON FUNCTION insert_volumes() TO rliu;
 
 CREATE TRIGGER insert_volumes_trigger
-  AFTER INSERT
+  BEFORE INSERT
   ON raw_data
   FOR EACH ROW
   EXECUTE PROCEDURE insert_volumes();
