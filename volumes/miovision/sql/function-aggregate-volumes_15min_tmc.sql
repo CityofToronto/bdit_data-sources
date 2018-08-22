03c69a89a4e1cc5934dcf83f0146aa9c8ff02a42
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-08-22 10:56:14 -0400

    #138 all zero rows inserted in x-over table
    Getting min(volume_uid) for each bin instead of joining back onto
    volumes

--- volumes/miovision/sql/function-aggregate-volumes_15min_tmc.sql
+++ volumes/miovision/sql/function-aggregate-volumes_15min_tmc.sql
@@ -20,7 +20,8 @@ BEGIN
 		start_time timestamp without time zone,
 		end_time timestamp without time zone,
 		span integer,
-		interpolated boolean);
+		interpolated boolean,
+		a_volume_uid int);
 	
 	INSERT INTO bins
 	SELECT 	intersection_uid, 
@@ -29,8 +30,9 @@ BEGIN
 		MIN(A.datetime_bin) AS start_time,
 		MAX(A.datetime_bin) AS end_time,
 		(EXTRACT(minutes FROM MAX(A.datetime_bin) - MIN(A.datetime_bin))::INT+1) AS span,
-		NULL as interpolated
-	FROM volumes A
+		NULL as interpolated,
+		MIN(volume_uid) as a_volume_uid
+	FROM miovision.volumes A
 	WHERE volume_15min_tmc_uid IS NULL
 	GROUP BY intersection_uid, TIMESTAMP WITHOUT TIME ZONE 'epoch' + INTERVAL '1 second' * (floor((extract('epoch' from A.datetime_bin)) / 900) * 900)
 	HAVING COUNT(DISTINCT A.datetime_bin) > 5;
@@ -38,7 +40,7 @@ BEGIN
 -- IF one of two 1-minute time bins BEFORE and AFTER 15-minute bin are populated, assume no interpolation needed
 	UPDATE bins A 
 	SET interpolated = FALSE
-	FROM (SELECT DISTINCT intersection_uid, datetime_bin from miovision.volumes) B
+	FROM (SELECT DISTINCT intersection_uid, datetime_bin from miovision.volumes WHERE volume_15min_tmc_uid IS NULL) B
 	WHERE 	A.interpolated IS NULL 
 		AND A.avail_minutes < 15 
 		AND A.intersection_uid = B.intersection_uid 
@@ -48,28 +50,33 @@ BEGIN
 	-- IF # of populated 1-minute bins exceeds difference between start and end time, assume no interpolation needed	
 	UPDATE bins A
 	SET interpolated = CASE
-				WHEN 	(EXTRACT(minutes FROM A.end_time - A.start_time)+1) > A.avail_minutes AND A.avail_minutes < 15
-				THEN 	FALSE
-				WHEN 	interpolated IS NULL AND A.avail_minutes < 15	
-	-- ASSUME for all other 15-minute bins with missing data, interpolation needed due to missing video
-				THEN	TRUE
-				END;
+		WHEN 	(EXTRACT(minutes FROM A.end_time - A.start_time)+1) > A.avail_minutes AND A.avail_minutes < 15	THEN
+		 	FALSE
+		WHEN 	interpolated IS NULL AND A.avail_minutes < 15	THEN
+		-- ASSUME for all other 15-minute bins with missing data, interpolation needed due to missing video
+			TRUE
+		END;
 
 	-- FOR 15-minute bins with interpolation needed, IF missing data at start of 15-minute period, SET start_time = start_time + 1 minute to account for potential partial count
 	-- FOR 15-minute bins with interpolation needed, IF missing data at end of 15-minute period, SET end_time = end_time - 1 minute to account for potential partial count
 	UPDATE bins 
-	SET end_time = CASE
-		WHEN interpolated = TRUE AND datetime_bin = start_time THEN end_time - INTERVAL '1 minute' ELSE end_time
-		END,
-	start_time = CASE
-		WHEN interpolated = TRUE AND datetime_bin + INTERVAL '14 minutes' = end_time THEN start_time + INTERVAL '1 minute' ELSE start_time
-		END,
-    span =  CASE
-		WHEN interpolated = TRUE AND datetime_bin = start_time THEN span - 1
-		WHEN interpolated = TRUE AND datetime_bin + INTERVAL '14 minutes' = end_time THEN span -1
-		WHEN interpolated = TRUE AND datetime_bin = start_time 
-			AND datetime_bin + INTERVAL '14 minutes' = end_time THEN span - 2 
- 		ELSE span END;
+		SET end_time = CASE
+			WHEN interpolated = TRUE AND datetime_bin = start_time THEN 
+				end_time - INTERVAL '1 minute' 
+			ELSE end_time END,
+		start_time = CASE
+			WHEN interpolated = TRUE AND datetime_bin + INTERVAL '14 minutes' = end_time THEN 
+				start_time + INTERVAL '1 minute' 
+			ELSE start_time	END,
+    	span =  CASE
+			WHEN interpolated = TRUE AND datetime_bin = start_time THEN 
+				span - 1
+			WHEN interpolated = TRUE AND datetime_bin + INTERVAL '14 minutes' = end_time THEN 
+				span -1
+			WHEN interpolated = TRUE AND datetime_bin = start_time 
+				AND datetime_bin + INTERVAL '14 minutes' = end_time THEN
+				span - 2 
+			ELSE span END;
 
 	BEGIN 
 		-- INSERT INTO volumes_15min_tmc, with interpolated volumes
@@ -107,25 +114,24 @@ BEGIN
 		)
 		, zero_insert AS(
 			/*Link each 0-volume 15-min TMC record to any *one* 1-min volume record at that same intersection/time-bin so that deleting
-			a volume record there and then will CASCADE up to the 0-volume aggregated rows */
-		INSERT INTO volumes_tmc_zeroes 
-		SELECT DISTINCT ON(a.volume_15min_tmc_uid) volume_uid, a.volume_15min_tmc_uid
-		FROM aggregate_insert a
-			INNER JOIN volumes B USING (intersection_uid)
-			WHERE a.volume = 0 
-			AND B.datetime_bin < A.datetime_bin +INTERVAL '15 minutes' AND B.datetime_bin>= A.datetime_bin
-			ORDER BY a.volume_15min_tmc_uid
+			a volume record there and then will CASCADE up to the 0-volume aggregated rows. Currently just uses minimum volume_uid for
+			each bin */
+			INSERT INTO volumes_tmc_zeroes 
+			SELECT a_volume_uid, a.volume_15min_tmc_uid
+			FROM aggregate_insert a
+			INNER JOIN bins USING(intersection_uid, datetime_bin)
+			WHERE a.volume = 0
 		)
 			/*For the non-zero 15-min volumes, link the 1-min disaggregate volumes via FOREIGN KEY relationship*/
-		UPDATE volumes a
+		UPDATE miovision.volumes a
 		SET volume_15min_tmc_uid = b.volume_15min_tmc_uid
 		FROM aggregate_insert b
 		WHERE b.volume > 0 
-		AND a.intersection_uid  = b.intersection_uid 
-		AND a.datetime_bin >= b.datetime_bin AND a.datetime_bin < b.datetime_bin + INTERVAL '15 minutes'
-		AND a.classification_uid  = b.classification_uid 
-		AND a.leg = b.leg
-		AND a.movement_uid = b.movement_uid
+			AND a.intersection_uid  = b.intersection_uid 
+			AND a.datetime_bin >= b.datetime_bin AND a.datetime_bin < b.datetime_bin + INTERVAL '15 minutes'
+			AND a.classification_uid  = b.classification_uid 
+			AND a.leg = b.leg
+			AND a.movement_uid = b.movement_uid
 		;
 	EXCEPTION
 		WHEN unique_violation THEN 
@@ -140,4 +146,4 @@ ALTER FUNCTION miovision.aggregate_15_min_tmc()
     OWNER TO dbadmin;
 
 GRANT EXECUTE ON FUNCTION miovision.aggregate_15_min_tmc() TO dbadmin WITH GRANT OPTION;
-GRANT EXECUTE ON FUNCTION miovision.aggregate_15_min_tmc() TO bdit_humans WITH GRANT OPTION;
+GRANT EXECUTE ON FUNCTION miovision.aggregate_15_min_tmc() TO bdit_humans;
