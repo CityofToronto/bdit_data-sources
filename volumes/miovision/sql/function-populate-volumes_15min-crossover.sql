00141e72b7b8f07e0c5ba9cdefe2996f3ecc9281
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-08-27 16:10:29 -0400

    Updated query with mechanism to filter out invalid movements

--- volumes/miovision/sql/function-populate-volumes_15min-crossover.sql
+++ volumes/miovision/sql/function-populate-volumes_15min-crossover.sql
@@ -1,9 +1,5 @@
 ﻿
--- Function: aggregate_15_min()
-
--- DROP FUNCTION aggregate_15_min();
-
-CREATE OR REPLACE FUNCTION aggregate_15_min()
+CREATE OR REPLACE FUNCTION aggregate_15_min_crossover()
   RETURNS integer AS
 $BODY$
 BEGIN
@@ -18,25 +14,28 @@ BEGIN
 			SUM(A.volume) AS volume,
 			array_agg(volume_15min_tmc_uid) as uids
 
-		FROM rliu.volumes_15min_tmc A
-		INNER JOIN miovision.movement_map B ON B.leg_old = A.leg AND B.movement_uid = A.movement_uid
+		FROM miovision.volumes_15min_tmc A
+		INNER JOIN 
+			(SELECT intersection_uid, classification_uid, leg_old, A.movement_uid, leg_new, dir FROM miovision.movement_map A
+			JOIN miovision.intersection_movements B ON B.movement_uid=A.movement_uid AND B.leg=A.leg_old) B 
+		ON B.leg_old = A.leg AND B.movement_uid = A.movement_uid AND B.intersection_uid=A.intersection_uid AND B.classification_uid=A.classification_uid
 		WHERE A.processed IS NULL
 		GROUP BY A.intersection_uid, A.datetime_bin, A.classification_uid, B.leg_new, B.dir
 		ORDER BY A.datetime_bin, A.intersection_uid, A.classification_uid, B.leg_new, B.dir
 	)
 
-	INSERT INTO rliu.volumes_15min(intersection_uid, datetime_bin, classification_uid, leg, dir, volume)
-	SELECT A.intersection_uid, B.datetime_bin, A.classification_uid, A.leg, A.dir, COALESCE(C.volume,0) AS volume
+	INSERT INTO miovision.volumes_15min(intersection_uid, datetime_bin, classification_uid, leg, dir, volume)
+	SELECT A.intersection_uid, B.datetime_bin, A.classification_uid, A.leg, A.dir, COALESCE(C.volume,-1) AS volume
 	FROM (SELECT intersection_uid, classification_uid, leg, dir FROM transformed GROUP BY intersection_uid, classification_uid, leg, dir) AS A
 	INNER JOIN (SELECT intersection_uid, datetime_bin FROM transformed GROUP BY intersection_uid, datetime_bin) AS B USING (intersection_uid)
 	LEFT JOIN transformed C USING (intersection_uid, datetime_bin, classification_uid, leg, dir);
-		
+
 	--Updates crossover table with new IDs
 	INSERT INTO atr_tmc_uid (volume_15min_tmc_uid, volume_15min_uid)
 	SELECT c.volume_15min_tmc_uid, a.volume_15min_uid
-	FROM rliu.volumes_15min A
+	FROM miovision.volumes_15min A
 	LEFT JOIN miovision.movement_map B on B.leg_new = A.leg AND B.dir = A.dir
-	FULL OUTER JOIN rliu.volumes_15min_tmc C
+	FULL OUTER JOIN miovision.volumes_15min_tmc C
 	ON a.datetime_bin=c.datetime_bin
 	AND a.intersection_uid=c.intersection_uid
 	AND b.leg_old=c.leg
@@ -46,35 +45,30 @@ BEGIN
 	ORDER BY volume_15min_uid;
 
 	--Sets processed column to TRUE
-	UPDATE rliu.volumes_15min_tmc a
+	UPDATE miovision.volumes_15min_tmc a
 	SET processed = TRUE
-	FROM rliu.atr_tmc_uid b
+	FROM miovision.atr_tmc_uid b
 	WHERE processed IS NULL
 	AND a.volume_15min_tmc_uid=b.volume_15min_tmc_uid;
 
-	UPDATE rliu.volumes_15min a
+	UPDATE miovision.volumes_15min a
 	SET processed = TRUE
-	FROM rliu.atr_tmc_uid b
+	FROM miovision.atr_tmc_uid b
 	WHERE processed IS NULL
 	AND a.volume_15min_uid=b.volume_15min_uid;
 
 
 	--Sets remaning records to FALSE if the record cannot be processed
-	UPDATE rliu.volumes_15min a
+	UPDATE miovision.volumes_15min a
 	SET processed = FALSE
 	WHERE processed IS NULL;
 	
-	UPDATE rliu.volumes_15min_tmc a
+	UPDATE miovision.volumes_15min_tmc a
 	SET processed = FALSE
 	WHERE processed IS NULL;
-RETURN 1;
+	
+	RETURN NULL;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION aggregate_15_min()
-  OWNER TO rliu;
-GRANT EXECUTE ON FUNCTION aggregate_15_min() TO public;
-GRANT EXECUTE ON FUNCTION aggregate_15_min() TO dbadmin WITH GRANT OPTION;
-GRANT EXECUTE ON FUNCTION aggregate_15_min() TO bdit_humans WITH GRANT OPTION;
-GRANT EXECUTE ON FUNCTION aggregate_15_min() TO rliu;
