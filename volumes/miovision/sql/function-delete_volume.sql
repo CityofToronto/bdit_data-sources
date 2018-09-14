d5bda60b283289dc927209c742711f0551a86262
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-09-14 10:05:39 -0400

    Deletes from volumes now
    146

--- volumes/miovision/sql/function-delete_volume.sql
+++ volumes/miovision/sql/function-delete_volume.sql
@@ -1,6 +1,6 @@
 ﻿-- DROP FUNCTION miovision.delete_raw_data(date, date);
 
-CREATE OR REPLACE FUNCTION miovision.delete_raw_data(
+CREATE OR REPLACE FUNCTION miovision.delete_volumes(
     start_date date,
     end_date date)
   RETURNS integer AS
@@ -8,7 +8,7 @@ $BODY$
 BEGIN	
 	IF end_date::date - start_date::date <30 THEN
 
-		DELETE FROM miovision.raw_data
+		DELETE FROM miovision.volumes
 		WHERE datetime_bin BETWEEN start_date AND end_date;
 		RETURN 1;
 	ELSE 
@@ -20,5 +20,5 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION miovision.delete_raw_data(date, date)
+ALTER FUNCTION miovision.delete_volumes(date, date)
   OWNER TO rliu;
