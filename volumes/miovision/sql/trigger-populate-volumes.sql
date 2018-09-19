5a854f8f6429cbcd8e969a59278800520d515f5b
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-09-19 17:28:42 -0400

    Bugfix on populate volumes trigger

--- volumes/miovision/sql/trigger-populate-volumes.sql
+++ volumes/miovision/sql/trigger-populate-volumes.sql
@@ -11,7 +11,8 @@ BEGIN
 	SELECT B.intersection_uid, (NEW.datetime_bin AT TIME ZONE 'America/Toronto') AS datetime_bin, C.classification_uid, NEW.entry_dir_name as leg, D.movement_uid, NEW.volume
 	FROM miovision.intersections B 
 	INNER JOIN miovision.movements D ON (D.movement = NEW.movement)
-	INNER JOIN miovision.classifications C ON C.classification = NEW.classification AND C.location_only = D.location_only;
+	INNER JOIN miovision.classifications C ON C.classification = NEW.classification AND C.location_only = D.location_only
+  WHERE NEW.study_name = B.intersection_name;
 	RETURN NULL;
 END;
 $BODY$
