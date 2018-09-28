c7334e0a01afa2ade37676ed439b70a69dee9943
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-09-28 15:52:39 -0400

    SQL query to create exceptions table
    #166

---
+++ volumes/miovision/sql/create-table-exceptions.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE miovision.exceptions
+(
+  exceptions_uid serial NOT NULL,
+  intersection_uid integer NOT NULL,
+  excluded_datetime tsrange NOT NULL,
+  classification_uid integer NOT NULL,
+  reason text NOT NULL,
+  CONSTRAINT exceptions_uid_pkey PRIMARY KEY (exceptions_uid)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE miovision.exceptions
+  OWNER TO rliu;
