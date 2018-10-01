3a26affa326cf8530535065add36f768690fb6d6
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-10-01 13:46:56 -0400

    Added query for exceptions table and revised report_volume_15min view
    #164 #163

--- volumes/miovision/sql/create-table-exceptions.sql
+++ volumes/miovision/sql/create-table-exceptions.sql
@@ -3,12 +3,10 @@
   exceptions_uid serial NOT NULL,
   intersection_uid integer NOT NULL,
   excluded_datetime tsrange NOT NULL,
-  classification_uid integer NOT NULL,
+  class_type text NOT NULL,
   reason text NOT NULL,
   CONSTRAINT exceptions_uid_pkey PRIMARY KEY (exceptions_uid)
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE miovision.exceptions
-  OWNER TO rliu;
--- volumes/miovision/sql/create-view-report_volumes_15min.sql
+++ volumes/miovision/sql/create-view-report_volumes_15min.sql
@@ -1,30 +1,38 @@
-﻿DROP MATERIALIZED VIEW IF EXISTS miovision.report_volumes_15min CASCADE;
-CREATE MATERIALIZED VIEW miovision.report_volumes_15min AS
-	WITH valid_bins AS (
-		SELECT	intersection_uid,
-			class_type,
-			(dt + B::time) AS datetime_bin,
-			period_type
-		FROM 	miovision.report_dates A
-		CROSS JOIN generate_series('2017-01-01 06:00'::timestamp,'2017-01-01 19:45'::timestamp,'15 minutes'::interval) B
-		ORDER BY intersection_uid, class_type, (dt + B::time)
-	),
-	int_avg AS (
-		SELECT 	intersection_uid, 
-			class_type, 
-			dir, 
-			leg, 
-			period_type, 
-			datetime_bin::time as time_bin, 
-			AVG(total_volume) AS avg_volume
-		FROM 	miovision.volumes_15min_by_class
-		GROUP BY intersection_uid, class_type, period_type, dir, leg, datetime_bin::time
-	)
-		
+﻿CREATE MATERIALIZED VIEW miovision_new.report_volumes_15min AS 
+ WITH valid_bins AS (
+         SELECT a_1.intersection_uid,
+            a_1.class_type,
+            a_1.dt + b_1.b::time without time zone AS datetime_bin,
+            a_1.period_type
+           FROM miovision_new.report_dates a_1
+             CROSS JOIN generate_series('2017-01-01 06:00:00'::timestamp without time zone, '2017-01-01 19:45:00'::timestamp without time zone, '00:15:00'::interval) b_1(b)
+             LEFT JOIN miovision.exceptions c ON a_1.intersection_uid = c.intersection_uid AND a_1.class_type = c.class_type AND (a_1.dt + b_1.b) <@ c.excluded_datetime
+          WHERE c.excluded_datetime IS NULL
+          ORDER BY a_1.intersection_uid, a_1.class_type, (a_1.dt + b_1.b::time without time zone)
+        ), int_avg AS (
+         SELECT volumes_15min_by_class.intersection_uid,
+            volumes_15min_by_class.class_type,
+            volumes_15min_by_class.dir,
+            volumes_15min_by_class.leg,
+            volumes_15min_by_class.period_type,
+            volumes_15min_by_class.datetime_bin::time without time zone AS time_bin,
+            avg(volumes_15min_by_class.total_volume) AS avg_volume
+           FROM miovision_new.volumes_15min_by_class
+          GROUP BY volumes_15min_by_class.intersection_uid, volumes_15min_by_class.class_type, volumes_15min_by_class.period_type, volumes_15min_by_class.dir, volumes_15min_by_class.leg, (volumes_15min_by_class.datetime_bin::time without time zone)
+        )
+ SELECT a.intersection_uid,
+    a.period_type,
+    a.datetime_bin,
+    a.class_type,
+    b.dir,
+    b.leg,
+    COALESCE(c.total_volume, b.avg_volume) AS volume
+   FROM valid_bins a
+     JOIN int_avg b USING (intersection_uid, class_type, period_type)
+     LEFT JOIN miovision_new.volumes_15min_by_class c USING (datetime_bin, intersection_uid, class_type, dir, leg, period_type)
+  WHERE b.time_bin = a.datetime_bin::time without time zone
+  ORDER BY a.intersection_uid, a.period_type, a.datetime_bin, a.class_type, b.dir, b.leg
+WITH DATA;
 
-	SELECT intersection_uid, period_type, datetime_bin, class_type, dir, leg, COALESCE(total_volume, avg_volume) AS volume
-	FROM valid_bins A
-	INNER JOIN int_avg B USING (intersection_uid, class_type, period_type)
-	LEFT JOIN miovision.volumes_15min_by_class C USING (datetime_bin, intersection_uid, class_type, dir, leg, period_type)
-	WHERE B.time_bin = A.datetime_bin::time
-	ORDER BY intersection_uid, period_type, datetime_bin, class_type, dir, leg
+ALTER TABLE miovision_new.report_volumes_15min
+  OWNER TO bdit_humans;
