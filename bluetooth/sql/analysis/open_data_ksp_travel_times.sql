82b6739abe6c5009be60a3297521cfc595d0c978
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-10-11 14:23:53 -0400

    KSP TT exclude last month
    Also Updated aggregate based on #164

--- bluetooth/sql/analysis/open_data_ksp_travel_times.sql
+++ bluetooth/sql/analysis/open_data_ksp_travel_times.sql
@@ -36,7 +36,7 @@ SELECT
 	JOIN king_pilot.date_lookup c ON dt <= datetime_bin AND dt + INTERVAL '1 day' > datetime_bin
 	LEFT OUTER JOIN king_pilot.periods d ON c.day_type = d.day_type AND datetime_bin::TIME <@ d.period_range
 	JOIN bluetooth.segments USING (analysis_id)
-	WHERE datetime_bin >= '2018-01-01' AND B.datetime_bin < LEAST(date_trunc('month', current_date), '2018-01-01'::DATE + INTERVAL '1 year');
+	WHERE datetime_bin >= '2018-01-01' AND B.datetime_bin < LEAST(date_trunc('month', current_date) - INTERVAL '1 Month', '2018-01-01'::DATE + INTERVAL '1 year');
 GRANT ALL ON open_data.ksp_travel_times_2018 TO rds_superuser WITH GRANT OPTION;
 GRANT ALL ON open_data.ksp_travel_times_2018 TO dbadmin;
  GRANT SELECT ON open_data.ksp_travel_times_2018 TO bdit_humans;
