59eb854d1a5cd000d8abb05febae430e40e02534
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-10-19 16:01:03 -0400

    Updated API with config file and write to CSV
    #170

--- wys/api/wys_api.py
+++ wys/api/wys_api.py
@@ -12,7 +12,7 @@ from requests import Session
 import datetime
 import pytz
 import dateutil.parser
-
+import configparser
 
 
 def get_signs():
@@ -25,10 +25,9 @@ def get_signs():
         return signs
 
 
-def get_statistics():
+def get_statistics(location):
     headers={'Content-Type':'application/json','x-api-key':api_key}
-    #params = {'datetime': '2018-09-14 13:10:00',}
-    response=session.get(url+statistics_endpoint,
+    response=session.get(url+statistics_endpoint+str(location)+end, 
                          headers=headers, proxies=session.proxies)
     if response.status_code==200:
         statistics=json.loads(response.content)
@@ -36,25 +35,39 @@ def get_statistics():
     else:
         return response.status_code
 
-def get_settings():
-    headers={'Content-Type':'application/json','x-api-key':api_key}
-    #params = {'datetime': '2018-09-14 13:10:00',}
-    response=session.get(url+settings_endpoint,
-                         headers=headers, proxies=session.proxies)
-    if response.status_code==200:
-        settings=json.loads(response.content)
-        return settings
-    else:
-        return response.status_code
+
 
 session = Session()
 session.proxies = {'https': 'https://137.15.73.132:8080'}
 url='https://api.streetsoncloud.com/sv2'
 signs_endpoint = '/signs'
-statistics_endpoint='/signs/statistics/location/1845/period/5/speed_units/'
+statistics_endpoint='/signs/statistics/location/'
+end='/period/1440/speed_units/0'
 settings_endpoint='/signs/settings/location/1845/speed_units/0'
-api_key='8MuhAWow9ia9TswH6FDMk9o83dorioQKa7ta6dhk'
+CONFIG = configparser.ConfigParser()
+CONFIG.read('config.cfg')
+key=CONFIG['API']
+api_key=key['key']
 signs=get_signs()
-statistics=get_statistics()
-settings=get_settings()
+table=[]
+#for item in signs:
+location='7562'
+#location=item['location_id']
+#address=item['address']
+statistics=get_statistics(location)
+raw_data=statistics['LocInfo']
+raw_records=raw_data['raw_records']
+for item in raw_records:
+    datetime_bin=item['datetime']
+    for item in item['counter']:
+        temp=[location, datetime_bin, item['speed'], item['count']]
+        table.append(temp)
+
+
+with open('wys_api_'+str(datetime.date.today())+'.csv','w', newline='') as csvfile:
+    fieldnames=['location_id', 'datetime_bin', 'speed', 'count']
+    writer=csv.writer(csvfile, delimiter=',')
+    writer.writerow(fieldnames)
+    for item in table:
+        writer.writerow(item)
 
