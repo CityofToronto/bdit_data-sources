690a0c5de56f1c1f5e48a3a20a3ee1c0a8408c7b
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-10-22 16:35:36 -0400

    Inserts data to database and aggregates data
    #170

---
+++ wys/api/populate-table-counts-15min.sql
@@ -0,0 +1,54 @@
+﻿DROP TABLE IF EXISTS bins;
+
+	CREATE TEMPORARY TABLE bins (
+			api_id integer,
+			datetime_bin timestamp without time zone,
+			start_time timestamp without time zone,
+			end_time timestamp without time zone
+			);
+			
+	WITH bin_grouping AS (
+
+		SELECT 	api_id, 
+			min(datetime_bin) AS start_time,
+			max(datetime_bin) AS end_time,
+			TIMESTAMP WITHOUT TIME ZONE 'epoch' + INTERVAL '1 second' * (floor((extract('epoch' from A.datetime_bin)) / 900) * 900) AS datetime_bin
+		FROM 	wys.raw_data A
+		GROUP BY api_id, datetime_bin
+	)
+	
+	INSERT INTO 	bins
+	SELECT 		api_id,
+			datetime_bin,
+			start_time,
+			end_time
+	FROM 		bin_grouping
+	ORDER BY api_id, datetime_bin;
+
+
+
+	-- INSERT INTO volumes_15min_tmc, with interpolated volumes
+	WITH speed_bins AS (
+		/*Cross product of legal movement for cars, bikes, and peds and the bins to aggregate*/
+		SELECT api_id, datetime_bin, round(speed/5,0)*5 AS speed, sum(count) AS count
+		FROM wys.raw_data
+		GROUP BY api_id, datetime_bin, round(speed/5,0)*5
+		)
+	
+		/*Inner join volume data with bins on intersection/datetime_bin then add zero padding for select movements*/
+		--INSERT INTO miovision_api.volumes_15min_tmc(intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume)
+	INSERT INTO wys.counts_15min (api_id, datetime_bin, speed, count)
+	SELECT 	B.api_id,
+		B.datetime_bin,
+		A.speed,
+		sum(A.count) AS count
+	FROM bins B
+	INNER JOIN speed_bins A 
+								ON A.api_id=B.api_id
+								AND A.datetime_bin BETWEEN B.start_time AND B.end_time
+	/*Only join the zero padding movements to the left side when everything matches, including the bin's datetime_bin
+	Otherwise zero-pad*/
+
+	GROUP BY B.api_id, B.datetime_bin, A.speed
+	ORDER BY B.api_id, B.datetime_bin, A.speed
+
--- wys/api/wys_api.py
+++ wys/api/wys_api.py
@@ -10,10 +10,18 @@ import json
 import csv
 from requests import Session
 import datetime
-import pytz
-import dateutil.parser
 import configparser
+from psycopg2 import connect
+from psycopg2.extras import execute_values
+import dateutil.parser
 
+def roundTime(dt=None, roundTo=60):
+   """function to round to nearest 5 minutes
+   Author: Thierry Husson 2012 - Use it as you want but don't blame me.
+   """
+   seconds = (dt.replace(tzinfo=None) - dt.min).seconds
+   rounding = (seconds+roundTo/2) // roundTo * roundTo
+   return dt + datetime.timedelta(0,rounding-seconds,-dt.microsecond)
 
 def get_signs():
     headers={'Content-Type':'application/json','x-api-key':api_key}
@@ -42,36 +50,50 @@ session.proxies = {'https': 'https://137.15.73.132:8080'}
 url='https://api.streetsoncloud.com/sv2'
 signs_endpoint = '/signs'
 statistics_endpoint='/signs/statistics/location/'
-end='/period/1440/speed_units/0'
-settings_endpoint='/signs/settings/location/1845/speed_units/0'
-<<<<<<< HEAD
+end='/period/1443/speed_units/0'
 CONFIG = configparser.ConfigParser()
 CONFIG.read('config.cfg')
 key=CONFIG['API']
 api_key=key['key']
-=======
-api_key=______
->>>>>>> origin/wys_api
-signs=get_signs()
+dbset = CONFIG['DBSETTINGS']
+conn = connect(**dbset)
+
 table=[]
-#for item in signs:
-location='7562'
-#location=item['location_id']
-#address=item['address']
-statistics=get_statistics(location)
-raw_data=statistics['LocInfo']
-raw_records=raw_data['raw_records']
-for item in raw_records:
-    datetime_bin=item['datetime']
-    for item in item['counter']:
-        temp=[location, datetime_bin, item['speed'], item['count']]
-        table.append(temp)
+
+with conn:
+    with conn.cursor() as cur:
+        string="SELECT * from wys.locations"
+        cur.execute(str(string))
+        signs_list=cur.fetchall()
+        signs_iterator=signs_list
+        conn.commit()
+for signs_iterator in signs_iterator:
+    location=signs_iterator[1]
+    #address=item['address']
+    statistics=get_statistics(location)
+    raw_data=statistics['LocInfo']
+    raw_records=raw_data['raw_records']
+    for item in raw_records:
+        datetime_bin=item['datetime']
+        datetime_bin= dateutil.parser.parse(str(datetime_bin))
+        datetime_bin=roundTime(datetime_bin,roundTo=5*60)
+        counter=item['counter']
+        for item in counter:
+            temp=[location, datetime_bin, item['speed'], item['count']]
+            table.append(temp)   
+    signs_iterator=signs_list
+    
+    
+with conn.cursor() as cur:
+    execute_values(cur, 'INSERT INTO wys.raw_data (api_id, datetime_bin, speed, count) VALUES %s', table)
+    conn.commit()
 
 
-with open('wys_api_'+str(datetime.date.today())+'.csv','w', newline='') as csvfile:
-    fieldnames=['location_id', 'datetime_bin', 'speed', 'count']
-    writer=csv.writer(csvfile, delimiter=',')
-    writer.writerow(fieldnames)
-    for item in table:
-        writer.writerow(item)
 
+#with open('wys_api_'+str(datetime.date.today())+'.csv','w', newline='') as csvfile:
+#    fieldnames=['location_id', 'datetime_bin', 'speed', 'count']
+#    writer=csv.writer(csvfile, delimiter=',')
+#    writer.writerow(fieldnames)
+#    for item in table:
+#        writer.writerow(item)
+#
