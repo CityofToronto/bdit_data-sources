d49d90d95c8190eabf307c8839410bee28b12284
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-10-25 16:26:19 -0400

    Added query for locations table
    #170

---
+++ wys/api/create-table-locations.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE wys.locations
+(
+  api_id integer NOT NULL,
+  sign_name text,
+  address text,
+  serial_num integer,
+  dir text,
+  CONSTRAINT locations_api_id_pkey PRIMARY KEY (api_id)
+)
+WITH (
+  OIDS=FALSE
+);
