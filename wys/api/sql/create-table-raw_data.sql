c254d2b0173f5eeab77ebe15c12d1dc856f89dd3
rickl4 <38917106+rickl4@users.noreply.github.com>
2018-11-20 13:24:54 -0500

    Created trigger from raw_data to speed_counts
    #170
    Data is now inserted into raw_data

---
+++ wys/api/sql/create-table-raw_data.sql
@@ -0,0 +1,41 @@
+﻿-- DROP TABLE wys.raw_data;
+
+CREATE TABLE wys.raw_data
+(
+  raw_data_uid serial NOT NULL,
+  api_id integer,
+  datetime_bin timestamp without time zone,
+  speed integer,
+  count integer,
+  speed_count_uid integer,
+  CONSTRAINT wys_raw_data_uid_pkey PRIMARY KEY (raw_data_uid)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE wys.raw_data
+  OWNER TO rliu;
+GRANT ALL ON TABLE wys.raw_data TO rds_superuser WITH GRANT OPTION;
+GRANT ALL ON TABLE wys.raw_data TO dbadmin;
+GRANT SELECT, REFERENCES, TRIGGER ON TABLE wys.raw_data TO bdit_humans WITH GRANT OPTION;
+GRANT ALL ON TABLE wys.raw_data TO rliu;
+
+-- Trigger: insert_raw_data_trigger on wys.raw_data
+
+-- DROP TRIGGER insert_raw_data_trigger ON wys.raw_data;
+
+CREATE TRIGGER insert_raw_data_trigger
+  BEFORE INSERT
+  ON wys.raw_data
+  FOR EACH ROW
+  EXECUTE PROCEDURE wys.aggregate_speeds();
+
+-- Trigger: raw_data_delete on wys.raw_data
+
+-- DROP TRIGGER raw_data_delete ON wys.raw_data;
+
+CREATE TRIGGER raw_data_delete
+  AFTER DELETE
+  ON wys.raw_data
+  FOR EACH ROW
+  EXECUTE PROCEDURE wys.trgr_raw_data_delete();
---
+++ wys/api/sql/trigger-aggregate_speeds.sql
@@ -0,0 +1,26 @@
+﻿CREATE OR REPLACE FUNCTION wys.aggregate_speeds()
+  RETURNS trigger AS
+$BODY$
+
+BEGIN
+	INSERT INTO wys.speed_counts (api_id, datetime_bin, speed_id, count)
+	SELECT NEW.api_id, NEW.datetime_bin, A.speed_id, NEW.count
+	FROM wys.speed_bins A
+	WHERE NEW.speed<@A.speed_bin;
+	RETURN NEW;
+
+	END;
+
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION wys.aggregate_speeds()
+  OWNER TO rliu;
+
+-- DROP TRIGGER insert_raw_data_trigger ON wys.raw_data;
+
+CREATE TRIGGER insert_raw_data_trigger
+  BEFORE INSERT
+  ON wys.raw_data
+  FOR EACH ROW
+  EXECUTE PROCEDURE wys.aggregate_speeds();
