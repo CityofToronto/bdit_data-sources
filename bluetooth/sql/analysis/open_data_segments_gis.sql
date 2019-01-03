e8a31a5557897d841f3c4ce50e6b5e547acd2a50
Raphael Dumas <raphael.a.dumas@gmail.com>
2019-01-03 17:17:54 -0500

    #62 Create new view
    Add sql for generating geojson from the view
    Kind of also #179 , since columns changed a little.

--- bluetooth/sql/analysis/open_data_segments_gis.sql
+++ bluetooth/sql/analysis/open_data_segments_gis.sql
@@ -1,6 +1,23 @@
-CREATE VIEW open_data.bluetooth_segments AS 
-	SELECT segment_name, analysis_id, street, direction, start_crossstreet, end_street, end_crossstreet, start_date, end_date, length, bluetooth, wifi,  geom
-	
-	FROM bluetooth.segments
-	INNER JOIN bluetooth.report_active_dates USING (analysis_id)
-	WHERE NOT duplicate
+ DROP VIEW open_data.bluetooth_segments;
+
+CREATE OR REPLACE VIEW open_data.bluetooth_segments AS
+ SELECT segments.segment_name AS "resultId",
+    segments.street AS start_street,
+    segments.direction,
+    segments.start_crossstreet as start_cross_street,
+    segments.end_street,
+    segments.end_crossstreet AS end_cross_street,
+    report_active_dates.start_date,
+    report_active_dates.end_date,
+    segments.length,
+    segments.geom
+   FROM bluetooth.segments
+     JOIN bluetooth.report_active_dates USING (analysis_id)
+  WHERE NOT segments.duplicate;
+
+ALTER TABLE open_data.bluetooth_segments
+    OWNER TO rdumas;
+
+GRANT SELECT ON TABLE open_data.bluetooth_segments TO od_extract_svc;
+GRANT ALL ON TABLE open_data.bluetooth_segments TO rdumas;
+GRANT SELECT ON TABLE open_data.bluetooth_segments TO bdit_humans;
