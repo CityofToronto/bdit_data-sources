a794b9c2bbfa67344e1678b86cb2f70504821a71
Raphael Dumas <radumas@users.noreply.github.com>
2019-01-24 15:10:49 -0500

    Bug in link_dir creation

--- here/traffic/sql/create_here_routing.sql
+++ here/traffic/sql/create_here_routing.sql
@@ -12,7 +12,7 @@ INNER JOIN here_gis.streets_17_4 USING(link_id)
 WHERE dir_travel IN ('F', 'B')
 UNION ALL
 --Links in the TO direction of travel, need to duplicate because HERE links are unique to both directions of travel (`dir_travel`)
-SELECT link_id ||'F' AS link_dir, nref_in_id as "source", ref_in_id AS target, geom
+SELECT link_id ||'T' AS link_dir, nref_in_id as "source", ref_in_id AS target, geom
 FROM here_gis.traffic_streets_17_4
 INNER JOIN here_gis.streets_17_4 USING(link_id)
 WHERE dir_travel IN ('T', 'B') 
