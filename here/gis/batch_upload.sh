95a7f8a9f6ee662a8fb79924bed1125f0a5790e9
Raphael Dumas <raphael.a.dumas@gmail.com>
2019-01-28 15:48:17 -0500

    Include uploading turn restriction dbfs

--- here/gis/batch_upload.sh
+++ here/gis/batch_upload.sh
@@ -8,4 +8,15 @@ do
 	psql -h 10.160.12.47 -d bigdata -c "ALTER TABLE here_gis.$tablename OWNER TO here_admins; SELECT here_gis.clip_to('$tablename', '$rev');"
 
 done
+
+dbfs=(Cdms.dbf CdmsDtmod.dbf Rdms.dbf)
+
+for f in ${dbfs[@]}
+do 
+	f_lower="$(echo ${f,,})"
+	tablename="$(echo ${f_lower%.*}_$rev)"
+	shp2pgsql -D -d -n $f here_gis.$tablename | psql -h 10.160.12.47 -d bigdata 
+	psql -h 10.160.12.47 -d bigdata -c "ALTER TABLE here_gis.$tablename OWNER TO here_admins;"
+done
+
 psql -h 10.160.12.47 -d bigdata -c "SELECT here_gis.split_streets_att('$rev');"
