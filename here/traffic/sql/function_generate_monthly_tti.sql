d9db51fd93305c48cad786610c2b089e8c5d41bf
Raphael Dumas <raphael.a.dumas@gmail.com>
2019-02-05 17:18:54 -0500

    Optimising tti generation

--- here/traffic/sql/function_generate_monthly_tti.sql
+++ here/traffic/sql/function_generate_monthly_tti.sql
@@ -1,10 +1,10 @@
 -- FUNCTION: here.generate_monthly_tti(text)
 
--- DROP FUNCTION here.generate_monthly_tti(text);
+ DROP FUNCTION here.generate_monthly_tti(text);
 
 CREATE OR REPLACE FUNCTION here.generate_monthly_tti(
 	yyyymm text)
-    RETURNS integer
+    RETURNS VOID
     LANGUAGE 'plpgsql'
 
     COST 100
@@ -15,33 +15,40 @@ DECLARE
 	_tablename TEXT := 'ta_' || yyyymm;
 
 BEGIN
+	DROP TABLE IF EXISTS avgs ;
 	EXECUTE FORMAT($$
-		WITH free_flow AS(SELECT link_dir, 3.6 * length / PERCENTILE_CONT(0.95) WITHIN GROUP(ORDER BY A.pct_50) AS free_flow_tt
-						  FROM here.%I A
-						  GROUP BY link_dir, "length"
-						 )
-				   
-		INSERT INTO here.monthly_hourly_tti
-
+		
+		CREATE TEMP TABLE avgs AS
+		
 		SELECT 		A.link_dir, 
 				date_trunc('month', A.tx) AS mth,
 				EXTRACT(HOUR from A.tx) as hh,
 				3.6 * length * AVG(1.0/A.pct_50) AS tt_avg,
 				3.6 * length / (PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY A.pct_50)) AS tt_med,
-				3.6 * length * AVG(1.0/A.pct_50) / free_flow_tt  AS tti_avg,
 				COUNT(A.pct_50) AS obs
+		
 		FROM		here.%I A 
-		INNER JOIN free_flow USING (link_dir)
 		LEFT JOIN	ref.holiday E ON A.tx >= e.dt AND A.tx < e.dt + INTERVAL '1 day'
 
 		WHERE		EXTRACT(isodow FROM A.tx) < 6
 				AND E.holiday IS NULL
 		GROUP BY 	A.link_dir,
 				mth,
-				hh, free_flow_tt,
-				"length"
-		$$, _tablename, _tablename);
-	RETURN 1;
+				hh, 
+				"length";
+	$$, _tablename);
+	CREATE INDEX ON avgs USING btree(link_dir);
+	ANALYZE avgs;
+	INSERT INTO here.monthly_hourly_tti
+
+	SELECT link_dir, mth, hh, tt_avg, tt_med, tt_avg / free_flow_tt AS tti_avg, obs, free_flow_tt
+	FROM avgs
+	INNER JOIN (SELECT link_dir, percentile_cont(0.05) WITHIN GROUP (ORDER BY tt_avg) AS free_flow_tt FROM avgs GROUP BY link_dir) AS free_flow USING (link_dir);
+		
+	
+		
+		
+	RETURN ;
 END;
 
 $BODY$;
