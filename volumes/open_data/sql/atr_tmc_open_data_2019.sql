c6eef7daa1bf1a072f6a6a6b81998c92e5c53406
Chelsea Rosic <chelzzr@gmail.com>
2019-03-19 10:04:14 -0400

    added the open data ATR, TMC view code from 2019

---
+++ volumes/open_data/sql/atr_tmc_open_data_2019.sql
@@ -0,0 +1,185 @@
+﻿CREATE OR REPLACE VIEW open_data.volumes_atr_cyclists_permanent AS 
+ SELECT flow.centreline_id,
+    flow.direction,
+    flow.location,
+    flow.class_type,
+    flow.datetime_bin,
+    flow.volume AS volume_15min
+   FROM ( SELECT flow_atr.centreline_id,
+            flow_atr.direction,
+            flow_atr.location,
+            flow_atr.class_type,
+            flow_atr.datetime_bin,
+            flow_atr.volume_15min AS volume
+           FROM open_data.flow_atr
+          WHERE flow_atr.station_type = 'Permanent'::text AND flow_atr.volume_15min >= 0::numeric AND flow_atr.class_type = 'Cyclists'::text) flow
+  ORDER BY flow.datetime_bin DESC, flow.centreline_id, flow.direction;
+
+
+
+  CREATE OR REPLACE VIEW open_data.volumes_atr_cyclists_shortterm AS 
+ SELECT i.centreline_id,
+        CASE
+            WHEN i.dir::text = 'EB'::text THEN 'Eastbound'::text
+            WHEN i.dir::text = 'WB'::text THEN 'Westbound'::text
+            WHEN i.dir::text = 'NB'::text THEN 'Northbound'::text
+            WHEN i.dir::text = 'SB'::text THEN 'Southbound'::text
+            ELSE NULL::text
+        END AS direction,
+    i.location,
+    'Cyclists'::text AS class_type,
+    i.temperature AS daily_temperature,
+    i.precipitation AS daily_percipitation,
+    ( SELECT ((i.count_date || ' '::text) || d.start_time)::timestamp without time zone AS "timestamp") AS datetime_bin_start,
+        CASE
+            WHEN d.end_time <> '00:00:00'::time without time zone THEN ( SELECT ((i.count_date || ' '::text) || d.end_time)::timestamp without time zone AS "timestamp")
+            ELSE ( SELECT (((i.count_date + '1 day'::interval)::date || ' '::text) || d.end_time)::timestamp without time zone AS "timestamp")
+        END AS datetime_bin_end,
+    d.volume
+   FROM cycling.count_data d
+     JOIN cycling.count_info i ON d.count_id = i.count_id
+  ORDER BY i.count_date DESC, i.centreline_id, (
+        CASE
+            WHEN i.dir::text = 'EB'::text THEN 'Eastbound'::text
+            WHEN i.dir::text = 'WB'::text THEN 'Westbound'::text
+            WHEN i.dir::text = 'NB'::text THEN 'Northbound'::text
+            WHEN i.dir::text = 'SB'::text THEN 'Southbound'::text
+            ELSE NULL::text
+        END), (( SELECT ((i.count_date || ' '::text) || d.start_time)::timestamp without time zone AS "timestamp")) DESC;
+
+
+
+DROP MATERIALIZED VIEW IF EXISTS open_data.volumes_atr_vehicles_permanent;
+
+
+CREATE MATERIALIZED VIEW open_data.volumes_atr_vehicles_permanent AS 
+ SELECT DISTINCT flow.centreline_id,
+    flow.direction,
+    flow.location,
+    flow.class_type,
+    flow.datetime_bin,
+    flow.volume_15min
+   FROM ( SELECT flow_atr.centreline_id,
+            flow_atr.direction,
+            flow_atr.location,
+            flow_atr.class_type,
+            flow_atr.datetime_bin,
+            flow_atr.volume_15min
+           FROM open_data.flow_atr
+          WHERE flow_atr.station_type = 'Permanent'::text AND flow_atr.volume_15min >= 0::numeric AND NOT (EXISTS ( SELECT volumes_atr_permanent_exceptions.day,
+                    volumes_atr_permanent_exceptions.location,
+                    volumes_atr_permanent_exceptions.class_type
+                   FROM open_data.volumes_atr_permanent_exceptions
+                  WHERE flow_atr.location::text = volumes_atr_permanent_exceptions.location::text AND flow_atr.datetime_bin::date = volumes_atr_permanent_exceptions.day))) flow
+UNION ALL
+ SELECT a.centreline_id,
+    a.direction,
+    a.location_desc AS location,
+    'Cyclists'::text AS class_type,
+    b.datetime_bin,
+    b.volume_15min
+   FROM ecocounter.sites a
+     JOIN ecocounter.volumes b USING (site_id)
+  WHERE b.volume_15min >= 0
+  ORDER BY 1, 5, 2;
+
+
+
+CREATE OR REPLACE VIEW open_data.volumes_atr_vehicles_shortterm AS 
+ SELECT flow_atr.centreline_id,
+    flow_atr.direction,
+    flow_atr.location,
+    flow_atr.class_type,
+    flow_atr.datetime_bin,
+    flow_atr.volume_15min
+   FROM open_data.flow_atr
+  WHERE flow_atr.station_type = 'Short Term'::text AND flow_atr.volume_15min >= 0::numeric AND NOT (EXISTS ( SELECT exceptions.datetime_bin,
+            exceptions.location
+           FROM open_data_staging.volumes_atr_shortterm_exceptions exceptions
+          WHERE exceptions.datetime_bin = flow_atr.datetime_bin::date AND exceptions.location::text = flow_atr.location::text));
+
+
+
+
+DROP MATERIALIZED VIEW IF EXISTS open_data.volumes_tmc_all_permanent;
+
+CREATE MATERIALIZED VIEW open_data.volumes_tmc_all_permanent AS 
+ SELECT g.node_id AS int_id,
+    c.px,
+    c.display_name AS location,
+    d.class_type,
+        CASE
+            WHEN b.movement_alt IS NULL THEN NULL::text
+            WHEN a.leg = 'W'::text THEN 'Eastbound'::text
+            WHEN a.leg = 'E'::text THEN 'Westbound'::text
+            WHEN a.leg = 'S'::text THEN 'Northbound'::text
+            WHEN a.leg = 'N'::text THEN 'Southbound'::text
+            ELSE NULL::text
+        END AS dir_approach,
+    a.leg,
+    b.movement_alt AS movement,
+    a.datetime_bin,
+    round(sum(a.volume), 0)::integer AS volume_15min
+   FROM miovision_new.volumes_15min_tmc a
+     JOIN miovision_new.movements b USING (movement_uid)
+     JOIN miovision_new.intersections c USING (intersection_uid)
+     JOIN miovision_new.classifications d USING (classification_uid)
+     JOIN gis.traffic_signals g USING (px)
+  WHERE b.movement_uid <> 4 AND d.classification_uid <> 3
+  GROUP BY c.px, c.display_name, d.class_type, a.leg, b.movement_alt, a.datetime_bin, g.node_id
+  ORDER BY c.px, a.datetime_bin, d.class_type, a.leg, b.movement_alt;
+ 
+
+
+
+ DROP MATERIALIZED VIEW IF EXISTS open_data.volumes_tmc_all_shortterm;
+
+CREATE MATERIALIZED VIEW open_data.volumes_tmc_all_shortterm AS 
+ SELECT g.node_id AS int_id,
+    "substring"("substring"(a.location::text, 'PX \d{1,5}'::text), '\d{1,5}'::text)::integer AS px,
+    a.location,
+    d.class_type,
+        CASE
+            WHEN d.movement IS NULL THEN NULL::text
+            WHEN d.leg = 'W'::text THEN 'Eastbound'::text
+            WHEN d.leg = 'E'::text THEN 'Westbound'::text
+            WHEN d.leg = 'S'::text THEN 'Northbound'::text
+            WHEN d.leg = 'N'::text THEN 'Southbound'::text
+            ELSE NULL::text
+        END AS dir_approach,
+    d.leg,
+    d.movement,
+    b.count_date::date + (c.count_time::time without time zone - '00:15:00'::interval) AS datetime_bin,
+    round(avg(d.volume_15min), 0)::integer AS volume_15min
+   FROM traffic.arterydata a
+     JOIN traffic.countinfomics b USING (arterycode)
+     JOIN traffic.det c USING (count_info_id)
+     JOIN open_data.flow_tmc_long d USING (id)
+     LEFT JOIN gis.traffic_signals g ON g.px = "substring"("substring"(a.location::text, 'PX \d{1,5}'::text), '\d{1,5}'::text)::integer
+  WHERE "substring"("substring"(a.location::text, 'PX \d{1,5}'::text), '\d{1,5}'::text) IS NOT NULL
+  GROUP BY a.arterycode, a.apprdir, a.location, d.class_type, d.leg, d.movement, b.count_date, c.count_time, g.node_id
+ HAVING round(avg(d.volume_15min), 0)::integer > 0
+  ORDER BY a.arterycode, b.count_date, c.count_time;
+
+
+
+
+ DROP MATERIALIZED VIEW IF EXISTS open_data.volumes_tmc_permanent_bikes;
+
+CREATE MATERIALIZED VIEW open_data.volumes_tmc_permanent_bikes AS 
+ SELECT g.node_id AS int_id,
+    c.px,
+    c.intersection_name AS location,
+    d.class_type,
+    b.movement_alt AS movement,
+    a.leg,
+    a.datetime_bin,
+    round(sum(a.volume), 0)::integer AS volume_15min
+   FROM miovision_new.volumes_15min_tmc a
+     JOIN miovision_new.movements b USING (movement_uid)
+     JOIN miovision_new.intersections c USING (intersection_uid)
+     JOIN miovision_new.classifications d USING (classification_uid)
+     LEFT JOIN gis.traffic_signals g ON g.px = c.px
+  WHERE b.movement_uid <> 4 AND d.classification_uid <> 3 AND d.class_type = 'Cyclists'::text
+  GROUP BY c.px, c.intersection_name, d.class_type, a.leg, b.movement_alt, a.datetime_bin, g.node_id
+  ORDER BY c.px, a.datetime_bin, d.class_type, a.leg, b.movement_alt;
