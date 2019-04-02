42f050381ba730f5fa260a15762902ad8d626b07
Raphael Dumas <raphael.a.dumas@gmail.com>
2019-04-02 17:34:01 -0400

    Bugfixes and modifying to match pgr_dijkstra #187

--- here/traffic/sql/function_routing_network.sql
+++ here/traffic/sql/function_routing_network.sql
@@ -2,13 +2,13 @@
 DROP FUNCTION here.get_network_for_tx(timestamp without time zone);
 
 CREATE OR REPLACE FUNCTION here.get_network_for_tx (_tx TIMESTAMP)
-RETURNS TABLE (id bigint, source numeric, target numeric, cost numeric)
+RETURNS TABLE (id bigint, source int, target int, cost int)
 AS $$
  SELECT routing_streets.id,
-    routing_streets.source,
-    routing_streets.target,
-    3600.0 * ta.length::numeric / (1000 *     COALESCE(ta.pct_50, ref_spds.pattern_speed))::numeric AS cost
-   FROM here.routing_streets
+    routing_streets.source::INT,
+    routing_streets.target::INT,
+    (3600.0 * routing_streets.length::numeric / (1000 *     COALESCE(ta.pct_50, ref_spds.pattern_speed))::numeric)::INT AS cost
+   FROM here.routing_streets_18_3 routing_streets
    LEFT OUTER JOIN here.ta ON routing_streets.link_dir = ta.link_dir AND tx = _tx
    INNER JOIN here.traffic_pattern_18_spd_15min ref_spds ON ref_spds.link_dir = routing_streets.link_dir 
    WHERE _tx::TIME <@ ref_spds.trange AND EXTRACT(isodow FROM _tx)::INT = ref_spds.isodow;
