6d192b771cd6b996d092c15b4db81189acefa456
katiRG <cnangini@gmail.com>
2019-08-30 14:00:27 -0400

    airflow connects to gcc oracle db using OracleOperator

--- dags/traffic_signals.py
+++ dags/traffic_signals.py
@@ -8,6 +8,9 @@ import psycopg2
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.oracle_operator import OracleOperator
+
+import cx_Oracle
 
 # # since `pull_traffic_signal_functions.py` does not exist in this dir
 # # we need to put a try block around it so the linter wont think its an error
@@ -30,6 +33,8 @@ DEFAULT_ARGS = {
     'task_concurrency': 1
 }
 
+# ------------------------------------------------------------------------------
+# DAG 1 - BashOperator
 TRAFFIC_SIGNAL_DAG = DAG(
     'traffic_signals',
     default_args=DEFAULT_ARGS,
@@ -43,3 +48,26 @@ CREATE_SIGNAL_QUERY_TABLES = BashOperator(
     bash_command='{0} '.format(CREATE_SIGNAL_QUERY_TABLES_SH),
     dag=TRAFFIC_SIGNAL_DAG
 )
+
+# Run DAG 1 on command line:
+# airflow test traffic_signals traffic_signals 29/08/2019
+
+# ------------------------------------------------------------------------------
+# DAG 2 - OracleOperator
+# https://stackoverflow.com/questions/53084753/how-to-execute-multiple-oracle-statements-from-sql-script-in-airflow-oracleoper
+ORACLE_DAG = DAG(
+    'oracle_sql',
+    default_args=DEFAULT_ARGS,
+    max_active_runs=1,
+    template_searchpath=[os.path.join(AIRFLOW_ROOT, 'assets/traffic_signals/airflow/tasks')],
+    schedule_interval='10 6-22 * * 1-5')
+
+RUN_ORACLE_SQL = OracleOperator(
+    task_id='oracle_sql',
+    oracle_conn_id='gcc_gpa2',
+    sql='/oracle_query.sql',
+    dag=ORACLE_DAG
+)
+
+# Run DAG 2 on command line:
+# airflow test oracle_sql oracle_sql 29/08/2019
