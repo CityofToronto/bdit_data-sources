1685c40e6dfefa2b4104b1c7db1a3e0548e64979
jovenc <joven.chew@toronto.ca>
2019-09-24 14:01:57 -0400

    #224 Script to read 2018 & 2019 school safety zone

---
+++ VisionZero/schools.py
@@ -0,0 +1,126 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+import configparser
+from psycopg2 import connect
+from psycopg2.extras import execute_values
+
+CONFIG = configparser.ConfigParser()
+CONFIG.read(r'/home/jchew/google_api/db.cfg')
+dbset = CONFIG['DBSETTINGS']
+
+con = connect(**dbset)
+
+# If modifying these scopes, delete the file token.pickle. (readonly or remove that?)
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of the spreadsheet.
+SAMPLE_SPREADSHEET_ID_FIRST = '16ZmWa6ZoIrJ9JW_aMveQsBM5vuGWq7zH0Vw_rvmSC7A'
+SAMPLE_RANGE_NAME_FIRST = 'Master List!A4:AB91'
+SAMPLE_SPREADSHEET_ID_SECOND = '19JupdNNJSnHpO0YM5sHJWoEvKumyfhqaw-Glh61i2WQ'
+SAMPLE_RANGE_NAME_SECOND = '2019 Master List!A3:AC95'
+
+def first():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID_FIRST,
+                                range=SAMPLE_RANGE_NAME_FIRST).execute()
+    values = result.get('values', [])
+
+    rows = []
+    if not values:
+        print('No data found.')
+    else:
+        for row in values:
+            # Print columns A, B, E, F, X, Y, Z, AA which correspond to indices 0, 1, 4, 5, 23, 24, 25, 26.
+            i = (row[0], row[1], row[4], row[5], row[23], row[24], row[25], row[26])
+            rows.append(i)
+
+    sql='INSERT INTO jchew.school_safety_zone_2018_raw (school_name, address, work_order_fb, work_order_wyss,\
+    locations_zone, final_sign_installation, locations_fb, locations_wyss) VALUES %s'
+
+    print(rows)
+    with con:
+        with con.cursor() as cur:
+            execute_values(cur, sql, rows)  
+
+def second():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID_SECOND,
+                                range=SAMPLE_RANGE_NAME_SECOND).execute()
+    values = result.get('values', [])
+
+    rows = []
+
+    if not values:
+        print('No data found.')
+    else:
+        for row in values:
+            # Print columns A, B, E, F, Y, Z, AA, AB which correspond to indices 0, 1, 4, 5, 24, 25, 26, 27.
+            i = (row[0], row[1], row[4], row[5], row[24], row[25], row[26], row[27])
+            rows.append(i)
+
+    sql='INSERT INTO jchew.school_safety_zone_2019_raw (school_name, address, work_order_fb, work_order_wyss,\
+    locations_zone, final_sign_installation, locations_fb, locations_wyss) VALUES %s'
+
+    print(rows)
+    with con:
+        with con.cursor() as cur:
+            execute_values(cur, sql, rows)  
+
+if __name__ == '__main__':
+    first()
+    second()
