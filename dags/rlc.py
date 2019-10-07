55297e36d1dcc9f23cdfe521df82dec97f20f12b
katiRG <cnangini@gmail.com>
2019-10-07 10:02:16 -0400

    added slack notification to traffic and rlc dags

--- dags/rlc.py
+++ dags/rlc.py
@@ -21,6 +21,38 @@ vz_cred = PostgresHook("vzbigdata") # name of Conn Id defined in UI
 vz_pg_uri = vz_cred.get_uri() # connection to RDS for psql via BashOperator
 conn = vz_cred.get_conn() # connection to RDS for python via PythonOperator
 
+# ------------------------------------------------------------------------------
+# Slack notification
+from airflow.hooks.base_hook import BaseHook
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+
+SLACK_CONN_ID = 'slack'
+def task_fail_slack_alert(context):
+    slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
+    slack_msg = """
+            :red_circle: Task Failed / Tâche échouée.
+            *Task*: {task}
+            *Dag*: {dag}
+            *Execution Time*: {exec_date}
+            *Log Url*: {log_url}
+            """.format(
+            task=context.get('task_instance').task_id,
+            dag=context.get('task_instance').dag_id,
+            ti=context.get('task_instance'),
+            exec_date=context.get('execution_date'),
+            log_url=context.get('task_instance').log_url,
+        )
+    failed_alert = SlackWebhookOperator(
+        task_id='slack_test',
+        http_conn_id='slack',
+        webhook_token=slack_webhook_token,
+        message=slack_msg,
+        username='airflow',
+        proxy='http://137.15.73.132:8080'
+        )
+    return failed_alert.execute(context=context)
+
+# ------------------------------------------------------------------------------
 AIRFLOW_DAGS = os.path.dirname(os.path.realpath(__file__))
 AIRFLOW_ROOT = os.path.dirname(AIRFLOW_DAGS)
 AIRFLOW_TASKS = os.path.join(AIRFLOW_ROOT, 'assets/rlc/airflow/tasks')
@@ -31,7 +63,8 @@ DEFAULT_ARGS = {
     'email_on_retry': True,
     'owner': 'airflow',
     'start_date': datetime(2019, 9, 16), # YYYY, MM, DD
-    'task_concurrency': 1
+    'task_concurrency': 1,
+    'on_failure_callback': task_fail_slack_alert
 }
 
 # ------------------------------------------------------------------------------
--- dags/traffic_signals.py
+++ dags/traffic_signals.py
@@ -12,11 +12,44 @@ import psycopg2
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 
+# ------------------------------------------------------------------------------
 # Credentials
 from airflow.hooks.postgres_hook import PostgresHook
 vz_cred = PostgresHook("vzbigdata") # name of Conn Id defined in UI
 vz_pg_uri = vz_cred.get_uri()
 
+# ------------------------------------------------------------------------------
+# Slack notification
+from airflow.hooks.base_hook import BaseHook
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+
+SLACK_CONN_ID = 'slack'
+def task_fail_slack_alert(context):
+    slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
+    slack_msg = """
+            :red_circle: Task Failed / Tâche échouée.
+            *Task*: {task}
+            *Dag*: {dag}
+            *Execution Time*: {exec_date}
+            *Log Url*: {log_url}
+            """.format(
+            task=context.get('task_instance').task_id,
+            dag=context.get('task_instance').dag_id,
+            ti=context.get('task_instance'),
+            exec_date=context.get('execution_date'),
+            log_url=context.get('task_instance').log_url,
+        )
+    failed_alert = SlackWebhookOperator(
+        task_id='slack_test',
+        http_conn_id='slack',
+        webhook_token=slack_webhook_token,
+        message=slack_msg,
+        username='airflow',
+        proxy='http://137.15.73.132:8080'
+        )
+    return failed_alert.execute(context=context)
+
+# ------------------------------------------------------------------------------
 AIRFLOW_DAGS = os.path.dirname(os.path.realpath(__file__))
 AIRFLOW_ROOT = os.path.dirname(AIRFLOW_DAGS)
 AIRFLOW_TASKS = os.path.join(AIRFLOW_ROOT, 'assets/traffic_signals/airflow/tasks')
@@ -27,7 +60,8 @@ DEFAULT_ARGS = {
     'email_on_retry': True,
     'owner': 'airflow',
     'start_date': datetime(2019, 7, 9), # YYYY, MM, DD
-    'task_concurrency': 1
+    'task_concurrency': 1,
+    'on_failure_callback': task_fail_slack_alert
 }
 
 # ------------------------------------------------------------------------------
