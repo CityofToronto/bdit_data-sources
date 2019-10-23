bd34ed6fc19b1fb7ff7b33c01ffdf8ac18edb3d9
Raphael Dumas <raphael.a.dumas@gmail.com>
2019-10-23 17:40:48 -0400

    #219 api schema shouldn't depend on old schema

--- volumes/miovision/sql/create-view-report-daily.sql
+++ volumes/miovision/sql/create-view-report-daily.sql
@@ -1,48 +1,39 @@
-﻿CREATE OR REPLACE VIEW miovision.report_daily AS
+-- View: miovision_api.report_daily
 
-SELECT 	intersection_uid,
-	intersection_name,
-	street_main,
-	street_cross,
-	class_type,
-	dir,
-	period_type,
-	A.datetime_bin::date AS dt,
-	B.period_name,
-	SUM(volume) AS total_volume
+DROP MATERIALIZED VIEW miovision_api.report_daily;
 
-FROM miovision.report_volumes_15min A
-CROSS JOIN miovision.periods B
-INNER JOIN miovision.class_types USING (class_type_id)
-INNER JOIN miovision.intersections C USING (intersection_uid)
+CREATE MATERIALIZED VIEW miovision_api.report_daily
+TABLESPACE pg_default
+AS
+ SELECT intersection_uid,
+    intersection_name,
+    street_main,
+    street_cross,
+    class_type,
+    dir,
+    period_type,
+    datetime_bin::date AS dt,
+    period_name,
+    sum(volume) AS total_volume
+   FROM miovision_api.report_volumes_15min 
+     CROSS JOIN miovision_api.periods 
+     JOIN miovision_api.intersections USING (intersection_uid)
+  WHERE datetime_bin::time without time zone <@ period_range AND (period_name = ANY (ARRAY['14 Hour'::text, 'AM Peak Period'::text, 'PM Peak Period'::text])) 
+  AND (dir = ANY (ARRAY['EB'::text, 'WB'::text])) 
+  AND (street_cross = ANY (ARRAY['Bathurst'::text, 'Spadina'::text, 'Bay'::text, 'Jarvis'::text])) 
+  AND (street_cross = 'Bathurst'::text AND (leg = ANY (ARRAY['E'::text, 'S'::text, 'N'::text])) 
+	   OR street_cross = 'Jarvis'::text AND (leg = ANY (ARRAY['W'::text, 'S'::text, 'N'::text])) 
+	   OR (street_cross <> ALL (ARRAY['Bathurst'::text, 'Jarvis'::text])) AND (dir = 'EB'::text AND (leg = ANY (ARRAY['W'::text, 'N'::text, 'S'::text]))
+																			   OR dir = 'WB'::text AND (leg = ANY (ARRAY['E'::text, 'N'::text, 'S'::text]))))
+   AND NOT ((class_type = ANY (ARRAY['Vehicles'::text, 'Cyclists'::text])) 
+			AND (dir = 'EB'::text AND (street_main = ANY (ARRAY['Wellington'::text, 'Richmond'::text])) 
+				 OR dir = 'WB'::text AND street_main = 'Adelaide'::text))
+  GROUP BY intersection_uid, intersection_name, street_main, street_cross, period_type, class_type, dir, dt, period_name
+WITH NO DATA;
 
-WHERE 	A.datetime_bin::time <@ B.period_range AND
-	B.period_name IN ('14 Hour','AM Peak Period','PM Peak Period') AND
-	dir IN ('EB','WB') AND
-	street_cross IN ('Bathurst','Spadina','Bay','Jarvis') AND
-	(
-		(street_cross IN ('Bathurst') AND leg IN ('E','S','N')) OR
-		(street_cross IN ('Jarvis') AND leg IN ('W','S','N')) OR
-		(street_cross NOT IN ('Bathurst','Jarvis') AND ( 
-			(dir = 'EB' AND leg IN ('W','N','S')) OR
-			(dir = 'WB' AND leg IN ('E','N','S'))
-			)
-		)
-	) AND
-	NOT (
-		class_type IN ('Vehicles','Cyclists') AND 	
-		(
-			(dir = 'EB' AND street_main IN ('Wellington','Richmond')) OR 
-			(dir = 'WB' AND street_main IN ('Adelaide'))
-		)
-	)
-GROUP BY intersection_uid,
-	intersection_name,
-	street_main,
-	street_cross,
-	period_type,
-	class_type,
-	dir,
-	dt,
-	B.period_name
-;
+ALTER TABLE miovision_api.report_daily
+    OWNER TO miovision_admins;
+
+GRANT ALL ON TABLE miovision_api.report_daily TO bdit_students;
+GRANT SELECT, REFERENCES, TRIGGER ON TABLE miovision_api.report_daily TO bdit_humans WITH GRANT OPTION;
+GRANT ALL ON TABLE miovision_api.report_daily TO bdit_bots;
