8e09261c4a49eabdc6b6e08eac492d23306a7801
jovenc <joven.chew@toronto.ca>
2019-12-12 17:09:30 -0500

    removed tables that are no longer in use

--- volumes/miovision/sql/create-table-class_types.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE miovision.class_types (
-	class_type_id serial primary key,
-	class_type text NOT NULL,
-	UNIQUE(class_type)
-);
-
-GRANT SELECT, REFERENCES ON miovision.class_types TO bdit_humans WITH GRANT OPTION;
