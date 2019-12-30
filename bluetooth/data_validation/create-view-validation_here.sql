3006ba9cfa0f5c5893b6eff41f023d2c373785cc
jovenc <joven.chew@toronto.ca>
2019-12-30 16:08:13 -0500

    #247 seprated obs and spd comparison query

--- bluetooth/data_validation/create-view-validation_here.sql
+++ bluetooth/data_validation/create-view-validation_here.sql
@@ -24,7 +24,7 @@ FROM
 ORDER BY s.ogc_fid
 
 
---Using matched here and bt done for other project to get link_dir
+--Using matched here and bt done for other project to get link_dir (NO KING ST)
 CREATE OR REPLACE VIEW jchew.validation_bt_here_no_king AS
 SELECT * FROM king_pilot.bt_segments
 LEFT JOIN gis_shared_streets.ak_bt_here2
@@ -32,7 +32,7 @@ USING (analysis_id)
 ORDER BY analysis_id
 
 
---Match bt and here using reference_id (NOT SURE YET)
+--Match bt and here using reference_id (MAYBE)
 CREATE OR REPLACE VIEW jchew.validation_bt_here_maybe AS
 SELECT X.bt_id, X.analysis_id, X.reference_id, X.street_name, X.direction,
 X.from_intersection_name, X.to_intersection_name, X.reference_length, 
@@ -43,3 +43,15 @@ ON X.reference_id = Y.reference_id
 --AND X.from_intersection_id = Y.from_intersection_id
 --AND X.to_intersection_id = Y.to_intersection_id
 ORDER BY ogc_fid
+
+--Match bt and here using reference_id (after re-routing is done, AGAIN)
+CREATE OR REPLACE VIEW jchew.validation_bt_here_again AS
+SELECT X.bt_id, X.analysis_id, X.reference_id, Y."shstReferenceId" , X.street_name, X.direction,
+X.from_intersection_name, X.to_intersection_name, X.reference_length, 
+Y.id, Y.pp_link_dir, Y.geom
+ FROM jchew.validation_bluetooth X
+LEFT JOIN natalie.here_matched_180430 Y
+ON X.reference_id = Y."shstReferenceId"
+AND X.from_intersection_id = Y."shstFromIntersectionId"
+AND X.to_intersection_id = Y."shstToIntersectionId"
+ORDER BY id
