318a84a08a3d111fc56fe3c49ba89fd4c4442592
radumas <raphael.a.dumas@gmail.com>
2020-02-03 15:35:06 -0500

    #257 update here_api copy command to specify cols

--- here/traffic/here_api.py
+++ here/traffic/here_api.py
@@ -165,8 +165,14 @@ def send_data_to_database(ctx=None, datafile = None, dbsetting=None):
         unzip = subprocess.Popen(['gunzip','-c',datafile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         #Second uses check_call and 'ON_ERROR_STOP=1' to make sure errors are captured and that the third 
         #process doesn't run befor psql is finished.
+        copy = r'''\COPY here.ta ("link_dir","tx","epoch_min","length","mean",
+                "stddev","min_spd","max_spd","confidence","pct_5","pct_10",
+                "pct_15", "pct_20","pct_25","pct_30","pct_35","pct_40","pct_45",
+                "pct_50","pct_55", "pct_60","pct_65","pct_70","pct_75","pct_80",
+                "pct_85","pct_90","pct_95") FROM STDIN WITH (FORMAT csv, HEADER 
+                TRUE);'''
         LOGGER.info(subprocess.check_output(['psql','-h', dbsetting['host'],'-U',dbsetting['user'],'-d','bigdata','-v','ON_ERROR_STOP=1',
-                                        '-c',r'\COPY here.ta FROM STDIN WITH (FORMAT csv, HEADER TRUE);'],
+                                        '-c',], copy, 
                                         stdin=unzip.stdout))
         subprocess.check_call(['rm', datafile])
     except subprocess.CalledProcessError as err:
