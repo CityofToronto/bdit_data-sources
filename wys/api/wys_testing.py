04e087aefea44e4fb00caad2c9e3335b71005662
jovenc <joven.chew@toronto.ca>
2020-02-04 16:49:44 -0500

    #259 change to date format & catch duplicates of Pkey error

--- wys/api/wys_testing.py
+++ wys/api/wys_testing.py
@@ -7,6 +7,9 @@ import configparser
 from psycopg2 import connect
 from psycopg2.extras import execute_values
 from psycopg2 import sql
+from psycopg2 import Error
+
+from datetime import datetime
 import logging 
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
@@ -29,7 +32,7 @@ def read_masterlist(con, service):
             ward_list=cur.fetchall()
             LOGGER.debug(ward_list)
 
-    for i in range(3):
+    for i in range(25):
         dict_table.update({i+1:ward_list[i]})
         LOGGER.info('Working on ward %s', i+1)
         pull_from_sheet(con, service, dict_table, dict_table[i+1])
@@ -48,45 +51,47 @@ def pull_from_sheet(con, service, dict_table, ward, *args):
     values = result.get('values', [])
 
     rows = []
+    badrows = []
     if not values:
         LOGGER.warning('No data found.')
     else:
         for row in values:           
-            try:                   
-                if row[6] != '' and row[8] != '':
-                    i = (row[0], row[1], row[2], row[3], row[6], row[8], row[10], row[11], row[13])
-                    rows.append(i)
-                    #LOGGER.info('Reading %s columns of data from Google Sheet', len(row))
-                    LOGGER.debug(row)
+            try:             
+                if row[6] and row[8] and row[10]:
+                    try: 
+                        installation = datetime.strptime(row[6], '%m/%d/%Y').date()
+                        removal = datetime.strptime(row[8], '%m/%d/%Y').date()
+                        i = (ward_no, row[0], row[1], row[2], row[3], installation, removal, row[10], row[11], row[13])
+                        rows.append(i)
+                        LOGGER.debug(row)
+                    except:
+                        i = (ward_no, row[0], row[1], row[2], row[3], row[6], row[8], row[10], row[11], row[13])
+                        badrows.append(i)
+                        LOGGER.warning('Date format is not MM/DD/YYYY')
                 else:
-                    LOGGER.info('This row is not included: %s', row)
+                    LOGGER.debug('This row is not included: %s', row)
             except (IndexError, KeyError) as err:
                 LOGGER.error('An error occurs at %s', row)
                 LOGGER.error(err)
+        LOGGER.warning('Please fix date formats for %s rows in %s at %s', len(badrows), table_name, badrows)
 
-    insert = sql.SQL('''INSERT INTO {}.{} (location, from_street, to_street, direction, installation_date, removal_date,
-                       new_sign_number, comments, confirmed) VALUES %s''').format(sql.Identifier(schema_name), sql.Identifier(table_name))                                                                                             
-    LOGGER.info('Uploading %s rows to PostgreSQL', len(rows))
+    insert = sql.SQL('''INSERT INTO {}.{} (ward_no, location, from_street, to_street, direction, installation_date, removal_date,
+                       new_sign_number, comments, confirmed) VALUES %s 
+                       ON CONFLICT (installation_date, new_sign_number) DO UPDATE SET 
+                       removal_date=EXCLUDED.removal_date, new_sign_number=EXCLUDED.new_sign_number, comments=EXCLUDED.comments
+                       ''').format(sql.Identifier(schema_name), sql.Identifier(table_name)) 
+    LOGGER.info('Uploaded %s rows to PostgreSQL for %s', len(rows), table_name)
     LOGGER.debug(rows)
 
-    combine = sql.SQL('''INSERT INTO wys.all_wards 
-                SELECT {}::int AS ward_no, location, from_street, to_street, direction, installation_date, removal_date, new_sign_number, comments 
-                FROM {}.{}''').format(sql.Literal(ward_no), sql.Identifier(schema_name), sql.Identifier(table_name)) 
-
-
-# insert into gis.{} as a {} select {} from gis._{} on conflict({}) 
-# do update set {} = {} where a.{} <> Excluded.{}; 
-# comment on table gis.{} is 'last updated: {}'".format(output_table, insert_column, 
-# (insert_column[:-1])[1:], output_table, primary_key, insert_column,excluded_column,where_id, where_id, output_table, date)
-
-
-    with con:
-        with con.cursor() as cur:
-            cur.execute(truncate)
-            execute_values(cur, insert, rows)
-            cur.execute(combine)
-    LOGGER.info('Table %s is done', table_name)
-    LOGGER.info('Inserted %s into wys.all_wards', table_name)
+    try:
+        with con:
+            with con.cursor() as cur:
+                execute_values(cur, insert, rows)
+        LOGGER.info('Table %s is done', table_name)
+    except Error as err2:
+        LOGGER.error('Duplicates of primary keys in %s !!!', table_name)
+        LOGGER.error(err2)
+        
 
 if __name__ == '__main__':
     CONFIG = configparser.ConfigParser()
