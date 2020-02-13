50ede20b6c5a77bcef667b8aa6865ecdeb4f47d3
radumas <raphael.a.dumas@gmail.com>
2020-02-13 17:19:33 -0500

    #261 stationary locations

---
+++ wys/api/sql/open_data/mat-view-stationary-locations.sql
@@ -0,0 +1,19 @@
+CREATE MATERIALIZED VIEW open_data.wys_stationary_locations AS
+
+SELECT sign_id, address, sign_name, dir, schedule, 
+    min_speed, 
+    speed_limit, 
+    flash_speed, 
+    strobe_speed, 
+	CASE WHEN prev_start IS NOT NULL THEN start_date ELSE MIN(datetime_bin) END AS start_date,
+	LEAST(next_start, MAX(datetime_bin)) AS end_date,
+	geom
+	FROM wys.stationary_signs
+	JOIN wys.sign_schedules_list USING (api_id)
+     JOIN wys.sign_schedules_clean USING (schedule_name)
+	 JOIN wys.speed_counts_agg USING (api_id) 
+	 GROUP BY sign_id, address, sign_name, dir, schedule, 
+    min_speed, 
+    speed_limit, prev_start,next_start, start_date,
+    flash_speed, 
+    strobe_speed, geom
