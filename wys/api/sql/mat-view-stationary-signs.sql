e1b1b8d4334a08bc42d2e67312254055b05333fa
radumas <raphael.a.dumas@gmail.com>
2020-02-13 17:18:54 -0500

    #245 Mostly dedupe Stationary Signs

---
+++ wys/api/sql/mat-view-stationary-signs.sql
@@ -0,0 +1,16 @@
+DROP MATERIALIZED VIEW wys.stationary_signs;
+
+CREATE MATERIALIZED VIEW wys.stationary_signs AS
+	WITH distinctish_signs AS(
+	SELECT DISTINCT ON (api_id,  regexp_replace(sign_name, '([0-9]{5,8})','')) 
+	api_id, address, regexp_replace(sign_name, '([0-9]{5,8})','') AS sign_name, dir, start_date, st_setsrid(st_makepoint(split_part(regexp_replace(loc, '[()]', '', 'g'), ','::text, 2)::float, 
+						split_part(regexp_replace(loc, '[()]', '', 'g'), ','::text, 1)::float), 4326) AS geom
+	FROM wys.locations
+	WHERE length("substring"(reverse(locations.sign_name), '([0-9]{1,8})'::text)) > 3
+	ORDER BY api_id, regexp_replace(sign_name, '([0-9]{5,8})',''))
+	
+	SELECT 
+	api_id, address, sign_name, dir, start_date, lead(start_date) OVER w as next_start, lag(start_date) OVER w as prev_start, geom
+	FROM distinctish_signs
+	WINDOW w as (PARTITION BY api_id order by start_date)
+
