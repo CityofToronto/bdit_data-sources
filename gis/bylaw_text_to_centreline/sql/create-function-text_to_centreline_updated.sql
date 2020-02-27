3dccc7f020e0cf1acc832966fa25d0a4217fc6f4
jovenc <joven.chew@toronto.ca>
2020-02-27 17:45:46 -0500

    #268 IT IS NOW WORKING FOR NORMAL CASES

--- gis/bylaw_text_to_centreline/sql/create-function-text_to_centreline_updated.sql
+++ gis/bylaw_text_to_centreline/sql/create-function-text_to_centreline_updated.sql
@@ -1,4 +1,4 @@
---DROP FUNCTION jchew.text_to_centreline_updated(TEXT, TEXT, TEXT);
+DROP FUNCTION jchew.text_to_centreline_updated(TEXT, TEXT, TEXT);
 CREATE OR REPLACE FUNCTION jchew.text_to_centreline_updated(highway TEXT, frm TEXT, t TEXT)
 RETURNS TABLE(geo_id NUMERIC, centreline_name VARCHAR, con TEXT, notice TEXT, line_geom GEOMETRY, oid1_geom GEOMETRY, oid1_geom_translated GEOMETRY, oid2_geom geometry, oid2_geom_translated GEOMETRY) AS $$
 DECLARE
@@ -275,72 +275,21 @@ DECLARE
 	text_arr_oid1 TEXT[]:= jchew._get_intersection_geom_updated(highway2, btwn1, direction_btwn1::TEXT, metres_btwn1::FLOAT, 0);
 
 	-- used to keep track of the ID of the first intersection so we don't assign the same intersection twice
-	int_id1 INT := (text_arr_oid1[3])::INT;
+	--int_id1 INT := (text_arr_oid1[3])::INT;
 
 	oid1_int INT := (text_arr_oid1[3])::INT;
-	oid1_geom GEOMETRY := ST_GeomFromText(text_arr_oid1[1], 2952);
-	oid1_geom_translated GEOMETRY := ST_GeomFromText(text_arr_oid1[2], 2952);
+	oid1_geom GEOMETRY := ST_GeomFromText(text_arr_oid1[1],4326);
+	oid1_geom_translated GEOMETRY := ST_GeomFromText(text_arr_oid1[2], 4326);
 
 	text_arr_oid2 TEXT[] := (CASE WHEN btwn2_orig LIKE '%point%' AND (btwn2_check NOT LIKE '% of %' OR btwn2_check LIKE ('% of ' || TRIM(btwn1)))
 				THEN jchew._get_intersection_geom_updated(highway2, btwn2, direction_btwn2::TEXT, metres_btwn2::FLOAT, 0)
-				ELSE jchew._get_intersection_geom_updated(highway2, btwn2, direction_btwn2::TEXT, metres_btwn2::FLOAT, int_id1)
+				ELSE jchew._get_intersection_geom_updated(highway2, btwn2, direction_btwn2::TEXT, metres_btwn2::FLOAT, oid1_int)
 				END);
 
 	oid2_int INT := (text_arr_oid2[3])::INT;
-	oid2_geom  GEOMETRY := ST_GeomFromText(text_arr_oid2[1], 2952);
-	oid2_geom_translated GEOMETRY := ST_GeomFromText(text_arr_oid2[2], 2952);
+	oid2_geom  GEOMETRY := ST_GeomFromText(text_arr_oid2[1], 4326);
+	oid2_geom_translated GEOMETRY := ST_GeomFromText(text_arr_oid2[2], 4326);
 	
-	--route centrelines between two intersection geoms using pg routing
-	--after_routing TEXT[] := (SELECT * FROM jchew.get_lines_btwn_interxn(oid1_int, oid2_int))
-
-	line_geom geometry := (CASE WHEN oid1_geom IS NOT NULL AND oid2_geom IS NOT NULL
-						   THEN (SELECT ST_Union(ST_LineMerge(geom)) FROM jchew.get_lines_btwn_interxn(oid1_int, oid2_int))
-						   ELSE NULL END);
-	/* To test
-	SELECT int_start, int_end, ST_Union(ST_LineMerge(geom)) AS combined_geom
-	FROM jchew.get_lines_btwn_interxn(13464376, 13464279)
-	GROUP BY int_start, int_end*/
-
-/*
-	--normal case 
-	centreline_segments geometry := (
-		CASE 
-		--empty ones
-		WHEN line_geom IS NULL THEN NULL
-		--normal cases
-		WHEN COALESCE(metres_btwn1, metres_btwn2) IS NULL
-		THEN line_geom
-
-		-- special case 1
-		WHEN btwn1 = btwn2
-		THEN
-		(
-			gis._centreline_case1(direction_btwn2, metres_btwn2, ST_MakeLine(ST_LineMerge(match_line_to_centreline_geom)), line_geom,
-			ST_GeomFromText((gis._get_intersection_geom(highway2, btwn1, NULL::TEXT, NULL::FLOAT, 0))[1], 2952) )
-		)
-
-		-- special case 2
-		ELSE
-		(
-			gis._centreline_case2(direction_btwn1, direction_btwn2, metres_btwn1, metres_btwn2, match_line_to_centreline_geom, line_geom,
-			-- get the original intersection geoms (not the translated ones)
-			ST_GeomFromText((gis._get_intersection_geom(highway2, btwn1, NULL::TEXT, NULL::FLOAT, 0))[1], 2952),(CASE WHEN btwn2_orig LIKE '%point%' AND (btwn2_check NOT LIKE '% of %' OR btwn2_check LIKE ('% of ' || TRIM(btwn1)))
-			THEN ST_GeomFromText((gis._get_intersection_geom(highway2, btwn2, NULL::TEXT, NULL::FLOAT, 0))[1], 2952)
-			ELSE ST_GeomFromText((gis._get_intersection_geom(highway2, btwn2, NULL::TEXT, NULL::FLOAT, int_id1))[1], 2952)
-			END))
-		)
-		END
-		);
-
-
-	)
-*/
-
-	--name of the centreline
-	centreline_name varchar := (SELECT lf_name FROM jchew.get_lines_btwn_interxn(oid1_int, oid2_int) LIMIT 1);
-
-	--name of id to reference back to centreline table
-	geo_id numeric := (SELECT lf_name FROM jchew.get_lines_btwn_interxn(oid1_int, oid2_int));
 
 	-- sum of the levenshtein distance of both of the intersections matched
 	lev_sum INT := text_arr_oid1[4]::INT + text_arr_oid2[4]::INT;
@@ -362,15 +311,60 @@ DECLARE
 	);
 
 
-	notice TEXT := format('btwn1: %s btwn2: %s highway2: %s metres_btwn1: %s metres_btwn2: %s direction_btwn1: %s direction_btwn2: %s oid1: %s oid2: %s', btwn1, btwn2, highway2,
-	metres_btwn1, metres_btwn2, direction_btwn1, direction_btwn2, int_id1, (text_arr_oid2[2])::INT);
+	notice TEXT := format('btwn1: %s btwn2: %s highway2: %s metres_btwn1: %s metres_btwn2: %s direction_btwn1: %s direction_btwn2: %s oid1: %s oid2: %s', 
+	btwn1, btwn2, highway2,
+	metres_btwn1, metres_btwn2, direction_btwn1, direction_btwn2, oid1_int, oid2_int);
+
+-- 	/* To test
+-- 	SELECT int_start, int_end, ST_Union(ST_LineMerge(geom)) AS combined_geom
+-- 	FROM jchew.get_lines_btwn_interxn(13464376, 13464279)
+-- 	GROUP BY int_start, int_end*/
+
+-- /*
+-- 	--normal case 
+-- 	centreline_segments geometry := (
+-- 		CASE 
+-- 		--empty ones
+-- 		WHEN line_geom IS NULL THEN NULL
+-- 		--normal cases
+-- 		WHEN COALESCE(metres_btwn1, metres_btwn2) IS NULL
+-- 		THEN line_geom
+
+-- 		-- special case 1
+-- 		WHEN btwn1 = btwn2
+-- 		THEN
+-- 		(
+-- 			gis._centreline_case1(direction_btwn2, metres_btwn2, ST_MakeLine(ST_LineMerge(match_line_to_centreline_geom)), line_geom,
+-- 			ST_GeomFromText((gis._get_intersection_geom(highway2, btwn1, NULL::TEXT, NULL::FLOAT, 0))[1], 2952) )
+-- 		)
+
+-- 		-- special case 2
+-- 		ELSE
+-- 		(
+-- 			gis._centreline_case2(direction_btwn1, direction_btwn2, metres_btwn1, metres_btwn2, match_line_to_centreline_geom, line_geom,
+-- 			-- get the original intersection geoms (not the translated ones)
+-- 			ST_GeomFromText((gis._get_intersection_geom(highway2, btwn1, NULL::TEXT, NULL::FLOAT, 0))[1], 2952),(CASE WHEN btwn2_orig LIKE '%point%' AND (btwn2_check NOT LIKE '% of %' OR btwn2_check LIKE ('% of ' || TRIM(btwn1)))
+-- 			THEN ST_GeomFromText((gis._get_intersection_geom(highway2, btwn2, NULL::TEXT, NULL::FLOAT, 0))[1], 2952)
+-- 			ELSE ST_GeomFromText((gis._get_intersection_geom(highway2, btwn2, NULL::TEXT, NULL::FLOAT, int_id1))[1], 2952)
+-- 			END))
+-- 		)
+-- 		END
+-- 		);
+-- */
+
 
 BEGIN 
 
-raise notice 'btwn1: % btwn2: % btwn2_check: %  highway2: % metres_btwn1: %  metres_btwn2: % direction_btwn1: % direction_btwn2: %', btwn1, btwn2, btwn2_check, highway2, metres_btwn1, metres_btwn2, direction_btwn1, direction_btwn2;
+CREATE TEMP TABLE IF NOT EXISTS temp_table AS
+        SELECT int_start, int_end, seq, rout.geo_id, lf_name, geom AS line_geom 
+		FROM jchew.get_lines_btwn_interxn(oid1_int, oid2_int) rout;
 
 
-RETURN QUERY (SELECT geo_id, centreline_name, con, notice, line_geom, oid1_geom, oid1_geom_translated, oid2_geom, oid2_geom_translated);
+RAISE NOTICE 'btwn1: % btwn2: % btwn2_check: %  highway2: % metres_btwn1: %  metres_btwn2: % direction_btwn1: % direction_btwn2: %', 
+btwn1, btwn2, btwn2_check, highway2, metres_btwn1, metres_btwn2, direction_btwn1, direction_btwn2;
+
+
+RETURN QUERY (SELECT temp_table.geo_id, temp_table.lf_name, con, notice, temp_table.line_geom, oid1_geom, oid1_geom_translated, oid2_geom, oid2_geom_translated FROM temp_table);
 
 END;
 $$ LANGUAGE plpgsql;
