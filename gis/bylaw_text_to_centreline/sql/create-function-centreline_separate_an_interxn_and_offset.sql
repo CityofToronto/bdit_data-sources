13caf7e739d2cd9e518479cb74ce799a78ba1498
jovenc <joven.chew@toronto.ca>
2020-03-16 12:10:00 -0400

    #275 function now works but not for all types of case 1

--- gis/bylaw_text_to_centreline/sql/create-function-centreline_separate_an_interxn_and_offset.sql
+++ gis/bylaw_text_to_centreline/sql/create-function-centreline_separate_an_interxn_and_offset.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION jchew._centreline_separate_an_interxn_and_offset
 (line_geom_cut GEOMETRY, ind_line_geom GEOMETRY, oid1_geom GEOMETRY, combined_section NUMRANGE,
-OUT section NUMRANGE, line_geom_separated GEOMETRY)
+OUT section NUMRANGE, OUT line_geom_separated GEOMETRY)
 
 LANGUAGE 'plpgsql'
 AS $BODY$
@@ -13,6 +13,22 @@ section :=
 THEN numrange(0, 1,'[]')
 
 --for combined_section = '[%,1]' or '[0,%]'
+--where the whole centreline is within the buffer
+WHEN ST_Within(ST_Transform(ind_line_geom, 2952), ST_BUFFER(ST_Transform(line_geom_cut, 2952), 2, 'endcap=flat join=round')) = TRUE
+THEN numrange(0, 1, '[]')
+
+--where part of the centreline is within the buffer, and then find out the startpoint of the individual centreline to know which part of the centreline needs to be cut
+WHEN ST_Within(ST_StartPoint(ST_Transform(ind_line_geom, 2952)), ST_BUFFER(ST_Transform(line_geom_cut, 2952), 2, 'endcap=flat join=round')) = TRUE
+THEN numrange(0, (ST_LineLocatePoint(ind_line_geom, ST_EndPoint(line_geom_cut)))::numeric, '[]')
+
+WHEN ST_Within(ST_EndPoint(ST_Transform(ind_line_geom, 2952)), ST_BUFFER(ST_Transform(line_geom_cut, 2952), 2, 'endcap=flat join=round')) = TRUE
+THEN numrange((ST_LineLocatePoint(ind_line_geom, ST_EndPoint(line_geom_cut)))::numeric, 1, '[]')  
+
+ELSE NULL
+
+END);
+
+/*
 ELSE (CASE WHEN ST_Within(ST_Transform(ind_line_geom, 2952), ST_BUFFER(ST_Transform(line_geom_cut, 2952), 2, 'endcap=flat join=round')) = TRUE
     THEN numrange(0, 1, '[]') 
     ELSE
@@ -22,7 +38,7 @@ ELSE (CASE WHEN ST_Within(ST_Transform(ind_line_geom, 2952), ST_BUFFER(ST_Transf
         END)
     END)
 END);
-
+*/
 
 line_geom_separated :=
 --combined_section = '[0,1]'
@@ -30,6 +46,22 @@ line_geom_separated :=
 THEN ind_line_geom
 
 --for combined_section = '[%,1]' or '[0,%]'
+--where the whole centreline is within the buffer
+WHEN ST_Within(ST_Transform(ind_line_geom, 2952), ST_BUFFER(ST_Transform(line_geom_cut, 2952), 2, 'endcap=flat join=round')) = TRUE
+THEN ind_line_geom
+
+--where part of the centreline is within the buffer, and then find out the startpoint of the individual centreline to know which part of the centreline needs to be cut
+WHEN ST_Within(ST_StartPoint(ST_Transform(ind_line_geom, 2952)), ST_BUFFER(ST_Transform(line_geom_cut, 2952), 2, 'endcap=flat join=round')) = TRUE
+THEN ST_LineSubstring(ind_line_geom, 0 , ST_LineLocatePoint(ind_line_geom, ST_EndPoint(line_geom_cut)))
+
+WHEN ST_Within(ST_EndPoint(ST_Transform(ind_line_geom, 2952)), ST_BUFFER(ST_Transform(line_geom_cut, 2952), 2, 'endcap=flat join=round')) = TRUE
+THEN ST_LineSubstring(ind_line_geom, ST_LineLocatePoint(ind_line_geom, ST_EndPoint(line_geom_cut)), 1)  
+
+ELSE NULL
+
+END);
+
+/*
 ELSE (CASE WHEN ST_Within(ST_Transform(ind_line_geom, 2952), ST_BUFFER(ST_Transform(line_geom_cut, 2952), 2, 'endcap=flat join=round')) = TRUE
     THEN ind_line_geom
     ELSE
@@ -39,9 +71,9 @@ ELSE (CASE WHEN ST_Within(ST_Transform(ind_line_geom, 2952), ST_BUFFER(ST_Transf
         END)
     END)
 END);
+*/
 
-
-RAISE NOTICE 'combined_section: %, section: %',
+RAISE NOTICE 'Centrelines are now separated into their respective geo_id row. combined_section: %, section: %',
 combined_section, section;
 
 END;
