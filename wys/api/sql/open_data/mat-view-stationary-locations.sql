ffbd95cf4dcfcead6f56edb4b6a0ae9890bdd253
radumas <raphael.a.dumas@gmail.com>
2020-04-02 17:15:11 -0400

    #266 add unique index #243 use 5kph bin table

--- wys/api/sql/open_data/mat-view-stationary-locations.sql
+++ wys/api/sql/open_data/mat-view-stationary-locations.sql
@@ -1,3 +1,4 @@
+DROP MATERIALIZED VIEW open_data.wys_stationary_locations CASCADE;
 CREATE MATERIALIZED VIEW open_data.wys_stationary_locations AS
 
 SELECT area_short::INT as ward_no, sign_id, address, sign_name, dir, schedule, 
@@ -14,10 +15,23 @@ SELECT area_short::INT as ward_no, sign_id, address, sign_name, dir, schedule,
 	FROM wys.stationary_signs
 	JOIN wys.sign_schedules_list USING (api_id)
      JOIN wys.sign_schedules_clean USING (schedule_name)
-	 JOIN wys.speed_counts_agg USING (api_id) 
+	 JOIN wys.speed_counts_agg_5kph USING (api_id) 
      LEFT JOIN gis.wards2018 ON ST_Contains(wkb_geometry, geom)
-	 GROUP BY ward, sign_id, address, sign_name, dir, schedule, 
+	 GROUP BY ward_no, sign_id, address, sign_name, dir, schedule, 
             min_speed, 
             speed_limit, prev_start,next_start, start_date,
             flash_speed, 
-            strobe_speed, geom
+            strobe_speed, geom;
+CREATE UNIQUE INDEX ON open_data.wys_stationary_locations (sign_id);
+
+CREATE FUNCTION wys.refresh_od_mat_view() 
+RETURNS void
+    LANGUAGE 'sql'
+
+    COST 100
+    VOLATILE SECURITY DEFINER 
+AS $BODY$
+    REFRESH MATERIALIZED VIEW CONCURRENTLY open_data.wys_stationary_locations WITH DATA ;
+$BODY$;
+REVOKE EXECUTE ON FUNCTION wys.refresh_od_mat_view()FROM public;
+GRANT EXECUTE ON FUNCTION wys.refresh_od_mat_view()TO wys_bot;
