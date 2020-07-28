d81b83d14c9aecc8283c132cba860038288881dd
jovenc <joven.chew@toronto.ca>
2020-07-28 12:15:31 -0400

    #307 include gapzise lookup table

--- volumes/miovision/sql/create-mat-view-gapsize_lookup.sql
+++ volumes/miovision/sql/create-mat-view-gapsize_lookup.sql
@@ -3,24 +3,27 @@ TABLESPACE pg_default
 AS
  WITH temp(period, isodow) AS (
          VALUES ('Weekday'::text,'[1,6)'::int4range), ('Weekend'::text,'[6,8)'::int4range)
-	 )
- , mio AS (
+        ), mio AS (
          SELECT volumes.intersection_uid,
             datetime_bin(volumes.datetime_bin, 60) AS hourly_bin,
             sum(volumes.volume) AS vol
            FROM miovision_api.volumes
-          WHERE volumes.datetime_bin > current_date::timestamp without time zone - interval '60 days'
-	 		-- only get data from the past 60 days so that it's always up to date
+          WHERE volumes.datetime_bin > ('now'::text::date::timestamp without time zone - '60 days'::interval)
           GROUP BY volumes.intersection_uid, (datetime_bin(volumes.datetime_bin, 60))
         )
-SELECT intersection_uid, 
-	 d.period, hourly_bin::time AS time_bin, AVG(vol) AS avg_vol,
-	 CASE WHEN AVG(vol) < 100  THEN 20
-	 WHEN AVG(vol) >= 100 AND AVG(vol) < 500 THEN 10
-	 WHEN AVG(vol) >= 500 AND AVG(vol) < 1500 THEN 5
-	 WHEN AVG(vol) > 1500 THEN 2
-	 END AS gap_size
-	 FROM mio
-	 CROSS JOIN temp d
-	 WHERE date_part('isodow'::text, hourly_bin)::integer <@ d.isodow
-	 GROUP BY intersection_uid, d.period, hourly_bin::time
+ SELECT mio.intersection_uid,
+    d.period,
+    mio.hourly_bin::time without time zone AS time_bin,
+    avg(mio.vol) AS avg_vol,
+        CASE
+            WHEN avg(mio.vol) < 100::numeric THEN 20
+            WHEN avg(mio.vol) >= 100::numeric AND avg(mio.vol) < 500::numeric THEN 10
+            WHEN avg(mio.vol) >= 500::numeric AND avg(mio.vol) < 1500::numeric THEN 5
+            WHEN avg(mio.vol) > 1500::numeric THEN 2
+            ELSE NULL::integer
+        END AS gap_size
+   FROM mio
+     CROSS JOIN temp d
+  WHERE date_part('isodow'::text, mio.hourly_bin)::integer <@ d.isodow
+  GROUP BY mio.intersection_uid, d.period, (mio.hourly_bin::time without time zone)
+WITH DATA;
