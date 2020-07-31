287c6a9d1904b35e776c4ee48cac14dee166a525
jovenc <joven.chew@toronto.ca>
2020-07-31 14:04:58 -0400

    #310 updated sql files

--- volumes/miovision/sql/create-table-classifications.sql
+++ volumes/miovision/sql/create-table-classifications.sql
@@ -1,18 +1,11 @@
-﻿DROP TABLE IF EXISTS miovision.classifications;
-
-CREATE TABLE miovision.classifications (
-	classification_uid serial,
-	classification text,
-	location_only boolean, -- for peds and bikes, where movement isn't available, only which leg they were observed on
-	class_type_id smallint,
-    CONSTRAINT classifications_class_type_id_fkey FOREIGN KEY (class_type_id)
-        REFERENCES miovision.class_types (class_type_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-	);
-ALTER TABLE miovision.classifications
-  OWNER TO aharpal;
-GRANT ALL ON TABLE miovision.classifications TO rds_superuser WITH GRANT OPTION;
-GRANT ALL ON TABLE miovision.classifications TO dbadmin;
-GRANT SELECT, REFERENCES, TRIGGER ON TABLE miovision.classifications TO bdit_humans WITH GRANT OPTION;
-GRANT ALL ON TABLE miovision.classifications TO aharpal;
+﻿CREATE TABLE miovision_api.classifications
+(
+    classification_uid integer NOT NULL DEFAULT nextval('miovision_api.classifications_classification_uid_seq'::regclass),
+    classification text COLLATE pg_catalog."default",
+    location_only boolean,
+    class_type text COLLATE pg_catalog."default"
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
--- volumes/miovision/sql/create-table-intersections.sql
+++ volumes/miovision/sql/create-table-intersections.sql
@@ -1,10 +1,23 @@
-﻿DROP TABLE IF EXISTS miovision.intersections;
-
-CREATE TABLE miovision.intersections (
-	intersection_uid serial,
-	intersection_name text,
-	street_main text,
-	street_cross text,
-	lat numeric,
-	lng numeric
-	);
+﻿CREATE TABLE miovision_api.intersections_new
+(
+    intersection_uid integer,
+    id text COLLATE pg_catalog."default",
+    intersection_name text COLLATE pg_catalog."default",
+    date_installed date,
+    date_decommissioned date,
+    lat numeric,
+    lng numeric,
+    street_main text COLLATE pg_catalog."default",
+    street_cross text COLLATE pg_catalog."default",
+    int_id bigint,
+    px integer,
+    geom geometry(Point,4326),
+    n_leg_restricted boolean,
+    e_leg_restricted boolean,
+    s_leg_restricted boolean,
+    w_leg_restricted boolean
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
