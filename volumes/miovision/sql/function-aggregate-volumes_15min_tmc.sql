fbcacb7121f1e6e742377dda036009c8a78561bd
jovenc <joven.chew@toronto.ca>
2020-08-04 11:00:30 -0400

    #310 updated function after testing

--- volumes/miovision/sql/function-aggregate-volumes_15min_tmc.sql
+++ volumes/miovision/sql/function-aggregate-volumes_15min_tmc.sql
@@ -30,8 +30,8 @@ CASE WHEN un.accept = FALSE THEN NULL ELSE (COALESCE(SUM(A.volume), 0)) END AS v
 FROM zero_padding_movements pad
 --To set unacceptable ones to NULL instead (& only gap fill  light vehicles, cyclist and pedestrian)
 LEFT JOIN miovision_api.unacceptable_gaps un
-	ON un.intersection_uid = A.intersection_uid
-	AND DATE_TRUNC('hour', un.gap_start) = DATE_TRUNC('hour', dt.datetime_bin) 	
+	ON un.intersection_uid = pad.intersection_uid
+	AND DATE_TRUNC('hour', un.gap_start) = DATE_TRUNC('hour', pad.datetime_bin15) 	
 --To get 1min bins
 FULL OUTER JOIN miovision_api.volumes A
 	ON datetime_bin >= start_date - INTERVAL '1 hour' 
@@ -40,14 +40,14 @@ FULL OUTER JOIN miovision_api.volumes A
 	AND A.classification_uid = pad.classification_uid
 	AND A.leg = pad.leg 
 	AND A.movement_uid = pad.movement_uid
-	AND A.datetime_bin ≥ pad.datetime_bin15 
+	AND A.datetime_bin >= pad.datetime_bin15 
 	AND A.datetime_bin < pad.datetime_bin15 + interval '15 minutes'
 -- make sure that the intersection is still active
 WHERE pad.intersection_uid IN (SELECT intersection_uid FROM miovision_api.intersections_new 
 				WHERE start_date::date > date_installed 
 				AND date_decommissioned IS NULL)
 AND A.volume_15min_tmc_uid IS NULL
-GROUP BY pad.intersection_uid, pad.datetime_bin15, pad.classification_uid,pad.leg,pad.movement_uid, un.accept;
+GROUP BY pad.intersection_uid, pad.datetime_bin15, pad.classification_uid,pad.leg,pad.movement_uid, un.accept
 RETURNING intersection_uid, volume_15min_tmc_uid, datetime_bin, classification_uid, leg, movement_uid, volume
 )
 
@@ -58,7 +58,7 @@ UPDATE miovision_api.volumes a
 	WHERE a.datetime_bin >= start_date - interval '1 hour' AND a.datetime_bin < end_date -  interval '1 hour'
 	AND a.volume_15min_tmc_uid IS NULL AND b.volume > 0 
 	AND a.intersection_uid  = b.intersection_uid 
-	AND a.datetime_bin >= b.datetime_bin15 AND a.datetime_bin < b.datetime_bin15 + INTERVAL '15 minutes'
+	AND a.datetime_bin >= b.datetime_bin AND a.datetime_bin < b.datetime_bin + INTERVAL '15 minutes'
 	AND a.classification_uid  = b.classification_uid 
 	AND a.leg = b.leg
 	AND a.movement_uid = b.movement_uid
@@ -68,13 +68,3 @@ RAISE NOTICE '% Done aggregating to 15min TMC bin', timeofday();
 END;
 
 $BODY$;
-
-ALTER FUNCTION miovision_api.aggregate_15_min_tmc(date, date)
-    OWNER TO jchew;
-
-GRANT EXECUTE ON FUNCTION miovision_api.aggregate_15_min_tmc(date, date) TO PUBLIC;
-
-GRANT EXECUTE ON FUNCTION miovision_api.aggregate_15_min_tmc(date, date) TO miovision_api_bot;
-
-GRANT EXECUTE ON FUNCTION miovision_api.aggregate_15_min_tmc(date, date) TO jchew;
-
