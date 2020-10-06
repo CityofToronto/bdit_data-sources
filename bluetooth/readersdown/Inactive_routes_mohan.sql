5f1b9107bd60fa6c152ad6e1ead6e6000ce2af39
webgisgeek <64973203+webgisgeek@users.noreply.github.com>
2020-10-06 09:16:13 -0400

    #234 active-inactive routes function by date
    comparing the output with Jovan's routes not working yesterday the following four analysis IDs are also included in the output. 166985, 1422136, 1427976, 1427980. These four routes are there in observations but not in aggr_5min table. Pull data is true. May be there is something I did not understand.

---
+++ bluetooth/readersdown/Inactive_routes_mohan.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION mohan.route_statusb(
+	insert_value date
+)
+	returns table(
+		an_id bigint,
+		last_received date,
+		status text,
+		ddown int
+)
+	
+language plpgsql
+	as $$
+	
+	begin
+		return query
+			select DISTINCT(analysis_id), MAX (datetime_bin::date),
+			CASE
+			WHEN  MAX (datetime_bin::date)>= (insert_value-1) then 'active'::text
+		else 'inactive'::text
+		END 
+		AS route_status,
+		CASE
+		WHEN MAX (datetime_bin::date) >= (insert_value-1) then 0
+		else (insert_value - MAX (datetime_bin::date))
+		END AS days_down
+		from 
+		bluetooth.all_analyses
+		LEFT JOIN bluetooth.aggr_5min USING (analysis_id) 
+		where  bluetooth.all_analyses.pull_data = 'true'
+		--datetime_bin = insert_value
+		GROUP BY analysis_id;
+		end; 
+
