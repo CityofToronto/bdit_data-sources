18e300aba8aa270a66f198b7fba02dd1a16219b1
Raphael Dumas <radumas@users.noreply.github.com>
2021-01-02 22:36:27 -0500

    bugfix. * is a dangerous operator #251

--- volumes/miovision/sql/create-table-volumes.sql
+++ volumes/miovision/sql/create-table-volumes.sql
@@ -73,16 +73,16 @@ AS $BODY$
 BEGIN
 	IF new.datetime_bin >= '2021-01-01'::date AND new.datetime_bin < ('2021-01-01'::date + '1 year'::interval) THEN 
 		INSERT INTO miovision_api.volumes_2021 (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume) 
-		VALUES (NEW.*);
+		VALUES (NEW.intersection_uid, NEW.datetime_bin, NEW.classification_uid, NEW.leg, NEW.movement_uid, NEW.volume);
 	ELSIF new.datetime_bin >= '2018-01-01'::date AND new.datetime_bin < ('2018-01-01'::date + '1 year'::interval) THEN 
 		INSERT INTO miovision_api.volumes_2018 (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume) 
-		VALUES (NEW.*);
+		VALUES (NEW.intersection_uid, NEW.datetime_bin, NEW.classification_uid, NEW.leg, NEW.movement_uid, NEW.volume);
 	ELSIF new.datetime_bin >= '2019-01-01'::date AND new.datetime_bin < ('2019-01-01'::date + '1 year'::interval) THEN 
 		INSERT INTO miovision_api.volumes_2019 (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume) 
 		VALUES (NEW.intersection_uid, NEW.datetime_bin, NEW.classification_uid, NEW.leg, NEW.movement_uid, NEW.volume);
 	ELSIF new.datetime_bin >= '2020-01-01'::date AND new.datetime_bin < ('2020-01-01'::date + '1 year'::interval) THEN 
 		INSERT INTO miovision_api.volumes_2020 (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume) 
-		VALUES (NEW.*);
+		VALUES (NEW.intersection_uid, NEW.datetime_bin, NEW.classification_uid, NEW.leg, NEW.movement_uid, NEW.volume);
   ELSE 
     RAISE EXCEPTION 'Datetime_bin out of range.  Fix the volumes_insert_trigger() function!';
 	END IF;
