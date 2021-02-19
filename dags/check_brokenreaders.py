8af7e0b1afe253d2d40df8ae5eac74627b8cac72
Mohan <mohan@ip-10-160-2-198.ec2.internal>
2021-02-19 16:51:07 -0500

    #343 blip pipeline fail check all in one DAG

--- dags/check_brokenreaders.py
+++ dags/check_brokenreaders.py
@@ -18,25 +18,45 @@ CONFIG = configparser.ConfigParser()
 CONFIG.read(r'/home/mohan/cre.cfg')
 dbset = CONFIG['DBSETTINGS']
 con = connect(**dbset)
+check_date = datetime.date.today()
+broken_list = []
 
-def bad_readers(con, run_date):
+def pipeline_check(con, start_date, end_date):
+    with con.cursor() as cursor:
+        select_query1 = '''SELECT MAX (datetime_bin)::date from bluetooth.aggr_5min'''
+        cursor.execute(select_query1)
+        latest_date = cursor.fetchone()
+        if (latest_date[0]) == check_date - datetime.timedelta(days=1):
+            pass
+        else:
+            raise Exception ('pipeline failed')
+
+def broken_readers(con, start_date, end_date):
     with con.cursor() as cursor: 
-        select_query = """SELECT reader from mohan.broken_reader()"""
-        cursor.execute(select_query)
-        while True:
-            broken_reader = cursor.fetchall()
-            num_readers = len(broken_reader)
-            if num_readers > 0:
-                LOGGER.info(broken_reader)
-                raise Exception (num_readers, ' readers are broken as of', run_date)
-            if num_readers == 0:
-                LOGGER.info('All readers are working as of date = %s', run_date')
-                
+        select_query2 = '''SELECT reader from mohan.broken_readers(current_date)'''
+        cursor.execute(select_query2)
+        broken_readers = cursor.fetchall()
+        broken_list.append(broken_readers)
+        num_broken = len(broken_list)
+        if num_broken == 0:
+                pass
+        else:
+            LOGGER.info(broken_list)
+            raise Exception ('some readers are broken, check broken_readers_log')
+        
 SLACK_CONN_ID = 'slack'
 def task_fail_slack_alert(context):
     slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
-    slack_msg = """
-            :red_circle: A bt Reader may be broken!
+    
+    if context.get('task_instance').task_id == 'pipeline_check':
+        task_msg = ':among_us_dead: The bluetooth aggregation pipeline has failed, :among_us_ghost: <@U01858E603T> fix it asap !'
+    
+    elif context.get('task_instance').task_id == 'broken_readers':
+        task_msg = ':among_us_ghost: some readers have failed. Check the log <@U01858E603T>.'
+    else:
+        task_msg = 'Unknown error has occured. deep dive required <@U01858E603T>.'
+
+        slack_msg = task_msg + """
             *Task*: {task}  
             *Dag*: {dag} 
             *Execution Time*: {exec_date}  
@@ -59,23 +79,52 @@ def task_fail_slack_alert(context):
 
 default_args = {'owner':'mohan',
                 'depends_on_past':False,
-                'start_date': datetime(2021, 1, 26),
+                'start_date': datetime(2021, 2, 11),
                 'email': ['mohanraj.adhikari@toronto.ca'],
                 'email_on_failure': False,
-                 'email_on_success': False,
-                 'retries': 0,
-                 'retry_delay': timedelta(minutes=5),
-                 'on_failure_callback': task_fail_slack_alert
+                'email_on_success': False,
+                'retries': 0,
+                'retry_delay': timedelta(minutes=5),
+                'on_failure_callback': task_fail_slack_alert
                 }
-dag = DAG('bad_readers',default_args=default_args, schedule_interval='0 17 * * *')
-
-task1 = PythonOperator(
-    task_id = 'bad_readers',
-    python_callable = bad_readers,
-    dag=dag,
+WITH DAG('blip_check_update', default_args=default_args, schedule_interval='0 17 * * *') as blip_pipeline:
+    task1 = PythonOperator(
+    task_id = 'pipeline_check',
+    python_callable = pipeline_check,
+    dag=blip_pipeline,
     op_kwargs={
-      'con': con,
-      # execution date is by default a day before if the process runs daily
-      'run_date': '{{ ds }}',
-    }
-)
+    'con': con,
+    # execution date is by default a day before if the process runs daily
+    'start_date': '{{ ds }}',
+    'end_date' : '{{ macros.ds_add(ds, 1) }}'
+    })
+    task2 = PostgresOperator(sql='''SELECT * from mohan.insert_report_date()''',
+                            task_id='update_routes_table',
+                            postgres_conn_id='mohan',
+                            autocommit=True,
+                            retries = 0,
+                            dag=blip_pipeline
+                            )
+    task3 = PostgresOperator(sql='''SELECT * from mohan.reader_status_history(current_date)''',
+                            task_id='bt_reader_status_history',
+                            postgres_conn_id='mohan',
+                            autocommit=True,
+                            retries = 0,
+                            dag=blip_pipeline)
+    task4 = PostgresOperator(sql='''SELECT * from mohan.reader_locations_dt_update(current_date)''',
+                            task_id='bt_reader_locations',
+                            postgres_conn_id='mohan',
+                            autocommit=True,
+                            retries = 0,
+                            dag=blip_pipeline)                        
+    task5 = PythonOperator(
+    task_id = 'broken_readers',
+    python_callable = broken_readers,
+    dag=blip_pipeline,
+    op_kwargs={
+    'con': con,
+    # execution date is by default a day before if the process runs daily
+    'start_date': '{{ ds }}',
+    'end_date' : '{{ macros.ds_add(ds, 1) }}'
+    }) 
+task1 >> [task2, task3, task4] >> task5
