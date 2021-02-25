28b141756ac137fd2d62ed11677edf5b8c33cf66
KatiRG <cnangini@gmail.com>
2021-02-25 09:12:37 -0500

    PR #382: query to import wys atr table

--- wys/validation/_sql/create-table-wys_validation_counts.sql
+++ wys/validation/_sql/create-table-wys_validation_counts.sql
@@ -2,18 +2,18 @@
 Creates table to store data from xlsx files in
 L:\TDCSB\PROJECT\DATA COLLECTION GROUP\3 - MANUAL COUNTS\2020 Manual Counts\WYS SIGN OBSERVANCE\Kertcher
 
-Files were read in and coverted to csv in notebook:
+Files were read in and coverted to csv (semicolon-delimited) in notebook:
 wys/atr/notebooks/tmc_data_preprocessing.ipynb
 */
 
 CREATE TABLE data_analysis.wys_validation_counts
 (
+        atr_id integer,
 	datetime_bin timestamp without time zone,
-	location text,
-	class_type text,
-	count numeric
+        location text,
+        class_type text,
+        count numeric
 );
 
 ALTER TABLE data_analysis.wys_validation_counts
     OWNER to bdit_humans;
-
