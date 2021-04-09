386811c5b61c432f4c2b4a1738f4a9caeaca3e57
radumas <raphael.a.dumas@gmail.com>
2021-04-09 18:38:10 -0400

    #367 take shortcuts expect to fall in traps

--- wys/api/sql/create_table-mobile_sign_installations_dupes.sql
+++ wys/api/sql/create_table-mobile_sign_installations_dupes.sql
@@ -1,4 +1,24 @@
-CREATE TABLE wys.mobile_sign_installations_dupes (LIKE wys.mobile_sign_installations);
+CREATE TABLE wys.mobile_sign_installations_dupes
+(
+    ward_no integer,
+    location text,
+    from_street text,
+    to_street text,
+    direction text,
+    installation_date date,
+    removal_date date,
+    new_sign_number text,
+    comments text,
+    CONSTRAINT mobile_sign_installations_dup_location_from_street_to_stree_key UNIQUE (location, from_street, to_street, direction, installation_date, removal_date, new_sign_number, comments)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
 
-ALTER TABLE wys.mobile_sign_installations_dupes ADD UNIQUE( location, from_street, to_street, direction, 
-            installation_date, removal_date, new_sign_number, comments)
+ALTER TABLE wys.mobile_sign_installations_dupes
+    OWNER to wys_admins;
+
+
+COMMENT ON TABLE wys.mobile_sign_installations_dupes
+    IS 'A place to automate the storage of duplicate (sign_id, installation_date) instead of putting them in the mobile installations table until we can get the WYS maintainers to fix the spreadsheets.';
