20a61f42d8c4698e651715553c9203128d5ace8b
KatiRG <cnangini@gmail.com>
2021-04-26 14:49:21 -0400

    PR #394: define parent and child tables for ssz sheets

--- vision_zero/create-table-school_safety_zone_2021_raw.sql
+++ vision_zero/create-table-school_safety_zone_2021_raw.sql
@@ -1,22 +1,27 @@
-/* Create empty table to store data from 2021 SSZ Google Sheet
+/* Create parent table for all school safety zone sheets
 */
-CREATE TABLE vz_safety_programs_staging.school_safety_zone_2021_raw
-(
-    school_name text COLLATE pg_catalog."default",
-    address text COLLATE pg_catalog."default",
-    work_order_fb text COLLATE pg_catalog."default",
-    work_order_wyss text COLLATE pg_catalog."default",
-    locations_zone text COLLATE pg_catalog."default",
-    final_sign_installation text COLLATE pg_catalog."default",
-    locations_fb text COLLATE pg_catalog."default",
-    locations_wyss text COLLATE pg_catalog."default"
+CREATE TABLE vz_safety_programs_staging.school_safety_zone_raw_parent (
+   	like vz_safety_programs_staging.school_safety_zone_2021_raw 
+	including all
 );
+	
+ALTER TABLE vz_safety_programs_staging.school_safety_zone_raw_parent
+     OWNER to vz_admins;
 
-ALTER TABLE vz_safety_programs_staging.school_safety_zone_2021_raw
-    OWNER to vz_admins;
+GRANT ALL ON TABLE vz_safety_programs_staging.school_safety_zone_raw_parent TO bdit_humans;
+GRANT ALL ON TABLE vz_safety_programs_staging.school_safety_zone_raw_parent TO vz_admins;
+GRANT ALL ON TABLE vz_safety_programs_staging.school_safety_zone_raw_parent TO vz_api_bot;
 
-GRANT ALL ON TABLE vz_safety_programs_staging.school_safety_zone_2021_raw TO bdit_humans;
+/* Define the year-based tables as children of the above parent table
+*/
+ALTER TABLE vz_safety_programs_staging.school_safety_zone_2018_raw 
+	INHERIT vz_safety_programs_staging.school_safety_zone_raw_parent;
 
-GRANT ALL ON TABLE vz_safety_programs_staging.school_safety_zone_2021_raw TO vz_admins;
-
-GRANT ALL ON TABLE vz_safety_programs_staging.school_safety_zone_2021_raw TO vz_api_bot;
+ALTER TABLE vz_safety_programs_staging.school_safety_zone_2019_raw 
+	INHERIT vz_safety_programs_staging.school_safety_zone_raw_parent;
+	
+ALTER TABLE vz_safety_programs_staging.school_safety_zone_2020_raw 
+	INHERIT vz_safety_programs_staging.school_safety_zone_raw_parent;
+	
+ALTER TABLE vz_safety_programs_staging.school_safety_zone_2021_raw 
+	INHERIT vz_safety_programs_staging.school_safety_zone_raw_parent;
