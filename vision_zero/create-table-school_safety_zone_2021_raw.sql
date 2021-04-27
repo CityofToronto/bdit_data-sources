c08fc42d1f5e042d950e3725e9c2b0f5a05a473c
KatiRG <cnangini@gmail.com>
2021-04-27 08:29:53 -0400

    PR #394: query for 2021 table only

--- vision_zero/create-table-school_safety_zone_2021_raw.sql
+++ vision_zero/create-table-school_safety_zone_2021_raw.sql
@@ -1,27 +1,7 @@
-/* Create parent table for all school safety zone sheets
+/* Define the new table to store 2021 data as a child of the parent table.
+   Uses the structure of the existing 2018 table.
 */
-CREATE TABLE vz_safety_programs_staging.school_safety_zone_raw_parent (
-   	like vz_safety_programs_staging.school_safety_zone_2021_raw 
-	including all
-);
-	
-ALTER TABLE vz_safety_programs_staging.school_safety_zone_raw_parent
-     OWNER to vz_admins;
-
-GRANT ALL ON TABLE vz_safety_programs_staging.school_safety_zone_raw_parent TO bdit_humans;
-GRANT ALL ON TABLE vz_safety_programs_staging.school_safety_zone_raw_parent TO vz_admins;
-GRANT ALL ON TABLE vz_safety_programs_staging.school_safety_zone_raw_parent TO vz_api_bot;
-
-/* Define the year-based tables as children of the above parent table
-*/
-ALTER TABLE vz_safety_programs_staging.school_safety_zone_2018_raw 
-	INHERIT vz_safety_programs_staging.school_safety_zone_raw_parent;
-
-ALTER TABLE vz_safety_programs_staging.school_safety_zone_2019_raw 
-	INHERIT vz_safety_programs_staging.school_safety_zone_raw_parent;
-	
-ALTER TABLE vz_safety_programs_staging.school_safety_zone_2020_raw 
-	INHERIT vz_safety_programs_staging.school_safety_zone_raw_parent;
-	
-ALTER TABLE vz_safety_programs_staging.school_safety_zone_2021_raw 
-	INHERIT vz_safety_programs_staging.school_safety_zone_raw_parent;
+CREATE TABLE vz_safety_programs_staging.school_safety_zone_2021_raw (
+   	LIKE vz_safety_programs_staging.school_safety_zone_2018_raw
+	INCLUDING ALL
+) INHERITS (vz_safety_programs_staging.school_safety_zone_raw_parent);
