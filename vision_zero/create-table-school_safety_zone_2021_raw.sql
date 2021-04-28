f67f151ebaffc2e0b330546bf363a0262c61c52d
KatiRG <cnangini@gmail.com>
2021-04-28 09:26:59 -0400

    PR #394: rm query for staging table

--- vision_zero/create-table-school_safety_zone_2021_raw.sql
+++
@@ -1,7 +0,0 @@
-/* Define the new table to store 2021 data as a child of the parent table.
-   Uses the structure of the existing 2018 table.
-*/
-CREATE TABLE vz_safety_programs_staging.school_safety_zone_2021_raw (
-   	LIKE vz_safety_programs_staging.school_safety_zone_2018_raw
-	INCLUDING ALL
-) INHERITS (vz_safety_programs_staging.school_safety_zone_raw_parent);
