c0c461b33577653c3bd9b51453f040f186c3abd3
Charles Zhu <charles.zhu@toronto.ca>
2021-06-03 14:40:01 -0400

    Fixed stupid mistakes (#403).

--- volumes/miovision/sql/create-view-volumes_15min_tmc.sql
+++ volumes/miovision/sql/create-view-volumes_15min_tmc.sql
@@ -1,9 +1,8 @@
 CREATE VIEW miovision_api.volumes_15min_tmc AS (
     SELECT *
-    FROM miovision_api.volumes_15min_tmc
+    FROM miovision_api.volumes_15min_mvt
     WHERE NOT (classification_uid IN (6, 7, 10))
-)
-WITH DATA;
+);
 
 ALTER TABLE miovision_api.volumes_15min_tmc
     OWNER TO miovision_admins;
