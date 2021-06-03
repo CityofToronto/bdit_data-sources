fd9075a0990cbf99ade2aa287784cf7129ccc64e
Charles Zhu <charles.zhu@toronto.ca>
2021-06-03 12:30:28 -0400

    Add view generation script (#403).

---
+++ volumes/miovision/sql/create-view-volumes_15min_tmc.sql
@@ -0,0 +1,19 @@
+CREATE VIEW miovision_api.volumes_15min_tmc AS (
+    SELECT *
+    FROM miovision_api.volumes_15min_tmc
+    WHERE NOT (classification_uid IN (6, 7, 10))
+)
+WITH DATA;
+
+ALTER TABLE miovision_api.volumes_15min_tmc
+    OWNER TO miovision_admins;
+
+COMMENT ON MATERIALIZED VIEW miovision_api.volumes_15min_tmc
+    IS 'miovision_api.volumes_15min_mvt, but only including turning movement counts.';
+
+GRANT ALL ON VIEW miovision_api.volumes_15min_tmc TO bdit_humans;
+GRANT ALL ON VIEW miovision_api.volumes_15min_tmc TO bdit_bots;
+GRANT TRIGGER, REFERENCES, SELECT ON  VIEW miovision_api.volumes_15min_tmc TO bdit_humans WITH GRANT OPTION;
+GRANT ALL ON VIEW miovision_api.volumes_15min_tmc TO dbadmin;
+GRANT ALL ON VIEW miovision_api.volumes_15min_tmc TO miovision_admins;
+GRANT ALL ON VIEW miovision_api.volumes_15min_tmc TO rds_superuser WITH GRANT OPTION;
