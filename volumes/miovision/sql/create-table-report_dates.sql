74cd947d841424d0bd6e9383e949418076da9ab6
Charles Zhu <charles.zhu@toronto.ca>
2021-06-22 18:46:28 -0400

    Fix bug (#414).

--- volumes/miovision/sql/create-table-report_dates.sql
+++ volumes/miovision/sql/create-table-report_dates.sql
@@ -3,9 +3,9 @@
     intersection_uid integer,
     class_type text COLLATE pg_catalog."default",
     period_type text COLLATE pg_catalog."default",
-    dt date
+    dt date,
     CONSTRAINT intersection_uid_class_period_dt_unique_key UNIQUE (intersection_uid, class_type, period_type, dt)
-)
+);
 
 ALTER TABLE miovision_api.report_dates
     OWNER to miovision_admins;
