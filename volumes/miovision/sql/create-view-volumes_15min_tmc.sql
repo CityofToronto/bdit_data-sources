283b13c937b7a34fcfe1508f2f07245bc0343b97
Charles Zhu <charles.zhu@toronto.ca>
2021-07-12 16:05:53 -0400

    Fix bugs in TMC view creation (#407).

--- volumes/miovision/sql/create-view-volumes_15min_tmc.sql
+++ volumes/miovision/sql/create-view-volumes_15min_tmc.sql
@@ -7,12 +7,12 @@ CREATE VIEW miovision_api.volumes_15min_tmc AS (
 ALTER TABLE miovision_api.volumes_15min_tmc
     OWNER TO miovision_admins;
 
-COMMENT ON MATERIALIZED VIEW miovision_api.volumes_15min_tmc
+COMMENT ON VIEW miovision_api.volumes_15min_tmc
     IS 'miovision_api.volumes_15min_mvt, but only including turning movement counts.';
 
-GRANT ALL ON VIEW miovision_api.volumes_15min_tmc TO bdit_humans;
-GRANT ALL ON VIEW miovision_api.volumes_15min_tmc TO bdit_bots;
-GRANT TRIGGER, REFERENCES, SELECT ON  VIEW miovision_api.volumes_15min_tmc TO bdit_humans WITH GRANT OPTION;
-GRANT ALL ON VIEW miovision_api.volumes_15min_tmc TO dbadmin;
-GRANT ALL ON VIEW miovision_api.volumes_15min_tmc TO miovision_admins;
-GRANT ALL ON VIEW miovision_api.volumes_15min_tmc TO rds_superuser WITH GRANT OPTION;
+GRANT ALL ON TABLE miovision_api.volumes_15min_tmc TO bdit_humans;
+GRANT ALL ON TABLE miovision_api.volumes_15min_tmc TO bdit_bots;
+GRANT TRIGGER, REFERENCES, SELECT ON TABLE miovision_api.volumes_15min_tmc TO bdit_humans WITH GRANT OPTION;
+GRANT ALL ON TABLE miovision_api.volumes_15min_tmc TO dbadmin;
+GRANT ALL ON TABLE miovision_api.volumes_15min_tmc TO miovision_admins;
+GRANT ALL ON TABLE miovision_api.volumes_15min_tmc TO rds_superuser WITH GRANT OPTION;
