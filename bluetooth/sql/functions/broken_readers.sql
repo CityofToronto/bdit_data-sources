ee3a6981ef16876139ca77c39a8d65bbd3f236e5
webgisgeek <64973203+webgisgeek@users.noreply.github.com>
2021-07-29 09:07:16 -0400

    Updated broken_readers.sql
    Now this sql returns id and reader_name for broken readers from reader_status_history table.

--- bluetooth/sql/functions/broken_readers.sql
+++ bluetooth/sql/functions/broken_readers.sql
@@ -1,31 +1,34 @@
 -- FUNCTION: bluetooth.broken_readers(date)
 
+-- DROP FUNCTION bluetooth.broken_readers(date);
+
 CREATE OR REPLACE FUNCTION bluetooth.broken_readers(
 	insert_dt date)
-    RETURNS void
+    RETURNS TABLE(read_id integer, reader_name character varying) 
     LANGUAGE 'plpgsql'
 
     COST 100
     VOLATILE 
+    ROWS 1000
 AS $BODY$	
 	begin
-		with x as (
-select reader_id, last_active_date as active_date, active as active_two_days_before, dt as check_date
-from bluetooth.reader_status_history
-where dt = insert_dt - 1 and active IS TRUE
-)
+    return query 
+    with x as(
+select reader_id, last_active_date as yesterday, active as active_yesterday
+	from bluetooth.reader_status_history
+where last_active_date = insert_dt::date - 2
+order by reader_id
+) 
 ,y as (
-select *
-from bluetooth.reader_status_history
-left join x using (reader_id)
-where bluetooth.reader_status_history.active IS false and (bluetooth.reader_status_history.dt = insert_dt and last_active_date = insert_dt-2)
-
+select reader_id, last_active_date as today, active as active_today
+	from bluetooth.reader_status_history
+where last_active_date = insert_dt::date - 1 and active is false
+order by reader_id
 )
-INSERT INTO bluetooth.broken_readers_log(reader_id, reader_name, check_date)
-select reader_id, name, dt
-from y
-left join bluetooth.reader_locations using(reader_id)
-where active_two_days_before IS TRUE and active IS false
+select distinct (reader_id), read_name from x 
+left join y using(reader_id)
+left join bluetooth.detectors_history_corrected using(reader_id)
+where active_yesterday is true and active_today is false
 ;
 end; $BODY$;
 
