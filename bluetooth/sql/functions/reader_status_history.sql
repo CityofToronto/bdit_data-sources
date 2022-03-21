4f321c33729478f5da6e9ad3a17b120dd0d671f4
chmnata <chmnata@gmail.com>
2022-03-21 17:46:29 -0400

    #381, #250 address comment for reader_status_history sql

--- bluetooth/sql/functions/reader_status_history.sql
+++ bluetooth/sql/functions/reader_status_history.sql
@@ -2,121 +2,116 @@
 
 -- DROP FUNCTION bluetooth.reader_status_history(date);
 
-CREATE OR REPLACE FUNCTION bluetooth.reader_status_history(
-	insert_value date)
-    RETURNS TABLE(an_id bigint, last_received date, status text, ddown integer, start_route_point_id text, start_detector text, end_route_point_id text, end_detector text)
-    LANGUAGE 'plpgsql'
+CREATE OR REPLACE FUNCTION bluetooth.reader_status_history(_dt date)
 
-    COST 100
-    VOLATILE 
+   RETURNS TABLE(an_id bigint, 
+                  last_received date, 
+                  status text, 
+                  ddown integer, 
+                  start_route_point_id text, 
+                  start_detector text, 
+                  end_route_point_id text, 
+                  end_detector text)
+   LANGUAGE 'plpgsql'
+   COST 100
+   VOLATILE 
+
 AS $BODY$	
-	begin
-		--return query
-			with x AS 
-			(select DISTINCT(analysis_id), 
-			 CASE
-			 WHEN MAX (datetime_bin::date)>= (insert_value) then (insert_value)
-			 ELSE MAX (datetime_bin::date)
-			 END 
-			 as last_reported,
-			CASE
-			WHEN  MAX (datetime_bin::date)>= (insert_value) then 'True'::text
-		else 'False'::text
-		END 
-		AS route_status
-			 ,CASE
-		WHEN max(aggr_5min.datetime_bin::date) >= (insert_value::text::date) THEN insert_value::date
-                    ELSE (insert_value::date)
-		END AS dt
-		from 
-		bluetooth.all_analyses
-		LEFT JOIN bluetooth.aggr_5min USING (analysis_id) 
-		GROUP BY analysis_id)
-		, y AS (
-		 SELECT all_analyses.analysis_id,
-    (all_analyses.route_points -> 0) ->> 'id'::text AS start_route_point_id,
-    (all_analyses.route_points -> 0) ->> 'name'::text AS start_detector,
-    (all_analyses.route_points -> 1) ->> 'id'::text AS end_route_point_id,
-    (all_analyses.route_points -> 1) ->> 'name'::text AS end_detector
-   FROM bluetooth.all_analyses
-		)
-  , z AS (
-         SELECT x.analysis_id,
-            x.last_reported,
-            x.route_status,
-            x.dt,
-            y.start_route_point_id,
-            y.start_detector,
-            y.end_route_point_id,
-            y.end_detector
-           FROM x
-             LEFT JOIN y USING (analysis_id)
-        )
-		, b AS (
-         SELECT z.analysis_id,
-            z.start_detector AS detector_name,
-            z.route_status,
-            z.last_reported,
-			z.dt
-           FROM z
-        )
-		, c AS (
-         SELECT b.analysis_id,
-            b.detector_name,
-            b.route_status,
-            b.last_reported,
-			b.dt
-           FROM b
-        UNION
-         SELECT z.analysis_id,
-            z.end_detector AS detector_name,
-            z.route_status,
-            z.last_reported,
-			z.dt
-           FROM z
-        )
+	BEGIN
+		-- Return last reported date and route status for the executed date
+		WITH status_info AS (
+			SELECT      DISTINCT(analysis_id), 
+                     CASE
+                        WHEN MAX (datetime_bin::date)>= (_dt) then _dt
+                        ELSE MAX (datetime_bin::date)
+                     END as last_reported,
+                     CASE
+                        WHEN  MAX (datetime_bin::date)>= (_dt) then 'True'::text
+                        ELSE 'False'::text
+                     END AS route_status,
+                     _dt::date AS dt
+
+		   FROM        bluetooth.all_analyses
+		   LEFT JOIN   bluetooth.aggr_5min USING (analysis_id) 
+		   GROUP BY    analysis_id)
+      
+      -- Extract start and end route point id and detector name for each analysis_id
+      , route_info AS (
+         SELECT      status_info.analysis_id,
+                     status_info.last_reported,
+                     status_info.route_status,
+                     status_info.dt,
+                     (all_analyses.route_points -> 0) ->> 'id'::text AS start_route_point_id,
+                     (all_analyses.route_points -> 0) ->> 'name'::text AS start_detector,
+                     (all_analyses.route_points -> 1) ->> 'id'::text AS end_route_point_id,
+                     (all_analyses.route_points -> 1) ->> 'name'::text AS end_detector
+
+         FROM        status_info
+         INNER JOIN  bluetooth.all_analyses USING (analysis_id))
+
+      -- Create one row for the start and end detector for each analysis_id
+		, detector_list AS (
+         SELECT      route_info.analysis_id,
+                     route_info.start_detector AS detector_name,
+                     route_info.route_status,
+                     route_info.last_reported,
+                     route_info.dt
+         FROM        route_info
+         UNION
+         SELECT      route_info.analysis_id,
+                     route_info.end_detector AS detector_name,
+                     route_info.route_status,
+                     route_info.last_reported,
+			            route_info.dt
+         FROM        route_info)
+
+      -- Select only the detectors that are active
 		, active AS (
-         SELECT DISTINCT c.detector_name,
-            max(c.last_reported) AS max,
-            c.route_status,
-            bluetooth.detectors_history_final.reader_id as id,
-			c.dt
-           FROM c
-             LEFT JOIN bluetooth.detectors_history_final ON c.detector_name = bluetooth.detectors_history_final.read_name::text
-          WHERE c.route_status = 'True'::text
-          GROUP BY c.route_status, c.detector_name, bluetooth.detectors_history_final.reader_id, c.dt
-        ), final as (
- SELECT DISTINCT c.detector_name,
-    max(c.last_reported) AS max,
-    c.route_status,
-    bluetooth.detectors_history_final.reader_id,
-			c.dt
-   FROM c
-     LEFT JOIN bluetooth.detectors_history_final ON c.detector_name = bluetooth.detectors_history_final.read_name::text
-  WHERE c.route_status = 'False'::text AND NOT (c.detector_name IN ( SELECT active.detector_name
-           FROM active))
-  GROUP BY c.route_status, c.detector_name, bluetooth.detectors_history_final.reader_id, c.dt
-UNION
- SELECT active.detector_name,
-    active.max,
-    active.route_status,
-    active.id,
-	active.dt
-   FROM active)
+         SELECT      DISTINCT detector_list.detector_name,
+                     max(detector_list.last_reported) AS max,
+                     detector_list.route_status,
+                     detectors_history_final.reader_id AS id,
+                     detector_list.dt
+
+         FROM        detector_list
+         LEFT JOIN   bluetooth.detectors_history_final ON detector_list.detector_name = detectors_history_final.read_name
+         WHERE       detector_list.route_status = 'True'::text
+         GROUP BY    detector_list.route_status, detector_list.detector_name, detectors_history_final.reader_id, detector_list.dt)
+      
+      -- Combine active and deactive detector 
+      , detector_status as (
+         SELECT      DISTINCT detector_list.detector_name,
+                     max(detector_list.last_reported) AS last_reported,
+                     detector_list.route_status,
+                     detectors_history_final.reader_id,
+			            detector_list.dt
+         FROM        detector_list
+         LEFT JOIN   bluetooth.detectors_history_final ON detector_list.detector_name = detectors_history_final.read_name
+         WHERE       detector_list.route_status = 'False'::text AND 
+                     NOT (detector_list.detector_name IN (SELECT active.detector_name FROM active))
+         GROUP BY    detector_list.route_status, detector_list.detector_name, detectors_history_final.reader_id, detector_list.dt
+
+         UNION
+         
+         SELECT      active.detector_name,
+                     active.max,
+                     active.route_status,
+                     active.id,
+                     active.dt
+         FROM        active)
    
-   INSERT INTO bluetooth.reader_status_history (reader_id, last_active_date, active, dt)
-   SELECT DISTINCT reader_id, max(max),route_status::bool, dt
-   from final
-   where reader_id IS NOT NULL
-   group by reader_id, route_status, dt
-   ORDER BY reader_id
-   ;
+   INSERT INTO       bluetooth.reader_status_history (reader_id, last_active_date, active, dt)
+
+   SELECT            DISTINCT reader_id, max(last_reported), route_status, dt
+   FROM              detector_status
+   WHERE             reader_id IS NOT NULL
+   GROUP by          reader_id, route_status, dt
+   ORDER BY          reader_id;
 
    UPDATE bluetooth.reader_locations
-   set date_last_received = (SELECT DISTINCT max(max) from final where bluetooth.detectors_history_final.reader_id = bluetooth.reader_locations.reader_id)
-
-	;
-end; $BODY$;
+   set date_last_received = (SELECT DISTINCT max(last_reported) from detector_status where bluetooth.detectors_history_final.reader_id = bluetooth.reader_locations.reader_id);
+END;
+$BODY$;
 
 ALTER FUNCTION bluetooth.reader_status_history(date)
     OWNER TO bluetooth;
