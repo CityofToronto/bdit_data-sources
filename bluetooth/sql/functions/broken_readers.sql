4164e7706e188c74e5e02182c03601a33cd23e25
chmnata <chmnata@gmail.com>
2022-03-22 16:30:10 -0400

    #385, #343 Simplify broken_readers.sql and add comments

--- bluetooth/sql/functions/broken_readers.sql
+++ bluetooth/sql/functions/broken_readers.sql
@@ -2,39 +2,33 @@
 
 -- DROP FUNCTION bluetooth.broken_readers(date);
 
-CREATE OR REPLACE FUNCTION bluetooth.broken_readers(
-	insert_dt date)
+
+CREATE OR REPLACE FUNCTION bluetooth.broken_readers(_dt date)
     RETURNS TABLE(read_id integer, reader_name character varying) 
     LANGUAGE 'plpgsql'
-
     COST 100
     VOLATILE 
     ROWS 1000
+
 AS $BODY$	
-	begin
-    return query 
-    with x as(
-select reader_id, last_active_date as yesterday, active as active_yesterday
-	from bluetooth.reader_status_history
-where last_active_date = insert_dt::date - 2
-order by reader_id
-) 
-,y as (
-select reader_id, last_active_date as today, active as active_today
-	from bluetooth.reader_status_history
-where last_active_date = insert_dt::date - 1 and active is false
-order by reader_id
-)
-select distinct (reader_id), read_name from x 
-left join y using(reader_id)
-left join bluetooth.detectors_history_corrected using(reader_id)
-where active_yesterday is true and active_today is false
-;
-end; $BODY$;
+	BEGIN
+    RETURN query 
+
+
+SELECT          reader_id, read_name 
+
+FROM            bluetooth.reader_status_history USING (reader_id)
+INNER join      bluetooth.detectors_history_corrected using(reader_id) -- needs to be changed to something else or another version
+WHERE           active IS FALSE and last_active_date = _dt - 2 
+END; 
+
+$BODY$;
 
 ALTER FUNCTION bluetooth.broken_readers(date)
-    OWNER TO bluetooth;
+    OWNER TO bt_admins;
+
+COMMENT ON FUNCTION bluetooth.broken_readers(date) IS 'This function identifies the readers that were not active yesterday but were active the day before as broken readers, and returns a list of broken readers.';
 
 GRANT EXECUTE ON FUNCTION bluetooth.broken_readers(date) TO PUBLIC;
 
-GRANT EXECUTE ON FUNCTION bluetooth.broken_readers(date) TO bluetooth;
+GRANT EXECUTE ON FUNCTION bluetooth.broken_readers(date) TO bt_bot;
