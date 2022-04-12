607e67c2ca4a4f8748ea8ff15a0e4eec376196b0
Islam Taha <itaha@ip-10-160-2-198.ec2.internal>
2022-04-12 00:47:22 -0400

    Transforming wys.locations.geom and updating new locations insertion #449

--- wys/api/sql/i0449_wys_locations_geom_col.sql
+++ wys/api/sql/i0449_wys_locations_geom_col.sql
@@ -6,6 +6,11 @@ ALTER TABLE wys.locations
 ADD COLUMN geom geometry;
 
 UPDATE wys.locations
-SET geom = st_setsrid(st_makepoint(split_part(regexp_replace(loc, '[()]'::text, ''::text, 'g'::text), ','::text, 2)::double precision, 
-                                   split_part(regexp_replace(loc, '[()]'::text, ''::text, 'g'::text), ','::text, 1)::double precision), 
-                        4326);
+SET geom = ST_Transform(ST_SetSRID(ST_MakePoint(split_part(regexp_replace(loc, '[()]'::text, ''::text, 'g'::text), ','::text, 2)::double precision, 
+                                                split_part(regexp_replace(loc, '[()]'::text, ''::text, 'g'::text), ','::text, 1)::double precision), 
+                                   4326),
+                        2952);
+
+CREATE INDEX locations_geom
+ON wys.locations
+USING GIST (geom);
