e2f359d267b9791b4769564bf6be2cdc3d3b07c7
chmnata <chmnata@gmail.com>
2022-08-16 18:58:16 -0400

    #496 Add questionable move data function

--- here/traffic/sql/move_tables.sql
+++ here/traffic/sql/move_tables.sql
@@ -1,35 +1,62 @@
--- Move data from here.ta_* to the newly structured here.ta_*
--- while performing separation of tx column to dt (Date) and tod (time)
--- loop through each table, move data to new table, then truncate the old one
---Loops through the years and then months for which we currently have data in order to create a partitioned table for each month
+-- Create new table while performing separation of tx column to dt (Date) and tod (time)
+-- Attach as a partition table to the new parent table
+-- then drops the old child table
 
-DO $do$
-DECLARE
-	startdate DATE;
-	yyyymm TEXT;
-	basetablename TEXT := 'ta_';
-	tablename TEXT;
-	newtablename TEXT;
+CREATE OR REPLACE FUNCTION here.move_data(_yyyy text, _mm text)
+    RETURNS void
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE STRICT SECURITY DEFINER PARALLEL UNSAFE
+AS $BODY$
+
+DECLARE 
+	_startdate DATE;
+	_enddate DATE;
+	_oldtablename TEXT;
+	_newtablename TEXT;
+	_constraintname TEXT;
+	
 BEGIN
+	_startdate:= to_date(_yyyy||'-'||_mm||'-01', 'YYYY-MM-DD');
+	_enddate:= _startdate + INTERVAL '1 month';
+    _oldtablename:= 'ta_'||_yyyy||_mm;
+	_newtablename:= 'ta_'||_yyyy||_mm||'_new';
+	_constraintname:= 'y'||_yyyy||'m'||_mm;
 
-	for yyyy IN 2012..2018 LOOP
-		FOR mm IN 01..12 LOOP
-			startdate:= to_date(yyyy||'-'||mm||'-01', 'YYYY-MM-DD');
-			IF mm < 10 THEN
-				yyyymm:= yyyy||'0'||mm;
-			ELSE
-				yyyymm:= yyyy||''||mm;
-			END IF;
-			tablename:= basetablename||yyyymm;
-			newtablename:= basetablename||yyyymm||'_new';
-			EXECUTE format($$
-						   INSERT INTO here.%I
-						   SELECT link_dir, tx::date, tx::time without time zone, tx, length, mean, stddev, min_spd, max_spd, confidence, null
-						   FROM here.%I;
-						   TRUNCATE TABLE here.%I;
-						   $$   
-                            ,  newtablename, tablename, tablename);
-		END LOOP;
-	END LOOP;
+	EXECUTE format($$CREATE TABLE here.%I  AS
+						SELECT link_dir, tx::date as dt, tx::time without time zone as tod, tx, length, mean, stddev, min_spd, max_spd, pct_50, pct_85, confidence, null::integer as sample_size
+						FROM here.%I ;
+					ALTER TABLE here.%I OWNER TO natalie;
+				    ALTER TABLE here.%I  ADD CONSTRAINT %I
+        				CHECK (dt>= %L AND dt< %L);
+				    ALTER TABLE here.%I ALTER COLUMN link_dir SET NOT NULL ;
+					ALTER TABLE here.%I ALTER COLUMN dt SET NOT NULL ;
+					ALTER TABLE here.%I ALTER COLUMN tod SET NOT NULL ;
+					ALTER TABLE here.%I ALTER COLUMN tx SET NOT NULL ;
+					ALTER TABLE here.%I ALTER COLUMN length SET NOT NULL ;
+					ALTER TABLE here.%I ALTER COLUMN mean SET NOT NULL ;
+					ALTER TABLE here.%I ALTER COLUMN stddev SET NOT NULL ;
+					ALTER TABLE here.%I ALTER COLUMN min_spd SET NOT NULL ;
+					ALTER TABLE here.%I ALTER COLUMN max_spd SET NOT NULL ;
+					ALTER TABLE here.%I ALTER COLUMN pct_50 SET NOT NULL ;
+					ALTER TABLE here.%I ALTER COLUMN pct_85 SET NOT NULL ;
+					ALTER TABLE here.%I ALTER COLUMN confidence SET NOT NULL ;
+					ALTER TABLE here.%I ADD UNIQUE(dt, tod, link_dir);
+					ALTER TABLE here.ta_new ATTACH PARTITION here.%I  
+        				FOR VALUES FROM (%L) TO (%L);
+    				ALTER TABLE here.%I DROP CONSTRAINT %I; 
+					DROP TABLE here.%I;$$,  _newtablename, _oldtablename, 
+											   _newtablename,
+				   							   _newtablename, 
+											   _constraintname, 
+											   _startdate, _enddate, 
+                                                _newtablename,_newtablename, _newtablename, _newtablename, _newtablename, _newtablename,
+                                              _newtablename, _newtablename, _newtablename, _newtablename, _newtablename, _newtablename, _newtablename, 
+                                              _newtablename, 
+				  							   _startdate, 
+											   _enddate,
+				   								_newtablename,
+											   _constraintname, 
+											   _oldtablename);
 END;
-$do$ LANGUAGE plpgsql
+$BODY$;
