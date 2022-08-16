8bcf757616c72c589fba55ea2a48c8e56a01fea9
chmnata <chmnata@gmail.com>
2022-08-16 17:25:42 -0400

    #494, #495 Add columns to parent table and change view columns

--- here/traffic/sql/create_tables.sql
+++ here/traffic/sql/create_tables.sql
@@ -1,5 +1,5 @@
 -- Parent table structure
-CREATE TABLE IF NOT EXISTS here.ta
+CREATE TABLE IF NOT EXISTS here.ta_new
 (
     link_dir text NOT NULL,
 	dt date NOT NULL,
@@ -10,18 +10,21 @@ CREATE TABLE IF NOT EXISTS here.ta
     stddev numeric(4,1) NOT NULL,
     min_spd integer NOT NULL,
     max_spd integer NOT NULL,
+    pct_50 integer NOT NULL,
+    pct_85 integer NOT NULL,
     confidence integer NOT NULL,
 	sample_size integer
 ) PARTITION BY RANGE (dt);
 
-ALTER TABLE here.ta
+ALTER TABLE here.ta_new
   OWNER TO here_admins;
 
 -- simple view structure
 CREATE VIEW here.ta_view AS
+ 
+select link_dir, tx, length, mean, stddev, min_spd, max_spd, pct_50, pct_85, confidence 
 
-SELECT *
-FROM here.ta;
+from here.ta_new
 
 ALTER TABLE here.ta_view
   OWNER TO here_admins;
@@ -35,38 +38,4 @@ CREATE TRIGGER transform_trigger
         FOR EACH ROW
         EXECUTE PROCEDURE here.here_insert_trigger();
 
-
---Loops through the years and then months for which we currently have data in order to create a partitioned table for each month
-DO $do$
-DECLARE
-	startdate DATE;
-	enddate DATE;
-	yyyymm TEXT;
-	basetablename TEXT := 'ta_';
-	tablename TEXT;
-BEGIN
-
-	for yyyy IN 2012..2021 LOOP
-		FOR mm IN 01..12 LOOP
-			startdate:= to_date(yyyy||'-'||mm||'-01', 'YYYY-MM-DD');
-			enddate:= startdate + INTERVAL '1 month';
-			IF mm < 10 THEN
-				yyyymm:= yyyy||'0'||mm;
-			ELSE
-				yyyymm:= yyyy||''||mm;
-			END IF;
-			tablename:= basetablename||yyyymm||'_new';
-			EXECUTE format($$CREATE TABLE here.%I 
-                            PARTITION OF here.ta_new
-                            FOR VALUES FROM  (%L) TO (%L);
-                            ALTER TABLE here.%I ADD UNIQUE(dt, tod, link_dir);
-                            CREATE INDEX ON here.%I USING brin(tod);
-                            CREATE INDEX ON here.%I (link_dir);
-                            ALTER TABLE here.%I OWNER TO here_admins;
-                            $$
-                            , tablename, startdate, enddate, tablename, tablename, tablename, tablename);
-		END LOOP;
-	END LOOP;
-END;
-$do$ LANGUAGE plpgsql
 
