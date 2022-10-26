4d894be79c84c9101374a4a3b3113b3869b3071d
Jedwin Mok <jedwinmok@gmail.com>
2022-10-26 16:43:37 -0400

    fixes and WORKING VER of prediction_daily #470

--- weather/prediction_import.py
+++ weather/prediction_import.py
@@ -19,12 +19,12 @@ from psycopg2 import connect, sql
 from psycopg2.extras import execute_values
 
 CONFIG=ConfigParser()
-CONFIG.read('config/db.cfg') # Change DB Settings in db.cfg
+CONFIG.read('config.cfg') # Change DB Settings in db.cfg
 dbset=CONFIG['DBSETTINGS']
 conn=connect(**dbset)
 
 
-#### pull_prediction(today) ####
+#### pull_prediction(tmrw) ####
 #
 # Pulls a week forecast from environment canada, returns forecast dict for the day after the current date
 # Params:
@@ -32,7 +32,7 @@ conn=connect(**dbset)
 #
 def pull_prediction(tmrw):
 
-    tmrw_text = today.strftime("%A")
+    tmrw_text = tmrw.strftime("%A")
 
 
     ec_en = env_canada.ECWeather(station_id='ON/s0000458', language='english')
@@ -43,14 +43,13 @@ def pull_prediction(tmrw):
     loop = asyncio.get_event_loop()
     loop.run_until_complete(ec_en.update())
 
-    forecast = ec_en.daily_forecast
+    forecast = ec_en.daily_forecasts
     for day in forecast:
         if(day['period'] == tmrw_text):
             daytime_forecast = day
         elif (day['period'] == tmrw_text + ' night'):
             nighttime_forecast = day
-
-
+    
     tmrw_forecast = {
         "date": tmrw,
         "max_temp": daytime_forecast['temperature'],
@@ -60,20 +59,24 @@ def pull_prediction(tmrw):
         "text_summary_night": nighttime_forecast['text_summary']
     }
     return tmrw_forecast
+    
 
 
 def insert_weather(conn, weather_df):
     weather_fields = ['date', 'max_temp', 'min_temp', 'precip_prob', 'text_summary_day', 'text_summary_night']
     with conn:
         with conn.cursor() as cur:
-            insert_sql = '''INSERT INTO weather.historical_daily(dt, max_temp, min_temp, mean_temp, total_rain, total_snow, total_precip) VALUES %s'''
+            insert_sql = '''INSERT INTO weather.prediction_daily(dt, temp_max, temp_min, precip_prob, text_summary_day, text_summary_night) VALUES %s'''
             execute_values(cur, insert_sql, weather_df[weather_fields].values)
 
 
 if __name__ == '__main__':
     #Get current date to pull
     today = datetime.date.today()
-    tmrw_forecast = pull_prediction(today)
+    tmrw = today + datetime.timedelta(days=1)
+    tmrw_forecast = pull_prediction(tmrw)
    
-    weather_df = pd.DataFrame.from_dict(tmrw_forecast)
+    weather_df = pd.DataFrame.from_dict([tmrw_forecast])
+    print(weather_df)
     insert_weather(conn, weather_df)
+    print("Process Complete")
