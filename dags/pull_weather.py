78d605d0ae4bb2b6493d3379c3c44ee70519e929
Jedwin Mok <jedwinmok@gmail.com>
2022-11-03 04:58:51 -0400

    dag v1 and imports #470

---
+++ dags/pull_weather.py
@@ -0,0 +1,24 @@
+"""
+Pipeline to pull weather data from Envrionment Canada and upsert into weather.prediction_daily table.
+A Slack notification is raised when the airflow process fails.
+"""
+import sys
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.base_hook import BaseHook
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import Variable
+from dags.pull_wys import SLACK_CONN_ID 
+
+SLACK_CONN_ID = 'slack_data_pipeline'
+dag_config = Variable.get('slack_member_id', deserialize_json=True)
+list_names = dag_config['raphael'] + ' ' + dag_config['islam'] + ' ' + dag_config['natalie'] 
+
+
+def task_fail_slack_alert(context):
+    return
+
+
+#import python scripts
