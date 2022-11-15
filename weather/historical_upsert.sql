fa60a802effe644945f02fb332088dd9575f17da
Jedwin Mok <jedwinmok@gmail.com>
2022-11-15 17:37:08 -0500

    historical_upsert fixes #509

--- weather/historical_upsert.sql
+++ weather/historical_upsert.sql
@@ -1,9 +1,7 @@
 INSERT INTO weather.historical_daily
-    (
-        (dt, temp_max, temp_min, date_pulled)
-    ) VALUES %s
+        (dt, temp_max, temp_min, total_precip_mm, date_pulled)
+    VALUES %s
     ON CONFLICT (dt)
     DO UPDATE
-    SET (temp_max, temp_min, date_pulled)
-        = (EXCLUDED.temp_max, EXCLUDED.temp_min, EXCLUDED.date_pulled);
-
+    SET (temp_max, temp_min, total_precip_mm, date_pulled)
+        = (EXCLUDED.temp_max, EXCLUDED.temp_min, EXCLUDED.total_precip_mm, EXCLUDED.date_pulled);
