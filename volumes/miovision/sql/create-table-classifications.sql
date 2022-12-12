8b58a3f863b0151787f94338b312a988fa843ac9
Nate Wessel <bike756@gmail.com>
2022-12-12 11:46:29 -0500

    set primary key

--- volumes/miovision/sql/create-table-classifications.sql
+++ volumes/miovision/sql/create-table-classifications.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE miovision_api.classifications
 (
-    classification_uid integer NOT NULL DEFAULT nextval('miovision_api.classifications_classification_uid_seq'::regclass),
+    classification_uid integer DEFAULT nextval('miovision_api.classifications_classification_uid_seq'::regclass) PRIMARY KEY,
     classification text COLLATE pg_catalog."default",
     location_only boolean, -- for peds and bikes, where movement isn't available, only which leg they were observed on
     class_type text COLLATE pg_catalog."default"
