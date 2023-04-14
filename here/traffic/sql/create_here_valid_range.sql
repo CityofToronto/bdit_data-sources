fd9290fb26cd25ceb1381f7a0e80c4a1bac4f686
chmnata <chmnata@gmail.com>
2023-04-14 11:43:17 -0400

    #558 update sql

--- here/traffic/sql/create_here_valid_range.sql
+++ here/traffic/sql/create_here_valid_range.sql
@@ -5,6 +5,7 @@ CREATE TABLE here.street_valid_range AS
 
 WITH street_v(street_version, valid_range) AS (
 	VALUES 
+    ('17_4', '["2012-01-01","2014-01-01")'::daterange),
 	('18_3', '["2014-01-01","2017-01-01")'::daterange),
 	('19_4_tc', '["2017-01-01","2017-09-01")'::daterange),
 	('21_1', '["2017-09-01","2022-08-15")'::daterange),
