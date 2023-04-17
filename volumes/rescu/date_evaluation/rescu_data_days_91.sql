6a5666a09aebf1b8064ae48bbe2402a642e8984d
Sarah Cannon <scannon@ip-10-160-2-198.ec2.internal>
2023-04-17 17:24:25 -0400

    #573 sqls that id good detectors in 2021

---
+++ volumes/rescu/date_evaluation/rescu_data_days_91.sql
@@ -0,0 +1,44 @@
+-- Make a table of the number of good data days for each RESCU detector where a good data day has 91 or more 15 minute bins (96 is the max) 
+CREATE TABLE scannon.rescu_data_days_91 AS (
+
+    --first, count the bins and only move the detectors with 91 or more bins per day
+    WITH bin_ct AS (
+        SELECT
+            v.arterycode,
+            v.detector_id,
+            date_trunc('day', v.datetime_bin)::date AS dt,
+            COUNT (datetime_bin) as bin_ct
+        FROM rescu.volumes_15min AS v
+        WHERE 
+            datetime_bin >= '2021-01-01' 
+            AND datetime_bin < '2022-01-01'
+        GROUP BY 
+            v.arterycode,
+            v.detector_id,
+            date_trunc('day', v.datetime_bin)::date
+        HAVING COUNT (datetime_bin) >= 91
+)
+
+    -- here's a table of those detectors with helpful attribute info including location
+    SELECT
+        bc.arterycode,
+        bc.detector_id,
+        di.det_group,
+        di.number_of_lanes,
+        di.primary_road || ' and ' || di.cross_road AS gen_loc,
+        ST_SETSRID(ST_MakePoint(di.longitude, di.latitude), 4326) AS geom,
+        COUNT(dt) AS good_data_days,
+        '2022-01-01'::date - '2021-01-01'::date AS total_days
+    FROM bin_ct AS bc
+    LEFT JOIN rescu.detector_inventory di USING (detector_id)
+    WHERE ST_SETSRID(ST_MakePoint(di.longitude, di.latitude), 4326) IS NOT NULL
+    GROUP BY
+        bc.arterycode,
+        bc.detector_id,
+        di.det_group,
+        di.number_of_lanes,
+        di.primary_road || ' and ' || di.cross_road,
+        ST_SETSRID(ST_MakePoint(di.longitude, di.latitude), 4326)
+    ORDER BY
+        COUNT(dt) DESC
+);
---
+++ volumes/rescu/date_evaluation/rescu_enuf_vol.sql
@@ -0,0 +1,23 @@
+CREATE TABLE scannon.rescu_enuf_vol AS (
+    WITH vol_asmt AS (
+        SELECT 
+            dv.*,
+            di.number_of_lanes,
+            CASE
+                WHEN di.primary_road LIKE 'RAMP%' THEN 'ramp'
+                WHEN di.det_group IN ('FGG', 'ALLEN', 'DVP') AND dv.d_vol >= 20000 THEN 'enough volume'
+                WHEN di.det_group IN ('LAKE') AND dv.d_vol >= 10000 THEN 'enough volume'
+                ELSE 'check volume'
+            END AS vol_check
+        FROM scannon.rescu_dayvol_stats_21 AS dv
+        LEFT JOIN rescu.detector_inventory AS di USING(detector_id)
+)
+
+SELECT 
+    va.* 
+FROM vol_asmt AS va
+WHERE va.vol_check = 'enough volume'
+);
+    
+COMMENT ON TABLE scannon.rescu_enuf_vol IS 'enough volume for the Gardiner, DVP and Allen is 20000 per day; for Lakeshore the figure is 10000 per day.
+Ramps are excluded (for now)';
