7ce97259e83887eb35a49e35611dd789d1dafc7c
Nate Wessel <nate.wessel@toronto.ca>
2023-04-19 15:13:30 -0400

    another demo

--- cycling_app/sql/insert_trip_survey_data.sql
+++ cycling_app/sql/insert_trip_survey_data.sql
@@ -5,16 +5,16 @@ Since on RDS there's no superuser privilege, can't perform the COPY [FROM|TO] co
 
 /*First create a temporary table to store the raw survey data*/
 CREATE TABLE cycling.trip_surveys_temp(
-  trip_id bigint NOT NULL,
-  app_user_id integer NOT NULL,
-  started_at timestamp without time zone NOT NULL,
-  purpose TEXT NOT NULL,
-  notes text
+    trip_id bigint NOT NULL,
+    app_user_id integer NOT NULL,
+    started_at timestamp without time zone NOT NULL,
+    purpose text NOT NULL,
+    notes text
 );
 
 
 --Execute in psql
-\COPY trip_surveys FROM 'H:/Data/CyclingApp/2017trip_surveys.csv' WITH (FORMAT 'csv', HEADER TRUE);
+--\COPY trip_surveys FROM 'H:/Data/CyclingApp/2017trip_surveys.csv' WITH (FORMAT 'csv', HEADER TRUE);
 
 WITH ins AS (
 	INSERT INTO cycling.trip_purpose (purpose)
@@ -22,11 +22,17 @@ WITH ins AS (
 	FROM cycling.trip_surveys_temp
 	ON CONFLICT DO NOTHING
 )
+
 INSERT INTO cycling.trip_surveys
-SELECT trip_id, app_user_id, started_at, purpose_id, notes
-  FROM cycling.trip_surveys_temp 
-  INNER JOIN cycling.trip_purpose USING (purpose)
- ON CONFLICT DO NOTHING;
+SELECT
+    trip_id,
+    app_user_id,
+    started_at,
+    purpose_id,
+    notes
+FROM cycling.trip_surveys_temp 
+INNER JOIN cycling.trip_purpose USING (purpose)
+    ON CONFLICT DO NOTHING;
 
 TRUNCATE cycling.trip_surveys_temp;
 
