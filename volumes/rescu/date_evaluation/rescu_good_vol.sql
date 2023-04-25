3ff83e26e9e70b86ff260546928ac8cd141ea6a8
Sarah Cannon <scannon@ip-10-160-2-198.ec2.internal>
2023-04-25 18:26:20 -0400

    #573 lane stats, modified final table sql

--- volumes/rescu/date_evaluation/rescu_good_vol.sql
+++ volumes/rescu/date_evaluation/rescu_good_vol.sql
@@ -1,27 +1,20 @@
 -- Make a table that stores 15 minute volume data from RESCU detectors that recorded enough volume
-CREATE TABLE scannon.rescu_good_vol_20 AS (
+DROP TABLE scannon.rescu_good_vol_21;
+CREATE TABLE scannon.rescu_good_vol_21 AS (
 
-    -- I initially made this table because I was running the query using subsets of the data for speeeeeeeed and now I see I don't need it but anyway
-    WITH ev_sub AS (
-        SELECT 
-            ev.detector_id,
-            ev.arterycode,
-            ev.gen_loc,
-            ev.dt
-        FROM scannon.rescu_enuf_vol_20 AS ev
-    )
-    
-    -- Using geo_id as a proxy for centreline_id 
+    -- Using THE TABLE of centreline_id and arterycode as directed by MOVE; it lives on the flashcrow server as counts.arteries_centreline 
     SELECT 
-        es.detector_id,
-        es.arterycode,
-        art.geo_id,
-        es.gen_loc,
+        ev.detector_id,
+        ev.arterycode,
+        art.centreline_id,
+        ev.gen_loc,
+        di.direction,
         v.datetime_bin,
         v.volume_15min
-    FROM ev_sub AS es
-    LEFT JOIN traffic.arterydata AS art USING(arterycode)
+    FROM scannon.rescu_enuf_vol_21 AS ev
+    LEFT JOIN scannon.cent_art AS art USING(arterycode)
+    LEFT JOIN rescu.detector_inventory AS di USING(detector_id)
     LEFT JOIN rescu.volumes_15min AS v 
-        ON es.detector_id = v.detector_id
-        AND es.dt = v.datetime_bin::date
+        ON ev.detector_id = v.detector_id
+        AND ev.dt = v.datetime_bin::date
 );
