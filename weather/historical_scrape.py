8aa89eb5111665515da09a7b25829e222b7f0603
chmnata <chmnata@gmail.com>
2023-05-01 12:25:00 -0400

    #470 update station id and bug fix for historical py

--- weather/historical_scrape.py
+++ weather/historical_scrape.py
@@ -1,15 +1,5 @@
-######  mrc_import.py  ######
-# Pulls daily weather forecast from Environment Canada.
-# 
-# Original script from https://github.com/Toronto-Big-Data-Innovation-Team/activeto/blob/jasonlee/weekend_closures/scripts/import_weather.py
-
-#Environment Canada imports
-import asyncio
-from types import coroutine
-import env_canada
-#from env_canada import ECHistoricalRange, get_historical_stations
-
-#website scraping
+# Pulls historical daily weather from Environment Canada
+ 
 import requests
 from requests import exceptions
 from pathlib import Path
@@ -30,21 +20,26 @@ import datetime
 import pandas as pd
 import numpy as np
 import click
+import datetime
 from configparser import ConfigParser
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level = logging.INFO)
 
-
-"""CONFIG=ConfigParser()
-CONFIG.read('config.cfg') # Change DB Settings in db.cfg
-dbset=CONFIG['DBSETTINGS']
-conn=connect(**dbset)"""
-
+'''
+# Uncomment when running script directly
+CONFIG=ConfigParser()
+CONFIG.read(str(Path.home().joinpath('db.cfg')))
+dbset = CONFIG['DBSETTINGS']
+conn = connect(**dbset)
+'''
 
 def request_url(url, payload):
     '''
     Request content from Weather Canada website
+
+    url: base URL of weather canada website
+    payload: additional options applied to the url to specify what data we want to pull 
     '''
     try:
         logger.info('Scraping data from Weather Canada for %s...', str(run_date))
@@ -55,19 +50,28 @@ def request_url(url, payload):
         logger.error('Failed to request url. Exception: %s', str(e))
 
 def get_payload(run_date, station):
+    '''
+    Construct payload for url 
+
+    run_date: Day of interested weather data
+    station: station id to specify which station to pull weather data from
+    '''
+
     year = run_date.strftime("%Y")
-    month = run_date.strftime("%m")
-    day = run_date.strftime("%d")
+    month = run_date.strftime("%-m")
+    day = run_date.strftime("%-d")
 
     if station == 1:
-        stationid = 54239
-        stationname = 'Toronto'
+        # Toronto City Centre
+        stationid = 31688
+        stationname = 'Toronto City'
     else:
+        # Airport
         stationid = 51459
-        stationname = 'YYZ'
+        stationname = 'Toronto INTL A'
     
     payload = {
-    'StationID': 54239,
+    'StationID': stationid,
     'Prov': 'ON',
     'StartYear': 2019,
     'EndYear': year,
@@ -76,97 +80,81 @@ def get_payload(run_date, station):
     'searchMethod': 'contains',
     'Month': month,
     'Day': day,
-    'txtStationName': 'Toronto',
+    'txtStationName': stationname ,
     'timeframe': 1,
     'Year': year}
 
     return payload
 
 def pull_weather(run_date, station):
-    
     '''
-    ec_en = env_canada.ECWeather(station_id='ON/s0000458', language='english')
+    Pull weather data for specified run_date and station
 
-    #for python 3.7+ ONLY:
-    # asyncio.run(ec_en.update())
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(ec_en.update())
-    curr_weather = ec_en.conditions
+    run_date: Day of interested weather data
+    station: station id to specify which station to pull weather data from
     '''
     url = 'https://climate.weather.gc.ca/climate_data/daily_data_e.html'
+    # Use payload to query for specific day and station
     payload = get_payload(run_date, station)
-
 
     try:
         weather_context = request_url(url, payload)
-        tmr_date = (run_date + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-
-        date_query = run_date.strftime("")
-        #find day in weather table
-        days = []
-        abbr_contents = weather_context.find_all("abbr")
-        days.append(abbr_contents[0].text)
-
-        for i in abbr_contents[1:]:
-            data = []
-            if day_contents = i.find(title=date_query):
-                #add all <td> elements into data[]
-                td_contents = day_contents.find_all("td")
-                data.append(td_contents[0].text)
-
-
-                
+        # Construct date structure to e.g. January 1, 2017
+        # to find row for specific run_date within the monthly table
+        date_query = run_date.strftime("%B %-d, %Y")
+        page_detail = weather_context.find(title=date_query)
+        # Grab table contents for specific day
+        table_content = page_detail.parent.parent.find_all("td")
+        # Construct dict with values we want     
         weather_dict = { 
             "today_dict": {
-                "date": today,
-                "max_temp": data[0],
-                "min_temp": data[1],
-                "mean_temp": data[2],
-                "total_rain": data[5],
-                "total_snow": data[6],
-                "total_precip": data[7]
+                        "date": run_date,
+                        "max_temp": table_content[0].get_text(strip=True), # strip to get rid of white space
+                        "min_temp": table_content[1].get_text(strip=True),
+                        "mean_temp": table_content[2].get_text(strip=True),
+                        "total_rain": table_content[5].get_text(strip=True),
+                        "total_snow": table_content[6].get_text(strip=True),
+                        "total_precip": table_content[7].get_text(strip=True)}
         }
-    }
 
-        logger.info("Data inserted!")
-            
+        weather_dict = pd.DataFrame.from_dict([weather_dict['today_dict']]).replace({'': None})
 
     except Exception as e:
         logger.error('Failed to collect historical data. Exception: %s', str(e))
 
     return weather_dict
 
+def upsert_weather(conn, weather_df, station):
 
-
-def upsert_weather(conn, weather_df):
     weather_fields = ['date', 'max_temp', 'min_temp', 'mean_temp', 'total_rain', 'total_snow', 'total_precip']
+
+    # Define schema and table name for upsert 
+    schema_name = 'weather'
+
+    if station == 1:
+        station_table_name = 'historical_daily_city'
+    elif station == 2:
+        station_table_name = 'historical_daily_airport' 
+
     with conn:
         with conn.cursor() as cur:
-            upsert_sql = ''' INSERT INTO weather.historical_daily_city
-                                (dt, temp_max, temp_min, mean_temp, total_rain, total_snow, total_precip)
-                            VALUES %s
-                            ON CONFLICT (dt)
-                            DO UPDATE
-                            SET (temp_max, temp_min, mean_temp, total_rain, total_snow, total_precip)
-                                = (EXCLUDED.temp_max, EXCLUDED.temp_min, EXCLUDED.mean_temp, EXCLUDED.total_rain, EXCLUDED.total_snow, EXCLUDED.total_precip); '''
+            upsert_sql = sql.SQL(
+                '''
+                INSERT INTO {table} (dt, temp_max, temp_min, mean_temp, total_rain, total_snow, total_precip)
+                VALUES %s
+                ON CONFLICT (dt)
+                DO UPDATE
+                SET (temp_max, temp_min, mean_temp, total_rain, total_snow, total_precip)
+                    = (EXCLUDED.temp_max, EXCLUDED.temp_min, EXCLUDED.mean_temp, EXCLUDED.total_rain, EXCLUDED.total_snow, EXCLUDED.total_precip);
+                ''').format(table = sql.Identifier(schema_name, station_table_name))
             execute_values(cur, upsert_sql, weather_df[weather_fields].values)
 
 #if __name__ == '__main__':
-def historical_upsert(cred, run_date):
+def historical_upsert(conn, run_date, station_id):
     #Get current date to pull
 
-    #use connection
-    conn = cred.get_conn()
+    weather_dict = pull_weather(run_date, station = station_id)
+
+    upsert_weather(conn, weather_dict, station_id)
     
-    print("process start")
-    weather_dict = pull_weather(run_date, station = 1)
-
-    #weather_csv = pull_weather_df(today)
-    #print(weather_csv)
-
-    today_df = pd.DataFrame.from_dict([weather_dict['today_dict']])
-    upsert_weather(conn, yday_df)
-    
-    print("Process Complete")
-
+    logger.info('Process Complete')
