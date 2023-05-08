b629bc6321c26d36aec06723160af4565a29f58e
Nate Wessel <nate.wessel@toronto.ca>
2023-05-08 09:52:03 -0400

    add trigger for last_updated column

--- volumes/miovision/dev_notebooks/qc_sqls/create_dq.sql
+++ volumes/miovision/dev_notebooks/qc_sqls/create_dq.sql
@@ -5,19 +5,20 @@ ALTER TABLE scannon.miovision_qc SET SCHEMA miovision_api;
 ALTER TABLE miovision_api.miovision_qc RENAME qa;
 ALTER TABLE nwessel.qc_investigation_levels SET SCHEMA miovision_api;
 ALTER TABLE nwessel.qc_problem_levels SET SCHEMA miovision_api;
+
+CREATE FUNCTION miovision_api.update_qa_last_updated_ts()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.last_updated = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_qa_last_updated_ts_trigger
+BEFORE UPDATE ON miovision_api.qa
+FOR EACH ROW
+EXECUTE PROCEDURE miovision_api.update_qa_last_updated_ts();
 */
-CREATE TABLE scannon.miovision_qc (
-    uid serial PRIMARY KEY,
-    intersection_uid smallint REFERENCES miovision_api.intersections (intersection_uid),
-    intersection_name text,
-    classification_uid smallint REFERENCES miovision_api.classifications (classification_uid),
-    last_updated timestamp DEFAULT NOW(),
-    time_range tsrange NOT NULL,
-    notes text,
-    investigation_level text NOT NULL REFERENCES nwessel.qa_investigation_levels (uid),
-    problem_level text NOT NULL REFERENCES nwessel.qa_problem_levels (uid),
-    UNIQUE (intersection_uid, classification_uid, time_range)
-);
 
 CREATE TABLE nwessel.qa_investigation_levels (
     uid text PRIMARY KEY,
@@ -32,6 +33,19 @@ CREATE TABLE nwessel.qa_problem_levels (
 );
 COMMENT ON TABLE nwessel.qa_problem_levels
 IS 'What is the nature of the problem indicated for the given subset of miovision data?';
+
+CREATE TABLE scannon.miovision_qc (
+    uid serial PRIMARY KEY,
+    intersection_uid smallint REFERENCES miovision_api.intersections (intersection_uid),
+    intersection_name text,
+    classification_uid smallint REFERENCES miovision_api.classifications (classification_uid),
+    last_updated timestamp DEFAULT NOW(),
+    time_range tsrange NOT NULL,
+    notes text,
+    investigation_level text NOT NULL REFERENCES nwessel.qa_investigation_levels (uid),
+    problem_level text NOT NULL REFERENCES nwessel.qa_problem_levels (uid),
+    UNIQUE (intersection_uid, classification_uid, time_range)
+);
 
 -- this table was initially populated using data from mio_dq_notes thusly:
 /*
