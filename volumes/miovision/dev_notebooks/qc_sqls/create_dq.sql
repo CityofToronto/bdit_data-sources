f05b8cc3cc617375cf407999973609862c6ee76e
Nate Wessel <nate.wessel@toronto.ca>
2023-05-08 08:20:23 -0400

    add comments, note necessary schema changes

--- volumes/miovision/dev_notebooks/qc_sqls/create_dq.sql
+++ volumes/miovision/dev_notebooks/qc_sqls/create_dq.sql
@@ -1,4 +1,11 @@
 -- Make a table to store time ranges with bad/questionable/susicious data
+-- TODO:
+/*
+ALTER TABLE scannon.miovision_qc SET SCHEMA miovision_api;
+ALTER TABLE miovision_api.miovision_qc RENAME qa;
+ALTER TABLE nwessel.qc_investigation_levels SET SCHEMA miovision_api;
+ALTER TABLE nwessel.qc_problem_levels SET SCHEMA miovision_api;
+*/
 CREATE TABLE scannon.miovision_qc (
     intersection_uid smallint REFERENCES miovision_api.intersections (intersection_uid),
     intersection_name text,
@@ -15,11 +22,15 @@ CREATE TABLE nwessel.qa_investigation_levels (
     uid text PRIMARY KEY,
     description text UNIQUE NOT NULL
 );
+COMMENT ON TABLE nwessel.qa_investigation_levels
+    IS 'Indicates the furthest degree to which the movision QA issue has been investigated. Is this only a suspicion? Or has the issue been fully confirmed/resolved?';
 
 CREATE TABLE nwessel.qa_problem_levels (
     uid text PRIMARY KEY,
     description text UNIQUE NOT NULL
 );
+COMMENT ON TABLE nwessel.qa_problem_levels
+    IS 'What is the nature of the problem indicated for the given subset of miovision data?';
 
 -- this table was initially populated using data from mio_dq_notes thusly:
 /*
