300f8675b0df5b77286c24f1c1566214c953810e
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-05-12 09:41:47 -0400

    sql linting changes

--- wys/api/sql/open_data/mat-view-stationary-locations.sql
+++ wys/api/sql/open_data/mat-view-stationary-locations.sql
@@ -2,7 +2,6 @@ DROP MATERIALIZED VIEW open_data.wys_stationary_locations CASCADE;
 CREATE MATERIALIZED VIEW open_data.wys_stationary_locations AS
 
 SELECT 
-    wrd.area_short::INT as ward_no, 
     ss.sign_id,
     ss.address,
     ss.sign_name,
@@ -10,22 +9,23 @@ SELECT
     ssc.schedule,
     ssc.min_speed,
     ssc.speed_limit,
-    ssc.flash_speed, 
+    ssc.flash_speed,
     ssc.strobe_speed,
-	CASE
+    ss.geom,
+    wrd.area_short::int AS ward_no,
+    CASE
         WHEN ss.prev_start IS NOT NULL THEN ss.start_date 
-        ELSE MIN(agg.datetime_bin) 
+        ELSE MIN(agg.datetime_bin)
     END AS start_date,
-	CASE 
+    CASE
         WHEN MAX(agg.datetime_bin) < date_trunc('month'::text, now()) - INTERVAL '1 day'
-		    THEN LEAST(next_start::timestamp without time zone, MAX(agg.datetime_bin))
-	END AS end_date,
-	ss.geom
+        THEN LEAST(ss.next_start::timestamp without time zone, MAX(agg.datetime_bin))
+    END AS end_date
 FROM wys.stationary_signs AS ss
-JOIN wys.sign_schedules_list AS ssl USING (api_id)
+--JOIN wys.sign_schedules_list AS ssl USING (api_id) --never used in SELECT statement
 LEFT JOIN wys.sign_schedules_clean AS ssc USING (schedule_name)
 JOIN wys.speed_counts_agg_5kph AS agg USING (api_id)
-LEFT JOIN gis.wards2018 wrd ON ST_Contains(wrd.wkb_geometry, ss.geom)
+LEFT JOIN gis.wards2018 AS wrd ON ST_Contains(wrd.wkb_geometry, ss.geom)
 GROUP BY
     wrd.area_short,
     ss.sign_id, 
@@ -44,14 +44,13 @@ GROUP BY
     
 CREATE UNIQUE INDEX ON open_data.wys_stationary_locations (sign_id);
 
-CREATE FUNCTION wys.refresh_od_mat_view() 
+CREATE FUNCTION wys.refresh_od_mat_view()
 RETURNS void
-    LANGUAGE 'sql'
-
-    COST 100
-    VOLATILE SECURITY DEFINER 
+LANGUAGE 'sql'
+COST 100
+VOLATILE SECURITY DEFINER 
 AS $BODY$
     REFRESH MATERIALIZED VIEW CONCURRENTLY open_data.wys_stationary_locations WITH DATA ;
 $BODY$;
-REVOKE EXECUTE ON FUNCTION wys.refresh_od_mat_view()FROM public;
-GRANT EXECUTE ON FUNCTION wys.refresh_od_mat_view()TO wys_bot;
+REVOKE EXECUTE ON FUNCTION wys.refresh_od_mat_view() FROM public;
+GRANT EXECUTE ON FUNCTION wys.refresh_od_mat_view() TO wys_bot;
