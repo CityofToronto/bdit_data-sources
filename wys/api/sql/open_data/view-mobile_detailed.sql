e645e03270e4f1a095b6b46f621e031f780d6752
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-05-24 16:59:57 -0400

    fix outdated ref to speed_bins and sql fluff

--- wys/api/sql/open_data/view-mobile_detailed.sql
+++ wys/api/sql/open_data/view-mobile_detailed.sql
@@ -2,8 +2,8 @@
 -- DROP VIEW open_data.wys_mobile_detailed;
 
 CREATE OR REPLACE VIEW open_data.wys_mobile_detailed
- AS
- SELECT 
+  AS
+SELECT 
     location_id,
     loc.ward_no,
     loc.location,
@@ -11,13 +11,16 @@ CREATE OR REPLACE VIEW open_data.wys_mobile_detailed
     loc.to_street,
     loc.direction,
     agg.datetime_bin,
-    speed_bins.speed_bin::text AS speed_bin,
+    bins.speed_bin::text AS speed_bin,
     agg.volume
-   FROM wys.mobile_api_id loc
-     INNER JOIN open_data.wys_mobile_summary USING (location_id)
-     JOIN wys.speed_counts_agg_5kph agg ON loc.api_id = agg.api_id AND agg.datetime_bin > loc.installation_date AND agg.datetime_bin < loc.removal_date
-     JOIN wys.speed_bins USING (speed_id)
-  WHERE loc.removal_date < date_trunc('month', now());
+FROM wys.mobile_api_id loc
+INNER JOIN open_data.wys_mobile_summary USING (location_id)
+JOIN wys.speed_counts_agg_5kph agg ON 
+    loc.api_id = agg.api_id 
+    AND agg.datetime_bin > loc.installation_date 
+    AND agg.datetime_bin < loc.removal_date
+JOIN wys.speed_bins_old AS bins USING (speed_id)
+WHERE loc.removal_date < date_trunc('month', now());
 
 ALTER TABLE open_data.wys_mobile_detailed
     OWNER TO rdumas;
