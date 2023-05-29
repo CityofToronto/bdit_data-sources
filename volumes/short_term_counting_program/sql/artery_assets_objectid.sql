cf5dc5e0efdeeb7d992a17a0dc768938c351fc76
Adam Duyvestyn <aduyves@ip-10-160-2-198.ec2.internal>
2023-05-29 16:35:08 -0400

    sql fluff

--- volumes/short_term_counting_program/sql/artery_assets_objectid.sql
+++ volumes/short_term_counting_program/sql/artery_assets_objectid.sql
@@ -2,10 +2,10 @@ CREATE OR REPLACE VIEW traffic.artery_objectid_pavement_asset
 AS
 WITH clpav18 AS ( 
     SELECT 
-    clpav.geo_id, 
-    clpav.objectid, 
-        row_number() OVER (PARTITION BY clpav.geo_id ORDER BY (SELECT clpav.geo_id)) AS rownum
-    FROM gis_shared_streets.centreline_pavement_180430 AS clpav
+        clpav.geo_id, 
+        clpav.objectid, 
+            row_number() OVER (PARTITION BY clpav.geo_id ORDER BY (SELECT clpav.geo_id)) AS rownumber
+        FROM gis_shared_streets.centreline_pavement_180430 AS clpav
 )
 
 SELECT 
@@ -14,11 +14,11 @@ SELECT
     clpav18.objectid
 FROM traffic.arterydata AS ad
 JOIN traffic.arteries_centreline AS ac USING (arterycode)
-JOIN clpav18 ON clpav18.geo_id = ac.centreline_id AND clpav18.rownum = 1;
+JOIN clpav18 ON clpav18.geo_id = ac.centreline_id AND clpav18.rownumber = 1;
 
 COMMENT ON VIEW traffic.artery_objectid_pavement_asset IS 'Lookup between artery codes and objectid (to join pavement asset data e.g. vz_analysis.gcc_pavement_asset)';
 ALTER TABLE IF EXISTS traffic.artery_objectid_pavement_asset
-    OWNER TO traffic_admins;
+OWNER TO traffic_admins;
 
 GRANT ALL ON TABLE traffic.artery_objectid_pavement_asset TO bdit_humans;
 GRANT ALL ON TABLE traffic.artery_objectid_pavement_asset TO traffic_admins;
