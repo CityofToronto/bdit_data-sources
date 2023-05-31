40bc6131f3d4be7121401d9523d4cb315eb5a4bd
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-05-31 13:16:25 -0400

    use scannon's method for identifying streaks

--- volumes/rescu/create-mat-view-individual-outages.sql
+++ volumes/rescu/create-mat-view-individual-outages.sql
@@ -1,17 +1,74 @@
-DROP TABLE gwolofs.rescu_individual_outages;
+DROP MATERIALIZED VIEW gwolofs.rescu_individual_outages;
 
-CREATE TABLE gwolofs.rescu_individual_outages(
-    detector_id text,
-    time_start TIMESTAMP,
-    time_end TIMESTAMP,
-    time_range tsrange,
-    date_start DATE,
-    date_end DATE,
-    date_range daterange,
-    duration_days numeric
-);
+CREATE MATERIALIZED VIEW gwolofs.rescu_individual_outages
+AS
 
-INSERT INTO gwolofs.rescu_individual_outages (
+--make a table of outages by rescu detector.
+    --excludes network wide outages. 
+--runs in 2 minutes for all of rescu.volumes15min
+--doesn't include outages that end after last data point (by detector)
+
+WITH bin_gaps AS (
+    SELECT
+        detector_id,
+        datetime_bin,
+        volume_15min,
+        LAG(
+            datetime_bin, 1
+        ) OVER (
+            PARTITION BY detector_id ORDER BY datetime_bin
+        ) + INTERVAL '15 minutes' AS gap_start,
+        datetime_bin - INTERVAL '15 minutes' AS gap_end,
+        CASE
+            WHEN
+                datetime_bin - LAG(
+                    datetime_bin, 1
+                ) OVER (PARTITION BY detector_id ORDER BY datetime_bin) = '00:15:00' THEN 0
+            ELSE 1
+        END AS bin_break,
+        datetime_bin - LAG(
+            datetime_bin, 1
+        ) OVER (PARTITION BY detector_id ORDER BY datetime_bin) AS bin_gap
+    FROM (
+        SELECT
+            detector_id,
+            datetime_bin,
+            volume_15min
+        FROM rescu.volumes_15min
+        WHERE COALESCE(volume_15min, 0) > 0
+    ) AS v15_remove_zeros
+--WHERE datetime_bin >= '2023-05-01'
+),
+
+--individual outages joined with network wide outages. 
+joined_outages AS (
+    SELECT
+        bt.detector_id,
+        bt.gap_start AS time_start,
+        bt.gap_end AS time_end,
+        ro.time_start AS nwout_start,
+        ro.time_end AS nwout_end,
+        tsrange(bt.gap_start, bt.gap_end, '[]') AS time_range,
+        (bt.gap_start)::DATE AS date_start,
+        (bt.gap_end)::DATE AS date_end,
+        daterange((bt.gap_start)::DATE, (bt.gap_end)::DATE, '[]') AS date_range,
+        EXTRACT(
+            epoch FROM (bt.gap_end + INTERVAL '15 MINUTES') - bt.gap_start
+        ) / 86400 AS duration_days
+    FROM bin_gaps AS bt
+    LEFT JOIN gwolofs.network_outages AS ro ON
+        --left join to network wide outages, include WHERE ro.time_range is null
+        tsrange(bt.gap_start, bt.gap_end, '[]') @> ro.time_range
+    WHERE
+        bt.bin_break = 1
+        AND bt.bin_gap IS NOT NULL
+        --Start and end are inclusive so interval 15 minutes implies a gap of 2 bins or 30 minutes.  
+        AND bt.gap_end - bt.gap_start >= INTERVAL '15 MINUTES'
+    ORDER BY bt.gap_start
+)
+
+--detector outages that did not overlap with network wide outages 
+SELECT
     detector_id,
     time_start,
     time_end,
@@ -20,70 +77,41 @@ INSERT INTO gwolofs.rescu_individual_outages (
     date_end,
     date_range,
     duration_days
-)
+--null::tsrange as nwout_range
+FROM joined_outages
+WHERE nwout_start IS NULL
 
---outages by sensor
---1 month takes 12 minutes
-WITH time_bins AS (
-    SELECT generate_series(
-        '2023-05-01 00:00'::TIMESTAMP,
-        '2023-05-28 23:45',
-        '15 minutes') AS time_bin
-),
+UNION
 
-detectors AS (
-    SELECT DISTINCT detector_id
-    FROM rescu.volumes_15min AS v15
-    JOIN time_bins AS tb ON v15.datetime_bin = tb.time_bin
---WHERE detector_id IN ('DW0161DWG', 'DW0161DEG', 'DN0140DND', 'DN0140DSD') --for testing
-),
-
-individual_detector_bins AS (
-    SELECT
-        d.detector_id,
-        tb.time_bin,
-        COUNT(v15.*) FILTER (WHERE v15.volume_15min > 0) AS detector_active
-    FROM detectors AS d
-    CROSS JOIN time_bins AS tb
-    LEFT JOIN gwolofs.rescu_outages AS ro ON ro.time_range @> tb.time_bin
-    LEFT JOIN rescu.volumes_15min AS v15 ON
-        v15.datetime_bin = tb.time_bin
-        AND v15.detector_id = d.detector_id
-    WHERE ro.time_range IS NULL
-    GROUP BY 1, 2
-    ORDER BY 1, 2
-),
-
-run_groups AS (
-    SELECT
-        time_bin,
-        detector_id,
-        detector_active,
-        (
-            SELECT COUNT(*) AS count
-            FROM individual_detector_bins AS g
-            WHERE g.detector_active <> gr.detector_active
-                AND g.time_bin <= gr.time_bin
-                AND g.detector_id = gr.detector_id
-        ) AS run_group
-    FROM individual_detector_bins
-)
-
+--portion of detector outages intersecting with network wide outages that occured before nwout
 SELECT
     detector_id,
-    MIN(time_bin) AS time_start,
-    MAX(time_bin) AS time_end,
-    tsrange(MIN(time_bin), MAX(time_bin), '[]') AS time_range,
-    MIN(time_bin)::DATE AS date_start,
-    MAX(time_bin)::DATE AS date_end,
-    daterange(MIN(time_bin)::DATE, MAX(time_bin)::DATE, '[]') AS date_range,
-    COUNT(*) / (60.0 / 15 * 24) AS duration_days
-FROM run_groups
-WHERE detector_active = 0 --only count length of outages
-GROUP BY
+    time_start,
+    nwout_start - INTERVAL '15 MINUTES' AS time_end,
+    tsrange(time_start, nwout_start - INTERVAL '15 MINUTES', '[]') AS time_range,
+    date_start,
+    (nwout_start - INTERVAL '15 MINUTES')::DATE AS date_end,
+    daterange(date_start, (nwout_start - INTERVAL '15 MINUTES')::DATE, '[]') AS date_range,
+    EXTRACT(epoch FROM nwout_start - time_start) / 86400 AS duration_days
+--tsrange(nwout_start, nwout_end, '[]') as nwout_range --for testing
+FROM joined_outages
+WHERE time_start < nwout_start - INTERVAL '15 MINUTES' --more than one time bin in excess of nwout
+
+UNION
+
+--portion of detector outages intersecting with network wide outages that occured after nwout
+SELECT
     detector_id,
-    run_group
-HAVING COUNT(*) > 1 --don't include single bin outages
+    nwout_end + INTERVAL '15 MINUTES' AS time_start,
+    time_end,
+    tsrange(nwout_end + INTERVAL '15 MINUTES', time_end, '[]') AS time_range,
+    (nwout_end + INTERVAL '15 MINUTES')::DATE AS date_start,
+    date_end,
+    daterange((nwout_end + INTERVAL '15 MINUTES')::DATE, date_end, '[]') AS date_range,
+    EXTRACT(epoch FROM time_end - nwout_end) / 86400 AS duration_days
+--tsrange(nwout_start, nwout_end, '[]') as nwout_range --for testing 
+FROM joined_outages
+WHERE time_end > nwout_end + INTERVAL '15 MINUTES' --more than one time bin in excess of nwout
 ORDER BY
     detector_id,
-    MIN(time_bin)
+    time_start
