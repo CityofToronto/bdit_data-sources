ff977eba7ff4f8d6427fd61e10d0f6169b4162ac
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-05-31 13:54:21 -0400

    sql to identify sensors to repair

---
+++ volumes/rescu/Data validation/identify_sensors_to_repair_i0617.sql
@@ -0,0 +1,113 @@
+DROP TABLE gwolofs.i0617_rescu_sensor_eval;
+
+CREATE TABLE gwolofs.i0617_rescu_sensor_eval(
+    detector_id text,
+    number_of_lanes int,
+    primary_road text,
+    cross_road text,
+    avg_volume numeric,
+    total_volume bigint,
+    bins_active_percent numeric,
+    bins_active bigint,
+    last_active TIMESTAMP,
+    geom geometry,
+    classify text
+);
+
+INSERT INTO gwolofs.i0617_rescu_sensor_eval (
+    detector_id,
+    number_of_lanes,
+    primary_road,
+    cross_road,
+    avg_volume,
+    total_volume,
+    bins_active_percent,
+    bins_active,
+    last_active,
+    geom,
+    classify
+)
+
+WITH time_bins AS (
+    SELECT generate_series(
+        '2023-01-01 00:00'::TIMESTAMP,
+        '2023-05-29 23:45',
+        '15 minutes') AS time_bin
+),
+
+tbins_omit_outages AS (
+    SELECT tb.time_bin
+    FROM time_bins AS tb
+    LEFT JOIN gwolofs.network_outages AS ro ON ro.time_range @> tb.time_bin
+    WHERE ro.time_range IS NULL
+),
+
+--add number of outages from individual_outages to summary?
+
+sensor_volumes AS (
+    SELECT
+        v1.detector_id,
+        AVG(v1.volume_15min) AS avg_volume,
+        SUM(v1.volume_15min) AS total_volume,
+        ROUND(
+            (COUNT(DISTINCT v1.datetime_bin)::decimal / (SELECT COUNT(*) FROM tbins_omit_outages)),
+            3
+        ) AS bins_active_percent,
+        COUNT(DISTINCT v1.datetime_bin) AS bins_active,
+        MAX(v1.datetime_bin) AS last_active
+    FROM tbins_omit_outages AS tboo
+    LEFT JOIN rescu.volumes_15min AS v1 ON v1.datetime_bin = tboo.time_bin
+    GROUP BY 1
+    ORDER BY 3
+)
+
+--active sensors during period 'time_bins'
+SELECT
+    di.detector_id,
+    di.number_of_lanes,
+    di.primary_road,
+    di.cross_road,
+    sv.avg_volume,
+    sv.total_volume,
+    sv.bins_active_percent,
+    sv.bins_active,
+    sv.last_active,
+    ST_SetSRID(ST_MakePoint(di.longitude, di.latitude), 4326) AS geom,
+    CASE
+        WHEN sv.last_active >= '2023-05-15'
+            AND sv.bins_active_percent >= 0.7 THEN 'good'
+        WHEN sv.last_active < '2023-05-15' --the most recent day with data
+            OR sv.bins_active_percent < 0.7 THEN 'bad' --<70% of days have data
+    END AS classify
+FROM sensor_volumes AS sv
+JOIN rescu.detector_inventory AS di ON di.detector_id = sv.detector_id
+WHERE di.primary_road NOT LIKE 'RAMP%'
+
+UNION
+
+--inactive sensors:
+SELECT
+    di.detector_id,
+    di.number_of_lanes,
+    di.primary_road,
+    di.cross_road,
+    NULL::numeric AS avg_volume,
+    NULL::numeric AS total_volume,
+    NULL::numeric AS bins_active_percent,
+    NULL::numeric AS bins_active,
+    inactive.last_active,
+    ST_SetSRID(ST_MakePoint(di.longitude, di.latitude), 4326) AS geom,
+    'inactive' AS classify
+FROM (
+    SELECT
+        v1.detector_id,
+        MAX(v1.datetime_bin) AS last_active
+    FROM rescu.volumes_15min AS v1
+    LEFT JOIN sensor_volumes AS sv ON v1.detector_id = sv.detector_id
+    WHERE sv.detector_id IS NULL
+    GROUP BY 1
+) AS inactive
+LEFT JOIN rescu.detector_inventory AS di ON di.detector_id = inactive.detector_id
+WHERE di.primary_road NOT LIKE 'RAMP%'
+ORDER BY di.detector_id
+
