322c6981664598d2b0fbc873658be5f471b55e10
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-05-31 15:46:29 -0400

    fluff

--- volumes/rescu/create-view-network-outages.sql
+++ volumes/rescu/create-view-network-outages.sql
@@ -9,8 +9,8 @@ WITH rescu_summary AS (
     SELECT
         datetime_bin,
         --occasional timeslot where a single sensor reports zero 
-            --and no other data. Need to use sum and not count as criteria. 
-        COALESCE(SUM(volume_15min), 0) as volume
+        --and no other data. Need to use sum and not count as criteria. 
+        COALESCE(SUM(volume_15min), 0) AS volume
     FROM rescu.volumes_15min
     GROUP BY 1
     HAVING COALESCE(SUM(volume_15min), 0) > 0
@@ -18,12 +18,13 @@ WITH rescu_summary AS (
 ),
 
 bin_gaps AS (
-    SELECT 
+    SELECT
         datetime_bin,
-        datetime_bin - INTERVAL '15 minutes' as gap_end,
         volume,
+        datetime_bin - INTERVAL '15 minutes' AS gap_end,
         CASE
-            WHEN datetime_bin - LAG(datetime_bin, 1) OVER (ORDER BY datetime_bin) = '00:15:00' THEN 0
+            WHEN
+                datetime_bin - LAG(datetime_bin, 1) OVER (ORDER BY datetime_bin) = '00:15:00' THEN 0
             ELSE 1
         END AS bin_break,
         datetime_bin - LAG(datetime_bin, 1) OVER (ORDER BY datetime_bin) AS bin_gap,
@@ -33,15 +34,16 @@ bin_gaps AS (
 
 -- calculate the start and end times of gaps that are longer than 15 minutes
 SELECT
-    bt.gap_start AS time_start,
-    bt.gap_end AS time_end,
-    tsrange(bt.gap_start, bt.gap_end, '[]') AS time_range,
-    (bt.gap_start)::DATE AS date_start,
-    (bt.gap_end)::DATE AS date_end,
-    daterange((bt.gap_start)::DATE, (bt.gap_end)::DATE, '[]') AS date_range,
-    EXTRACT(epoch FROM bt.gap_end - bt.gap_start)/86400 AS duration_days --no minimum duration for a network wide outage
-FROM bin_gaps AS bt
-WHERE 
-    bt.bin_break = 1
+    gap_start AS time_start,
+    gap_end AS time_end,
+    tsrange(gap_start, gap_end, '[]') AS time_range,
+    (gap_start)::DATE AS date_start,
+    (gap_end)::DATE AS date_end,
+    daterange((gap_start)::DATE, (gap_end)::DATE, '[]') AS date_range,
+    --no minimum duration for a network wide outage
+    EXTRACT(epoch FROM gap_end - gap_start) / 86400 AS duration_days
+FROM bin_gaps
+WHERE
+    bin_break = 1
     AND bin_gap IS NOT NULL
 ORDER BY datetime_bin;
