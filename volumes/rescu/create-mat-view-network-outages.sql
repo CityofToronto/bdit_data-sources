734c7cd214185679dfae29563558b1ec55e024fb
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-05-31 10:00:18 -0400

    speed improvements

--- volumes/rescu/create-mat-view-network-outages.sql
+++ volumes/rescu/create-mat-view-network-outages.sql
@@ -1,50 +1,47 @@
---runs in 23s for 2023. 
+--network outages using partition, lag (sarah's method). 
+--much faster, runs in 1s for all of rescu. 
+--omits outage that is ongoing right now. 
+DROP VIEW gwolofs.network_outages;
 
 CREATE VIEW gwolofs.network_outages AS
 
---continuous outages
-WITH time_bins AS (
-    SELECT generate_series(
-        '2023-01-01 00:00'::TIMESTAMP,
-        '2023-05-28 23:45',
-        '15 minutes'
-        ) AS time_bin
-), 
-
-network_bins AS (
+WITH rescu_summary AS (
     SELECT
-        tb.time_bin,
+        datetime_bin,
         --occasional timeslot where a single sensor reports zero 
             --and no other data. Need to use sum and not count as criteria. 
-        COALESCE(SUM(v15.volume_15min), 0) > 0 AS network_active --coalesce null to zero
-    FROM time_bins AS tb
-    LEFT JOIN rescu.volumes_15min AS v15 ON v15.datetime_bin = tb.time_bin
+        COALESCE(SUM(volume_15min), 0) as volume
+    FROM rescu.volumes_15min
     GROUP BY 1
+    HAVING COALESCE(SUM(volume_15min), 0) > 0
     ORDER BY 1
 ),
 
-run_groups AS (
-    SELECT
-        time_bin,
-        network_active,
-        (
-            SELECT COUNT(*)
-            FROM network_bins AS g
-            WHERE g.network_active <> gr.network_active --state change from active to inactive (or reverse)
-                AND g.time_bin <= gr.time_bin --running total
-        ) AS run_group
-    FROM network_bins AS gr
+bin_gaps AS (
+    SELECT 
+        datetime_bin,
+        datetime_bin - INTERVAL '15 minutes' as gap_end,
+        volume,
+        CASE
+            WHEN datetime_bin - LAG(datetime_bin, 1) OVER (ORDER BY datetime_bin) = '00:15:00' THEN 0
+            ELSE 1
+        END AS bin_break,
+        datetime_bin - LAG(datetime_bin, 1) OVER (ORDER BY datetime_bin) AS bin_gap,
+        LAG(datetime_bin, 1) OVER (ORDER BY datetime_bin) + INTERVAL '15 minutes' AS gap_start
+    FROM rescu_summary
 )
 
+-- calculate the start and end times of gaps that are longer than 15 minutes
 SELECT
-    MIN(time_bin) AS time_start,
-    MAX(time_bin) AS time_end,
-    tsrange(MIN(time_bin), MAX(time_bin), '[]') AS time_range,
-    MIN(time_bin)::DATE AS date_start,
-    MAX(time_bin)::DATE AS date_end,
-    daterange(MIN(time_bin)::DATE, MAX(time_bin)::DATE, '[]') AS date_range,
-    COUNT(*) / (60.0 / 15 * 24) AS duration_days --no minimum duration for a network wide outage
-FROM run_groups
-WHERE network_active = FALSE --only count length of outages
-GROUP BY run_group
-ORDER BY MIN(time_bin);
+    bt.gap_start AS time_start,
+    bt.gap_end AS time_end,
+    tsrange(bt.gap_start, bt.gap_end, '[]') AS time_range,
+    (bt.gap_start)::DATE AS date_start,
+    (bt.gap_end)::DATE AS date_end,
+    daterange((bt.gap_start)::DATE, (bt.gap_end)::DATE, '[]') AS date_range,
+    EXTRACT(epoch FROM bt.gap_end - bt.gap_start)/86400 AS duration_days --no minimum duration for a network wide outage
+FROM bin_gaps AS bt
+WHERE 
+    bt.bin_break = 1
+    AND bin_gap IS NOT NULL
+ORDER BY datetime_bin;
