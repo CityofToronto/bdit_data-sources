5d95df0fd7d4e47402b2f78c9fcd4eb602029fb1
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-06-12 17:39:28 -0400

    updates to dag+ipynb, remove readme,

--- volumes/rescu/itscentral_pipeline/read_vdsdata.py
+++ volumes/rescu/itscentral_pipeline/read_vdsdata.py
@@ -30,22 +30,11 @@ def parse_lane_data(laneData):
             occupancy = struct.unpack('<H', mv[index + 5] + mv[index + 6])[0]
             occupancyPercent = None if occupancy == 65535 else occupancy / 100.0
 
-            # Get volume by vehicle lengths - these columns are empty 
-            #Each class stored in vehicles per hour. 65535 for null value.
-            #passengerVolume = struct.unpack('<H', mv[index + 7] + mv[index + 8])[0]
-            #volumePassengerVehiclesPerHour = None if passengerVolume == 65535 else passengerVolume
-            #singleUnitTrucksVolume = struct.unpack('<H', mv[index + 9] + mv[index + 10])[0]
-            #volumeSingleUnitTrucksPerHour = None if singleUnitTrucksVolume == 65535 else singleUnitTrucksVolume
-            #comboTrucksVolume = struct.unpack('<H', mv[index + 11] + mv[index + 12])[0]
-            #volumeComboTrucksPerHour = None if comboTrucksVolume == 65535 else comboTrucksVolume
-            #multiTrailerTrucksVolume = struct.unpack('<H', mv[index + 13] + mv[index + 14])[0]
-            #volumeMultiTrailerTrucksPerHour = None if multiTrailerTrucksVolume == 65535 else multiTrailerTrucksVolume
-
             # Increment index by 15 to move to the next lane
             index += 15
 
-            result.append([lane, speedKmh, volumeVehiclesPerHour, occupancyPercent]) #, volumePassengerVehiclesPerHour, volumeSingleUnitTrucksPerHour, volumeComboTrucksPerHour, volumeMultiTrailerTrucksPerHour])
-            
+            result.append([lane, speedKmh, volumeVehiclesPerHour, occupancyPercent]) 
+
     return result
 
 
@@ -62,21 +51,74 @@ sql = f'''SELECT
     d.timestamputc,
     d.vdsid,
     d.lanedata
-FROM public.vdsdata AS d
-JOIN public.vdsconfig AS c ON
+FROM vdsdata AS d
+JOIN vdsconfig AS c ON
     d.vdsid = c.vdsid
     AND d.divisionid = c.divisionid
     AND to_timestamp(d.timestamputc) >= c.starttimestamputc
     AND (
         to_timestamp(d.timestamputc) <= c.endtimestamputc
         OR c.endtimestamputc IS NULL) --no end date
+JOIN EntityLocationLatest AS e ON
+    c.vdsid = e.entityid
+    AND c.divisionid = e.divisionid
 WHERE 
     timestamputc >= {date_start}
-    AND timestamputc < {date_end} + 86400;'''
+    AND timestamputc < {date_end} + 86400
+    AND e.entityid IS NOT NULL --we only have locations for these ids
+    AND d.divisionid = 2 --other is 8001 which are signals
+    AND substring(sourceid, 1, 3) <> 'BCT' --bluecity.ai sensors; '''
 # AND sourceid = 'de0020dwg';''' #for testing
- 
+
+detector_sql = '''SELECT 
+    c.divisionid,
+    c.vdsid,
+    upper(c.sourceid) AS detector_id,
+    c.starttimestamputc,
+    c.endtimestamputc,
+    c.lanes,
+    c.hasgpsunit,
+    c.managementurl,
+    c.description,
+    c.fssdivisionid,
+    c.fssid,
+    c.rtmsfromzone,
+    c.rtmstozone,
+    c.detectortype,
+    c.createdby,
+    c.createdbystaffid,
+    c.signalid,
+    c.signaldivisionid,
+    c.movement,
+    e.entitytype,                  
+    e.locationtimestamputc,        
+    e.latitude,                    
+    e.longitude,                   
+    e.altitudemetersasl,           
+    e.headingdegrees,              
+    e.speedkmh,                    
+    e.numsatellites,               
+    e.dilutionofprecision,         
+    e.mainroadid,                  
+    e.crossroadid,                 
+    e.secondcrossroadid,           
+    e.mainroadname,                
+    e.crossroadname,               
+    e.secondcrossroadname,         
+    e.streetnumber,                
+    e.offsetdistancemeters,        
+    e.offsetdirectiondegrees,      
+    e.locationsource,              
+    e.locationdescriptionoverwrite
+FROM vdsconfig AS c 
+JOIN EntityLocationLatest AS e ON 
+    c.vdsid = e.entityid 
+    AND c.divisionid = e.divisionid 
+ORDER BY c.vdsid'''
+
 with con: 
     df = pd.read_sql(sql, con)
+    detector_inventory = pd.read_sql(detector_sql, con)
 
 print(f"Number of rows: {df.shape[0]}")
 
@@ -86,10 +128,11 @@ row_lengths = df['lanedata'].map(len)
 empty_rows = df[row_lengths == 0]
 
 if empty_rows.empty is False:
-    print(f'Rows with empty lanedata discarded: {empty_rows.shape[0]}')
+    print(f'Rows with empty lanedata: {empty_rows.shape[0]}')
 else: 
     print(f'No empty rows discarded.')
 
+#drop empty rows
 df.drop(empty_rows.index, inplace = True) #remove empty rows
 
 df['datetime'] = df['timestamputc'].map(datetime.fromtimestamp)
@@ -97,75 +140,28 @@ df['datetime_15min'] = df['timestamputc'].map(lambda a: 60 * 15 * (a // (60 * 15
 
 #parse each `lanedata` column entry 
 lane_data = df['lanedata'].map(parse_lane_data)
+n_rows = lane_data.map(len)
 
 #flatten the nested list structure
-lane_data_flat = [item for sublist in lane_data for item in sublist]
+lane_data = [item for sublist in lane_data for item in sublist]
 
 #convert list structure to df
-cols = ['lane', 'speedKmh', 'volumeVehiclesPerHour', 'occupancyPercent']
-#removed columns:, 'volumePassengerVehiclesPerHour', 'volumeSingleUnitTrucksPerHour', 'volumeComboTrucksPerHour', 'volumeMultiTrailerTrucksPerHour'])
-lane_data_df = pd.DataFrame(lane_data_flat, columns = cols) 
+cols = ['lane', 'speedKmh', 'volumeVehiclesPerHour', 'occupancyPercent'] #removed columns:, #, 'volumePassengerVehiclesPerHour', 'volumeSingleUnitTrucksPerHour', 'volumeComboTrucksPerHour', 'volumeMultiTrailerTrucksPerHour']
+lane_data_df = pd.DataFrame(lane_data, columns = cols) 
 
 #repeat original index based on number of rows in result as a join column
-n_rows = lane_data.map(len)
 lane_data_df.set_index(df.index.repeat(n_rows), inplace=True)
 
 #join with other columns on index 
-final = df[['detector_id', 'datetime', 'datetime_15min']].join(lane_data_df)
+raw_20sec = df[['detector_id', 'datetime', 'datetime_15min']].join(lane_data_df)
 
-print(final.head(20))
+print(raw_20sec.head(20))
 
 #aggregate from 20s hourly rate to 15 minute volume
 #this method of aggregation assumes missing bins are zeros. 
 def rate_to_volume(x): 
     return x.sum() / 4 / 45 #/4 is for hourly to 15 minute volume. /45 is for 45 20sec bins per 15 minutes (assumes missing bins are zeros).   
 
-summary = final.groupby('datetime_15min').agg(
-        volume_15min = ('volumeVehiclesPerHour', rate_to_volume),
-        bin_count = ('datetime_15min', 'count'))
-
-with pd.option_context("display.max_rows", 1000): 
-    print(summary)
-
-print(f"Total daily volume: {summary['volume_15min'].sum():.0f}")
-
-volumes_15min = final.groupby(['detector_id', 'datetime_15min']).agg(
+volumes_15min = raw_20sec.groupby(['detector_id', 'datetime_15min']).agg(
         volume_15min = ('volumeVehiclesPerHour', rate_to_volume),
         bin_count = ('datetime_15min', 'count')).reset_index()
-
-print(f"Total daily volume: {volumes_15min['volume_15min'].sum():.2f}")
-
-
-#look at sensor daily totals: 
-with pd.option_context("display.max_rows", 1000): 
-    print(volumes_15min.groupby(['detector_id']).agg(
-            volume_15min = ('volume_15min', sum)).head(1000))
-
-volumes_15min.groupby(['detector_id']).agg(
-            volume_15min = ('volume_15min', sum),
-            bins = ('datetime_15min', 'count')).to_csv('/home/gwolofs/rescu_itscentral/sample_data.csv')
-
-#look at a single sensor (one which doesn't match total!)
-#rawest data:
-with pd.option_context("display.max_rows", 1000): 
-    print(final[final['detector_id'] == 'de0041deg'])
-
-#15 minute data: 
-#overall difference for the day is 14. 
-with pd.option_context("display.max_rows", 1000): 
-    print(volumes_15min[volumes_15min['detector_id'] == 'de0041deg'].reset_index())
-
-
-#are there both zeros and nulls? should we treat them differently?
-total_raw_rows = empty_rows.shape[0] + lane_data.shape[0]
-print(f"{100 * empty_rows.shape[0] / total_raw_rows:.3f}% rows have null lanedata.")
-
-#distinct bins expected: 
-bins_expected = final[['detector_id', 'lane']].drop_duplicates().shape[0] * 24 * 60 * 3
-print(f"{100 * final.shape[0] / bins_expected:.3f}% of expected bins present based on # detectors and lanes.")
-
-#bins expected but only highways & not ramps
-only_highways = final[[x[0] == 'D' and x[-1] != 'R' for x in final['detector_id']]]
-bins_expected = only_highways[['detector_id', 'lane']].drop_duplicates().shape[0] * 24 * 60 * 3
-print(f"{100 * only_highways.shape[0] / bins_expected:.3f}% of expected bins present based on # detectors and lanes for only highways (not including ramps).")
-
--- volumes/rescu/itscentral_pipeline/vds_pull.py
+++ volumes/rescu/itscentral_pipeline/vds_pull.py
@@ -13,21 +13,41 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('data_processing', default_args=default_args, schedule_interval='@daily')
+dag = DAG('data_processing', default_args=default_args, schedule_interval='0 4 * * *') #daily at 4am
 
 def pull_raw_data():
     # Pull raw data from Postgres database
     pg_hook = PostgresHook(postgres_conn_id='your_postgres_conn_id')
-    raw_data = pg_hook.get_records("SELECT * FROM your_raw_data_table")
+    raw_sql = f'''SELECT 
+        d.divisionid,
+        d.vdsid,
+        d.timestamputc,
+        d.lanedata
+    FROM vdsdata AS d
+    JOIN vdsconfig AS c ON
+        d.vdsid = c.vdsid
+        AND d.divisionid = c.divisionid
+        AND to_timestamp(d.timestamputc) >= c.starttimestamputc
+        AND (
+            to_timestamp(d.timestamputc) <= c.endtimestamputc
+            OR c.endtimestamputc IS NULL) --no end date
+    JOIN EntityLocationLatest AS e ON
+        c.vdsid = e.entityid
+        AND c.divisionid = e.divisionid
+    WHERE 
+        timestamputc >= extract(epoch from timestamptz '{{ds}}')
+        AND timestamputc < extract(epoch from timestamptz '{{ds}}') + 86400
+        AND e.entityid IS NOT NULL --we only have locations for these ids
+        AND d.divisionid = 2 --other is 8001 which are traffic signal detectors
+        AND substring(sourceid, 1, 3) <> 'BCT' --bluecity.ai sensors; '''
+
+    raw_data = pg_hook.get_records(raw_sql)
     
     # Transform raw data
-    transformed_data = your_transform_function(raw_data)
-    
-    # Get the current date
-    current_date = datetime.now().date().isoformat()
-    
+    transformed_data = transform_function(raw_data)
+        
     # Drop records for the current date
-    drop_query = f"DELETE FROM rescu.raw_20sec WHERE date = '{current_date}'"
+    drop_query = f"DELETE FROM rescu.raw_20sec WHERE date = '{{ds}}'"
     pg_hook.run(drop_query)
     
     # Insert cleaned data into the database
@@ -44,11 +64,11 @@ def summarize_data():
     # Perform summarization logic on raw_20sec table
     pg_hook = PostgresHook(postgres_conn_id='your_postgres_conn_id')
     summarize_query = '''
-        INSERT INTO volumes_15min (summary_column1, summary_column2, summary_column3)
-        SELECT SUM(column1), AVG(column2), COUNT(*)
+        INSERT INTO rescu.volumes_15min (detector_id, datetime_bin, volume_15min)
+        SELECT detector_id, datetime_bin, SUM(volume) AS volume_15min
         FROM rescu.raw_20sec
-        WHERE date = current_date - interval '1 day'
-        GROUP BY summary_column1, summary_column2
+        WHERE datetime_bin::date = {{ds}}
+        GROUP BY detector_id, datetime_bin
     '''
     pg_hook.run(summarize_query)
 
@@ -58,4 +78,73 @@ summarize_data_task = PythonOperator(
     dag=dag
 )
 
-pull_raw_data_task >> summarize_data_task
+def pull_detector_inventory():
+    # Pull data from the detector_inventory table
+    detector_sql = '''SELECT 
+        c.divisionid,
+        c.vdsid,
+        upper(c.sourceid) AS detector_id,
+        c.starttimestamputc,
+        c.endtimestamputc,
+        c.lanes,
+        c.hasgpsunit,
+        c.managementurl,
+        c.description,
+        c.fssdivisionid,
+        c.fssid,
+        c.rtmsfromzone,
+        c.rtmstozone,
+        c.detectortype,
+        c.createdby,
+        c.createdbystaffid,
+        c.signalid,
+        c.signaldivisionid,
+        c.movement,
+        e.entitytype,                  
+        e.locationtimestamputc,        
+        e.latitude,                    
+        e.longitude,                   
+        e.altitudemetersasl,           
+        e.headingdegrees,              
+        e.speedkmh,                    
+        e.numsatellites,               
+        e.dilutionofprecision,         
+        e.mainroadid,                  
+        e.crossroadid,                 
+        e.secondcrossroadid,           
+        e.mainroadname,                
+        e.crossroadname,               
+        e.secondcrossroadname,         
+        e.streetnumber,                
+        e.offsetdistancemeters,        
+        e.offsetdirectiondegrees,      
+        e.locationsource,              
+        e.locationdescriptionoverwrite
+    FROM vdsconfig AS c 
+    JOIN EntityLocationLatest AS e ON 
+        c.vdsid = e.entityid 
+        AND c.divisionid = e.divisionid 
+    ORDER BY c.vdsid'''
+
+    pg_hook = PostgresHook(postgres_conn_id='your_postgres_conn_id')
+    detector_data = pg_hook.get_records(detector_sql)
+    
+    # Transform and upsert data into the detector_inventory table
+    transformed_data = detector_transform_function(detector_data)
+    
+    # Generate the upsert query
+    upsert_query = '''
+        INSERT INTO detector_inventory (column1, column2, column3)
+        VALUES %s
+        ON CONFLICT (column1) DO UPDATE
+        SET column2 = EXCLUDED.column2, column3 = EXCLUDED.column3
+    '''
+    pg_hook.run(upsert_query, parameters=transformed_data)
+
+pull_detector_inventory_task = PythonOperator(
+    task_id='pull_and_upsert_detector_inventory',
+    python_callable=pull_detector_inventory,
+    dag=dag
+)
+
+pull_raw_data_task >> summarize_data_task >> pull_detector_inventory_task
