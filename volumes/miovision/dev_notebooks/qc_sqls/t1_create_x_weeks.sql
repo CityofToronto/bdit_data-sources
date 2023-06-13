11f99de90316ee55e7e2669602abad840b790c66
Nate Wessel <nate.wessel@toronto.ca>
2023-06-13 11:03:55 -0400

    change schema
    don't want to give the impression that this table is retained in miovision_api; it was just temporary

--- volumes/miovision/dev_notebooks/qc_sqls/t1_create_x_weeks.sql
+++ volumes/miovision/dev_notebooks/qc_sqls/t1_create_x_weeks.sql
@@ -1,5 +1,5 @@
 -- This table was created to store weeks that were identified as having low or no car volumes via the "graph weekly data and note the dips" process
-CREATE TABLE miovision_api.miovision_bad_weeks (
+CREATE TABLE scannon.miovision_bad_weeks (
     intersection_uid smallint,
     intersection_name text,
     bad_week date,
