27d9bba3d9cb39b446ac2b949bf300a45078f630
Sarah Cannon <scannon@ip-10-160-2-198.ec2.internal>
2023-06-16 16:15:10 -0400

    #596 updated dag's slack alert

--- dags/collisions_replicator_transfer.py
+++ dags/collisions_replicator_transfer.py
@@ -1,14 +1,6 @@
 #This script should run after the MOVE dag dumps data into collisions_replicator."ACC"
 #Also I heavily cribbed this off of refresh_wys_monthly.py...
 
-# Operators; we need this to operate!
-from airflow import DAG
-from datetime import datetime, timedelta
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.hooks.base_hook import BaseHook
-from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
-SLACK_CONN_ID = 'slack_data_pipeline'
-
 #This script does things with those operators:
 #1) uses ACC to update acc_safe_copy
 #2) updates the mat view called collisions_no, which generates ids needed to...
@@ -19,23 +11,69 @@ SLACK_CONN_ID = 'slack_data_pipeline'
 #7) updates the MOVE events mat view
 #8) throws a sassy slack alert message when it fails
 
-def task_fail_sassy_slack_alert(context):
+# Operators; we need this to operate!
+
+from datetime import datetime, timedelta
+import os
+import sys
+from threading import local
+import psycopg2
+from psycopg2 import sql
+import requests
+from psycopg2.extras import execute_values
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable 
+from dateutil.parser import parse
+
+dag_name = 'collisions_replicator_transfer'
+
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+from airflow.hooks.base_hook import BaseHook
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+
+dag_owners = Variable.get('dag_owners', deserialize_json=True)
+slack_ids = Variable.get('slack_member_id', deserialize_json=True)
+
+names = dag_owners.get(dag_name, ['Unknown']) #find dag owners w/default = Unknown    
+
+list_names = []
+for name in names:
+    list_names.append(slack_ids.get(name, '@Unknown Slack ID')) #find slack ids w/default = Unkown
+
+
+SLACK_CONN_ID = 'slack_data_pipeline'
+def task_fail_slack_alert(context):
     slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
-    task_msg = 'The {task} in updating collisions failed, <@U02NSCSKFEU> go fix it meow :meow_tableflip: '.format(
-            task=context.get('task_instance').task_id,) #K just gotta point out that the last 3 letters in my slack id spell FIRE in French!!!   
-        
-    slack_msg = task_msg + """(<{log_url}|log>)""".format(
-            log_url=context.get('task_instance').log_url,)
+    slack_msg = """
+            :red_circle: Task Failed / Tâche échouée.
+            {slack_name} please check.
+            *Task*: {task}
+            *Dag*: {dag}
+            *Execution Time*: {exec_date}
+            *Log Url*: {log_url}
+            """.format(
+            slack_name=' '.join(list_names),
+            task=context.get('task_instance').task_id,
+            dag=context.get('task_instance').dag_id,
+            ti=context.get('task_instance'),
+            exec_date=context.get('execution_date'),
+            log_url=context.get('task_instance').log_url,
+        )
     failed_alert = SlackWebhookOperator(
         task_id='slack_test',
         http_conn_id='slack',
         webhook_token=slack_webhook_token,
         message=slack_msg,
-        username='airflow'
+        username='airflow',
         )
     return failed_alert.execute(context=context)
 
-default_args = {'owner':'scannon',
+
+
+default_args = {'owner':','.join(names),
                 'depends_on_past':False,
                 'start_date': datetime(2022, 5, 26), #start this Thursday, why not?
                 'email': ['sarah.cannon@toronto.ca'],
@@ -43,7 +81,7 @@ default_args = {'owner':'scannon',
                  'email_on_success': False,
                  'retries': 0,
                  'retry_delay': timedelta(minutes=5),
-                 'on_failure_callback': task_fail_sassy_slack_alert
+                 'on_failure_callback': task_fail_slack_alert
                 }
 
 
