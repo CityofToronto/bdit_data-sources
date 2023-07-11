c5425b963d8bb36dd2444329d668370c4f2bf226
tahaislam <islam.taha@toronto.ca>
2023-07-11 11:58:13 -0400

    sqlfluff #607

--- gis/gccview/sql/create-function-refresh_mat_view_centreline_version_date.sql
+++ gis/gccview/sql/create-function-refresh_mat_view_centreline_version_date.sql
@@ -2,23 +2,22 @@
 
 -- DROP FUNCTION IF EXISTS gis.refresh_mat_view_centreline_version_date();
 
-CREATE OR REPLACE FUNCTION gis.refresh_mat_view_centreline_version_date(
-	)
-    RETURNS void
-    LANGUAGE 'sql'
-    COST 100
-    VOLATILE SECURITY DEFINER PARALLEL UNSAFE
+CREATE OR REPLACE FUNCTION gis.refresh_mat_view_centreline_version_date()
+RETURNS void
+LANGUAGE 'sql'
+COST 100
+VOLATILE SECURITY DEFINER PARALLEL UNSAFE
 AS $BODY$
     REFRESH MATERIALIZED VIEW CONCURRENTLY gis.centreline_version_date WITH DATA ;
 $BODY$;
 
 COMMENT ON FUNCTION gis.refresh_mat_view_centreline_version_date()
-    IS 'Refresh the materialized view that lists all dates of different centreline versions';
+IS 'Refresh the materialized view that lists all dates of different centreline versions';
 
 ALTER FUNCTION gis.refresh_mat_view_centreline_version_date()
-    OWNER TO gis_admins;
+OWNER TO gis_admins;
 
-REVOKE ALL ON FUNCTION gis.refresh_mat_view_centreline_version_date() FROM PUBLIC;
+REVOKE ALL ON FUNCTION gis.refresh_mat_view_centreline_version_date() FROM public;
 
 GRANT EXECUTE ON FUNCTION gis.refresh_mat_view_centreline_version_date() TO gis_admins;
 
