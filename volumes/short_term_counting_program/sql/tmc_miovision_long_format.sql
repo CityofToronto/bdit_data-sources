e5ebea3f8cb5cd338c55341a70718824ad88b994
Nate Wessel <nate.wessel@toronto.ca>
2023-07-14 16:58:22 -0400

    Add view, column comments

--- volumes/short_term_counting_program/sql/tmc_miovision_long_format.sql
+++ volumes/short_term_counting_program/sql/tmc_miovision_long_format.sql
@@ -115,3 +115,15 @@ CREATE INDEX ON traffic.tmc_miovision_long_format (arterycode);
 CREATE INDEX ON traffic.tmc_miovision_long_format (classification_uid);
 CREATE INDEX ON traffic.tmc_miovision_long_format (datetime_bin);
 CREATE INDEX ON traffic.tmc_miovision_long_format (count_info_id);
+
+COMMENT ON MATERIALIZED VIEW traffic.tmc_miovision_long_format
+IS 'converts traffic.det TMC table into a long format more compatible with the way we store Miovision TMC data';
+
+COMMENT ON COLUMN traffic.tmc_miovision_long_format.count_date
+IS 'This indicates the START of a 15 minute bin for compatibility with Miovision data, NOT the end, as in traffic.det';
+
+COMMENT ON COLUMN traffic.tmc_miovision_long_format.movement_uid
+IS 'This maps the (_l, _t, _r) of traffic.det to the miovision_api.movements movement_uids';
+
+COMMENT ON COLUMN traffic.tmc_miovision_long_format.classification_uid
+IS 'This roughly maps the (peds, bikes, bus, cars, truck) of traffic.det to the numeric classification_uids of miovision_api.classifications';
