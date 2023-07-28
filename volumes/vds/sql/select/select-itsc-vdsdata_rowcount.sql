ec199443dc1d7f7dc655600ffd9484b14248a4e2
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-07-28 15:35:59 -0400

    #603 add in div 8001

--- volumes/vds/sql/select/select-itsc-vdsdata_rowcount.sql
+++ volumes/vds/sql/select/select-itsc-vdsdata_rowcount.sql
@@ -3,7 +3,7 @@ SELECT
     SUM(LENGTH(lanedata)/15) AS count --each 15 bytes represents one row in final expanded data
 FROM public.vdsdata
 WHERE
-    divisionid = 2 --other is 8001 which are traffic signal detectors and are mostly empty
+    divisionid IN (2, 8001)
     AND timestamputc >= extract('epoch' FROM {start}::timestamptz - interval {lookback})::integer
     AND timestamputc < extract('epoch' FROM {start}::timestamptz)::integer
     AND length(lanedata) > 0
--- volumes/vds/sql/select/select-itsc_vdsdata.sql
+++ volumes/vds/sql/select/select-itsc_vdsdata.sql
@@ -7,5 +7,5 @@ FROM public.vdsdata
 WHERE
     timestamputc >= extract('epoch' FROM {start}::timestamptz)::integer
     AND timestamputc < extract('epoch' FROM {start}::timestamptz + interval '1 DAY')::integer
-    AND divisionid = 2 --other is 8001 which are traffic signal detectors and are mostly empty.
+    AND divisionid IN (2, 8001)
     AND length(lanedata) > 0; --these records don't have any data to unpack.
