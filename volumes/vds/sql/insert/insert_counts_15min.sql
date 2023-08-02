1c41deca63d840f5301850118eff595d8f3fee8d
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-08-02 16:36:49 -0400

    #603 improve performnace of insert counts_15

--- volumes/vds/sql/insert/insert_counts_15min.sql
+++ volumes/vds/sql/insert/insert_counts_15min.sql
@@ -5,22 +5,6 @@ INSERT INTO vds.counts_15min (division_id, vds_id, detector_id, num_lanes, datet
 /* Conversion of hourly volumes to count depends on size of bin.
 These bin counts were determined by looking at the most common bin gap using:
 bdit_data-sources/volumes/vds/exploration/time_gaps.sql */
-WITH detector_inventory AS (
-    SELECT
-        *, 
-        CASE
-            WHEN detector_id LIKE 'D%' AND division_id = 2
-                THEN 45 --20 sec bins
-            WHEN detector_id LIKE ANY('{"YONGE HEATH%", "YONGE DAVISVILLE%", "BCT%"}')
-                THEN 1 --15 min bins
-            WHEN detector_id LIKE ANY(
-                    '{"%SMARTMICRO%", "%YONGE AND ROXBOROUGH%"}'
-                ) THEN 3 --5 min bins
-            WHEN division_id = 8001
-                THEN 1 --15 min bins
-        END AS expected_bins
-    FROM vds.vdsconfig 
-)
 
 SELECT 
     d.division_id,
@@ -28,29 +12,42 @@ SELECT
     c.detector_id,
     c.lanes AS num_lanes,
     d.datetime_15min,
-    SUM(d.volume_veh_per_hr) / 4 / c.expected_bins AS count_15min,
+    SUM(d.volume_veh_per_hr) / 4 / b.expected_bins AS count_15min,
     -- / 4 to convert hourly volume to 15 minute volume
     -- / (expected_bins) to get average 15 minute volume depending on bin size
     -- (assumes blanks are 0)
-    c.expected_bins,
+    b.expected_bins,
     COUNT(*) AS num_obs,
     COUNT(DISTINCT d.lane) AS num_distinct_lanes
 FROM vds.raw_vdsdata AS d
-JOIN detector_inventory AS c ON
+JOIN vds.vdsconfig AS c ON
     d.vds_id = c.vds_id
     AND d.division_id = c.division_id
     AND d.datetime_15min >= c.start_timestamp
     AND (
         d.datetime_15min <= c.end_timestamp
-        OR c.end_timestamp IS NULL) --no end date
+        OR c.end_timestamp IS NULL), --no end date
+    LATERAL(
+        SELECT CASE
+                WHEN c.detector_id LIKE 'D%' AND c.division_id = 2
+                    THEN 45 --20 sec bins
+                WHEN c.detector_id LIKE ANY('{"YONGE HEATH%", "YONGE DAVISVILLE%", "BCT%"}')
+                    THEN 1 --15 min bins
+                WHEN c.detector_id LIKE ANY(
+                        '{"%SMARTMICRO%", "%YONGE AND ROXBOROUGH%"}'
+                    ) THEN 3 --5 min bins
+                WHEN c.division_id = 8001
+                    THEN 1 --15 min bins
+            END AS expected_bins
+    ) AS b
 WHERE 
-    d.datetime_15min >= '{{ ds }} 00:00:00'::timestamp --'2023-07-05 00:00:00'::timestamp
-    AND d.datetime_15min < '{{ ds }} 00:00:00'::timestamp + interval '1 DAY' --'2023-07-06 00:00:00'::timestamp
+    d.dt >= '{{ ds }} 00:00:00'::timestamp --'2023-07-05 00:00:00'::timestamp
+    AND d.dt < '{{ ds }} 00:00:00'::timestamp + interval '1 DAY' --'2023-07-06 00:00:00'::timestamp
 GROUP BY
     d.division_id,
     d.vds_id,
     c.detector_id,
     c.lanes,
-    c.expected_bins,
+    b.expected_bins,
     d.datetime_15min
 ON CONFLICT DO NOTHING;
