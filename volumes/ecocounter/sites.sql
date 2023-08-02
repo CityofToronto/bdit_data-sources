7eb2eb33f52293aad3a64b9098fdc798d64b90ec
Nate Wessel <nate.wessel@toronto.ca>
2023-08-02 12:46:17 -0400

    updates sites table with facility description field

--- volumes/ecocounter/sites.sql
+++ volumes/ecocounter/sites.sql
@@ -1,7 +1,13 @@
 CREATE TABLE ecocounter.sites (
     site_id numeric PRIMARY KEY,
     site_description text NOT NULL,
-    geom geometry(POINT, 4326)
+    geom geometry(POINT, 4326),
+    facility_description text
 );
 
+ALTER TABLE ecocounter.sites OWNER TO ecocounter_admins;
+
 GRANT SELECT ON ecocounter.sites TO bdit_humans;
+
+COMMENT ON COLUMN ecocounter.sites.facility_description
+IS 'description of bike-specific infrastructure which the sensor is installed within';
