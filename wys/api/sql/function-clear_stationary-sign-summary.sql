80989365d571df78ab91d978e0378a1c3f3e4016
Islam Taha <islam.taha@toronto.ca>
2023-08-04 12:58:02 -0400

    sqlfluff clearing functions #663

--- wys/api/sql/function-clear_stationary-sign-summary.sql
+++ wys/api/sql/function-clear_stationary-sign-summary.sql
@@ -4,10 +4,10 @@ Name | Type | Description
 _mon | DATE | Month to clear
 
 Return: Void
-Purpose: Clear stationary summary for the given month
+Purpose: Clears stationary WYS summary for the given month
 */
 CREATE OR REPLACE FUNCTION wys.clear_stationary_summary_for_month ( -- noqa: PRS
-    _mon DATE
+    _mon date
 )
 RETURNS void
 LANGUAGE 'sql'
@@ -18,7 +18,10 @@ AS $BODY$
 
 DELETE FROM wys.stationary_summary
 WHERE mon = _mon;
+
 $BODY$;
 
-REVOKE EXECUTE ON FUNCTION wys.clear_stationary_summary_for_month (DATE)FROM public;
-GRANT EXECUTE ON FUNCTION wys.clear_stationary_summary_for_month (DATE) TO wys_bot;
+ALTER FUNCTION wys.clear_stationary_summary_for_month(date) OWNER TO wys_admins;
+
+REVOKE EXECUTE ON FUNCTION wys.clear_stationary_summary_for_month (date) FROM public;
+GRANT EXECUTE ON FUNCTION wys.clear_stationary_summary_for_month (date) TO wys_bot;
