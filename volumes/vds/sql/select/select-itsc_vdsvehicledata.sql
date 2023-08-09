9dc35720f7869cc23a2ac9bb3b8b07e83e2f6477
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-08-09 16:58:53 -0400

    #603 fix issue with DST when adding interval

--- volumes/vds/sql/select/select-itsc_vdsvehicledata.sql
+++ volumes/vds/sql/select/select-itsc_vdsvehicledata.sql
@@ -10,4 +10,4 @@ FROM public.vdsvehicledata
 WHERE
     divisionid = 2 --8001 and 8046 have only null values for speed/length/occupancy
     AND timestamputc >= TIMEZONE('UTC', {start}::timestamptz) --need tz conversion on RH side to make use of index.
-    AND timestamputc < TIMEZONE('UTC', {start}::timestamptz) + interval '1 DAY';
+    AND timestamputc < TIMEZONE('UTC', {start}::timestamptz + interval '1 DAY');
