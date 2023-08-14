c277ec97125dcca2eaaf96e063f65c1c55343521
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-08-14 16:58:48 -0400

    #669 flatten aggregate_15_min_mvt CTE

--- volumes/miovision/sql/function-aggregate-volumes_15min_mvt.sql
+++ volumes/miovision/sql/function-aggregate-volumes_15min_mvt.sql
@@ -10,43 +10,39 @@ AS $BODY$
 
 BEGIN
 
-WITH zero_padding_movements AS (
-	-- Cross product of legal movement for cars, bikes, and peds and the bins to aggregate
-	SELECT m.*, datetime_bin15
-	FROM miovision_api.intersection_movements m
+WITH aggregate_insert AS (
+	INSERT INTO miovision_api.volumes_15min_mvt(intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume)
+	SELECT im.intersection_uid,
+		dt.datetime_bin15 AS datetime_bin,
+		im.classification_uid,
+		im.leg,
+		im.movement_uid,
+		CASE WHEN un.accept = FALSE THEN NULL ELSE (COALESCE(SUM(v.volume), 0)) END AS volume
+	FROM miovision_api.intersection_movements im
 	CROSS JOIN generate_series(start_date - interval '1 hour', end_date - interval '1 hour 15 minutes', INTERVAL '15 minutes') AS dt(datetime_bin15)
 	-- Make sure that the intersection is still active
 	JOIN miovision_api.intersections mai USING (intersection_uid)
-	-- Only include dates during which intersection is active.
-	WHERE datetime_bin15> mai.date_installed + INTERVAL '1 day' AND (mai.date_decommissioned IS NULL OR (datetime_bin15< mai.date_decommissioned - INTERVAL '1 day'))
-), aggregate_insert AS (
-	INSERT INTO miovision_api.volumes_15min_mvt(intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume)
-	SELECT pad.intersection_uid,
-		pad.datetime_bin15 AS datetime_bin,
-		pad.classification_uid,
-		pad.leg,
-		pad.movement_uid,
-		CASE WHEN un.accept = FALSE THEN NULL ELSE (COALESCE(SUM(A.volume), 0)) END AS volume
-	FROM zero_padding_movements pad
 	--To set unacceptable ones to NULL instead (& only gap fill light vehicles,
 	--cyclist/cyclist ATR and pedestrian)
 	LEFT JOIN miovision_api.unacceptable_gaps un
-		ON un.intersection_uid = pad.intersection_uid
-		AND pad.datetime_bin15 >= DATE_TRUNC('hour', gap_start)
-		AND pad.datetime_bin15 < DATE_TRUNC('hour', gap_end) + interval '1 hour' -- may get back to this later on for fear of removing too much data
+		ON un.intersection_uid = im.intersection_uid
+		AND dt.datetime_bin15 >= DATE_TRUNC('hour', gap_start)
+		AND dt.datetime_bin15 < DATE_TRUNC('hour', gap_end) + interval '1 hour' -- may get back to this later on for fear of removing too much data
 	--To get 1min bins
-	LEFT JOIN miovision_api.volumes A
-		ON A.datetime_bin >= start_date - INTERVAL '1 hour'
-		AND A.datetime_bin < end_date - INTERVAL '1 hour'
-		AND A.datetime_bin >= pad.datetime_bin15
-		AND A.datetime_bin < pad.datetime_bin15 + interval '15 minutes'
-		AND A.intersection_uid = pad.intersection_uid
-		AND A.classification_uid = pad.classification_uid
-		AND A.leg = pad.leg
-		AND A.movement_uid = pad.movement_uid
-	WHERE A.volume_15min_mvt_uid IS NULL
-	GROUP BY pad.intersection_uid, pad.datetime_bin15, pad.classification_uid, pad.leg, pad.movement_uid, un.accept
-	HAVING pad.classification_uid IN (1,2,6,10) OR SUM(A.volume) > 0
+	LEFT JOIN miovision_api.volumes v
+		ON v.datetime_bin >= start_date - INTERVAL '1 hour'
+		AND v.datetime_bin < end_date - INTERVAL '1 hour'
+		AND v.datetime_bin >= dt.datetime_bin15
+		AND v.datetime_bin < dt.datetime_bin15 + interval '15 minutes'
+		AND v.intersection_uid = im.intersection_uid
+		AND v.classification_uid = im.classification_uid
+		AND v.leg = im.leg
+		AND v.movement_uid = im.movement_uid
+    -- Only include dates during which intersection is active.
+	WHERE datetime_bin15> mai.date_installed + INTERVAL '1 day' AND (mai.date_decommissioned IS NULL OR (datetime_bin15< mai.date_decommissioned - INTERVAL '1 day'))
+	    AND v.volume_15min_mvt_uid IS NULL
+	GROUP BY im.intersection_uid, dt.datetime_bin15, im.classification_uid, im.leg, im.movement_uid, un.accept
+	HAVING im.classification_uid IN (1,2,6,10) OR SUM(v.volume) > 0
 	RETURNING intersection_uid, volume_15min_mvt_uid, datetime_bin, classification_uid, leg, movement_uid, volume
 )
 --To update foreign key for 1min bin table
