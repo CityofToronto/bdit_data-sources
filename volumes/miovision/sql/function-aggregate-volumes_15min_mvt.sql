2cee573cac19503fea5ed2066614c2fe9e273e3a
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-08-15 13:29:44 -0400

    #669 fluff

--- volumes/miovision/sql/function-aggregate-volumes_15min_mvt.sql
+++ volumes/miovision/sql/function-aggregate-volumes_15min_mvt.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE FUNCTION miovision_api.aggregate_15_min_mvt(
-	start_date date,
-	end_date date)
-    RETURNS void
-    LANGUAGE 'plpgsql'
+    start_date date,
+    end_date date)
+RETURNS void
+LANGUAGE 'plpgsql'
 
-    COST 100
-    VOLATILE
+COST 100
+VOLATILE
 AS $BODY$
 
 BEGIN
@@ -14,9 +14,9 @@ WITH aggregate_insert AS (
     INSERT INTO miovision_api.volumes_15min_mvt(
         intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume
     )
-	SELECT
+    SELECT
         im.intersection_uid,
-		dt.datetime_bin15 AS datetime_bin,
+        dt.datetime_bin15 AS datetime_bin,
         im.classification_uid,
         im.leg,
         im.movement_uid,
@@ -33,25 +33,25 @@ WITH aggregate_insert AS (
         end_date - interval '1 hour 15 minutes',
         interval '15 minutes'
     ) AS dt(datetime_bin15)
-	JOIN miovision_api.intersections AS mai USING (intersection_uid)     
+    JOIN miovision_api.intersections AS mai USING (intersection_uid)     
     --To avoid aggregating unacceptable gaps
-	LEFT JOIN miovision_api.unacceptable_gaps AS un ON
+    LEFT JOIN miovision_api.unacceptable_gaps AS un ON
         un.intersection_uid = im.intersection_uid
         --remove the complete hour containing any unacceptable gaps
-		AND dt.datetime_bin15 >= date_trunc('hour', un.gap_start)
-		AND dt.datetime_bin15 < date_trunc('hour', un.gap_end) + interval '1 hour'
-	--To get 1min bins
-	LEFT JOIN miovision_api.volumes AS v ON
+        AND dt.datetime_bin15 >= date_trunc('hour', un.gap_start)
+        AND dt.datetime_bin15 < date_trunc('hour', un.gap_end) + interval '1 hour'
+    --To get 1min bins
+    LEFT JOIN miovision_api.volumes AS v ON
         --help query choose correct partition
         v.datetime_bin >= start_date - interval '1 hour'
-		AND v.datetime_bin < end_date - interval '1 hour'
+        AND v.datetime_bin < end_date - interval '1 hour'
         AND v.datetime_bin >= dt.datetime_bin15
-		AND v.datetime_bin < dt.datetime_bin15 + interval '15 minutes'
-		AND v.intersection_uid = im.intersection_uid
-		AND v.classification_uid = im.classification_uid
-		AND v.leg = im.leg
-		AND v.movement_uid = im.movement_uid
-	WHERE
+        AND v.datetime_bin < dt.datetime_bin15 + interval '15 minutes'
+        AND v.intersection_uid = im.intersection_uid
+        AND v.classification_uid = im.classification_uid
+        AND v.leg = im.leg
+        AND v.movement_uid = im.movement_uid
+    WHERE
         -- Only include dates during which intersection is active 
         -- (excludes entire day it was added/removed)
         dt.datetime_bin15 > mai.date_installed + interval '1 day'
@@ -61,24 +61,24 @@ WITH aggregate_insert AS (
         )
         --exclude movements already aggregated
         AND v.volume_15min_mvt_uid IS NULL
-	GROUP BY
+    GROUP BY
         im.intersection_uid,
         dt.datetime_bin15,
         im.classification_uid,
         im.leg,
         im.movement_uid,
         un.accept
-	HAVING
+    HAVING
         --retain 0s for certain modes (padding)
         im.classification_uid IN (1,2,6,10)
         OR SUM(v.volume) > 0 
-	RETURNING intersection_uid, volume_15min_mvt_uid, datetime_bin, classification_uid, leg, movement_uid, volume
+    RETURNING intersection_uid, volume_15min_mvt_uid, datetime_bin, classification_uid, leg, movement_uid, volume
 )
 --To update foreign key for 1min bin table
 UPDATE miovision_api.volumes AS a
-	SET volume_15min_mvt_uid = b.volume_15min_mvt_uid
-	FROM aggregate_insert b
-	WHERE
+    SET volume_15min_mvt_uid = b.volume_15min_mvt_uid
+    FROM aggregate_insert b
+    WHERE
         a.datetime_bin >= start_date - interval '1 hour'
         AND a.datetime_bin < end_date - interval '1 hour'
         AND a.volume_15min_mvt_uid IS NULL
@@ -96,7 +96,7 @@ END;
 $BODY$;
 
 ALTER FUNCTION miovision_api.aggregate_15_min_mvt(date, date)
-    OWNER TO miovision_admins;
+OWNER TO miovision_admins;
 
 GRANT EXECUTE ON FUNCTION miovision_api.aggregate_15_min_mvt(date, date) TO PUBLIC;
 
--- volumes/miovision/sql/function-aggregate-volumes_15min_mvt_single_intersection.sql
+++ volumes/miovision/sql/function-aggregate-volumes_15min_mvt_single_intersection.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE FUNCTION miovision_api.aggregate_15_min_mvt_single_intersection(
-	start_date date,
-	end_date date,
+    start_date date,
+    end_date date,
     target_intersection int)
-    RETURNS void
-    LANGUAGE 'plpgsql'
+RETURNS void
+LANGUAGE 'plpgsql'
 
-    COST 100
-    VOLATILE
+COST 100
+VOLATILE
 AS $BODY$
 
 BEGIN
@@ -15,9 +15,9 @@ WITH aggregate_insert AS (
     INSERT INTO miovision_api.volumes_15min_mvt(
         intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume
     )
-	SELECT
+    SELECT
         im.intersection_uid,
-		dt.datetime_bin,
+        dt.datetime_bin,
         im.classification_uid,
         im.leg,
         im.movement_uid,
@@ -34,31 +34,31 @@ WITH aggregate_insert AS (
         end_date - interval '1 hour 15 minutes',
         interval '15 minutes'
     ) AS dt(datetime_bin)
-	JOIN miovision_api.intersections AS mai USING (intersection_uid)
+    JOIN miovision_api.intersections AS mai USING (intersection_uid)
     --add an anti join on destination table to prevent duplicate key errors with zero padding movements.
     LEFT JOIN miovision_api.volumes_15min_mvt AS v15mvt USING (
         intersection_uid, datetime_bin, classification_uid, leg, movement_uid
     )
     --To avoid aggregating unacceptable gaps
-	LEFT JOIN miovision_api.unacceptable_gaps AS un ON
+    LEFT JOIN miovision_api.unacceptable_gaps AS un ON
         un.intersection_uid = im.intersection_uid
         --remove the complete hour containing any unacceptable gaps
-		AND dt.datetime_bin >= date_trunc('hour', un.gap_start)
-		AND dt.datetime_bin < date_trunc('hour', un.gap_end) + interval '1 hour'
+        AND dt.datetime_bin >= date_trunc('hour', un.gap_start)
+        AND dt.datetime_bin < date_trunc('hour', un.gap_end) + interval '1 hour'
         --only join unacceptable gaps labeled as do not accept.
         AND un.accept = FALSE
-	--To get 1min bins
-	LEFT JOIN miovision_api.volumes AS v ON
+    --To get 1min bins
+    LEFT JOIN miovision_api.volumes AS v ON
         --help query choose correct partition
         v.datetime_bin >= start_date - interval '1 hour'
-		AND v.datetime_bin < end_date - interval '1 hour'
+        AND v.datetime_bin < end_date - interval '1 hour'
         AND v.datetime_bin >= dt.datetime_bin
-		AND v.datetime_bin < dt.datetime_bin + interval '15 minutes'
-		AND v.intersection_uid = im.intersection_uid
-		AND v.classification_uid = im.classification_uid
-		AND v.leg = im.leg
-		AND v.movement_uid = im.movement_uid
-	WHERE
+        AND v.datetime_bin < dt.datetime_bin + interval '15 minutes'
+        AND v.intersection_uid = im.intersection_uid
+        AND v.classification_uid = im.classification_uid
+        AND v.leg = im.leg
+        AND v.movement_uid = im.movement_uid
+    WHERE
         mai.intersection_uid = target_intersection
         -- Only include dates during which intersection is active 
         -- (excludes entire day it was added/removed)
@@ -71,24 +71,24 @@ WITH aggregate_insert AS (
         AND v.volume_15min_mvt_uid IS NULL
         --exclude values already in destination table
         AND v15mvt.intersection_uid IS NULL
-	GROUP BY
+    GROUP BY
         im.intersection_uid,
         dt.datetime_bin,
         im.classification_uid,
         im.leg,
         im.movement_uid,
         un.accept
-	HAVING
+    HAVING
         --retain 0s for certain modes (padding)
         im.classification_uid IN (1,2,6,10)
         OR SUM(v.volume) > 0  
-	RETURNING intersection_uid, volume_15min_mvt_uid, datetime_bin, classification_uid, leg, movement_uid, volume
+    RETURNING intersection_uid, volume_15min_mvt_uid, datetime_bin, classification_uid, leg, movement_uid, volume
 )
 --To update foreign key for 1min bin table
 UPDATE miovision_api.volumes AS a
-	SET volume_15min_mvt_uid = b.volume_15min_mvt_uid
-	FROM aggregate_insert AS b
-	WHERE
+    SET volume_15min_mvt_uid = b.volume_15min_mvt_uid
+    FROM aggregate_insert AS b
+    WHERE
         a.datetime_bin >= start_date - interval '1 hour'
         AND a.datetime_bin < end_date - interval '1 hour'
         AND a.volume_15min_mvt_uid IS NULL
@@ -106,9 +106,9 @@ END;
 $BODY$;
 
 ALTER FUNCTION miovision_api.aggregate_15_min_mvt_single_intersection(date, date, int)
-    OWNER TO miovision_admins;
+OWNER TO miovision_admins;
 
-GRANT EXECUTE ON FUNCTION miovision_api.aggregate_15_min_mvt_single_intersection(date, date, int) TO PUBLIC;
+GRANT EXECUTE ON FUNCTION miovision_api.aggregate_15_min_mvt_single_intersection(date, date, int) TO public;
 
 GRANT EXECUTE ON FUNCTION miovision_api.aggregate_15_min_mvt_single_intersection(date, date, int) TO miovision_api_bot;
 
