289e24b0367d482db1075a398c1eeea1c50706d0
Nate Wessel <nate.wessel@toronto.ca>
2023-08-17 16:48:16 -0400

    update all flow counts (very inefficiently)

--- volumes/ecocounter/pull-data-from-api.py
+++ volumes/ecocounter/pull-data-from-api.py
@@ -1,6 +1,7 @@
 import requests, json
 from configparser import ConfigParser
 from psycopg2 import connect
+from datetime import datetime
 
 config = ConfigParser()
 config.read(r'/home/nwessel/db-creds.config')
@@ -38,24 +39,66 @@ def getChannelData(channel_id):
         f'{endpoint}/api/data/site/{channel_id}',
         headers={'Authorization': f'Bearer {token}'},
         params={
-            'begin': '2023-07-01T00:00:00',
+            'begin': '2010-01-01T00:00:00',
             'end':   '2023-07-02T00:00:00',
             'complete': 'false'
-            #'step'
         }
     )
     return response.json()
 
-for site in getSites():
-    # check that we know of the site
+def siteIsKnownToUs(site_id):
+    # do we have a record of this site in the database?
     with connection.cursor() as cursor:
         cursor.execute(
             "SELECT 1 FROM ecocounter.sites WHERE site_id = %s",
-            (site['id'],)
+            (site_id,)
         )
-        if cursor.rowcount < 1:
+        return cursor.rowcount > 0
+
+# do we have a record of this flow in the database?
+def flowIsKnownToUs(flow_id):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT 1 FROM ecocounter.flows WHERE flow_id = %s",
+            (flow_id,)
+        )
+        return cursor.rowcount > 0
+
+# DANGER delete all count data for a given flow
+def truncateFlow(flow_id):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "DELETE FROM ecocounter.counts WHERE flow_id = %s",
+            (flow_id,)
+        )
+
+def insertFlowCount(flow_id, datetime_bin, volume):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            INSERT INTO ecocounter.counts (flow_id, datetime_bin, volume)
+            VALUES ( %s, %s, %s );
+            """,
+            (flow_id, datetime_bin, volume)
+        )
+
+for site in getSites():
+    if not siteIsKnownToUs(site['id']):
+        continue
+
+    for channel in site['channels']:
+        if not flowIsKnownToUs(channel['id']):
             continue
-        for channel in site['channels']:
-            channel_id = channel['id']
-            print(channel_id)
-            print(getChannelData(channel_id))
+
+        # we have this site and channel in the database
+        channel_id = channel['id']
+        print(channel_id)
+        # empty the count table for this flow
+        truncateFlow(channel_id)
+        # and fill it back up!
+        counts = getChannelData(channel_id)
+        for count in counts:
+            volume = count['counts']
+            insertFlowCount(channel_id, count['date'], volume)
+            print(count['date'])
+        #raise SystemExit
