9c45d9d8107ba01d0b4cf5440d674747a732df8a
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-08-18 15:06:20 -0400

    #673 cleaner dedup to hour bins

--- volumes/miovision/sql/function-find_gaps.sql
+++ volumes/miovision/sql/function-find_gaps.sql
@@ -27,16 +27,15 @@ WITH fluffed_data AS (
             end_date::timestamp,
             interval '1 hour'
     ) AS bins(datetime_bin)
-    --LEFT JOIN miovision_api.volumes AS v USING (intersection_uid, datetime_bin)
-    --WHERE v.intersection_uid IS NULL --row does not exist in volumes
-
-    UNION 
+    
+    --group by in next step takes care of duplicates
+    UNION ALL
 
     SELECT 
         intersection_uid,
         datetime_bin,
         interval '1 minute' AS gap_adjustment --need to reduce gap by 1 minute for real data
-    FROM miovision_api.volumes AS v
+    FROM miovision_api.volumes
     WHERE
         datetime_bin >= start_date
         AND datetime_bin < end_date
@@ -47,36 +46,27 @@ bin_times AS (
         intersection_uid,
         datetime_bin AS gap_start,
         LEAD(datetime_bin, 1) OVER (PARTITION BY intersection_uid ORDER BY datetime_bin) AS gap_end,
-        --these variables are no longer needed to determine allowable_gap.    
-        --CASE
-        --    WHEN date_part('isodow', datetime_bin) <= 5 AND holiday.holiday IS NULL THEN False
-        --    ELSE True
-        --END as weekend,
-        --date_part('hour', datetime_bin) AS day_hour,
         LEAD(datetime_bin, 1) OVER (PARTITION BY intersection_uid ORDER BY datetime_bin)
-            - datetime_bin != interval '1 minute' AS bin_break,
+            - datetime_bin != interval '1 minute' AS bin_break, --True means gap between bins is larger than 1 minute
         LEAD(datetime_bin, 1) OVER (PARTITION BY intersection_uid ORDER BY datetime_bin) 
             - datetime_bin
-            - gap_adjustment
-        AS bin_gap,
-        gap_adjustment
+            - SUM(gap_adjustment)
+        AS bin_gap
     FROM fluffed_data
-    LEFT JOIN ref.holiday ON holiday.dt = datetime_bin::date
     GROUP BY
         intersection_uid, 
-        gap_start,
-        gap_adjustment
-), 
+        datetime_bin
+),
 
 gaps AS (
-	-- calculate the start and end times of gaps that are longer than 1 minutes
-	INSERT INTO miovision_api.unacceptable_gaps(intersection_uid, gap_start, gap_end, gap_minute, allowed_gap, accept)
+	-- calculate the start and end times of gaps that are longer than 15 minutes
+	--INSERT INTO miovision_api.unacceptable_gaps(intersection_uid, gap_start, gap_end, gap_minute, allowed_gap, accept)
 	-- Distinct on means multiple gaps during the hour won't be stored
-	SELECT DISTINCT ON (intersection_uid, date_part('hour', gap_start))
+	SELECT
 		intersection_uid,
-		gap_start,
-		gap_end,
-		(date_part('epoch', bin_gap) / 60::integer) AS gap_minute,
+		date_trunc('hour', gap_start) AS gap_start,
+		date_trunc('hour', gap_end) + interval '1 hour' AS gap_end,
+		SUM(date_part('epoch', bin_gap)) / 60::integer AS gap_minute,
 		15 AS allowed_gap,
 		False AS accept
 	FROM bin_times
@@ -85,9 +75,10 @@ gaps AS (
 		AND gap_end IS NOT NULL
 		--change from a dynamicly calculated gap to a standard one across all cases
 		AND bin_gap >= interval '15 minutes'
-	ORDER BY
-		intersection_uid,
-		date_part('hour', gap_start)
+    GROUP BY
+    	intersection_uid,
+		date_trunc('hour', gap_start),
+        date_trunc('hour', gap_end)
 	ON CONFLICT DO NOTHING
 	RETURNING *
 )
