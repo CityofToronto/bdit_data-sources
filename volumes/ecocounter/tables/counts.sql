dbb28029e79597cc7ca7368ef3dbd1ddef42da82
Nate Wessel <nate.wessel@toronto.ca>
2023-08-21 09:54:11 -0400

    add comment

--- volumes/ecocounter/tables/counts.sql
+++ volumes/ecocounter/tables/counts.sql
@@ -8,3 +8,6 @@ CREATE TABLE ecocounter.counts (
 CREATE INDEX ON ecocounter.counts (datetime_bin);
 
 GRANT SELECT ON ecocounter.counts TO bdit_humans;
+
+COMMENT ON COLUMN ecocounter.counts.datetime_bin
+IS 'indicates start time of the time bin. Note that not all time bins are the same size!';
