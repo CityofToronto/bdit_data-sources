906fb3e786b8ea2a0528b2b9a5cec7ef32d961ea
chmnata <chmnata@gmail.com>
2023-08-21 16:11:18 -0400

    #679 add new batch script to load layers and update README

--- here/gis/batch_upload.sh
+++ here/gis/batch_upload.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-rev=17_4
-for f in *.shp
+rev=23_1
+filename = 2EAM231E0N2E000AACU8.tar.gz
+tablename="$(echo ${f_lower%.*}_$rev)"
+
+for f in Adminbndy3 Streets Zlevels
 do 
 	f_lower="$(echo ${f,,})"
 	tablename="$(echo ${f_lower%.*}_$rev)"
-	shp2pgsql -D -I -d -s 4326 -S $f here_gis.$tablename | psql -h 10.160.12.47 -d bigdata 
+    ogr2ogr -f "PostgreSQL" PG:"host=10.160.8.132 dbname=bigdata user=your_username password=your_password" -nlt PROMOTE_TO_MULTI /vsitar/$filename/${f_lower}.shp -nln "here_gis.$tablename" -progress
 	psql -h 10.160.12.47 -d bigdata -c "ALTER TABLE here_gis.$tablename OWNER TO here_admins; SELECT here_gis.clip_to('$tablename', '$rev');"
 
 done
 
-dbfs=(Cdms.dbf CdmsDtmod.dbf Rdms.dbf)
-
-for f in ${dbfs[@]}
-do 
-	f_lower="$(echo ${f,,})"
-	tablename="$(echo ${f_lower%.*}_$rev)"
-	shp2pgsql -D -d -n $f here_gis.$tablename | psql -h 10.160.12.47 -d bigdata 
-	psql -h 10.160.12.47 -d bigdata -c "ALTER TABLE here_gis.$tablename OWNER TO here_admins;"
-done
-
 psql -h 10.160.12.47 -d bigdata -c "SELECT here_gis.split_streets_att('$rev');"
+
+
