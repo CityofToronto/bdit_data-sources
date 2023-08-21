1bc6a3327626312747e6da3fa76edd7fb83edb7b
Nate Wessel <nate.wessel@toronto.ca>
2023-08-21 09:51:45 -0400

    add a couple comments

--- volumes/ecocounter/tables/sites.sql
+++ volumes/ecocounter/tables/sites.sql
@@ -10,5 +10,11 @@ ALTER TABLE ecocounter.sites OWNER TO ecocounter_admins;
 
 GRANT SELECT ON ecocounter.sites TO bdit_humans;
 
+COMMENT ON COLUMN ecocounter.sites.site_id
+IS 'sit identifier used by ecocounter';
+
 COMMENT ON COLUMN ecocounter.sites.facility_description
 IS 'description of bike-specific infrastructure which the sensor is installed within';
+
+COMMENT ON COLUMN ecocounter.sites.replaced_by
+IS 'Several sites had their sensors replaced and show up now as "new" sites though we should treat the data as continuous with the replaced site.';
