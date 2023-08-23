d4e2398b12126ea5cc9242e0d8f56e032ccc658b
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-08-23 14:29:16 -0400

    #638 combine fkey triggers into one

--- volumes/vds/sql/trigger/create-trigger-vdsvehicledata_vdsconfig_fkey.sql
+++ volumes/vds/sql/trigger/create-trigger-vdsvehicledata_vdsconfig_fkey.sql
@@ -1,23 +1,39 @@
-CREATE OR REPLACE FUNCTION vds.update_vdsvehicledata_vdsconfig_fkey()
+CREATE OR REPLACE FUNCTION vds.update_vdsvehicledata_fkey()
 RETURNS TRIGGER AS $$
 BEGIN
+    RAISE NOTICE 'Trigger function update_vdsvehicledata_fkey is activating to add vdsconfig_uid.';
+    
     UPDATE vds.raw_vdsvehicledata AS d
     SET vdsconfig_uid = c.uid
     FROM vds.vdsconfig AS c
     WHERE
-        NEW.volume_uid = d.volume_uid    
-        AND NEW.vds_id = c.vds_id
-        AND NEW.division_id = c.division_id
-        AND NEW.dt >= c.start_timestamp
+        d.vdsconfig_uid IS NULL 
+        AND d.vds_id = c.vds_id
+        AND d.division_id = c.division_id
+        AND d.dt >= c.start_timestamp
         AND (
-            NEW.dt < c.end_timestamp
+            d.dt < c.end_timestamp
             OR c.end_timestamp IS NULL); --no end date
-  
-  RETURN NEW;
+
+    RAISE NOTICE 'Trigger function update_vdsvehicledata_fkey is activating to add entity_location_uid.';
+
+    UPDATE vds.raw_vdsvehicledata AS d
+    SET entity_location_uid = e.uid
+    FROM vds.entity_locations AS e
+    WHERE
+        d.entity_location_uid IS NULL    
+        AND d.vds_id = e.vds_id
+        AND d.division_id = e.division_id
+        AND d.dt >= e.start_timestamp
+        AND (
+            d.dt < e.end_timestamp
+            OR e.end_timestamp IS NULL); --no end date
+
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER update_vdsvehicledata_vdsconfig_fkey
-BEFORE INSERT ON vds.raw_vdsvehicledata
+CREATE TRIGGER update_vdsvehicledata_fkey_trigger
+AFTER INSERT ON vds.raw_vdsvehicledata
 FOR EACH STATEMENT
-EXECUTE FUNCTION vds.update_vdsvehicledata_vdsconfig_fkey();
+EXECUTE FUNCTION vds.update_vdsvehicledata_fkey();
