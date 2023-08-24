cbf7a21e8623cebc8ce6515714b41bcac76f668a
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-08-24 11:47:02 -0400

    #603 readme updates

--- volumes/vds/exploration/compare_rescu_vds.sql
+++ volumes/vds/exploration/compare_rescu_vds.sql
@@ -6,8 +6,8 @@ WITH vds_count AS (
     FROM vds.counts_15min AS v
     WHERE
         v.division_id = 2
---        AND v.datetime_15min >= '2022-01-01 00:00:00'
---        AND v.datetime_15min < '2023-01-01 00:00:00'
+        AND v.datetime_15min >= '2023-01-01 00:00:00'
+        --AND v.datetime_15min < '2023-01-01 00:00:00'
     GROUP BY v.datetime_15min::date
 )
 
@@ -22,7 +22,8 @@ RIGHT JOIN vds_count AS v ON
     r.datetime_bin::date = v.datetime_15min
 GROUP BY
     v.datetime_15min,
-    v.vds_count
+    v.vds_count,
+    v.vds_sum
 ORDER BY 
     v.datetime_15min
     
