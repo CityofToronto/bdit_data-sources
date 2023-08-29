fe85ad3a187e5f567cb756ac2402c0376b0fbf39
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-08-29 14:52:25 -0400

    #638 update backfill command with fkeys

--- volumes/vds/sql/insert/backfill_v15_from_rescu.sql
+++ volumes/vds/sql/insert/backfill_v15_from_rescu.sql
@@ -1,26 +1,35 @@
 --INSERT 0 19955760
 --Query returned successfully in 3 min 18 secs.
 
-INSERT INTO vds.counts_15min (detector_id, division_id, vds_id, num_lanes, datetime_15min, count_15min)
-
-SELECT DISTINCT ON (v15.detector_id, v15.datetime_bin)
-    v15.detector_id,
+INSERT INTO vds.counts_15min (division_id, vdsconfig_uid, entity_location_uid, num_lanes, datetime_15min,
+    count_15min)
+    
+SELECT
     c.division_id,
-    c.vds_id,
-    c.lanes,
+    c.uid AS vdsconfig_uid,
+    e.uid AS entity_location_uid,
+    c.lanes AS num_lanes,
     v15.datetime_bin,
-    v15.volume_15min
+    v15.volume_15min AS count_15min
 FROM rescu.volumes_15min AS v15
 INNER JOIN vds.vdsconfig AS c ON --don't want to include the truncated detector_id bc we can't be sure which they correspond to. 
     v15.detector_id = c.detector_id 
+    AND c.division_id = 2
     AND v15.datetime_bin >= c.start_timestamp
     AND (
         v15.datetime_bin < c.end_timestamp
         OR c.end_timestamp IS NULL
     )
-WHERE v15.datetime_bin <= '2021-10-31'
+INNER JOIN vds.entity_locations AS e ON
+    e.entity_id = c.vds_id 
+    AND e.division_id = 2
+    AND v15.datetime_bin >= e.start_timestamp
+    AND (
+        v15.datetime_bin < e.end_timestamp
+        OR e.end_timestamp IS NULL
+    )
+WHERE v15.datetime_bin < '2021-11-01'
 ORDER BY
     v15.detector_id ASC,
     v15.datetime_bin ASC,
     c.start_timestamp DESC
-    
