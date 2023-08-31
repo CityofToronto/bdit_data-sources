32d3c2fc0ab394c5daf87f4e62a2c8d12d720eee
Nate Wessel <nate.wessel@toronto.ca>
2023-08-31 15:23:25 -0400

    add table comments

--- volumes/ecocounter/tables/counts.sql
+++ volumes/ecocounter/tables/counts.sql
@@ -11,5 +11,10 @@ CREATE INDEX ON ecocounter.counts (datetime_bin);
 
 GRANT SELECT ON ecocounter.counts TO bdit_humans;
 
+ALTER TABLE ecocounter.counts OWNER TO ecocounter_admins;
+
+COMMENT ON TABLE ecocounter.counts
+IS 'Table contains the actual binned counts for ecocounter flows. Please note that bin size varies for older data, so averaging these numbers may not be straightforward.';
+
 COMMENT ON COLUMN ecocounter.counts.datetime_bin
 IS 'indicates start time of the time bin. Note that not all time bins are the same size!';
--- volumes/ecocounter/tables/flows.sql
+++ volumes/ecocounter/tables/flows.sql
@@ -8,6 +8,11 @@ CREATE TABLE ecocounter.flows (
 
 GRANT SELECT ON ecocounter.flows TO bdit_humans;
 
+ALTER TABLE ecocounter.flows OWNER TO ecocounter_admins;
+
+COMMENT ON TABLE ecocounter.flows
+IS 'A flow is usually a direction of travel associated with a sensor at an ecocounter installation site. For earlier sensors that did not detect directed flows, a flow may be both directions of travel together, i.e. just everyone who passed over the sensor any which way.';
+
 COMMENT ON COLUMN ecocounter.flows.bin_size
 IS 'temporal bins are either 15 or 30 minutes, depending on the sensor';
 
