0a4f9280d8d921203a9fe7eaf911ff7217fa52eb
Nate Wessel <nate.wessel@toronto.ca>
2023-09-07 14:43:15 -0400

    switch to time range

--- volumes/ecocounter/tables/anomalous_ranges.sql
+++ volumes/ecocounter/tables/anomalous_ranges.sql
@@ -2,11 +2,11 @@ CREATE TABLE ecocounter.anomalous_ranges (
     uid smallserial PRIMARY KEY,
     flow_id numeric REFERENCES ecocounter.flows (flow_id),
     site_id numeric REFERENCES ecocounter.sites (site_id),
-    date_range daterange NOT NULL,
+    time_range tsrange NOT NULL,
     notes text NOT NULL,
     investigation_level text NOT NULL REFERENCES miovision_api.anomaly_investigation_levels,
     problem_level text NOT NULL REFERENCES miovision_api.anomaly_problem_levels,
-    UNIQUE (flow_id, site_id, date_range, investigation_level, problem_level, notes),
+    UNIQUE (flow_id, site_id, time_range, investigation_level, problem_level, notes),
     CHECK (flow_id IS NOT NULL OR site_id IS NOT NULL)
 );
 
