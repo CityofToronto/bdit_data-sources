694d5166e28b8b1b5a0c6eaaffdce42e1ea31630
Sarah Cannon <scannon@ip-10-160-2-198.ec2.internal>
2023-09-07 20:58:54 +0000

    #652 dag alert revert

--- dags/traffic_transfer.py
+++ dags/traffic_transfer.py
@@ -38,28 +38,18 @@ for name in names:
 
 def task_fail_slack_alert(context):
     slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
-    slack_msg = """
-            :red_circle: Task Failed / Tâche échouée.
-            {slack_name} please check.
-            *Task*: {task}
-            *Dag*: {dag}
-            *Execution Time*: {exec_date}
-            *Log Url*: {log_url}
-            """.format(
-            slack_name=' '.join(list_names),
+    task_msg = 'The {task} in updating traffic failed, {slack_name} go fix it meow :meow_notlike: '.format(
             task=context.get('task_instance').task_id,
-            dag=context.get('task_instance').dag_id,
-            ti=context.get('task_instance'),
-            exec_date=context.get('execution_date'),
-            log_url=context.get('task_instance').log_url,
-        )
-
+            slack_name = ' '.join(list_names))
+        
+    slack_msg = task_msg + """(<{log_url}|log>)""".format(
+            log_url=context.get('task_instance').log_url,)
     failed_alert = SlackWebhookOperator(
         task_id='slack_test',
         http_conn_id='slack',
         webhook_token=slack_webhook_token,
         message=slack_msg,
-        username='airflow',
+        username='airflow'
         )
     return failed_alert.execute(context=context)
 
