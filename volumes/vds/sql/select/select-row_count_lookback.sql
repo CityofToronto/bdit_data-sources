d7681f9b61bc0ade0e9c10e00f09df88eaee4c1f
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-09-26 16:37:02 -0400

    #702 add generic sql with row count lookback

---
+++ volumes/vds/sql/select/select-row_count_lookback.sql
@@ -0,0 +1,27 @@
+--query to be used in `check_row_count` tasks to compare
+--row count to average over lookback period.
+
+WITH lookback AS (
+    SELECT
+        date_trunc('day', {{ params.dt_col }}),
+        COUNT(*) AS lookback_count
+    FROM {{ params.table }}
+    WHERE
+        {{ params.dt_col }} >= '{{ ds }} 00:00:00'::timestamp - interval '{{ params.lookback }}'
+        AND {{ params.dt_col }} < '{{ ds }} 00:00:00'::timestamp
+        AND division_id = {{ params.div_id }}::int
+    GROUP BY date_trunc('day', {{ params.dt_col }})
+)
+
+SELECT
+    COUNT(*) >= {{ params.threshold }}::numeric *
+        (SELECT AVG(lookback_count) FROM lookback) AS check, 
+    COUNT(*) AS ds_count,
+    (SELECT AVG(lookback_count) FROM lookback) AS lookback_avg,
+    {{ params.threshold }}::numeric *
+        (SELECT AVG(lookback_count) FROM lookback) AS passing_value
+FROM {{ params.table }}
+WHERE
+    {{ params.dt_col }} >= '{{ ds }} 00:00:00'::timestamp
+    AND {{ params.dt_col }} < '{{ ds }} 00:00:00'::timestamp + interval '1 day'
+    AND division_id = {{ params.div_id }}::int
