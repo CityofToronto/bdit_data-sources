20a157336c2682f54316e351c6824ba321d20df7
chmnata <chmnata@gmail.com>
2023-09-27 20:12:46 +0000

    #704 update slack alert in dag

--- dags/pull_here_path.py
+++ dags/pull_here_path.py
@@ -14,9 +14,12 @@ from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperato
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.models import Variable 
 
-repo_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-sys.path.insert(0, repo_path)
-from dags.dag_functions import task_fail_slack_alert
+try:
+    repo_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    sys.path.insert(0, repo_path)
+    from dags.dag_functions import task_fail_slack_alert
+except:
+    raise ImportError("Cannot import slack alert functions")
 
 dag_name = 'pull_here_path'
 
