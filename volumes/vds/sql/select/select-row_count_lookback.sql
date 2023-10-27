7b6b4350a2806dde9a98415facd2ca936fc65c98
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-10-27 16:49:18 -0400

    #730 rewrite sql for ultimate efficiency

--- volumes/vds/sql/select/select-row_count_lookback.sql
+++ volumes/vds/sql/select/select-row_count_lookback.sql
@@ -13,23 +13,27 @@ WITH lookback AS ( --noqa: L045
         AND division_id = {{ params.div_id }}::int
     --group by day then avg excludes missing days.
     GROUP BY _dt --noqa: L003
+),
+
+today AS (
+    SELECT
+        SUM({{ params.col_to_sum }}) AS today_count
+    FROM {{ params.table }}
+    WHERE
+        {{ params.dt_col }} >= '{{ ds }} 00:00:00'::timestamp - interval '{{ params.lookback }}'
+        AND {{ params.dt_col }} < '{{ ds }} 00:00:00'::timestamp
+        AND division_id = {{ params.div_id }}::int
 )
 
 SELECT
-    SUM({{ params.col_to_sum }}) >= FLOOR({{ params.threshold }}::numeric * lb.lookback_avg) AS check, 
-    'Daily count: ' || to_char(SUM({{ params.col_to_sum }}), 'FM9,999,999,999') AS ds_count,
+    a.today_count >= FLOOR({{ params.threshold }}::numeric * AVG(lb.lookback_count)) AS check, 
+    'Daily count: ' || to_char(a.today_count, 'FM9,999,999,999') AS ds_count,
     initcap('{{ params.lookback }}') || ' Lookback Avg: '
-        || to_char(lb.lookback_avg, 'FM9,999,999,999') AS lookback_avg,
+        || to_char(AVG(lb.lookback_count), 'FM9,999,999,999') AS lookback_avg,
     'Pass threshold: ' || to_char(
-            FLOOR({{ params.threshold }}::numeric * lb.lookback_avg),
+            FLOOR({{ params.threshold }}::numeric * AVG(lb.lookback_count)),
             'FM9,999,999,999'
             ) AS passing_value
-FROM {{ params.table }} AS a,
-    LATERAL (
-        SELECT AVG(lookback_count) lookback_avg FROM lookback
-    ) AS lb
-WHERE
-    a.{{ params.dt_col }} >= '{{ ds }} 00:00:00'::timestamp
-    AND a.{{ params.dt_col }} < '{{ ds }} 00:00:00'::timestamp + interval '1 day'
-    AND a.division_id = {{ params.div_id }}::int
-GROUP BY lb.lookback_avg
+FROM today AS a,
+    lookback AS lb
+GROUP BY a.today_count
