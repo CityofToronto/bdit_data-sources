6a34ee77665621f22816a2432b890e943df5fc6a
chmnata <chmnata@gmail.com>
2023-11-13 18:40:10 +0000

    #704 floff

--- here/traffic/sql/function_detach_partition.sql
+++ here/traffic/sql/function_detach_partition.sql
@@ -1,7 +1,7 @@
 -- Detach partitioned tables in here.ta 
 -- input yyyy text
 CREATE OR REPLACE FUNCTION here.detach_partition_ta(
-	yyyy text)
+    yyyy text)
     RETURNS void
     LANGUAGE 'plpgsql'
     COST 100
@@ -9,16 +9,16 @@ CREATE OR REPLACE FUNCTION here.detach_partition_ta(
 AS $BODY$
 
 DECLARE 
-	startdate DATE;
-	enddate DATE;
-	yyyymm TEXT;
-	basetablename TEXT := 'ta_';
-	tablename TEXT;
+    startdate DATE;
+    enddate DATE;
+    yyyymm TEXT;
+    basetablename TEXT := 'ta_';
+    tablename TEXT;
 
 BEGIN
     FOR mm IN 01..12 LOOP
         startdate:= to_date(yyyy||'-'||mm||'-01', 'YYYY-MM-DD');
-		enddate:= startdate + INTERVAL '1 month';
+        enddate:= startdate + INTERVAL '1 month';
         IF mm < 10 THEN
             yyyymm:= yyyy||'0'||mm;
         ELSE
