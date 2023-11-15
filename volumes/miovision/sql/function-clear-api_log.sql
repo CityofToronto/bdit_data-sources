92fd56653b3ae8ce18e04ddad283a3d9c7824b92
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-11-15 22:56:27 +0000

    #746 add clear-api_log func

---
+++ volumes/miovision/sql/function-clear-api_log.sql
@@ -0,0 +1,32 @@
+﻿CREATE OR REPLACE FUNCTION miovision_api.clear_api_log(
+    start_date date,
+    end_date date,
+    intersection integer default null)
+RETURNS VOID
+LANGUAGE plpgsql
+VOLATILE
+COST 100
+
+AS $BODY$
+BEGIN
+
+IF intersection IS NULL THEN
+    DELETE FROM miovision_api.api_log
+    WHERE
+        datetime_bin >= start_date
+        AND datetime_bin < end_date;
+ELSE 
+    DELETE FROM miovision_api.api_log
+    WHERE
+        intersection_uid = intersection
+        AND datetime_bin >= start_date
+        AND datetime_bin < end_date;
+END IF; 
+
+RETURN NULL;
+END;
+
+$BODY$;
+
+ALTER FUNCTION miovision_api.clear_api_log(date, date, integer) OWNER TO miovision_admins;
+GRANT EXECUTE ON FUNCTION miovision_api.clear_api_log(date, date, integer) TO miovision_api_bot;
