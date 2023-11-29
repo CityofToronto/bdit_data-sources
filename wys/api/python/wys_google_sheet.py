9b069e3c6885b2d3cf75f574c9c0282d669464da
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-11-29 18:18:22 +0000

    #781 change insert target for wys_google_sheets

--- wys/api/python/wys_google_sheet.py
+++ wys/api/python/wys_google_sheet.py
@@ -156,9 +156,9 @@ def pull_from_sheet(
                 comments=EXCLUDED.comments
             RETURNING new_sign_number, installation_date
         )
-        INSERT INTO {}.{} AS existing (ward_no, location, from_street, to_street, 
-                           direction, installation_date, removal_date, 
-                           new_sign_number, comments, confirmed, work_order) 
+        INSERT INTO wys.mobile_sign_installations AS existing (
+            ward_no, location, from_street, to_street, direction, installation_date,
+            removal_date, new_sign_number, comments, confirmed, work_order) 
         SELECT new_data.ward_no::INT, location, from_street, to_street, 
                 direction, installation_date, removal_date::DATE, 
                 new_sign_number, comments, confirmed, work_order
@@ -190,7 +190,7 @@ def pull_from_sheet(
                 existing.to_street!=EXCLUDED.to_street)
             OR (existing.work_order IS NULL OR 
                 existing.work_order!=EXCLUDED.work_order)
-        ''').format(sql.Identifier(schema_name), sql.Identifier(table_name)) 
+        ''')
     
     LOGGER.debug(rows)
 
