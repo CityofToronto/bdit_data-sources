55cb4b460623e2b21dc407ed47cd2a7731628818
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-11-29 19:09:57 +0000

    #781 adjust pkey/unique constraints of raw_data

--- wys/api/sql/create-table-raw_data.sql
+++ wys/api/sql/create-table-raw_data.sql
@@ -1,25 +1,22 @@
-﻿-- Table: wys.raw_data
+-- Table: wys.raw_data
 
 -- DROP TABLE wys.raw_data;
 
-CREATE TABLE wys.raw_data
-(
-  raw_data_uid bigserial NOT NULL,
-  api_id integer,
-  datetime_bin timestamp without time zone,
-  speed integer,
-  count integer,
-  speed_count_uid integer,
-  PRIMARY KEY (raw_data_uid),
-  UNIQUE(api_id, datetime_bin, speed)
-
+CREATE TABLE wys.raw_data (
+    raw_data_uid bigint NOT NULL DEFAULT nextval('wys_raw_data_raw_data_uid_seq'::regclass),
+    api_id integer NOT NULL,
+    datetime_bin timestamp without time zone NOT NULL,
+    speed integer NOT NULL,
+    count integer,
+    speed_count_uid integer,
+    CONSTRAINT wys_raw_data_pkey PRIMARY KEY (api_id, datetime_bin, speed)
 )
+PARTITION BY RANGE (datetime_bin)
 WITH (
-  OIDS=FALSE
+    OIDS=FALSE
 );
-ALTER TABLE wys.raw_data
-  OWNER TO wys_admins;
-GRANT SELECT, REFERENCES, TRIGGER ON TABLE wys.raw_data TO bdit_humans WITH GRANT OPTION;
--- Trigger: insert_raw_data_trigger on wys.raw_data
 
 -- DROP TRIGGER insert_raw_data_trigger ON wys.raw_data;
+
+ALTER TABLE wys.raw_data OWNER TO wys_admins;
+GRANT SELECT, REFERENCES ON TABLE wys.raw_data TO bdit_humans WITH GRANT OPTION;
