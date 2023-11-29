db78f8d415e7432962371ceb769a0393115fba6b
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-11-29 20:03:13 +0000

    #781 don't rename new wys check_* tasks

--- dags/pull_wys.py
+++ dags/pull_wys.py
@@ -102,8 +102,8 @@ def pull_wys_dag():
             autocommit=True
         )
 
-        check_jan_1st.override(task_id="check_annual_partition")() >> create_annual_partition
-        check_1st_of_month.override(task_id="check_month_partition")() >> create_month_partition
+        check_jan_1st() >> create_annual_partition
+        check_1st_of_month() >> create_month_partition
 
     @task(trigger_rule='none_failed')
     def pull_wys(ds=None):
