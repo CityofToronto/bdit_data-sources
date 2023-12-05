25a968688b306f8b93d36165c236e310ff6df64d
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-12-05 21:26:38 +0000

    #737 modify find_gaps data check sql into 2 sql funcs

--- dags/sql/create-function-generic_find_gaps.sql
+++ dags/sql/create-function-generic_find_gaps.sql
@@ -1,77 +1,102 @@
---a generic find_gaps function to find gaps in sensor data
---includes a lookback of `gap_threshold` so it catches gaps overlapping the start of the interval
---includes synthetic start/end time points for all sensors so we catch gaps that don't start or end in the interval
-    --doesn't catch sensors with no data in the interval. 
 --noqa: disable=TMP, PRS
-
-WITH raw AS (
-    SELECT DISTINCT
-        {{ params.id_col }} AS sensor_id_col,
-        {{ params.dt_col }} AS dt_col
-    FROM {{ params.table }}
-    WHERE
-        {{ params.dt_col }} >= '{{ ds }} 00:00:00'::timestamp - '{{ params.gap_threshold }}'::interval -- eg. >= 23:00
-        AND {{ params.dt_col }} < '{{ ds }} 00:00:00'::timestamp + '1 day'::interval
-),
-
---add a start and end dt for each ID which appears in raw data
-fluffed_data AS (
-    --synthetic data
-    SELECT DISTINCT
-        raw.sensor_id_col,
-        bins.dt_col,
-        --don't need to reduce gap width for synthetic data since that minute does not contain data
-        '0 minutes'::interval AS gap_adjustment
-    FROM raw, 
-        (VALUES 
-            ('{{ ds }} 00:00:00'::timestamp - '{{ params.gap_threshold }}'::interval),
-            ('{{ ds }} 00:00:00'::timestamp + '1 day'::interval)
-        ) AS bins(dt_col)
-    
-    UNION ALL
-    
-    SELECT
-        sensor_id_col,
-        dt_col,
-        --need to reduce gap length by 1 minute for real data since that minute contains data
-        '{{ params.default_bin }}'::interval AS gap_adjustment
-    FROM raw
-),
-
---looks at sequential bins to identify gaps
-bin_times AS (
-    SELECT 
-        sensor_id_col,
-        dt_col + sum(gap_adjustment) AS gap_start,
-        LEAD(dt_col, 1) OVER (PARTITION BY sensor_id_col ORDER BY dt_col) AS gap_end,
-        LEAD(dt_col, 1) OVER (PARTITION BY sensor_id_col ORDER BY dt_col)
-            - dt_col
-            --sum works because between 0 (synthetic) and 1 (real), we want 1 (implies real data)
-            - SUM(gap_adjustment)
-        AS bin_gap
-    FROM fluffed_data
-    GROUP BY
-        sensor_id_col, 
-        dt_col
-),
-
---summarize gaps by sensor.
-summarized_gaps AS (
-    SELECT
-        sensor_id_col,
-        gap_start,
-        gap_end
-    FROM bin_times
-    WHERE
-        gap_end IS NOT NULL --NULL gap_end occurs at the end of the search period
-        AND bin_gap >= '{{ params.gap_threshold }}'::interval
+CREATE OR REPLACE FUNCTION public.generic_find_gaps(
+    start_date date,
+    end_date date,
+    id_col text,
+    dt_col text,
+    sch_name text,
+    tbl_name text,
+    gap_threshold interval,
+    default_bin interval
 )
+RETURNS TABLE(sensor_id_col integer, gap_start timestamp, gap_end timestamp)
+LANGUAGE plpgsql
+COST 100
+VOLATILE 
 
-SELECT
-    NOT(COUNT(summarized_gaps.*) > 0) AS check,
-    CASE WHEN COUNT(summarized_gaps.*) = 1 THEN 'There was ' ELSE 'There were ' END ||
-        COALESCE(COUNT(summarized_gaps.*), 0) ||
-        CASE WHEN COUNT(summarized_gaps.*) = 1 THEN ' gap' ELSE ' gaps' END
-        || ' larger than {{ params.gap_threshold }}.' AS summ, 
-    array_agg(summarized_gaps || chr(10)) AS gaps
-FROM summarized_gaps
+AS $BODY$
+BEGIN
+    RETURN QUERY EXECUTE FORMAT($$
+        WITH raw AS (
+            SELECT DISTINCT
+                %I AS sensor_id_col, --id_col
+                %I AS dt_col --dt_col
+            FROM %I.%I --sch_name, tbl_name
+            WHERE
+                %I >= %L::date - %L::interval --dt_col, start_date, gap_threshold
+                AND %I < %L::date + '1 day'::interval --dt_col, end_date
+        ),
+
+        --add a start and end dt for each ID which appears in raw data
+        fluffed_data AS (
+            --synthetic data
+            SELECT DISTINCT
+                raw.sensor_id_col,
+                bins.dt_col,
+                --don't need to reduce gap width for synthetic data since that minute does not contain data
+                '0 minutes'::interval AS gap_adjustment
+            FROM raw, 
+                (VALUES 
+                    (%L::date - %L::interval), --start_date, gap_threshold
+                    (%L::date + '1 day'::interval) --end_date
+                ) AS bins(dt_col)
+
+            UNION ALL
+
+            SELECT
+                sensor_id_col,
+                dt_col,
+                --need to reduce gap length by 1 minute for real data since that minute contains data
+                %L::interval AS gap_adjustment --default_bin
+            FROM raw
+        ),
+
+        --looks at sequential bins to identify gaps
+        bin_times AS (
+            SELECT 
+                sensor_id_col,
+                dt_col + sum(gap_adjustment) AS gap_start,
+                LEAD(dt_col, 1) OVER (PARTITION BY sensor_id_col ORDER BY dt_col) AS gap_end,
+                LEAD(dt_col, 1) OVER (PARTITION BY sensor_id_col ORDER BY dt_col)
+                    - dt_col
+                    --sum works because between 0 (synthetic) and 1 (real), we want 1 (implies real data)
+                    - SUM(gap_adjustment)
+                AS bin_gap
+            FROM fluffed_data
+            GROUP BY
+                sensor_id_col, 
+                dt_col
+        )
+
+        --summarize gaps by sensor.
+        SELECT
+            sensor_id_col,
+            gap_start,
+            gap_end
+        FROM bin_times
+        WHERE
+            gap_end IS NOT NULL --NULL gap_end occurs at the end of the search period
+            AND bin_gap >= %L::interval --gap_threshold
+    $$,
+    id_col,
+    dt_col,
+    sch_name, tbl_name, 
+    dt_col, start_date, gap_threshold,
+    dt_col, end_date,
+    start_date, gap_threshold,
+    end_date,
+    default_bin,
+    gap_threshold);
+    
+END;
+$BODY$;
+
+ALTER FUNCTION public.generic_find_gaps OWNER TO gwolofs;
+GRANT EXECUTE ON FUNCTION public.generic_find_gaps TO bdit_humans;
+GRANT EXECUTE ON FUNCTION public.generic_find_gaps TO bdit_bots;
+
+COMMENT ON FUNCTION public.generic_find_gaps
+IS '''A generic find_gaps function to find gaps in sensor data.
+Includes a lookback of `gap_threshold` so it catches gaps overlapping the start of the interval.
+Includes synthetic start/end time points for all sensors so we catch gaps that don''t start or don''t end in the interval.
+Doesn''t catch sensors with no data in the interval.''';
