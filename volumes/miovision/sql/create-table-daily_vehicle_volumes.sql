164b492f7d0079942b7e5e3bc8c077c36a3979d7
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-12-12 21:29:36 +0000

    #780 update daily_vehicle_volumes to daily_volumes

--- volumes/miovision/sql/create-table-daily_vehicle_volumes.sql
+++ volumes/miovision/sql/create-table-daily_vehicle_volumes.sql
@@ -1,29 +1,36 @@
-CREATE TABLE miovision_api.daily_vehicle_volumes(
-    intersection_uid integer,
+CREATE TABLE miovision_api.daily_volumes(
     dt date,
-    vehicle_volume integer,
+    intersection_uid integer,
+    classification_uid integer,
+    daily_volume int,
     isodow smallint,
     holiday boolean,
-    unacceptable_gap_minutes smallint,
     datetime_bins_missing smallint,
-    CONSTRAINT daily_vehicle_volumes_pkey
-    PRIMARY KEY (intersection_uid, dt)
+    unacceptable_gap_minutes smallint,
+    avg_historical_gap_vol int
+    CONSTRAINT daily_volumes_pkey
+    PRIMARY KEY (intersection_uid, dt, classification_uid)
 ); 
 
-ALTER TABLE miovision_api.daily_vehicle_volumes OWNER TO miovision_admins;
-GRANT ALL ON TABLE miovision_api.daily_vehicle_volumes TO dbadmin;
-GRANT SELECT ON TABLE miovision_api.daily_vehicle_volumes TO bdit_humans;
-GRANT SELECT, INSERT, DELETE ON TABLE miovision_api.daily_vehicle_volumes TO miovision_api_bot;
+ALTER TABLE miovision_api.daily_volumes OWNER TO miovision_admins;
+GRANT SELECT ON TABLE miovision_api.daily_volumes TO bdit_humans;
+GRANT SELECT, INSERT, DELETE ON TABLE miovision_api.daily_volumes TO miovision_api_bot;
 
-COMMENT ON TABLE miovision_api.daily_vehicle_volumes
-IS '''Daily vehicle volumes. Excludes `anomalous_ranges` (use discouraged based on investigations)
+COMMENT ON TABLE miovision_api.daily_volumes
+IS '''Daily volumes by intersection_uid, classification_uid.
+Excludes `anomalous_ranges` (use discouraged based on investigations)
 but does not exclude time around `unacceptable_gaps` (zero volume periods).''';
 
-COMMENT ON COLUMN miovision_api.daily_vehicle_volumes.isodow
+COMMENT ON COLUMN miovision_api.daily_volumes.isodow
 IS 'Use `WHERE isodow <= 5 AND holiday is False` for non-holiday weekdays.';
 
-COMMENT ON COLUMN miovision_api.daily_vehicle_volumes.unacceptable_gap_minutes
-IS 'Periods of consecutive zero volumes deemed unacceptable based on avg intersection volume in that hour.';
+COMMENT ON COLUMN miovision_api.daily_volumes.unacceptable_gap_minutes
+IS 'Periods of consecutive zero volumes deemed unacceptable
+based on avg intersection volume in that hour.';
 
-COMMENT ON COLUMN miovision_api.daily_vehicle_volumes.datetime_bins_missing
+COMMENT ON COLUMN miovision_api.daily_volumes.datetime_bins_missing
 IS 'Minutes with zero vehicle volumes out of a total of possible 1440 minutes.';
+
+COMMENT ON COLUMN miovision_api.daily_volumes.avg_historical_gap_vol
+IS 'Avg historical volume for that classification and gap duration 
+based on averages from a 60 day lookback in that hour.';
--- volumes/miovision/sql/function-daily_vehicle_volumes.sql
+++ volumes/miovision/sql/function-daily_vehicle_volumes.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION miovision_api.agg_daily_vehicle_volumes(
+CREATE OR REPLACE FUNCTION miovision_api.agg_daily_volumes(
     start_date date,
     end_date date
 )
@@ -10,77 +10,89 @@ VOLATILE
 AS $BODY$
 BEGIN
 
-DELETE FROM miovision_api.daily_vehicle_volumes
-WHERE 
-    dt >= start_date
-    AND dt < end_date;
+    DELETE FROM miovision_api.daily_volumes
+    WHERE 
+        dt >= start_date
+        AND dt < end_date;
 
-WITH unacceptable_gaps_summarized AS (
-    SELECT
-        intersection_uid,
-        gap_start::date AS gap_date,
-        SUM(gap_minute) AS unacceptable_gap_minutes
-    FROM miovision_api.unacceptable_gaps
-    GROUP BY
-        intersection_uid,
-        gap_start::date
-)
-
-INSERT INTO miovision_api.daily_vehicle_volumes(
-    intersection_uid, dt, vehicle_volume, isodow, holiday, unacceptable_gap_minutes, datetime_bins_missing
-)
-SELECT
-    v.intersection_uid,
-    v.datetime_bin::date AS dt,
-    SUM(v.volume) AS vehicle_volume,
-    date_part('isodow', v.datetime_bin::date) AS isodow,
-    CASE 
-        WHEN hol.holiday IS NOT NULL THEN True
-        ELSE False
-    END AS holiday,
-    COALESCE(un.unacceptable_gap_minutes, 0) AS unacceptable_gap_minutes,
-    60 * 24 - COUNT(DISTINCT datetime_bin) AS datetime_bins_missing
---raw data
-FROM miovision_api.volumes AS v
-INNER JOIN miovision_api.classifications AS c USING (classification_uid)
-LEFT JOIN ref.holiday AS hol ON hol.dt = v.datetime_bin::date
---identify duration of unacceptable gaps (extended zero periods)
-LEFT JOIN unacceptable_gaps_summarized AS un ON
-    un.intersection_uid = v.intersection_uid
-    AND v.datetime_bin::date = un.gap_date
---anti join anomalous_ranges table
-LEFT JOIN miovision_api.anomalous_ranges AS ar ON
-    (
-        ar.intersection_uid = v.intersection_uid
-        OR ar.intersection_uid IS NULL
-    ) AND (
-        --only anomalous ranges for vehicles
-        ar.classification_uid = v.classification_uid
-        OR ar.classification_uid IS NULL
+    --identify duration of unacceptable gaps per day, intersection
+    WITH unacceptable_gaps_summarized AS (
+        SELECT
+            un.intersection_uid,
+            avg_vols.classification_uid,
+            un.gap_start::date AS gap_date,
+            SUM(un.gap_minutes_15min) AS unacceptable_gap_minutes,
+            SUM(un.gap_minutes_15min * avg_vols.avg_hour_vol)/60 AS avg_historical_gap_vol
+        FROM miovision_api.unacceptable_gaps AS un
+        --find average historical volume by classification during those gaps
+        LEFT JOIN miovision_api.gapsize_lookup AS avg_vols ON
+            avg_vols.intersection_uid = un.intersection_uid
+            AND avg_vols.hour_bin = date_part('hour', un.datetime_bin)
+            AND avg_vols.dt = un.dt
+        WHERE avg_vols.classification_uid IS NOT NULL
+        GROUP BY
+            un.intersection_uid,
+            un.gap_start::date,
+            avg_vols.classification_uid
     )
-    AND v.datetime_bin >= LOWER(ar.time_range)
-    AND v.datetime_bin < UPPER(ar.time_range)
-    AND ar.problem_level IN ('do-not-use', 'questionable')
-WHERE
-    ar.time_range IS NULL --anti join anomalous ranges
-    AND c.class_type = 'Vehicles' --all types
-    AND v.datetime_bin >= start_date
-    AND v.datetime_bin < end_date
-GROUP BY
-    v.intersection_uid,
-    v.datetime_bin::date,
-    hol.holiday,
-    un.unacceptable_gap_minutes
-HAVING SUM(v.volume) > 0
-ORDER BY
-    v.datetime_bin::date,
-    v.intersection_uid;
+
+    INSERT INTO miovision_api.daily_volumes (
+        intersection_uid, dt, classification_uid, daily_volume, isodow,
+        holiday, unacceptable_gap_minutes, datetime_bins_missing, avg_historical_gap_vol
+    )
+    SELECT
+        v.intersection_uid,
+        v.datetime_bin::date AS dt,
+        v.classification_uid,
+        SUM(v.volume) AS daily_volume,
+        date_part('isodow', v.datetime_bin::date) AS isodow,
+        hol.holiday IS NOT NULL AS holiday,
+        COALESCE(un.unacceptable_gap_minutes, 0) AS unacceptable_gap_minutes,
+        60 * 24 - COUNT(DISTINCT datetime_bin) AS datetime_bins_missing,
+        un.avg_historical_gap_vol::int
+    --raw data
+    FROM miovision_api.volumes AS v
+    LEFT JOIN ref.holiday AS hol ON hol.dt = v.datetime_bin::date
+    --identify duration of unacceptable gaps (extended zero periods)
+    --and avg historical volumes by classification for those gaps
+    LEFT JOIN unacceptable_gaps_summarized AS un ON
+        un.intersection_uid = v.intersection_uid
+        AND v.datetime_bin::date = un.gap_date
+        AND un.classification_uid = v.classification_uid
+    --anti join anomalous_ranges table
+    LEFT JOIN miovision_api.anomalous_ranges AS ar ON
+        (
+            ar.intersection_uid = v.intersection_uid
+            OR ar.intersection_uid IS NULL
+        ) AND (
+            ar.classification_uid = v.classification_uid
+            OR ar.classification_uid IS NULL
+        )
+        AND v.datetime_bin >= LOWER(ar.time_range)
+        AND v.datetime_bin < UPPER(ar.time_range)
+        AND ar.problem_level IN ('do-not-use', 'questionable')
+    WHERE
+        ar.time_range IS NULL --anti join anomalous ranges
+        AND v.datetime_bin >= start_date
+        AND v.datetime_bin < end_date
+    GROUP BY
+        v.intersection_uid,
+        v.classification_uid,
+        v.datetime_bin::date,
+        hol.holiday,
+        un.unacceptable_gap_minutes,
+        un.avg_historical_gap_vol
+    HAVING SUM(v.volume) > 0
+    ORDER BY
+        v.datetime_bin::date,
+        v.intersection_uid,
+        v.classification_uid;
 
 END;
 $BODY$;
 
-COMMENT ON FUNCTION miovision_api.agg_daily_vehicle_volumes IS
-'Function for inserting new daily vehicle volumes into miovision_api.daily_vehicle_volumes';
+COMMENT ON FUNCTION miovision_api.agg_daily_volumes IS
+'Function for inserting new daily vehicle volumes into miovision_api.daily_volumes';
 
-ALTER FUNCTION miovision_api.agg_daily_vehicle_volumes OWNER TO miovision_admins;
-GRANT EXECUTE ON FUNCTION miovision_api.agg_daily_vehicle_volumes TO miovision_api_bot;
+ALTER FUNCTION miovision_api.agg_daily_volumes OWNER TO miovision_admins;
+GRANT EXECUTE ON FUNCTION miovision_api.agg_daily_volumes TO miovision_api_bot;
