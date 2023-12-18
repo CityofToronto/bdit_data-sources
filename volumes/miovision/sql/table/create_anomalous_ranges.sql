8e3bb8289d05c671f788d8c9c50d0c64b0b9729f
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2023-12-18 21:48:16 +0000

    #630 update anomalous_ranges definition to match bigdata

--- volumes/miovision/sql/table/create_anomalous_ranges.sql
+++ volumes/miovision/sql/table/create_anomalous_ranges.sql
@@ -28,13 +28,64 @@ IS 'What is the nature of the problem indicated for the given subset of miovisio
 
 CREATE TABLE miovision_api.anomalous_ranges (
     uid serial PRIMARY KEY,
-    intersection_uid smallint REFERENCES miovision_api.intersections (intersection_uid),
-    classification_uid smallint REFERENCES miovision_api.classifications (classification_uid),
+    intersection_uid smallint,
+    classification_uid smallint,
     time_range tsrange NOT NULL,
     range_start timestamp NOT NULL,
     range_end timestamp,
     notes text NOT NULL,
-    investigation_level text NOT NULL REFERENCES miovision_api.anomaly_investigation_levels (uid),
-    problem_level text NOT NULL REFERENCES miovision_api.anomaly_problem_levels (uid),
-    UNIQUE (intersection_uid, classification_uid, time_range)
+    investigation_level text NOT NULL,
+    problem_level text NOT NULL,
+    CONSTRAINT miovision_qc_pkey PRIMARY KEY (uid),
+    CONSTRAINT miovision_qc_intersection_uid_classification_uid_time_range_key
+    UNIQUE (intersection_uid, classification_uid, range_start, range_end),
+    CONSTRAINT miovision_qc_classification_uid_fkey FOREIGN KEY (classification_uid)
+        REFERENCES miovision_api.classifications (classification_uid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT miovision_qc_intersection_uid_fkey FOREIGN KEY (intersection_uid)
+        REFERENCES miovision_api.intersections (intersection_uid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT miovision_qc_problem_level_fkey FOREIGN KEY (problem_level)
+        REFERENCES miovision_api.anomaly_problem_levels (uid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT miovision_qc_qa_level_fkey FOREIGN KEY (investigation_level)
+        REFERENCES miovision_api.anomaly_investigation_levels (uid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 );
+
+ALTER TABLE IF EXISTS miovision_api.anomalous_ranges
+OWNER TO miovision_admins;
+
+REVOKE ALL ON TABLE miovision_api.anomalous_ranges FROM bdit_humans;
+
+GRANT SELECT ON TABLE miovision_api.anomalous_ranges TO bdit_humans;
+
+GRANT ALL ON TABLE miovision_api.anomalous_ranges TO miovision_admins;
+
+GRANT ALL ON TABLE miovision_api.anomalous_ranges TO miovision_data_detectives;
+
+GRANT ALL ON TABLE miovision_api.anomalous_ranges TO nwessel;
+
+-- Trigger: audit_trigger_row
+
+-- DROP TRIGGER IF EXISTS audit_trigger_row ON miovision_api.anomalous_ranges;
+
+CREATE OR REPLACE TRIGGER audit_trigger_row
+    AFTER INSERT OR DELETE OR UPDATE 
+    ON miovision_api.anomalous_ranges
+    FOR EACH ROW
+    EXECUTE FUNCTION miovision_api.if_modified_func('true');
+
+-- Trigger: audit_trigger_stm
+
+-- DROP TRIGGER IF EXISTS audit_trigger_stm ON miovision_api.anomalous_ranges;
+
+CREATE OR REPLACE TRIGGER audit_trigger_stm
+    AFTER TRUNCATE
+    ON miovision_api.anomalous_ranges
+    FOR EACH STATEMENT
+    EXECUTE FUNCTION miovision_api.if_modified_func('true');
