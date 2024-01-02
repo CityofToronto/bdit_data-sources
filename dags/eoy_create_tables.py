3e4662e5a91c4b27a60672f0302e640c9bae99be
chmnata <chmnata@gmail.com>
2024-01-02 18:56:17 +0000

    #812 temp modify postgres conn

--- dags/eoy_create_tables.py
+++ dags/eoy_create_tables.py
@@ -25,6 +25,8 @@ from airflow.models import Variable
 from airflow.decorators import dag, task, task_group
 import holidays
 
+bt_bot = PostgresHook('bt_bot')
+
 try:
     repo_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
     sys.path.insert(0, repo_path)
@@ -34,7 +36,7 @@ except:
 
 try:
     sys.path.append('/data/airflow/data_scripts/bluetooth/sql/') 
-    from bt_eoy_create_tables import create_bt_obs_tables, replace_bt_trigger
+    from bt_eoy_create_tables import replace_bt_trigger
 except Exception as exc:
     err_msg = "Error importing functions for end of year Bluetooth maintenance \n" + str(exc)
     raise ImportError(err_msg)
@@ -55,48 +57,46 @@ default_args = {'owner': ','.join(DAG_OWNERS),
 
 @dag(dag_id=DAG_NAME,
      default_args=default_args,
-     schedule='5 9 1 12 *', #9:05 on December 1st of every year
+     schedule='@yearly', #9:05 on December 1st of every year
      tags=["partition_create"],
      doc_md=__doc__) 
 
 def eoy_create_table():
 
     def insert_holidays(dt):
-            next_year = datetime.strptime(dt, "%Y-%m-%d") + relativedelta(years=1)
-            holidays_year = holidays.CA(prov='ON', years=int(next_year.year))
-            ref_bot = PostgresHook('ref_bot')
-            with ref_bot.get_conn() as con, con.cursor() as cur:
-                for dt, name in holidays_year.items():
-                    name = name.replace('Observed', 'obs')
-                    cur.execute('INSERT INTO ref.holiday VALUES (%s, %s)', (dt, name))
-                
-    bt_create_tables = PythonOperator(task_id='bt_create_tables',
-                                            python_callable = create_bt_obs_tables,
-                                            dag = dag,
-                                            op_kwargs = {'pg_hook': bt_bot,
-                                                        'dt': '{{ ds }}'}
-                                            )
+        next_year = datetime.strptime(dt, "%Y-%m-%d") + relativedelta(years=1)
+        holidays_year = holidays.CA(prov='ON', years=int(next_year.year))
+        ref_bot = PostgresHook('ref_bot')
+        with ref_bot.get_conn() as con, con.cursor() as cur:
+            for dt, name in holidays_year.items():
+                name = name.replace('Observed', 'obs')
+                cur.execute('INSERT INTO ref.holiday VALUES (%s, %s)', (dt, name))
+
     bt_replace_trigger = PythonOperator(task_id='bt_replace_trigger',
-                                            python_callable = replace_bt_trigger,
-                                            dag = dag,
-                                            op_kwargs = {'pg_hook': bt_bot,
-                                                        'dt': '{{ ds }}'})
+                                        python_callable = replace_bt_trigger,
+                                        dag = dag,
+                                        op_kwargs = {'pg_hook': bt_bot,
+                                                    'dt': '{{ ds }}'})
 
     insert_holidays = PythonOperator(task_id='insert_holidays',
-                                        python_callable = insert_holidays,
-                                        dag = dag,
-                                        op_args = ['{{ ds }}'])
+                                    python_callable = insert_holidays,
+                                    dag = dag,
+                                    op_args = ['{{ ds }}'])
     
-    here_create_tables = PostgresOperator(
-                    task_id='here_create_tables',
-                    sql="SELECT here.create_yearly_tables('{{ macros.ds_format(ds, '%Y-%m-%d', '%Y') }}'::int)",
-                    postgres_conn_id='here_bot',
+    here_create_tables = PostgresOperator(task_id='here_create_tables',
+                                        sql="SELECT here.create_yearly_tables('{{ macros.ds_format(next_ds, '%Y-%m-%d', '%Y') }}')",
+                                        postgres_conn_id='here_bot',
+                                        autocommit=True)
+    
+    bt_create_tables = PostgresOperator(
+                    task_id='bluetooth_create_tables',
+                    sql="SELECT bluetooth.create_obs_tables('{{ macros.ds_format(next_ds, '%Y-%m-%d', '%Y') }}')",
+                    postgres_conn_id='bt_bot',
                     autocommit=True
                 )
-
     congestion_create_table = PostgresOperator(
                     task_id='congestion_create_table',
-                    sql="SELECT congestion.create_yearly_tables('{{ macros.ds_format(ds, '%Y-%m-%d', '%Y') }}'::int)",
+                    sql="SELECT congestion.create_yearly_tables('{{ macros.ds_format(next_ds, '%Y-%m-%d', '%Y') }}')",
                     postgres_conn_id='congestion_bot',
                     autocommit=True
                 )
