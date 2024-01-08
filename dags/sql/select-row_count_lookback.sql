962e6df0acb577b5e72e8a48617277989ec6504b
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2024-01-08 20:21:55 +0000

    #737 use is_weekend_or_holiday to simplify row_count_lookback

--- dags/sql/select-row_count_lookback.sql
+++ dags/sql/select-row_count_lookback.sql
@@ -4,60 +4,41 @@
 
 WITH lookback AS ( --noqa: L045
     SELECT
-        date_trunc('day', {{ params.dt_col }}) AS _dt, --noqa: L039
-        SUM({{ params.col_to_sum }}) AS lookback_count
+        {{ params.dt_col }}::date AS _dt, --noqa: L039
+        SUM({{ params.col_to_sum }}) AS count,
+		ref.is_weekend_or_holiday({{ params.dt_col }}::date) AS is_weekend_or_holiday
     FROM {{ params.table }}
     WHERE
         {{ params.dt_col }} >= '{{ ds }} 00:00:00'::timestamp - interval '{{ params.lookback }}'
-        AND {{ params.dt_col }} < '{{ ds }} 00:00:00'::timestamp
-    --group by day then avg excludes missing days.
-    GROUP BY _dt --noqa: L003
-),
-
-today AS (
-    SELECT
-        date_trunc('day', {{ params.dt_col }}) AS _dt, --noqa: L039
-        SUM({{ params.col_to_sum }}) AS today_count
-    FROM {{ params.table }}
-    WHERE
-        {{ params.dt_col }} >= '{{ ds }} 00:00:00'::timestamp
         AND {{ params.dt_col }} < '{{ ds }} 00:00:00'::timestamp + interval '1 day'
+    --group by day then avg excludes missing days.
     GROUP BY _dt --noqa: L003
 )
 
 SELECT
-    a.today_count >= FLOOR(thr.threshold * AVG(lb.lookback_count)) AS check, 
+    today.count >= FLOOR(thr.threshold * AVG(lb.count)) AS check, 
     'Daily count: ' || to_char(
-        a.today_count, 'FM9,999,999,999'
+        today.count, 'FM9,999,999,999'
     ) AS ds_count,
     initcap('{{ params.lookback }}') || ' Lookback Avg: ' || to_char(
-        AVG(lb.lookback_count), 'FM9,999,999,999'
+        AVG(lb.count), 'FM9,999,999,999'
     ) AS lookback_avg,
     'Pass threshold: ' || to_char(
-        FLOOR(thr.threshold * AVG(lb.lookback_count)),
+        FLOOR(thr.threshold * AVG(lb.count)),
         'FM9,999,999,999'
     ) AS passing_value
-FROM today AS a
-JOIN lookback AS lb ON
-    NOT (
-        date_part('isodow', a._dt) <= 5
-        AND (
-            SELECT hol.holiday FROM ref.holiday AS hol WHERE hol.dt = a._dt
-        ) IS NULL
-    )
-    = NOT (
-        date_part('isodow', lb._dt) <= 5
-        AND (
-            SELECT hol.holiday FROM ref.holiday AS hol WHERE hol.dt = lb._dt
-        ) IS NULL
-    ),
+FROM lookback AS today
+JOIN lookback AS lb USING (is_weekend_or_holiday),
     LATERAL(
         --change threshold if holiday is not null
-        SELECT CASE (SELECT hol.holiday FROM ref.holiday AS hol WHERE hol.dt = a._dt) IS NOT NULL
+        SELECT CASE (SELECT hol.holiday FROM ref.holiday AS hol WHERE hol.dt = today._dt) IS NOT NULL
             WHEN False THEN {{ params.threshold }}::numeric
             WHEN True THEN 0.5::numeric --50% of weekend volumes for holidays is acceptable
         END
     ) AS thr (threshold)
+WHERE
+    today._dt = '{{ ds }}'::date
+    AND lb._dt != '{{ ds }}'::date
 GROUP BY
-    a.today_count,
+    today.count,
     thr.threshold
