1a5156011be171aed44606fbcca05aa94823fd11
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-01-09 13:05:07 -0500

    737 pull miovision dag data pull checks (#738)
    * #737 basic changes to DAG from #717
    
    * #737 change location of data check sqls to dags/sql
    
    * #737 add data checks to miovision
    
    * #737 add distinct sensor dif list, change sensor_id_col to id_col
    
    * #737 adjust thresholds
    
    * #737 sql fluff (actually just noqa)
    
    * #737 get return value from sqlcheckoperator
    
    * #737 add check_gaps to pull_miovision, generic find_gaps query
    
    * #737 add a lookback and summary
    
    * #737 sql fluff
    
    * #737 add comments on new data check tasks
    
    * #737 readme update
    
    * #737 minor adjustments to find_gaps sql
    
    * #737 change checks to volumes_15min_mvt
    
    * #737 update readme to reflect data_checks
    
    * #737 add a try except to dag imports
    
    * #737 move on_failure_callback to default_args
    
    * #737 change get_return_value -> text
    
    * #737 test with no extra_msg
    
    * #737 replace partial on_failure_callback
    
    * #737 change threshold for testing
    
    * #737 set retries to zero for data_checks
    
    * #737 update SQLCheckOperatorWithReturnValue
    
    * #737 add key = "return_value"
    
    * add do_xcom_push=True
    
    * #737 add xcom_push
    
    * #737 change direction of find_gaps check + remove end_date
    
    * #737 adjust position of xcom_push
    
    * #737 revert some unnecessary changes
    
    * #737 revert threshold to 0.7
    
    * #737 change extra_msg to slack attachments
    
    * #737 rename find_gaps data check sql
    
    * #737 modify find_gaps data check sql into 2 sql funcs
    
    * #737 remove outdated reference in readme.
    
    * #737 change check_gaps task to new function: summarize_gaps_data_check
    
    * #737 add id_col_dtype anyelement
    
    * #737 explicitly name funciton parameters in summarize_gaps_data_check for readability
    
    * #737 move some data checks back to check_miovision
    
    * #737 add externaltask sensor/marker
    
    * #737 create new wys_check DAG with yellow card checks
    
    * #737 remove get_return_value, switch to using new extra_msg_from_task in SQLCheckOperatorWithReturnValue
    
    * #737 sqlfluff
    
    * #737 revert check_miovision and add new miovision_check
    
    * #737 adjust t_done of pull_miovision
    
    * #737 add tags and doc_md to wys + miovision DAGs
    
    * #737 update start_date in miovision_check
    
    * #737 update start_date in wys_check DAG
    
    * #737 add template_searchpath to miovision_check
    
    * #737 only collapse extra_msg_str if in the form of a list
    
    * #737 add weekday/weekend to row_count_lookback data check
    
    * #737 sqlfluff
    
    * #811 make sure monthly partitions created after yearly
    
    * #737 change location of `done` tasks to follow data pull
    
    * #737 50% threshold for row count if holiday.
    
    * #737 fix to task_fail_slack_alert attachment
    
    * #737 update data_check retries to 1
    
    * #737 pass psycopg errors to slack via xcom
    
    * #737 sqlfluff
    
    * #737 update retries to 0
    
    * #737 use is_weekend_or_holiday to simplify row_count_lookback
    
    * #737 remove today CTE from sensor_id lookback
    
    * #737 make else 'id_diff' statement more precise
    
    * #737 change 'etc' to 'data' in miovision sample miovision bash command

--- dags/sql/select-row_count_lookback.sql
+++ dags/sql/select-row_count_lookback.sql
@@ -4,34 +4,41 @@
 
 WITH lookback AS ( --noqa: L045
     SELECT
-        date_trunc('day', {{ params.dt_col }}) AS _dt, --noqa: L039
-        SUM({{ params.col_to_sum }}) AS lookback_count
+        {{ params.dt_col }}::date AS _dt, --noqa: L039
+        SUM({{ params.col_to_sum }}) AS count,
+		ref.is_weekend_or_holiday({{ params.dt_col }}::date) AS is_weekend_or_holiday
     FROM {{ params.table }}
     WHERE
         {{ params.dt_col }} >= '{{ ds }} 00:00:00'::timestamp - interval '{{ params.lookback }}'
-        AND {{ params.dt_col }} < '{{ ds }} 00:00:00'::timestamp
+        AND {{ params.dt_col }} < '{{ ds }} 00:00:00'::timestamp + interval '1 day'
     --group by day then avg excludes missing days.
     GROUP BY _dt --noqa: L003
-),
-
-today AS (
-    SELECT
-        SUM({{ params.col_to_sum }}) AS today_count
-    FROM {{ params.table }}
-    WHERE
-        {{ params.dt_col }} >= '{{ ds }} 00:00:00'::timestamp
-        AND {{ params.dt_col }} < '{{ ds }} 00:00:00'::timestamp + interval '1 day'
 )
 
 SELECT
-    a.today_count >= FLOOR({{ params.threshold }}::numeric * AVG(lb.lookback_count)) AS check, 
-    'Daily count: ' || to_char(a.today_count, 'FM9,999,999,999') AS ds_count,
-    initcap('{{ params.lookback }}') || ' Lookback Avg: '
-        || to_char(AVG(lb.lookback_count), 'FM9,999,999,999') AS lookback_avg,
+    today.count >= FLOOR(thr.threshold * AVG(lb.count)) AS check, 
+    'Daily count: ' || to_char(
+        today.count, 'FM9,999,999,999'
+    ) AS ds_count,
+    initcap('{{ params.lookback }}') || ' Lookback Avg: ' || to_char(
+        AVG(lb.count), 'FM9,999,999,999'
+    ) AS lookback_avg,
     'Pass threshold: ' || to_char(
-            FLOOR({{ params.threshold }}::numeric * AVG(lb.lookback_count)),
-            'FM9,999,999,999'
-            ) AS passing_value
-FROM today AS a,
-    lookback AS lb --noqa: L025
-GROUP BY a.today_count
+        FLOOR(thr.threshold * AVG(lb.count)),
+        'FM9,999,999,999'
+    ) AS passing_value
+FROM lookback AS today
+JOIN lookback AS lb USING (is_weekend_or_holiday),
+    LATERAL(
+        --change threshold if holiday is not null
+        SELECT CASE (SELECT hol.holiday FROM ref.holiday AS hol WHERE hol.dt = today._dt) IS NOT NULL
+            WHEN False THEN {{ params.threshold }}::numeric
+            WHEN True THEN 0.5::numeric --50% of weekend volumes for holidays is acceptable
+        END
+    ) AS thr (threshold)
+WHERE
+    today._dt = '{{ ds }}'::date
+    AND lb._dt != '{{ ds }}'::date
+GROUP BY
+    today.count,
+    thr.threshold
