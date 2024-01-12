27298508344f90c5090851dc5af525cfa8622f7e
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-01-12 16:26:56 -0500

    746 refactor miovision dag (#822)
    * #746 copy changes from 746-add-clear-miovision-task
    
    * #746 rename interesction (keyword) to target_intersection
    
    * #746 add notices of num rows deleted
    
    * #746 remove delete unacceptable_gaps (already in new function)
    
    * #746 integrate changes from 741, 744
    
    * #746 revert minor change
    
    * #746 update api_log to take intersection array param
    
    * #746 update clear_volumes to take intersection array param
    
    * #746 update clear_volumes_15min_mvt to take intersection array param
    
    * #746 pass time_period instead of start and end times to aggregate functions
    
    * #746 remove accidental test script from intersections_tmc
    
    * #746 port changes from #738 that won't get merged automatically (new file name)
    
    * #746 minor changes to intersections_tmc
    
    * #746 sqlfluff
    
    * #746 simplify conn + cursor statements
    
    * #746 fixes to DAG intersections param
    
    * #746 minor fixes to logging/comments
    
    * #746 change 'etc' to 'data' in miovision_pull api_config_path
    
    * #746 add html tags to identify dag readmes
    
    * #746 add DOC_MD, update start_date, add sql template_searchpath
    
    * #746 update data_check retries to 0
    
    * #746 add link to custom_operators.py
    
    * #746 task_group tooltips + dag doc_string
    
    * #746 temp doc_md fix
    
    * #746 missing s
    
    * #746 fix error in clear_volumes_15min_mvt
    
    * #746 change to raise info
    
    * #746 switch pull_miovision to use pull_data to resolve cli conflict
    
    * #746 fix bugs in insert_data, get_report_dates
    
    * #746 fix bugs with intersections param
    
    * #746 change miovision_check to listen for miovision_pull
    
    * #746 add get_intersection_uids function
    
    * #746 adapt volumes_15min function to take intersection array
    
    * #746 fix report_date functions to take intersections param
    
    * #746 fix errors in get_report_dates
    
    * #746 fix typo in clear report_dates
    
    * #746 report_dates for single/multiple intersections
    
    * #746 add insert_data uniqueviolation try except
    
    * #746 add sys.exits for failed psycopg executes, remove unnecessary dupes param
    
    * #746 fix removing unused path param in pull_data
    
    * #746 fix order of aggregations in run_api process_data

--- volumes/miovision/sql/function/function-api_log.sql
+++ volumes/miovision/sql/function/function-api_log.sql
@@ -1,21 +1,37 @@
 ﻿CREATE OR REPLACE FUNCTION miovision_api.api_log(
     start_date date,
-    end_date date)
-  RETURNS integer AS
-$BODY$
+    end_date date,
+    intersections integer [] DEFAULT ARRAY[]::integer []
+)
+RETURNS void
+LANGUAGE plpgsql
+
+COST 100
+VOLATILE
+AS $BODY$
+
+DECLARE
+    target_intersections integer [] = miovision_api.get_intersections_uids(intersections);
+    n_deleted integer;
+
 BEGIN
 
+    INSERT INTO miovision_api.api_log(intersection_uid, start_date, end_date, date_added)
+    SELECT
+        intersection_uid,
+        MIN(datetime_bin::date),
+        MAX(datetime_bin::date),
+        current_timestamp::date
+    FROM miovision_api.volumes
+    WHERE
+        intersection_uid = ANY(target_intersections)
+        AND datetime_bin >= start_date
+        AND datetime_bin < end_date
+    GROUP BY intersection_uid
+    ORDER BY intersection_uid;
 
-INSERT INTO miovision_api.api_log (intersection_uid, start_date, end_date, date_added)
-SELECT intersection_uid,  MIN(datetime_bin::date), MAX(datetime_bin::date), current_timestamp::date from miovision_api.volumes
-WHERE datetime_bin BETWEEN start_date AND end_date
-GROUP BY intersection_uid
-ORDER BY current_timestamp::date, intersection_uid;
-
-	RETURN NULL;
 END;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-ALTER FUNCTION miovision_api.api_log(date, date)
-  OWNER TO miovision_admins;
+$BODY$;
+
+ALTER FUNCTION miovision_api.api_log(date, date, integer []) OWNER TO miovision_admins;
+GRANT EXECUTE ON FUNCTION miovision_api.api_log(date, date, integer []) TO miovision_api_bot;
