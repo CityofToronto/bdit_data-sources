fbdb17e5895f0cd0cf418ff7fccbad1c8eb6d833
Nate Wessel <nate.wessel@toronto.ca>
2024-01-19 16:27:45 -0500

    remove flow_id reference, add table comment

--- volumes/ecocounter/tables/discontinuities.sql
+++ volumes/ecocounter/tables/discontinuities.sql
@@ -1,21 +1,18 @@
 CREATE TABLE ecocounter.discontinuities (
     uid serial PRIMARY KEY,
     -- a references to EITHER a site_id or flow_id
-    site_id numeric REFERENCES ecocounter.sites (site_id),
-    flow_id numeric REFERENCES ecocounter.flows (flow_id),
+    site_id numeric NOT NULL REFERENCES ecocounter.sites (site_id),
     -- moment the change takes place
     break timestamp NOT NULL,
     -- approximate bounds if the precise time is not known
     give_or_take interval,
     -- required description of what changed - be verbose!
     notes text NOT NULL
-    CHECK (
-        -- only one or the other specified but not both
-        (site_id IS NOT NULL OR flow_id IS NOT NULL)
-        AND NOT (site_id IS NOT NULL AND flow_id IS NOT NULL)
-    )
 );
 
 ALTER TABLE ecocounter.discontinuities OWNER TO ecocounter_admins;
 
 GRANT SELECT ON ecocounter.discontinuities TO bdit_humans;
+
+COMMENT ON TABLE ecocounter.discontinuities
+IS 'Moments in time when data collection methods changed in such a way that we would expect clear pre- and post-change paradigms that may not be intercomparable.';
