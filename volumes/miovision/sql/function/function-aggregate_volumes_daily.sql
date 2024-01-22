ac5b0ec839413a94870d9860a4fc24b01ee0539c
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2024-01-22 17:08:36 +0000

    #780 add raise notice statements to agg_volumes_daily

--- volumes/miovision/sql/function/function-aggregate_volumes_daily.sql
+++ volumes/miovision/sql/function/function-aggregate_volumes_daily.sql
@@ -1,20 +1,32 @@
 CREATE OR REPLACE FUNCTION miovision_api.aggregate_volumes_daily(
-    start_date date,
-    end_date date
+	start_date date,
+	end_date date
 )
 RETURNS void
 LANGUAGE 'plpgsql'
 
 COST 100
-VOLATILE 
+VOLATILE
 AS $BODY$
+
+DECLARE n_deleted int;
+DECLARE n_inserted int;
+
 BEGIN
+    
+    WITH deleted AS (
+        DELETE FROM miovision_api.volumes_daily
+        WHERE 
+            dt >= start_date
+            AND dt < end_date
+        RETURNING *
+    )
+    
+    SELECT COUNT(*) INTO n_deleted
+    FROM deleted;
 
-    DELETE FROM miovision_api.volumes_daily
-    WHERE 
-        dt >= start_date
-        AND dt < end_date;
-
+    RAISE NOTICE 'Deleted % rows from miovision_api.volumes_daily for % to %.', n_deleted, start_date, end_date;
+ 
     --identify duration of unacceptable gaps per day, intersection
     WITH unacceptable_gaps_summarized AS (
         SELECT
@@ -37,67 +49,78 @@ BEGIN
             un.intersection_uid,
             un.datetime_bin::date,
             avg_vols.classification_uid
-    )
-
-    INSERT INTO miovision_api.volumes_daily (
-        intersection_uid, dt, classification_uid, daily_volume, isodow,
-        holiday, unacceptable_gap_minutes, datetime_bins_missing, avg_historical_gap_vol
-    )
-    SELECT
-        v.intersection_uid,
-        v.datetime_bin::date AS dt,
-        v.classification_uid,
-        SUM(v.volume) AS daily_volume,
-        date_part('isodow', v.datetime_bin::date) AS isodow,
-        hol.holiday IS NOT NULL AS holiday,
-        COALESCE(un.unacceptable_gap_minutes, 0) AS unacceptable_gap_minutes,
-        60 * 24 - COUNT(DISTINCT datetime_bin) AS datetime_bins_missing,
-        un.avg_historical_gap_vol::int
-    --raw data
-    FROM miovision_api.volumes AS v
-    LEFT JOIN ref.holiday AS hol ON hol.dt = v.datetime_bin::date
-    --identify duration of unacceptable gaps (extended zero periods)
-    --and avg historical volumes by classification for those gaps
-    LEFT JOIN unacceptable_gaps_summarized AS un ON
-        un.intersection_uid = v.intersection_uid
-        AND v.datetime_bin::date = un.gap_date
-        AND un.classification_uid = v.classification_uid
-    --anti join anomalous_ranges table
-    LEFT JOIN miovision_api.anomalous_ranges AS ar ON
-        (
-            ar.intersection_uid = v.intersection_uid
-            OR ar.intersection_uid IS NULL
-        ) AND (
-            ar.classification_uid = v.classification_uid
-            OR ar.classification_uid IS NULL
+    ),
+    
+    inserted AS (
+        INSERT INTO miovision_api.volumes_daily (
+            intersection_uid, dt, classification_uid, daily_volume, isodow,
+            holiday, unacceptable_gap_minutes, datetime_bins_missing, avg_historical_gap_vol
         )
-        AND v.datetime_bin >= LOWER(ar.time_range)
-        AND v.datetime_bin < UPPER(ar.time_range)
-        AND ar.problem_level IN ('do-not-use', 'questionable')
-    WHERE
-        ar.time_range IS NULL --anti join anomalous ranges
-        AND v.datetime_bin >= start_date
-        AND v.datetime_bin < end_date
-    GROUP BY
-        v.intersection_uid,
-        v.classification_uid,
-        v.datetime_bin::date,
-        hol.holiday,
-        un.unacceptable_gap_minutes,
-        un.avg_historical_gap_vol
-    HAVING SUM(v.volume) > 0
-    ORDER BY
-        v.datetime_bin::date,
-        v.intersection_uid,
-        v.classification_uid;
+        SELECT
+            v.intersection_uid,
+            v.datetime_bin::date AS dt,
+            v.classification_uid,
+            SUM(v.volume) AS daily_volume,
+            date_part('isodow', v.datetime_bin::date) AS isodow,
+            hol.holiday IS NOT NULL AS holiday,
+            COALESCE(un.unacceptable_gap_minutes, 0) AS unacceptable_gap_minutes,
+            60 * 24 - COUNT(DISTINCT datetime_bin) AS datetime_bins_missing,
+            un.avg_historical_gap_vol::int
+        --raw data
+        FROM miovision_api.volumes AS v
+        LEFT JOIN ref.holiday AS hol ON hol.dt = v.datetime_bin::date
+        --identify duration of unacceptable gaps (extended zero periods)
+        --and avg historical volumes by classification for those gaps
+        LEFT JOIN unacceptable_gaps_summarized AS un ON
+            un.intersection_uid = v.intersection_uid
+            AND v.datetime_bin::date = un.gap_date
+            AND un.classification_uid = v.classification_uid
+        --anti join anomalous_ranges table
+        LEFT JOIN miovision_api.anomalous_ranges AS ar ON
+            (
+                ar.intersection_uid = v.intersection_uid
+                OR ar.intersection_uid IS NULL
+            ) AND (
+                ar.classification_uid = v.classification_uid
+                OR ar.classification_uid IS NULL
+            )
+            AND v.datetime_bin >= LOWER(ar.time_range)
+            AND v.datetime_bin < UPPER(ar.time_range)
+            AND ar.problem_level IN ('do-not-use', 'questionable')
+        WHERE
+            ar.time_range IS NULL --anti join anomalous ranges
+            AND v.datetime_bin >= start_date
+            AND v.datetime_bin < end_date
+        GROUP BY
+            v.intersection_uid,
+            v.classification_uid,
+            v.datetime_bin::date,
+            hol.holiday,
+            un.unacceptable_gap_minutes,
+            un.avg_historical_gap_vol
+        HAVING SUM(v.volume) > 0
+        ORDER BY
+            v.datetime_bin::date,
+            v.intersection_uid,
+            v.classification_uid
+        RETURNING *
+    )
+    
+    SELECT COUNT(*) INTO n_inserted
+    FROM inserted;
+
+    RAISE NOTICE 'Inserted % rows into miovision_api.volumes_daily for % to %.', n_inserted, start_date, end_date;
 
 END;
 $BODY$;
 
-COMMENT ON FUNCTION miovision_api.aggregate_volumes_daily
+COMMENT ON FUNCTION miovision_api.aggregate_volumes_daily(date, date)
 IS 'Function for inserting daily volumes into miovision_api.volumes_daily';
 
-ALTER FUNCTION miovision_api.aggregate_volumes_daily
+ALTER FUNCTION miovision_api.aggregate_volumes_daily(date, date)
 OWNER TO miovision_admins;
 
-GRANT EXECUTE ON FUNCTION miovision_api.aggregate_volumes_daily TO miovision_api_bot;
+GRANT EXECUTE ON FUNCTION miovision_api.aggregate_volumes_daily(date, date)
+TO miovision_api_bot;
+
+REVOKE ALL ON FUNCTION miovision_api.aggregate_volumes_daily(date, date) FROM PUBLIC;
