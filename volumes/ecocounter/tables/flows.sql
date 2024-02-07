5c0630a0c512adb0f90cac2c11b6e5c24ca6bf10
Nate Wessel <nate.wessel@toronto.ca>
2024-02-07 14:16:09 -0500

    add symetric field for back-reference

--- volumes/ecocounter/tables/flows.sql
+++ volumes/ecocounter/tables/flows.sql
@@ -3,8 +3,9 @@ CREATE TABLE ecocounter.flows (
     site_id numeric NOT NULL REFERENCES ecocounter.sites (site_id),
     flow_direction text NOT NULL,
     flow_geom geometry(LINESTRING,4326) CHECK(flow_geom IS NULL OR ST_NPoints(flow_geom) = 2),
-    bin_size interval NOT NULL
-    replaced_by_flow_id numeric REFERENCES ecocounter.flows (flow_id)
+    bin_size interval NOT NULL,
+    replaced_by_flow_id numeric REFERENCES ecocounter.flows (flow_id),
+    replaces_flow_id numeric REFERENCES ecocounter.flows (flow_id)
 );
 
 GRANT SELECT ON ecocounter.flows TO bdit_humans;
