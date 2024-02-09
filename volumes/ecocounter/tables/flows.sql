b237d8c259806cdd9f0805103169a6ad12896ba1
Nate Wessel <nate.wessel@toronto.ca>
2024-02-09 08:46:58 -0500

    add `directed` field

--- volumes/ecocounter/tables/flows.sql
+++ volumes/ecocounter/tables/flows.sql
@@ -5,7 +5,8 @@ CREATE TABLE ecocounter.flows (
     flow_geom geometry(LINESTRING,4326) CHECK(flow_geom IS NULL OR ST_NPoints(flow_geom) = 2),
     bin_size interval NOT NULL,
     replaced_by_flow_id numeric REFERENCES ecocounter.flows (flow_id),
-    replaces_flow_id numeric REFERENCES ecocounter.flows (flow_id)
+    replaces_flow_id numeric REFERENCES ecocounter.flows (flow_id),
+    directed boolean
 );
 
 GRANT SELECT ON ecocounter.flows TO bdit_humans;
@@ -20,3 +21,6 @@ IS 'temporal bins are either 15 or 30 minutes, depending on the sensor';
 
 COMMENT ON COLUMN ecocounter.flows.flow_geom
 IS 'a two-node line, where the first node indicates the position of the sensor and the second indicates the normal direction of travel over that sensor relative to the first node. I.e. the line segment is an arrow pointing in the direction of travel.';
+
+COMMENT ON COLUMN ecocounter.flows.directed
+IS 'indicates if the flow is strictly one-directional, ie. has a double sensor that separates the two directions of travel. Undirected flows will still have a primary direction of travel because of their location but would also count contraflow cyclists.';
