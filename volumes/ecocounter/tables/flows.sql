ffdc7c12d8160ea2cad12335889dc360a8817610
Nate Wessel <nate.wessel@toronto.ca>
2024-02-13 13:56:49 -0500

    fluff

--- volumes/ecocounter/tables/flows.sql
+++ volumes/ecocounter/tables/flows.sql
@@ -2,7 +2,7 @@ CREATE TABLE ecocounter.flows (
     flow_id numeric PRIMARY KEY,
     site_id numeric NOT NULL REFERENCES ecocounter.sites (site_id),
     flow_direction text NOT NULL,
-    flow_geom geometry(LINESTRING,4326) CHECK(flow_geom IS NULL OR ST_NPoints(flow_geom) = 2),
+    flow_geom GEOMETRY (LINESTRING, 4326) CHECK (flow_geom IS NULL OR ST_NPoints(flow_geom) = 2),
     bin_size interval NOT NULL,
     notes text,
     replaced_by_flow_id numeric REFERENCES ecocounter.flows (flow_id),
@@ -15,7 +15,13 @@ GRANT SELECT ON ecocounter.flows TO bdit_humans;
 ALTER TABLE ecocounter.flows OWNER TO ecocounter_admins;
 
 COMMENT ON TABLE ecocounter.flows
-IS 'A flow is usually a direction of travel associated with a sensor at an ecocounter installation site. For earlier sensors that did not detect directed flows, a flow may be both directions of travel together, i.e. just everyone who passed over the sensor any which way.';
+IS 'A flow is usually a direction of travel
+associated with a sensor at an ecocounter
+installation site. For earlier sensors that
+did not detect directed flows, a flow may be
+both directions of travel together,
+i.e. just everyone who passed over the sensor
+any which way.';
 
 COMMENT ON COLUMN ecocounter.flows.bin_size
 IS 'temporal bins are either 15, 30, or 60 minutes, depending on the sensor';
