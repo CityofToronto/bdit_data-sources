df0af179ef1d0c237112856c31dea5e10f227cff
Nate Wessel <nate.wessel@toronto.ca>
2024-02-13 13:53:25 -0500

    change name and description of contraflow field

--- volumes/ecocounter/tables/flows.sql
+++ volumes/ecocounter/tables/flows.sql
@@ -7,7 +7,7 @@ CREATE TABLE ecocounter.flows (
     notes text,
     replaced_by_flow_id numeric REFERENCES ecocounter.flows (flow_id),
     replaces_flow_id numeric REFERENCES ecocounter.flows (flow_id),
-    directed boolean
+    includes_contraflow boolean
 );
 
 GRANT SELECT ON ecocounter.flows TO bdit_humans;
@@ -18,10 +18,20 @@ COMMENT ON TABLE ecocounter.flows
 IS 'A flow is usually a direction of travel associated with a sensor at an ecocounter installation site. For earlier sensors that did not detect directed flows, a flow may be both directions of travel together, i.e. just everyone who passed over the sensor any which way.';
 
 COMMENT ON COLUMN ecocounter.flows.bin_size
-IS 'temporal bins are either 15 or 30 minutes, depending on the sensor';
+IS 'temporal bins are either 15, 30, or 60 minutes, depending on the sensor';
 
 COMMENT ON COLUMN ecocounter.flows.flow_geom
-IS 'a two-node line, where the first node indicates the position of the sensor and the second indicates the normal direction of travel over that sensor relative to the first node. I.e. the line segment is an arrow pointing in the direction of travel.';
+IS 'A two-node line, where the first node 
+indicates the position of the sensor and
+the second indicates the normal direction
+of travel over that sensor relative to the
+first node. I.e. the line segment is an
+arrow pointing in the direction of travel.';
 
-COMMENT ON COLUMN ecocounter.flows.directed
-IS 'indicates if the flow is strictly one-directional, ie. has a double sensor that separates the two directions of travel. Undirected flows will still have a primary direction of travel because of their location but would also count contraflow cyclists.';
+COMMENT ON COLUMN ecocounter.flows.includes_contraflow
+IS 'Does the flow also count travel in the reverse of
+the indicated flow direction?
+TRUE indicates that the flow, though installed
+in one-way infrastucture like a standard bike lane,
+also counts travel going the wrong direction within
+that lane.';
