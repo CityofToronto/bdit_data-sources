e06b862418062fc576d80c12eb4d89cbfa44a03d
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-03-13 17:55:34 +0000

    #894 create counts and collisions DAGs from variable

--- dags/counts_replicator.py
+++ dags/counts_replicator.py
@@ -24,66 +24,70 @@ sys.path.insert(0, repo_path)
 from dags.dag_functions import task_fail_slack_alert, send_slack_msg
 # pylint: enable=import-error
 
-DAG_NAME = "counts_replicator"
-DAG_OWNERS = Variable.get("dag_owners", deserialize_json=True).get(DAG_NAME, ["Unknown"])
+REPLICATORS = Variable.get('replicators', deserialize_json=True)
 
-default_args = {
-    "owner": ",".join(DAG_OWNERS),
-    "depends_on_past": False,
-    "start_date": pendulum.datetime(2023, 10, 31, tz="America/Toronto"),
-    "email_on_failure": False,
-    "retries": 3,
-    "retry_delay": timedelta(minutes=60),
-    "on_failure_callback": task_fail_slack_alert,
-}
+for replicator in REPLICATORS:
 
-@dag(
-    dag_id=DAG_NAME,
-    default_args=default_args,
-    catchup=False,
-    max_active_runs=5,
-    max_active_tasks=5,
-    schedule=None,
-    doc_md=__doc__,
-    tags=["counts", "replicator"]
-)
-def counts_replicator():
-    """The main function of the counts DAG."""
-    from dags.common_tasks import (
-        wait_for_external_trigger, get_variable, copy_table
+    DAG_NAME = REPLICATORS[replicator]['dag_name']
+    DAG_OWNERS = Variable.get("dag_owners", deserialize_json=True).get(DAG_NAME, ["Unknown"])
+
+    default_args = {
+        "owner": ",".join(DAG_OWNERS),
+        "depends_on_past": False,
+        "start_date": pendulum.datetime(2023, 10, 31, tz="America/Toronto"),
+        "email_on_failure": False,
+        "retries": 3,
+        "retry_delay": timedelta(minutes=60),
+        "on_failure_callback": task_fail_slack_alert,
+    }
+
+    @dag(
+        dag_id=DAG_NAME,
+        default_args=default_args,
+        catchup=False,
+        max_active_runs=5,
+        max_active_tasks=5,
+        schedule=None,
+        doc_md=__doc__,
+        tags=[replicator, "replicator"]
     )
+    def replicator_DAG():
+        f"""The main function of the {replicator} DAG."""
+        from dags.common_tasks import (
+            wait_for_external_trigger, get_variable, copy_table
+        )
 
-    # Returns a list of source and destination tables stored in the given
-    # Airflow variable.
-    tables = get_variable.override(task_id="get_list_of_tables")("counts_tables")
+        # Returns a list of source and destination tables stored in the given
+        # Airflow variable.
+        tables = get_variable.override(task_id="get_list_of_tables")(REPLICATORS[replicator]['tables'])
 
-    # Copies tables
-    copy_tables = copy_table.override(task_id="copy_tables", on_failure_callback = None).partial(conn_id="traffic_bot").expand(table=tables)
+        # Copies tables
+        copy_tables = copy_table.override(task_id="copy_tables", on_failure_callback = None).partial(conn_id=REPLICATORS[replicator]['conn']).expand(table=tables)
 
-    @task(
-        retries=0,
-        trigger_rule='all_done',
-        doc_md="""A status message to report DAG success OR any failures from the `copy_tables` task."""
-    )
-    def status_message(tables, **context):
-        ti = context["ti"]
-        failures = []
-        #iterate through mapped tasks to find any failure messages
-        for m_i in range(0, len(tables)):
-            failure_msg = ti.xcom_pull(key="extra_msg", task_ids="copy_tables", map_indexes=m_i)
-            if failure_msg is not None:
-                failures.append(failure_msg)
-        if failures == []:
-            send_slack_msg(
-                context=context,
-                msg=f"{DAG_NAME} DAG succeeded :white_check_mark:"
-            )
-        else: #add details of failures to task_fail_slack_alert
-            failure_extra_msg = ['One or more tables failed to copy:', failures]
-            context.get("task_instance").xcom_push(key="extra_msg", value=failure_extra_msg)
-            raise AirflowFailException('One or more tables failed to copy.')
-        
-    # Waits for an external trigger
-    wait_for_external_trigger() >> tables >> copy_tables >> status_message(tables=tables)    
+        @task(
+            retries=0,
+            trigger_rule='all_done',
+            doc_md="""A status message to report DAG success OR any failures from the `copy_tables` task."""
+        )
+        def status_message(tables, **context):
+            ti = context["ti"]
+            failures = []
+            #iterate through mapped tasks to find any failure messages
+            for m_i in range(0, len(tables)):
+                failure_msg = ti.xcom_pull(key="extra_msg", task_ids="copy_tables", map_indexes=m_i)
+                if failure_msg is not None:
+                    failures.append(failure_msg)
+            if failures == []:
+                send_slack_msg(
+                    context=context,
+                    msg=f"{DAG_NAME} DAG succeeded :white_check_mark:"
+                )
+            else: #add details of failures to task_fail_slack_alert
+                failure_extra_msg = ['One or more tables failed to copy:', failures]
+                context.get("task_instance").xcom_push(key="extra_msg", value=failure_extra_msg)
+                raise AirflowFailException('One or more tables failed to copy.')
+            
+        # Waits for an external trigger
+        wait_for_external_trigger() >> tables >> copy_tables >> status_message(tables=tables)    
 
-counts_replicator()
+    replicator_DAG()
