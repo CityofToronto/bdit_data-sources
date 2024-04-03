f41abf8ec941cfb89d2979c3d0e9a40778358baa
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-04-03 21:34:38 +0000

    #912 get run_api working

--- volumes/ecocounter/pull_data_from_api.py
+++ volumes/ecocounter/pull_data_from_api.py
@@ -1,10 +1,11 @@
 import requests
 from configparser import ConfigParser
 from psycopg2 import connect
+from psycopg2.extras import execute_values
 from datetime import datetime, timedelta
 #from tqdm import tqdm # this is a progress bar created by simply wrapping an iterable
 
-default_start = datetime.fromisoformat('2015-01-01')
+default_start = datetime.now().replace(hour = 0, minute = 0, second = 0, microsecond = 0)-timedelta(days=1)
 default_end = datetime.now().replace(hour = 0, minute = 0, second = 0, microsecond = 0)
 
 url = 'https://apieco.eco-counter-tools.com'
@@ -16,12 +17,12 @@ def getToken(api_config_path):
     response = requests.post(
         f'{url}/token',
         headers={
-            'Authorization': 'Basic ' + config['DEFAULT']['secret_api_hash']
+            'Authorization': 'Basic ' + config['API']['secret_api_hash']
         },
         data={
             'grant_type': 'password',
-            'username': config['DEFAULT']['username'],
-            'password': config['DEFAULT']['password']
+            'username': config['API']['username'],
+            'password': config['API']['password']
         }
     )
     return response.json()['access_token']
@@ -42,11 +43,11 @@ def getSites(token, sites=()):
         return response.json()
 
 # get all of a channel/flow's data from the API
-def getChannelData(token, channel_id, startDate, endDate):
+def getChannelData(token, channel_id: int, startDate: datetime, endDate: datetime):
     requestChunkSize = timedelta(days=100)
     requestStartDate = startDate
     data = []
-    while requestStartDate.timestamp() < endDate:
+    while requestStartDate < endDate:
         response = requests.get(
             f'{url}/api/data/site/{channel_id}',
             headers={'Authorization': f'Bearer {token}'},
@@ -63,7 +64,7 @@ def getChannelData(token, channel_id, startDate, endDate):
     return data
 
 # do we have a record of this site in the database?
-def siteIsKnownToUs(site_id, conn):
+def siteIsKnownToUs(site_id: int, conn):
     with conn.cursor() as cursor:
         cursor.execute(
             "SELECT 1 FROM ecocounter.sites WHERE site_id = %s",
@@ -72,7 +73,7 @@ def siteIsKnownToUs(site_id, conn):
         return cursor.rowcount > 0
 
 # do we have a record of this flow in the database?
-def flowIsKnownToUs(flow_id, conn):
+def flowIsKnownToUs(flow_id: int, conn):
     with conn.cursor() as cursor:
         cursor.execute(
             "SELECT 1 FROM ecocounter.flows WHERE flow_id = %s",
@@ -81,61 +82,54 @@ def flowIsKnownToUs(flow_id, conn):
         return cursor.rowcount > 0
 
 # DANGER delete all count data for a given flow
-def truncateFlowSince(flow_id, conn, startDate, endDate):
+def truncateFlowSince(flow_id: int, conn: any, startDate: datetime, endDate: datetime):
     with conn.cursor() as cursor:
         cursor.execute(
-            """DELETE FROM ecocounter.counts
+            """DELETE FROM gwolofs.ecocounter_counts_test
             WHERE flow_id = %s
             AND datetime_bin >= %s
             AND datetime_bin < %s""",
             (flow_id, startDate, endDate)
         )
 
-# insert a single count record
-def insertFlowCount(flow_id, datetime_bin, volume, conn):
-    with conn.cursor() as cursor:
-        cursor.execute(
-            "INSERT INTO ecocounter.counts (flow_id, datetime_bin, volume) VALUES %s",
-            (flow_id, datetime_bin, volume)
-        )
+# insert records
+def insertFlowCounts(conn, volume):
+    insert_query="INSERT INTO gwolofs.ecocounter_counts_test (flow_id, datetime_bin, volume) VALUES %s"
+    with conn.cursor() as cur:
+        execute_values(cur, insert_query, volume)
 
-def run_api(start_date=default_start, end_date=default_end, sites=()):
-
+def run_api(
+        start_date: datetime = default_start,
+        end_date: datetime = default_end,
+        sites: any = ()
+):
+    CONFIG_PATH = 'volumes/ecocounter/.api-credentials.config'
     config = ConfigParser()
-    config.read(r'/data/home/nwessel/db-creds.config')
-    conn = connect(**config['dbauth'])
+    config.read(CONFIG_PATH)
+    conn = connect(**config['DBSETTINGS'])
     conn.autocommit = True
-
-    API_CONFIG_PATH = 'volumes/ecocounter/.api-credentials.config'
-
-    token = getToken(API_CONFIG_PATH)
-       
+    token = getToken(CONFIG_PATH)
     for site in getSites(token, sites=sites): #optionally specify site_ids here. 
-
         # only update data for sites / channels in the database
         # but announce unknowns for manual validation if necessary
         if not siteIsKnownToUs(site['id'], conn):
             print('unknown site', site['id'], site['name'])
             continue
-
         for channel in site['channels']:
             if not flowIsKnownToUs(channel['id'], conn):
                 print('unknown flow', channel['id'])
                 continue
-
             # we do have this site and channel in the database; let's update its counts
             channel_id = channel['id']
             print(f'starting on flow {channel_id}')
-
             # empty the count table for this flow
             truncateFlowSince(channel_id, conn, start_date, end_date)
-    
             # and fill it back up!
             print(f'fetching data for flow {channel_id}')
             counts = getChannelData(token, channel_id, start_date, end_date)
-
             print(f'inserting data for flow {channel_id}')
-            #for count in tqdm(counts): # iterator with progress bar
-            for count in counts: # iterator with progress bar
-                volume = count['counts']
-                insertFlowCount(channel_id, count['date'], volume, conn)
+            volume=[]
+            for count in counts:
+                row=(channel_id, count['date'], count['counts'])
+                volume.append(row)
+            insertFlowCounts(conn, volume)
