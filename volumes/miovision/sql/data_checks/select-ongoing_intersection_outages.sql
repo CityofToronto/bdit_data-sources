98ee24bc429c91f388c5f2e3b8cb5dbcf04e630e
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-05-21 13:43:12 +0000

    #953 fix date param format in data check sql

--- volumes/miovision/sql/data_checks/select-ongoing_intersection_outages.sql
+++ volumes/miovision/sql/data_checks/select-ongoing_intersection_outages.sql
@@ -1,16 +1,16 @@
 WITH ongoing_outages AS (
     SELECT
         i.intersection_name || ' (uid: ' || i.intersection_uid || ') - data last received: '
-            || MAX(v.datetime_bin::date) || ' (' || '{{ data_interval_end }} 00:00:00'::timestamp - MAX(v.datetime_bin::date) || ')' AS description
+            || MAX(v.datetime_bin::date) || ' (' || '{{ data_interval_end }}'::date - MAX(v.datetime_bin::date) || ' days)' AS description
     FROM miovision_api.volumes AS v
     JOIN miovision_api.intersections AS i USING (intersection_uid)
     WHERE
-        v.datetime_bin >= '{{ data_interval_end }} 00:00:00'::timestamp - interval '{{ params.lookback }}'
-        AND v.datetime_bin < '{{ data_interval_end }} 00:00:00'::timestamp + interval '1 day'
+        v.datetime_bin >= '{{ data_interval_end }}'::date - interval '{{ params.lookback }}'
+        AND v.datetime_bin < '{{ data_interval_end }}'::date + interval '1 day'
     GROUP BY
         i.intersection_uid,
         i.intersection_name
-    HAVING MAX(v.datetime_bin::date) < '{{ data_interval_end }} 00:00:00'::timestamp - interval '{{ params.min_duration }}'
+    HAVING MAX(v.datetime_bin::date) < '{{ data_interval_end }}'::date - interval '{{ params.min_duration }}'
 )
 
 SELECT
