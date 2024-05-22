9fa43e7c530be929b5c272b8905df593f8379613
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2024-05-22 17:37:40 -0400

    #923 update vds_rawvdsdata insert to also insert new pairs

--- volumes/vds/py/vds_functions.py
+++ volumes/vds/py/vds_functions.py
@@ -71,9 +71,40 @@ def pull_raw_vdsdata(rds_conn, itsc_conn, start_date):
          start = sql.Literal(start_date + " 00:00:00 EST5EDT")
     )
 
-    insert_query = sql.SQL("""INSERT INTO vds.raw_vdsdata (
-                                 division_id, vds_id, dt, datetime_15min, lane, speed_kmh, volume_veh_per_hr, occupancy_percent
-                                ) VALUES %s;""")
+    insert_query = sql.SQL("""
+        WITH inserted AS (
+            INSERT INTO vds.raw_vdsdata (
+                division_id, vds_id, dt, datetime_15min, lane, speed_kmh, volume_veh_per_hr, occupancy_percent
+            ) VALUES %s
+            RETURNING division_id, vdsconfig_uid, entity_location_uid, dt
+        )
+
+        updated_dates AS (
+            SELECT
+                division_id,
+                vdsconfig_uid,
+                entity_location_uid,
+                MIN(dt) AS min_dt,
+                MAX(dt) AS max_dt
+            FROM inserted
+            WHERE
+                division_id + vdsconfig_uid + entity_location_uid IS NOT NULL
+            GROUP BY
+                division_id,
+                vdsconfig_uid,
+                entity_location_uid   
+        )
+            
+        UPDATE vds.vdsconfig_x_entity_locations AS x
+        SET
+            first_active = LEAST(ud.min_dt, x.first_active)
+            last_active = GREATEST(ud.max_dt, x.last_active)
+        FROM updated_dates AS ud
+        WHERE
+            x.division_id = ud.division_id
+            AND x.vdsconfig_uid = ud.vdsconfig_uid
+            AND x.entity_location_uid = ud.entity_location_uid;""")
+    
     batch_size = 100000
     
     #pull data in batches and transform + insert.
