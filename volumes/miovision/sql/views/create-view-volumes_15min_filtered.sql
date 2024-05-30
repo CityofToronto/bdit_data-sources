f6fb152796f34fc8989f64457a3508474f2f5b12
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-05-30 14:09:55 +0000

    #971 new views

---
+++ volumes/miovision/sql/views/create-view-volumes_15min_filtered.sql
@@ -0,0 +1,32 @@
+CREATE VIEW miovision_api.volumes_15min_filtered AS (
+    SELECT
+        v15.volume_15min_uid,
+        v15.intersection_uid,
+        v15.datetime_bin,
+        v15.classification_uid,
+        v15.leg,
+        v15.dir,
+        v15.volume
+    FROM miovision_api.volumes_15min AS v15
+    LEFT JOIN miovision_api.anomalous_ranges AS ar
+        ON ar.problem_level = ANY(ARRAY['do-not-use', 'questionable'])
+        AND ar.intersection_uid = v15.intersection_uid
+        AND (
+            ar.classification_uid = v15.classification_uid
+            OR ar.classification_uid IS NULL
+        ) AND (
+            ar.leg = v15.leg
+            OR ar.leg IS NULL
+        ) AND v15.datetime_bin >= ar.range_start
+        AND (
+                v15.datetime_bin <= ar.range_end
+                OR ar.range_end IS NULL
+            )
+    WHERE ar.uid IS NULL
+);
+
+COMMENT ON TABLE miovision_api.volumes_15min_filtered IS E''
+'miovision_api.volumes_15min with anomalous_ranges labeled '
+'''do-not-use'' or ''questionable'' filtered out.';
+
+GRANT SELECT ON TABLE miovision_api.volumes_15min_filtered TO bdit_humans;
---
+++ volumes/miovision/sql/views/create-view-volumes_15min_mvt_filtered.sql
@@ -0,0 +1,33 @@
+CREATE VIEW miovision_api.volumes_15min_mvt_filtered AS (
+    SELECT
+        v15.volume_15min_mvt_uid,
+        v15.intersection_uid,
+        v15.datetime_bin,
+        v15.classification_uid,
+        v15.leg,
+        v15.movement_uid,
+        v15.volume,
+        v15.processed
+    FROM miovision_api.volumes_15min_mvt AS v15
+    LEFT JOIN miovision_api.anomalous_ranges AS ar
+        ON ar.problem_level = ANY(ARRAY['do-not-use', 'questionable'])
+        AND ar.intersection_uid = v15.intersection_uid
+        AND (
+            ar.classification_uid = v15.classification_uid
+            OR ar.classification_uid IS NULL
+        ) AND (
+            ar.leg = v15.leg
+            OR ar.leg IS NULL
+        ) AND v15.datetime_bin >= ar.range_start
+        AND (
+                v15.datetime_bin <= ar.range_end
+                OR ar.range_end IS NULL
+            )
+    WHERE ar.uid IS NULL
+);
+
+COMMENT ON TABLE miovision_api.volumes_15min_mvt_filtered IS E''
+'miovision_api.volumes_15min_mvt with anomalous_ranges labeled '
+'''do-not-use'' or ''questionable'' filtered out.';
+
+GRANT SELECT ON TABLE miovision_api.volumes_15min_mvt_filtered TO bdit_humans;
