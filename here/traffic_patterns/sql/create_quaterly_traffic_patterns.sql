95e4d7b2edcd5f36743a13305d3ca8d7b8a4ebbc
chmnata <chmnata@gmail.com>
2024-05-31 14:33:12 +0000

    #937 fluff and stuff

--- here/traffic_patterns/sql/create_quaterly_traffic_patterns.sql
+++ here/traffic_patterns/sql/create_quaterly_traffic_patterns.sql
@@ -1,25 +1,24 @@
 CREATE TABLE here.traffic_pattern_yy_v_b_yyyyqq AS 
--- Aggregate link_dir based daily hourly speed
-WITH daily_time_cost as (
-	SELECT
-		routing_streets.link_dir,
-		dt, 
-		tod, 
-		harmean(ta.pct_50) AS daily_cost
-	FROM here.routing_streets_yy_q AS routing_streets
-	LEFT JOIN here.ta USING (link_dir)
-	LEFT JOIN ref.holiday USING (dt)
-	WHERE
-		dt >= _start_date AND dt < _end_date AND --
-		extract(isodow from dt) in (2,3,4) AND
-		holiday.dt IS null
-	GROUP BY routing_streets.link_dir, dt, tod
+
+WITH hourly_time_cost AS (
+    SELECT
+        routing_streets.link_dir,
+        dt,
+        datetime_bin(tod, 60) AS hr,
+        harmean(ta.pct_50) AS daily_cost
+    FROM here.routing_streets_yy_q AS routing_streets
+    LEFT JOIN here.ta USING (link_dir)
+    LEFT JOIN ref.holiday USING (dt)
+    WHERE
+        dt >= _start_date AND dt < _end_date AND
+        EXTRACT(isodow FROM dt) IN (2,3,4) AND -- only include tues-thurs traffic
+        holiday.dt IS NULL -- excluding holidays
+    GROUP BY routing_streets.link_dir, dt, hr
 )
 
-
 SELECT 
-	link_dir, 
- 	tod,
-	harmean(daily_cost)::INT AS cost
+    link_dir, 
+    hr,
+    harmean(daily_cost)::INT AS cost
 FROM daily_time_cost
-GROUP BY link_dir, tod;
+GROUP BY link_dir, hr;
--- here/traffic_patterns/sql/create_routing_speed_limit.sql
+++ here/traffic_patterns/sql/create_routing_speed_limit.sql
@@ -1,12 +1,12 @@
-CREATE TABLE here.routing_streets_yy_q_speed_limit AS 
+CREATE TABLE here.routing_streets_yy_q_speed_limit AS
 
-SELECT 
-	routing_streets.link_dir,
-	COALESCE(
-		CASE 
-			when right(routing_streets.link_dir, 1) = 'T' AND att.to_spd_lim > 0 then att.to_spd_lim
-			when right(routing_streets.link_dir, 1) = 'F' AND att.fr_spd_lim > 0 then att.fr_spd_lim
-		ELSE NULL
-		END, 50) AS spd_lim
+SELECT
+    routing_streets.link_dir,
+    COALESCE(
+        CASE
+            WHEN RIGHT(routing_streets.link_dir, 1) = 'T' AND att.to_spd_lim > 0 THEN att.to_spd_lim
+            WHEN RIGHT(routing_streets.link_dir, 1) = 'F' AND att.fr_spd_lim > 0 THEN att.fr_spd_lim
+        ELSE NULL
+        END, 50) AS spd_lim
 FROM here.routing_streets_yy_q AS routing_streets
-LEFT JOIN here_gis.streets_att_yy_q AS att on left(routing_streets.link_dir, -1)::int = att.link_id
+LEFT JOIN here_gis.streets_att_yy_q AS att on left(routing_streets.link_dir, -1)::int = att.link_id;
