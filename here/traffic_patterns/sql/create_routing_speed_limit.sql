d75320dff30251e94c47fa9e27cb2391e7496833
chmnata <chmnata@gmail.com>
2024-05-31 14:37:02 +0000

    #937 fluff again

--- here/traffic_patterns/sql/create_routing_speed_limit.sql
+++ here/traffic_patterns/sql/create_routing_speed_limit.sql
@@ -6,7 +6,8 @@ SELECT
         CASE
             WHEN RIGHT(routing_streets.link_dir, 1) = 'T' AND att.to_spd_lim > 0 THEN att.to_spd_lim
             WHEN RIGHT(routing_streets.link_dir, 1) = 'F' AND att.fr_spd_lim > 0 THEN att.fr_spd_lim
-        ELSE NULL
-        END, 50) AS spd_lim
+            ELSE NULL
+        END, 50
+    ) AS spd_lim
 FROM here.routing_streets_yy_q AS routing_streets
-LEFT JOIN here_gis.streets_att_yy_q AS att on left(routing_streets.link_dir, -1)::int = att.link_id;
+LEFT JOIN here_gis.streets_att_yy_q AS att on LEFT(routing_streets.link_dir, -1)::int = att.link_id;
