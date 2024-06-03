8b320bdd5f01b195f45a0b418ebbfdd280e3e4b7
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-06-03 17:40:19 +0000

    #975 add weather_pull + turn off pull_weather

---
+++ dags/weather_pull.py
@@ -0,0 +1,87 @@
+"""
+Pipeline to pull weather prediction data from Envrionment Canada and upsert into weather.prediction_daily table.
+Then, 
+A Slack notification is raised when the airflow process fails.
+"""
+import os
+import sys
+import pendulum
+from datetime import timedelta
+
+from airflow.decorators import dag, task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.models import Variable
+from airflow.operators.latest_only_operator import LatestOnlyOperator
+from airflow.macros import ds_add
+
+# DAG Information
+DAG_NAME = 'weather_pull'
+DAG_OWNERS = Variable.get('dag_owners', deserialize_json=True).get(DAG_NAME, ["Unknown"])
+
+#connection credentials
+cred = PostgresHook("weather_bot")
+
+#import python scripts
+try:
+    repo_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    sys.path.insert(0, repo_path)
+    from weather.prediction_import import prediction_upsert
+    from weather.historical_scrape import historical_upsert
+    from dags.dag_functions import task_fail_slack_alert
+except:
+    raise ImportError("script import failed")
+
+#DAG
+ 
+default_args = {
+    'owner': ','.join(DAG_OWNERS),
+    'depends_on_past':False,
+    'start_date': pendulum.datetime(2024, 6, 3, tz="America/Toronto"),
+    'email_on_failure': False,
+    'email_on_success': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+    'on_failure_callback': task_fail_slack_alert
+}
+
+@dag(
+    dag_id=DAG_NAME,
+    default_args=default_args, 
+    schedule='30 2 * * *', #daily at 2:30AM
+    catchup=False,
+    tags=['weather', 'data_pull'],
+    doc_md=__doc__
+)
+def weather_pull_dag():
+
+    no_backfill = LatestOnlyOperator(task_id="no_backfill")
+    no_backfill.doc_md = "Pull predicted weather data - can ONLY pull 5 days ahead of run date - no backfill."
+
+    @task()
+    def pull_prediction():
+        prediction_upsert(cred = PostgresHook("weather_bot"))
+    pull_prediction.doc_md = "Pull weather forcast for 5 days ahead of run date"
+
+    @task()
+    def pull_historical_city(ds = None):
+        historical_upsert(
+            cred = PostgresHook("weather_bot"),
+            run_date=ds_add(ds, -1),
+            station_id=31688
+        )
+    pull_historical_city.doc_md = "Pull yesterday's historical data for Toronto city centre"
+
+    @task()
+    def pull_historical_airport(ds = None):
+        historical_upsert(
+            cred = PostgresHook("weather_bot"),
+            run_date=ds_add(ds, -1),
+            station_id=51459
+        )
+    pull_historical_airport.doc_md = "Pull yesterday's historical data for Toronto Peason Airport"
+    
+    no_backfill >> pull_prediction()
+    pull_historical_city()
+    pull_historical_airport()
+
+weather_pull_dag()
