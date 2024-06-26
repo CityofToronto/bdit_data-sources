9f16532e76224c78f632c5412557010903795671
Nate Wessel <nate.wessel@toronto.ca>
2024-06-26 10:14:00 -0400

    update comments

--- volumes/ecocounter/views/create-view-counts.sql
+++ volumes/ecocounter/views/create-view-counts.sql
@@ -7,8 +7,10 @@ FROM ecocounter.counts_unfiltered
 JOIN ecocounter.flows_unfiltered USING (flow_id)
 JOIN ecocounter.sites_unfiltered USING (site_id)
 WHERE
-    flows_unfiltered.validated --is true
+    -- must be validated at the level of both site and flow
+    flows_unfiltered.validated
     AND sites_unfiltered.validated
+    -- must not intersect with any do-not-use level anomalous ranges
     AND NOT EXISTS (
         SELECT 1
         FROM ecocounter.anomalous_ranges
@@ -16,9 +18,11 @@ WHERE
             anomalous_ranges.problem_level = 'do-not-use'
             AND (
                 (
+                    -- flow has a problem is this time range
                     counts_unfiltered.flow_id = anomalous_ranges.flow_id
                     AND anomalous_ranges.time_range @> counts_unfiltered.datetime_bin
                 ) OR (
+                    -- site has a problem in this time range
                     sites_unfiltered.site_id = anomalous_ranges.site_id
                     AND anomalous_ranges.time_range @> counts_unfiltered.datetime_bin
                 )
@@ -26,9 +30,12 @@ WHERE
     );
 
 COMMENT ON VIEW ecocounter.counts
-IS 'This view contains the actual binned counts for ecocounter flows. Only flows
-marked as validated by a human are included here. Please note that bin size varies
-for older data, so averaging these numbers may not be straightforward.';
+IS 'This view contains the (somewhat) validated counts for Ecocounter flows. 
+Counts are only included for sites and flows and marked as validated by a human.
+Anomalous ranges at the do-not-use problem_level are also excluded here.
+For the complete, raw count data, see ecocounter.counts_unfiltered. 
+Please note that bin size varies for older data, so averaging these numbers may 
+not be straightforward.';
 
 ALTER VIEW ecocounter.counts OWNER TO ecocounter_admins;
 
