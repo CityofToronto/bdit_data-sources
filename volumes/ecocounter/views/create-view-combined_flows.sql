de313271a873af909f194a2ecd3ea5551ee939c4
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-07-18 14:02:39 +0000

    #914 fluff

--- volumes/ecocounter/views/create-view-combined_flows.sql
+++ volumes/ecocounter/views/create-view-combined_flows.sql
@@ -1,7 +1,9 @@
 CREATE VIEW ecocounter.combined_flows AS (
     WITH RECURSIVE recursive_flows AS (
         --flows which have never been replaced
-        SELECT flow_id, replaced_by_flow_id
+        SELECT
+            flow_id,
+            replaced_by_flow_id
         FROM ecocounter.flows_unfiltered
         WHERE replaced_by_flow_id IS NULL
 
@@ -10,7 +12,7 @@ CREATE VIEW ecocounter.combined_flows AS (
         --flows replaced by those in above recursive CTE.
         SELECT
             f.flow_id,
-            COALESCE(rf.replaced_by_flow_id, rf.flow_id)
+            COALESCE(rf.replaced_by_flow_id, rf.flow_id) AS replaced_by_flow_id
         FROM ecocounter.flows_unfiltered AS f
         JOIN recursive_flows AS rf ON rf.flow_id = f.replaced_by_flow_id
     )
@@ -31,6 +33,6 @@ GRANT SELECT ON TABLE ecocounter.combined_flows TO bdit_humans;
 
 GRANT SELECT ON TABLE ecocounter.combined_flows TO ecocounter_bot;
 
-COMMENT ON TABLE ecocounter.combined_flows
+COMMENT ON VIEW ecocounter.combined_flows
 IS 'Recursively combine flow_ids which have been replaced
 in order to get a continuous stream for monitoring project.';
