8b6036ccb7f0c9029c49862bfb480c37646f1062
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-07-19 21:19:39 +0000

    atr view

---
+++ volumes/miovision/sql/views/create-view-volumes_15min_atr.sql
@@ -0,0 +1,43 @@
+DROP VIEW gwolofs.miovision_15min_atr_filtered;
+CREATE VIEW gwolofs.miovision_15min_atr_filtered AS (
+
+    --entries
+    SELECT intersection_uid, datetime_bin, classification_uid, leg, entry_dir AS dir, SUM(volume) AS volume
+    FROM gwolofs.volumes_15min_mvt_filtered
+    JOIN gwolofs.miovision_movement_map_new USING (movement_uid, leg)
+    GROUP BY intersection_uid, datetime_bin, classification_uid, leg, entry_dir
+
+    UNION
+
+    --exits 
+    SELECT intersection_uid, datetime_bin, classification_uid, exit_leg, exit_dir, SUM(volume) AS volume
+    FROM gwolofs.volumes_15min_mvt_filtered
+    JOIN gwolofs.miovision_movement_map_new USING (movement_uid, leg)
+    GROUP BY intersection_uid, datetime_bin, classification_uid, exit_leg, exit_dir
+);
+
+--test: 0.2 s with primary key
+SELECT * FROM gwolofs.miovision_15min_atr_filtered
+WHERE intersection_uid = 6 AND classification_uid = 1 AND datetime_bin = '2024-06-25 12:00:00' AND leg <> LEFT(dir, 1)
+
+--DR i0627 test using new view
+--41s for original, 1:06 for view (1.3M rows)
+SELECT
+    volumes.intersection_uid,
+    date_trunc('hour', volumes.datetime_bin) AS datetime_bin,
+    volumes.leg,
+    volumes.dir,
+    classifications.classification,
+    SUM(volumes.volume) AS volume
+FROM gwolofs.miovision_15min_atr_filtered AS volumes
+INNER JOIN miovision_api.classifications USING (classification_uid)
+WHERE
+    volumes.classification_uid NOT IN (2, 7)
+    AND datetime_bin >= '2024-01-01' AND datetime_bin < '2024-05-22'
+    AND volumes.intersection_uid IN (76, 88, 81, 85, 84, 87, 75, 83, 86, 49, 80, 36)
+GROUP BY
+    volumes.intersection_uid,
+    classifications.classification,
+    date_trunc('hour', volumes.datetime_bin),
+    volumes.leg,
+    volumes.dir;
