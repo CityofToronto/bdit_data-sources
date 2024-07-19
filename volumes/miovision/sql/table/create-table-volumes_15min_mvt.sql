b2ea04bf5ffa58f0968ce042d55955ae062b8d4e
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-07-19 21:18:59 +0000

    unfiltered 15min aggregation

--- volumes/miovision/sql/table/create-table-volumes_15min_mvt.sql
+++ volumes/miovision/sql/table/create-table-volumes_15min_mvt.sql
@@ -1,14 +1,15 @@
-﻿CREATE TABLE miovision_api.volumes_15min_mvt
+﻿--testing as gwolofs.volumes_15min_mvt_unfiltered
+
+CREATE TABLE miovision_api.volumes_15min_mvt_unfiltered
 (
     volume_15min_mvt_uid integer NOT NULL
-    DEFAULT nextval('miovision_api.volumes_15min_mvt_volume_15min_mvt_uid_seq'::regclass),
+    DEFAULT nextval('miovision_api.volumes_15min_mvt_unfiltered_volume_15min_mvt_uid_seq'::regclass),
     intersection_uid integer,
     datetime_bin timestamp without time zone,
     classification_uid integer,
     leg text,
     movement_uid integer,
     volume numeric,
-    processed boolean,
     CONSTRAINT volumes_15min_mvt_int_dt_bin_class_leg_mvmt_uid_pkey
     PRIMARY KEY (
         intersection_uid, datetime_bin, classification_uid, leg, movement_uid
@@ -19,58 +20,88 @@ WITH (
     oids = FALSE
 );
 
-ALTER TABLE miovision_api.volumes_15min_mvt OWNER TO miovision_admins;
-GRANT SELECT, REFERENCES, TRIGGER ON TABLE miovision_api.volumes_15min_mvt
+ALTER TABLE miovision_api.volumes_15min_mvt_unfiltered OWNER TO miovision_admins;
+GRANT SELECT, REFERENCES, TRIGGER ON TABLE miovision_api.volumes_15min_mvt_unfiltered
 TO bdit_humans WITH GRANT OPTION;
-GRANT SELECT, INSERT, TRIGGER ON TABLE miovision_api.volumes_15min_mvt TO miovision_api_bot;
+GRANT SELECT, INSERT, TRIGGER ON TABLE miovision_api.volumes_15min_mvt_unfiltered TO miovision_api_bot;
 
-COMMENT ON TABLE miovision_api.volumes_15min_mvt IS E''
+COMMENT ON TABLE miovision_api.volumes_15min_mvt_unfiltered IS E''
 'NOTE: Refer instead to view volumes_15min_mvt_filtered to exclude anomalous_ranges. '
 'TMC formatted Miovision data in 15 minute bins. 0-padded for classifications 1,2,6,10 to '
 'make averaging easier.';
 
--- Index: miovision_api.volumes_15min_mvt_classification_uid_idx
--- DROP INDEX miovision_api.volumes_15min_mvt_classification_uid_idx;
+-- Index: miovision_api.volumes_15min_mvt_unfiltered_classification_uid_idx
+-- DROP INDEX miovision_api.volumes_15min_mvt_unfiltered_classification_uid_idx;
 CREATE INDEX volumes_15min_mvt_classification_uid_idx
-ON miovision_api.volumes_15min_mvt
+ON miovision_api.volumes_15min_mvt_unfiltered
 USING btree (classification_uid);
 
--- Index: miovision_api.volumes_15min_mvt_datetime_bin_idx
--- DROP INDEX miovision_api.volumes_15min_mvt_datetime_bin_idx;
+-- Index: miovision_api.volumes_15min_mvt_unfiltered_datetime_bin_idx
+-- DROP INDEX miovision_api.volumes_15min_mvt_unfiltered_datetime_bin_idx;
 CREATE INDEX volumes_15min_mvt_datetime_bin_idx
-ON miovision_api.volumes_15min_mvt
+ON miovision_api.volumes_15min_mvt_unfiltered
 USING btree (datetime_bin);
 
--- Index: miovision_api.volumes_15min_mvt_intersection_uid_idx
--- DROP INDEX miovision_api.volumes_15min_mvt_intersection_uid_idx;
+-- Index: miovision_api.volumes_15min_mvt_unfiltered_intersection_uid_idx
+-- DROP INDEX miovision_api.volumes_15min_mvt_unfiltered_intersection_uid_idx;
 CREATE INDEX volumes_15min_mvt_intersection_uid_idx
-ON miovision_api.volumes_15min_mvt
+ON miovision_api.volumes_15min_mvt_unfiltered
 USING btree (intersection_uid);
 
--- Index: miovision_api.volumes_15min_mvt_leg_movement_uid_idx
--- DROP INDEX miovision_api.volumes_15min_mvt_leg_movement_uid_idx;
+-- Index: miovision_api.volumes_15min_mvt_unfiltered_leg_movement_uid_idx
+-- DROP INDEX miovision_api.volumes_15min_mvt_unfiltered_leg_movement_uid_idx;
 CREATE INDEX volumes_15min_mvt_leg_movement_uid_idx
-ON miovision_api.volumes_15min_mvt
+ON miovision_api.volumes_15min_mvt_unfiltered
 USING btree (
     leg COLLATE pg_catalog."default",
     movement_uid
 );
 
--- Index: miovision_api.volumes_15min_mvt_volume_15min_mvt_uid_idx
--- DROP INDEX miovision_api.volumes_15min_mvt_volume_15min_mvt_uid_idx;
+-- Index: miovision_api.volumes_15min_mvt_unfiltered_volume_15min_mvt_uid_idx
+-- DROP INDEX miovision_api.volumes_15min_mvt_unfiltered_volume_15min_mvt_uid_idx;
 CREATE INDEX volumes_15min_mvt_volume_15min_mvt_uid_idx
-ON miovision_api.volumes_15min_mvt
+ON miovision_api.volumes_15min_mvt_unfiltered
 USING btree (volume_15min_mvt_uid ASC NULLS LAST);
 
--- Index: volumes_15min_mvt_processed_idx
--- DROP INDEX IF EXISTS miovision_api.volumes_15min_mvt_processed_idx;
-CREATE INDEX IF NOT EXISTS volumes_15min_mvt_processed_idx
-ON miovision_api.volumes_15min_mvt USING btree (processed ASC NULLS LAST)
-WHERE processed IS NULL;
-
 --create yearly partitions
-SELECT miovision_api.create_yyyy_volumes_15min_partition('volumes_15min_mvt', 2019::int);
-SELECT miovision_api.create_yyyy_volumes_15min_partition('volumes_15min_mvt', 2020::int);
-SELECT miovision_api.create_yyyy_volumes_15min_partition('volumes_15min_mvt', 2021::int);
-SELECT miovision_api.create_yyyy_volumes_15min_partition('volumes_15min_mvt', 2022::int);
-SELECT miovision_api.create_yyyy_volumes_15min_partition('volumes_15min_mvt', 2023::int);
+SELECT miovision_api.create_yyyy_volumes_15min_partition('volumes_15min_mvt_unfiltered', 2019::int);
+SELECT miovision_api.create_yyyy_volumes_15min_partition('volumes_15min_mvt_unfiltered', 2020::int);
+SELECT miovision_api.create_yyyy_volumes_15min_partition('volumes_15min_mvt_unfiltered', 2021::int);
+SELECT miovision_api.create_yyyy_volumes_15min_partition('volumes_15min_mvt_unfiltered', 2022::int);
+SELECT miovision_api.create_yyyy_volumes_15min_partition('volumes_15min_mvt_unfiltered', 2023::int);
+
+
+CREATE TABLE gwolofs.volume_15min_mvt_unfiltered_2024 PARTITION OF gwolofs.volumes_15min_mvt_unfiltered FOR VALUES FROM ('2024-01-01') TO ('2025-01-01');
+CREATE TABLE gwolofs.volume_15min_mvt_unfiltered_2023 PARTITION OF gwolofs.volumes_15min_mvt_unfiltered FOR VALUES FROM ('2023-01-01') TO ('2024-01-01');
+
+INSERT INTO gwolofs.volumes_15min_mvt_unfiltered (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume)
+SELECT
+    v.intersection_uid,
+    datetime_bin_15(v.datetime_bin) AS datetime_bin,
+    v.classification_uid,
+    v.leg,
+    v.movement_uid,
+    SUM(volume)
+FROM miovision_api.volumes_2023 AS v
+GROUP BY
+    v.intersection_uid,
+    datetime_bin_15(v.datetime_bin),
+    v.classification_uid,
+    v.leg,
+    v.movement_uid;
+
+INSERT INTO gwolofs.volumes_15min_mvt_unfiltered (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume)
+SELECT
+    v.intersection_uid,
+    datetime_bin_15(v.datetime_bin) AS datetime_bin,
+    v.classification_uid,
+    v.leg,
+    v.movement_uid,
+    SUM(volume)
+FROM miovision_api.volumes_2024 AS v
+GROUP BY
+    v.intersection_uid,
+    datetime_bin_15(v.datetime_bin),
+    v.classification_uid,
+    v.leg,
+    v.movement_uid;
