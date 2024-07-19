0fa44e482f337d2e8549fcff68f1de56ea9ea5a0
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-07-19 21:19:24 +0000

    filtered 15min aggregation (anomalous_ranges, intersection_movements, unacceptable_gaps)

--- volumes/miovision/sql/views/create-view-volumes_15min_filtered.sql
+++ volumes/miovision/sql/views/create-view-volumes_15min_filtered.sql
@@ -1,15 +1,18 @@
-CREATE VIEW miovision_api.volumes_15min_filtered AS (
+CREATE OR REPLACE VIEW gwolofs.miovision_15min_test_filtered AS (
     SELECT
-        v15.volume_15min_uid,
         v15.intersection_uid,
         v15.datetime_bin,
         v15.classification_uid,
         v15.leg,
-        v15.dir,
-        v15.volume
-    FROM miovision_api.volumes_15min AS v15
+        v15.movement_uid,
+        v15.sum
+    FROM gwolofs.miovision_volumes_15min_mvt_unfiltered AS v15
+    JOIN miovision_api.intersection_movements AS im USING (intersection_uid, classification_uid, leg, movement_uid)
+    --anti join unacceptable_gaps
+    LEFT JOIN miovision_api.unacceptable_gaps AS un USING (datetime_bin, intersection_uid)
+    --anti join anomalous_ranges
     LEFT JOIN miovision_api.anomalous_ranges AS ar
-        ON ar.problem_level = ANY(ARRAY['do-not-use', 'questionable'])
+        ON (ar.problem_level = ANY (ARRAY['do-not-use'::text, 'questionable'::text]))
         AND ar.intersection_uid = v15.intersection_uid
         AND (
             ar.classification_uid = v15.classification_uid
@@ -17,16 +20,20 @@ CREATE VIEW miovision_api.volumes_15min_filtered AS (
         ) AND (
             ar.leg = v15.leg
             OR ar.leg IS NULL
-        ) AND v15.datetime_bin >= ar.range_start
+        )
+        AND v15.datetime_bin >= ar.range_start
         AND (
             v15.datetime_bin <= ar.range_end
             OR ar.range_end IS NULL
         )
-    WHERE ar.uid IS NULL
+    WHERE
+        ar.uid IS NULL
+        AND un.datetime_bin IS NULL
 );
 
-COMMENT ON VIEW miovision_api.volumes_15min_filtered IS E''
+COMMENT ON VIEW gwolofs.miovision_15min_test_filtered IS E''
 'miovision_api.volumes_15min with anomalous_ranges labeled '
-'''do-not-use'' or ''questionable'' filtered out.';
+'''do-not-use'' or ''questionable'' filtered out, unacceptable_gaps anti-joined,
+and only common (>0.05%) intersection movements included.';
 
-GRANT SELECT ON TABLE miovision_api.volumes_15min_filtered TO bdit_humans;
+GRANT SELECT ON TABLE gwolofs.miovision_15min_test_filtered TO bdit_humans;
