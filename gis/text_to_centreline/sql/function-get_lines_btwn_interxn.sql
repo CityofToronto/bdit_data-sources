e0505ae1529bf79f14bfd39264a968df8d1f4f7d
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-07-23 16:59:58 +0000

    #1015 replace centreline_routing_undirected_lfname

--- gis/text_to_centreline/sql/function-get_lines_btwn_interxn.sql
+++ gis/text_to_centreline/sql/function-get_lines_btwn_interxn.sql
@@ -1,4 +1,6 @@
 --USING centrelines aka GIS network
+DROP FUNCTION IF EXISTS gwolofs._get_lines_btwn_interxn;
+
 CREATE OR REPLACE FUNCTION gwolofs._get_lines_btwn_interxn(
     _highway2 text,
     _int_start integer,
@@ -8,12 +10,12 @@ RETURNS TABLE (
     int_start integer,
     int_end integer,
     seq integer,
-    geo_id numeric,
-    lf_name character varying,
-    objectid numeric,
+    geo_id integer,
+    lf_name text,
+    objectid integer,
     geom geometry,
     fcode integer,
-    fcode_desc character varying
+    fcode_desc text
 )
 LANGUAGE 'plpgsql'
 
@@ -25,12 +27,25 @@ AS $BODY$
 BEGIN
 RETURN QUERY
 
-WITH 
-results AS (SELECT _int_start, _int_end, * FROM
-    pgr_dijkstra(format('SELECT id, source::int, target::int,
-                 CASE WHEN levenshtein(TRIM(lf_name), TRIM(%L), 1, 1,1) < 3 THEN (0.3*cost)::float ELSE cost END AS cost 
-                 from gwolofs.centreline_routing_undirected_lfname'::text, _highway2),
-                 _int_start::bigint, _int_end::bigint, FALSE)
+WITH results AS (
+    SELECT
+        _get_lines_btwn_interxn._int_start,
+        _get_lines_btwn_interxn._int_end,
+        dijkstra.seq,
+        dijkstra.edge
+    FROM pgr_dijkstra(format('SELECT
+                centreline_id AS id,
+                from_intersection_id AS source,
+                to_intersection_id AS target,
+                CASE WHEN levenshtein(TRIM(linear_name_full), TRIM(%L), 1, 1,1) < 3 THEN (0.3*costs.cost)::float ELSE costs.cost END AS cost 
+            FROM gis_core.centreline_latest,
+            LATERAL (
+                SELECT st_length(st_transform(geom, 98012))::integer AS cost
+            ) AS costs(cost)
+            WHERE feature_code_desc IN (''Collector'', ''Expressway Ramp'', ''Pending'', ''Expressway'', ''Major Arterial'', ''Local'', ''Minor Arterial'')
+            '::text, _highway2),
+        _int_start::bigint, _int_end::bigint, FALSE
+    ) AS dijkstra
 --or do pgr_dijkstra('SELECT id, source::int, target::int, 
     --CASE lf_name WHEN '''|| _highway2 ||''' THEN (0.3*cost)::float ELSE cost END AS cost from gwolofs.centreline_routing_undirected_lfname'::text, ... )
 )
@@ -45,14 +60,14 @@ SELECT
     centre.feature_code AS fcode,
     centre.feature_code_desc AS fcode_desc 
 FROM results
-JOIN gis_core.centreline_latest AS centre ON edge = centre.centreline_id
+JOIN gis_core.centreline_latest AS centre ON results.edge = centre.centreline_id
 --WHERE levenshtein(TRIM(centre.lf_name), TRIM(_highway2), 1, 1, 1) < 3
 --instead of `WHERE centre.lf_name = _highway2` because the street name might not be EXACTLY the same 
 --(or we can get the output of get_intersection_id for lf_name and input it here but lf_name is not an output for that function currently)
 ORDER BY
-    int_start,
-    int_end,
-    seq;
+    results._int_start,
+    results._int_end,
+    results.seq;
 
 RAISE NOTICE 'pg_routing done for int_start: % and int_end: %', 
 _int_start, _int_end;
