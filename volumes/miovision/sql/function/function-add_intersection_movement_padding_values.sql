77eb3e8e736d257078a00d7f40e3fc2967c692a9
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-07-24 20:59:44 +0000

    update miovision_intersection_movements_pad: also add real volumes using trigger

--- volumes/miovision/sql/function/function-add_intersection_movement_padding_values.sql
+++ volumes/miovision/sql/function/function-add_intersection_movement_padding_values.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE FUNCTION miovision_api.fn_add_intersection_movement_padding_values()
 RETURNS trigger
 LANGUAGE 'plpgsql'
+
 COST 100
 VOLATILE SECURITY DEFINER PARALLEL UNSAFE
 AS $BODY$
 
 BEGIN
 
-    INSERT INTO miovision_api.volumes_15min_mvt_unfiltered (
-        intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume
-    )
+WITH temp AS (
+    -- Cross product of dates, intersections, legal movement for cars, bikes, and peds to aggregate
     SELECT
         im.intersection_uid,
         dt.datetime_bin,
@@ -30,18 +30,93 @@ BEGIN
     WHERE
         --0 padding for certain modes (padding)
         im.classification_uid IN (1,2,6,10)
-    ON CONFLICT (intersection_uid, datetime_bin, classification_uid, leg, movement_uid)
-    --no need to insert a zero if there is already volume.
-    DO NOTHING
-    RETURN NULL;
+        
+    UNION ALL
+    
+    --real volumes
+    SELECT
+        v.intersection_uid,
+        datetime_bin_15(v.datetime_bin) AS datetime_bin,
+        v.classification_uid,
+        v.leg,
+        v.movement_uid,
+        SUM(volume)
+    FROM miovision_api.volumes AS v
+    JOIN new_rows USING (
+        intersection_uid, classification_uid, leg, movement_uid
+    )
+    GROUP BY
+        v.intersection_uid,
+        datetime_bin_15(v.datetime_bin),
+        v.classification_uid,
+        v.leg,
+        v.movement_uid
+),
+
+aggregate_insert AS (
+    INSERT INTO miovision_api.volumes_15min_mvt(
+        intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume
+    )
+    SELECT DISTINCT ON (
+        v.intersection_uid, v.datetime_bin, v.classification_uid, v.leg, v.movement_uid
+    )
+        v.intersection_uid,
+        v.datetime_bin,
+        v.classification_uid,
+        v.leg,
+        v.movement_uid,
+        v.volume
+    FROM temp AS v
+    JOIN miovision_api.intersections AS i USING (intersection_uid)
+    --set unacceptable gaps as null
+    WHERE
+        -- Only include dates during which intersection is active 
+        -- (excludes entire day it was added/removed)
+        v.datetime_bin >= i.date_installed + interval '1 day'
+        AND (
+            i.date_decommissioned IS NULL
+            OR (v.datetime_bin < i.date_decommissioned - interval '1 day')
+        )
+    ORDER BY 
+        v.intersection_uid,
+        v.datetime_bin,
+        v.classification_uid,
+        v.leg,
+        v.movement_uid,
+        --select real value instead of padding value if available
+        v.volume DESC
+    RETURNING intersection_uid, volume_15min_mvt_uid, datetime_bin, classification_uid, leg, movement_uid, volume
+)
+
+--To update foreign key for 1min bin table
+UPDATE miovision_api.volumes AS v
+SET volume_15min_mvt_uid = a_i.volume_15min_mvt_uid
+FROM aggregate_insert AS a_i
+WHERE
+    v.volume_15min_mvt_uid IS NULL
+    AND a_i.volume > 0
+    AND v.intersection_uid = a_i.intersection_uid
+    AND v.datetime_bin >= a_i.datetime_bin
+    AND v.datetime_bin < a_i.datetime_bin + interval '15 minutes'
+    AND v.classification_uid = a_i.classification_uid
+    AND v.leg = a_i.leg
+    AND v.movement_uid = a_i.movement_uid;
+
+RAISE NOTICE '% Done adding to 15min MVT bin based on intersection_movement new rows.', timeofday();
 END;
+
 $BODY$;
+
+ALTER FUNCTION miovision_api.fn_add_intersection_movement_padding_values()
+OWNER TO miovision_admins;
+
+GRANT EXECUTE ON FUNCTION miovision_api.fn_add_intersection_movement_padding_values()
+TO miovision_admins;
+
+GRANT EXECUTE ON FUNCTION miovision_api.fn_add_intersection_movement_padding_values()
+TO miovision_api_bot;
 
 COMMENT ON FUNCTION miovision_api.fn_add_intersection_movement_padding_values() IS
 'This function is called using a trigger after each statement on insert into
 miovision_api.intersection_movements. It uses newly inserted rows to update the zero padding
 values in miovision_api.volumes_15min_mvt_unfiltered';
-
-ALTER FUNCTION miovision_api.fn_add_intersection_movement_padding_values OWNER TO miovision_admins;
-
-GRANT EXECUTE ON FUNCTION miovision_api.fn_add_intersection_movement_padding_values() TO miovision_api_bot;
