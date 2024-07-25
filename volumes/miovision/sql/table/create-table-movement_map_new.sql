44f85f8e4f4584b403ce8b2c6034f78ab07e2241
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-07-25 13:56:58 +0000

    fluff

--- volumes/miovision/sql/table/create-table-movement_map_new.sql
+++ volumes/miovision/sql/table/create-table-movement_map_new.sql
@@ -10,14 +10,15 @@ CREATE TABLE gwolofs.miovision_movement_map_new AS (
         COALESCE(exits.dir, entries.dir) AS exit_dir
     FROM miovision_api.movement_map AS entries
     JOIN miovision_api.movements AS mov USING (movement_uid)
-    LEFT JOIN miovision_api.movement_map AS exits ON
-        exits.leg_old = entries.leg_old
+    LEFT JOIN miovision_api.movement_map AS exits
+        ON exits.leg_old = entries.leg_old
         AND exits.movement_uid = entries.movement_uid
         AND exits.leg_new = substr(exits.dir, 1, 1) --eg. E leg going East is an exit
     WHERE entries.leg_new <> substr(entries.dir, 1, 1) --eg. E leg going West is an entry
 );
 
-ALTER TABLE gwolofs.miovision_movement_map_new ADD CONSTRAINT movement_map_new_pkey PRIMARY KEY (movement_uid, leg);
+ALTER TABLE gwolofs.miovision_movement_map_new
+ADD CONSTRAINT movement_map_new_pkey PRIMARY KEY (movement_uid, leg);
 
 COMMENT ON TABLE gwolofs.miovision_movement_map_new
 IS 'A more intuitive version of miovision_api.movement_map.';
