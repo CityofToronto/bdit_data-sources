4f98605d99233e76da99f7d64b9e2da23d97e63e
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-07-26 20:25:38 +0000

    #855 add before insert triggers to exclude rows from either allowlist/denylist

--- volumes/miovision/sql/table/create-table-intersection_movements.sql
+++ volumes/miovision/sql/table/create-table-intersection_movements.sql
@@ -5,3 +5,8 @@ SELECT DISTINCT intersection_uid, classification_uid, leg, movement_uid
 	COMMENT ON TABLE miovision.intersection_movements IS 'Unique movements for each intersection by classification';
 	GRANT ALL ON TABLE miovision.intersection_movements TO miovision_admins;
 GRANT SELECT, REFERENCES, TRIGGER ON TABLE miovision.intersection_movements TO bdit_humans WITH GRANT OPTION;
+
+CREATE TRIGGER denylist_intersection_movements_exclusion
+BEFORE INSERT ON miovision_api.intersection_movements
+FOR EACH ROW
+EXECUTE FUNCTION miovision_api.exclude_denylist_from_intersection_movements();
--- volumes/miovision/sql/table/create-table-intersection_movements_denylist.sql
+++ volumes/miovision/sql/table/create-table-intersection_movements_denylist.sql
@@ -26,37 +26,3 @@ CREATE TRIGGER intersection_movements_denylist_exclusion
 BEFORE INSERT ON miovision_api.intersection_movements_denylist
 FOR EACH ROW
 EXECUTE FUNCTION miovision_api.exclude_intersection_movements_from_denylist();
-
-CREATE OR REPLACE FUNCTION miovision_api.exclude_intersection_movements_from_denylist()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $BODY$
-
-DECLARE row_in_im numeric = (
-    SELECT COUNT(*)
-    FROM miovision_api.intersection_movements AS im
-    WHERE
-        im.intersection_uid = new.intersection_uid
-        AND im.leg = new.leg
-        AND im.movement_uid = new.movement_uid
-        AND im.classification_uid = new.classification_uid
-);
-
-BEGIN
-    IF (row_in_im) THEN
-        RAISE NOTICE 'Row % is not being inserted. Already in intersection_movements table.', new;
-        --Return null to stop insert. 
-        RETURN NULL;
-    ELSE
-        RETURN new;
-    END IF;
-END;
-$BODY$;
-
-TRUNCATE miovision_api.intersection_movements_denylist;
-
-INSERT INTO miovision_api.intersection_movements_denylist
-SELECT * FROM miovision_api.intersection_movements LIMIT 1;
-
-INSERT INTO miovision_api.intersection_movements_denylist (intersection_uid, classification_uid, leg, movement_uid)
-VALUES (1,	2, 'E',	3);
