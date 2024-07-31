7535d316d576608e43b14b031cba8b95ec26e1ab
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-07-31 14:49:15 +0000

    #855 :hyperkitty: speed improvements, add minor modes

--- volumes/miovision/sql/views/create-view-monitor_intersection_movements.sql
+++ volumes/miovision/sql/views/create-view-monitor_intersection_movements.sql
@@ -1,49 +1,49 @@
 CREATE OR REPLACE VIEW miovision_api.monitor_intersection_movements AS (
-    WITH intersection_movements AS (
+    WITH intersection_classification_totals AS (
         SELECT
-            v.intersection_uid,
-            v.classification_uid,
-            v.leg,
-            v.movement_uid,
-            im.intersection_uid IS NULL AS is_not_in_intersection_movements,
-            SUM(v.volume) AS sum_volume,
-            SUM(SUM(v.volume)) OVER (PARTITION BY v.intersection_uid, v.classification_uid)
-            AS intersection_classification_total
-        FROM miovision_api.volumes AS v
-        LEFT JOIN miovision_api.intersection_movements AS im
-            USING (intersection_uid, classification_uid, leg, movement_uid)
-        --anti join intersection_movements_denylist
-        LEFT JOIN miovision_api.intersection_movements_denylist AS im_dl
-            USING (intersection_uid, classification_uid, leg, movement_uid)
-        WHERE
-            v.datetime_bin >= now()::date - interval '100 days'
-            AND v.classification_uid IN (1, 2, 6, 10)
-            AND NOT(v.classification_uid = 10 AND movement_uid = 8) --bike exit
-            AND im_dl.intersection_uid IS NULL
+            intersection_uid,
+            classification_uid,
+            SUM(daily_volume) AS volume
+        FROM miovision_api.volumes_daily_unfiltered
+        WHERE dt >= now()::date AT TIME ZONE 'Canada/Eastern' - interval ' 100 days'
         GROUP BY
-            v.intersection_uid,
-            v.classification_uid,
-            v.leg,
-            v.movement_uid,
-            im.intersection_uid
-        HAVING SUM(v.volume) > 1000 --10 per day avg
+            intersection_uid,
+            classification_uid
     )
 
     SELECT
-        im.intersection_uid,
-        im.classification_uid,
-        im.leg,
-        im.movement_uid,
-        i.intersection_name,
+        v.intersection_uid,
+        v.classification_uid,
+        v.leg,
+        v.movement_uid,
+        m.movement_name,
         c.classification,
-        im.sum_volume,
-        im.sum_volume / im.intersection_classification_total AS volume_frac
-    FROM intersection_movements AS im
-    JOIN miovision_api.intersections AS i USING (intersection_uid)
-    JOIN miovision_api.classifications AS c USING (classification_uid)
+        SUM(v.volume) AS sum_volume,
+        ict.volume AS intersection_classification_total,
+        SUM(v.volume) / ict.volume::numeric AS volume_frac
+    FROM miovision_api.volumes AS v
+    --anti join intersection_movements_denylist
+    LEFT JOIN miovision_api.intersection_movements_denylist AS im_dl
+        USING (intersection_uid, classification_uid, leg, movement_uid)
+    LEFT JOIN miovision_api.classifications AS c USING (classification_uid)
+    LEFT JOIN miovision_api.movements AS m USING (movement_uid)
+    LEFT JOIN intersection_classification_totals AS ict USING (intersection_uid, classification_uid)
     WHERE
-        im.is_not_in_intersection_movements
-        AND im.sum_volume / im.intersection_classification_total > 0.005
+        v.volume_15min_mvt_uid IS NULL --not aggregated
+        AND v.datetime_bin >= now()::date AT TIME ZONE 'Canada/Eastern' - interval '100 days'
+        AND NOT(v.classification_uid = 10 AND movement_uid = 8) --bike exit
+    GROUP BY
+        v.intersection_uid,
+        v.classification_uid,
+        v.leg,
+        v.movement_uid,
+        v.intersection_uid,
+        ict.volume,
+        m.movement_name,
+        c.classification
+    HAVING
+        SUM(v.volume) > 1000 --10 per day avg
+        AND SUM(v.volume) / ict.volume::numeric > 0.005
     ORDER BY volume_frac DESC
 );
 
