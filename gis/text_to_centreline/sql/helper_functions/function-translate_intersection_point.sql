801b77b71113c1e8fedfcc79afa09e883ddfefb2
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-08-01 16:06:50 +0000

    #628 recognize more direction formats

--- gis/text_to_centreline/sql/helper_functions/function-translate_intersection_point.sql
+++ gis/text_to_centreline/sql/helper_functions/function-translate_intersection_point.sql
@@ -5,16 +5,25 @@ CREATE OR REPLACE FUNCTION gis._translate_intersection_point(
 RETURNS geometry AS $translated_geom$
 DECLARE
 translated_geom text := (
-    (CASE WHEN TRIM(direction) = 'west' THEN ST_Translate(oid_geom, -metres, 0)
-    WHEN TRIM(direction) = 'east' THEN ST_Translate(oid_geom, metres, 0)
-    WHEN TRIM(direction) = 'north' THEN ST_Translate(oid_geom, 0, metres)
-    WHEN TRIM(direction) = 'south' THEN ST_Translate(oid_geom, 0, -metres)
-    WHEN TRIM(direction) IN ('southwest', 'south west') THEN ST_Translate(oid_geom, -cos(45)*metres, -sin(45)*metres)
-    WHEN TRIM(direction) IN ('southeast', 'south east') THEN ST_Translate(oid_geom, cos(45)*metres, -sin(45)*metres)
-    WHEN TRIM(direction) IN ('northwest', 'north west') THEN ST_Translate(oid_geom, -cos(45)*metres, sin(45)*metres)
-    WHEN TRIM(direction) IN ('northeast', 'north east') THEN ST_Translate(oid_geom, cos(45)*metres, sin(45)*metres)
-    END)
-    );
+    CASE
+        WHEN regexp_instr(trim(lower(direction)), '(?<!\S)(w|west)(?!\S)') = 1
+        THEN ST_Translate(oid_geom, -metres, 0)
+        WHEN regexp_instr(trim(lower(direction)), '(?<!\S)(e|east)(?!\S)') = 1
+        THEN ST_Translate(oid_geom, metres, 0)
+        WHEN regexp_instr(trim(lower(direction)), '(?<!\S)(n|north)(?!\S)') = 1
+        THEN ST_Translate(oid_geom, 0, metres)
+        WHEN regexp_instr(trim(lower(direction)), '(?<!\S)(s|south)(?!\S)') = 1
+        THEN ST_Translate(oid_geom, 0, -metres)
+        WHEN lower(regexp_replace(direction, '\s', '')) = 'southwest'
+        THEN ST_Translate(oid_geom, -cos(45)*metres, -sin(45)*metres)
+        WHEN lower(regexp_replace(direction, '\s', '')) = 'southeast'
+        THEN ST_Translate(oid_geom, cos(45)*metres, -sin(45)*metres)
+        WHEN lower(regexp_replace(direction, '\s', '')) = 'northwest'
+        THEN ST_Translate(oid_geom, -cos(45)*metres, sin(45)*metres)
+        WHEN lower(regexp_replace(direction, '\s', '')) = 'northeast'
+        THEN ST_Translate(oid_geom, cos(45)*metres, sin(45)*metres)
+    END
+);
 
 BEGIN
 --rotate every point to 17.5 degree anticlockwise (0.305 radians) based on the intersection point
@@ -23,12 +32,12 @@ RETURN ST_Transform(ST_SetSRID(ST_Rotate(translated_geom, 0.305, oid_geom), 2952
 END;
 $translated_geom$ LANGUAGE plpgsql;
 
-
 COMMENT ON FUNCTION gis._translate_intersection_point(
     oid_geom geometry, metres float, direction text
-) IS '
-Inputs are the geometry of a point, the number of units that you would like the point to be translated, and the direction that you would like the point to be translated.
-The function returns the input point geometry translated in the direction inputted, by the number of metres inputted.
-Note: if you would like to move your geometry by a certian number of metres, make sure the point geometry has a projection that has metres as the unit.
-If the input geometry is unprojected (i.e. SRID = 4326) then the function returns a geometry that has been translated by the number of degrees inputted.
-';
+) IS 'Inputs are the geometry of a point, the number of units that you would like the point to be
+translated, and the direction that you would like the point to be translated. The function
+returns the input point geometry translated in the direction inputted, by the number of metres
+inputted. Note: if you would like to move your geometry by a certian number of metres, make
+sure the point geometry has a projection that has metres as the unit. If the input geometry
+is unprojected (i.e. SRID = 4326) then the function returns a geometry that has been translated
+by the number of degrees inputted.';
