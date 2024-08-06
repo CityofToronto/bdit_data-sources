c6be6cba10d11f350fddb7cdd452b22740b27da2
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-08-06 18:37:26 +0000

    merge changes in notification formatting #855

--- volumes/miovision/sql/data_checks/select-ongoing_intersection_outages.sql
+++ volumes/miovision/sql/data_checks/select-ongoing_intersection_outages.sql
@@ -1,8 +1,8 @@
 WITH ongoing_outages AS (
     SELECT
-        i.intersection_name || ' (id: ' || i.id || ') - data last received: '
-        || MAX(v.dt) || ' (' || '{{ macros.ds_add(ds, 6) }}'::date --noqa: TMP, LT05
-        - MAX(v.dt) || ' days)' AS descrip
+        i.intersection_name || ' (id: `' || i.id || '`) - data last received: `'
+        || MAX(v.dt::date) || '` (' || '{{ macros.ds_add(ds, 6) }}'::date --noqa: TMP, LT05
+        - MAX(v.dt::date) || ' days)' AS descrip
     FROM miovision_api.volumes_daily_unfiltered AS v
     JOIN miovision_api.intersections AS i USING (intersection_uid)
     WHERE i.date_decommissioned IS NULL
@@ -20,5 +20,5 @@ SELECT
     || COALESCE(COUNT(ongoing_outages.*), 0)
     || CASE WHEN COUNT(ongoing_outages.*) = 1 THEN ' ongoing outage.' ELSE ' ongoing outages.'
     END AS summ, --gap_threshold
-    array_agg(ongoing_outages.descrip || chr(10)) AS gaps
+    array_agg(ongoing_outages.descrip) AS gaps
 FROM ongoing_outages
