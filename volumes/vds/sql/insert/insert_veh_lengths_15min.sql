04bfe71056489376debee55a163b718c03b6c3c6
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-08-06 13:34:45 +0000

    #1022 fluff

--- volumes/vds/sql/insert/insert_veh_lengths_15min.sql
+++ volumes/vds/sql/insert/insert_veh_lengths_15min.sql
@@ -17,13 +17,14 @@ SELECT
     ) AS total_count
 FROM vds.raw_vdsvehicledata AS v
 --left join to preserve null lengths in summary table
-LEFT JOIN traffic.mto_length_bin_classification AS mto ON v.length_meter::numeric <@ mto.length_range
+LEFT JOIN traffic.mto_length_bin_classification AS mto
+ON v.length_meter::numeric <@ mto.length_range
 WHERE
     v.dt >= '{{ ds }} 00:00:00'::timestamp -- noqa: TMP
     AND v.dt < '{{ ds }} 00:00:00'::timestamp + interval '1 DAY' -- noqa: TMP
     AND v.vdsconfig_uid IS NOT NULL
     AND v.entity_location_uid IS NOT NULL
-GROUP BY 
+GROUP BY
     v.division_id,
     v.vdsconfig_uid,
     v.entity_location_uid,
