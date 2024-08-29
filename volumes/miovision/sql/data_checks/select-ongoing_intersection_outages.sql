cf085affb380dd5bb488f21a297e772eaae0fbc4
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-08-29 21:43:03 +0000

    #1053 update alert sqls

--- volumes/miovision/sql/data_checks/select-ongoing_intersection_outages.sql
+++ volumes/miovision/sql/data_checks/select-ongoing_intersection_outages.sql
@@ -1,24 +1,23 @@
-WITH ongoing_outages AS (
+WITH all_outages AS (
+    SELECT COUNT(*) AS cnt
+    FROM miovision_api.open_issues_review
+    WHERE COALESCE(classification_uid, 0) != 2
+),
+
+total_outages AS (
     SELECT
-        i.intersection_name || ' (id: ' || i.id || ') - data last received: '
-        || MAX(v.dt) || ' (' || '{{ macros.ds_add(ds, 6) }}'::date --noqa: TMP, LT05
-        - MAX(v.dt) || ' days)' AS descrip
-    FROM miovision_api.volumes_daily_unfiltered AS v
-    JOIN miovision_api.intersections AS i USING (intersection_uid)
-    WHERE i.date_decommissioned IS NULL
-    GROUP BY
-        i.intersection_uid,
-        i.intersection_name
-    HAVING
-        MAX(v.dt)
-        < '{{ macros.ds_add(ds, 6) }}'::date - interval '{{ params.min_duration }}' --noqa: TMP
+        intersection_name || ' (id: ' || intersection_id || ') - data last received: '
+        || range_start || ' (' || CURRENT_DATE - range_start || ' days)' AS descrip
+    FROM miovision_api.open_issues_review
+    WHERE classification_uid IS NULL
 )
 
 SELECT
-    COUNT(ongoing_outages.*) < 1 AS _check,
-    CASE WHEN COUNT(ongoing_outages.*) = 1 THEN 'There is ' ELSE 'There are ' END
-    || COALESCE(COUNT(ongoing_outages.*), 0)
-    || CASE WHEN COUNT(ongoing_outages.*) = 1 THEN ' ongoing outage.' ELSE ' ongoing outages.'
+    (SELECT cnt FROM all_outages) < 1 AS _check,
+    CASE WHEN COUNT(total_outages.*) = 1 THEN 'There is ' ELSE 'There are ' END
+    || COALESCE(COUNT(total_outages.*), 0)
+    || CASE WHEN COUNT(total_outages.*) = 1 THEN ' full outages.' ELSE ' full outages '
+    || 'and ' || (SELECT cnt FROM all_outages) || ' partial outages. See `miovision_api.open_issues_review`.'
     END AS summ, --gap_threshold
-    array_agg(ongoing_outages.descrip || chr(10)) AS gaps
-FROM ongoing_outages
+    array_agg(total_outages.descrip || chr(10)) AS gaps
+FROM total_outages
