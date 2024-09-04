c01e25dac1f91a3e277954d406cd0fcb5fcd5540
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-09-04 14:16:04 +0000

    #1057 change from update to upsert

--- volumes/vds/sql/insert/insert_raw_vdsdata.sql
+++ volumes/vds/sql/insert/insert_raw_vdsdata.sql
@@ -1,32 +1,28 @@
 WITH inserted AS (
     INSERT INTO vds.raw_vdsdata (
-        division_id, vds_id, dt, datetime_15min, lane, speed_kmh, volume_veh_per_hr, occupancy_percent
+        division_id, vds_id, dt, datetime_15min, lane, speed_kmh, volume_veh_per_hr,
+        occupancy_percent
     ) VALUES %s
     RETURNING division_id, vdsconfig_uid, entity_location_uid, dt
-),
-
-updated_dates AS (
-    SELECT
-        division_id,
-        vdsconfig_uid,
-        entity_location_uid,
-        MIN(dt) AS min_dt,
-        MAX(dt) AS max_dt
-    FROM inserted
-    WHERE
-        division_id + vdsconfig_uid + entity_location_uid IS NOT NULL
-    GROUP BY
-        division_id,
-        vdsconfig_uid,
-        entity_location_uid
 )
 
-UPDATE vds.last_active AS x
-SET
-    first_active = LEAST(ud.min_dt, x.first_active),
-    last_active = GREATEST(ud.max_dt, x.last_active)
-FROM updated_dates AS ud
+INSERT INTO vds.last_active AS existing (
+    division_id, vdsconfig_uid, entity_location_uid, first_active, last_active
+)
+SELECT
+    division_id,
+    vdsconfig_uid,
+    entity_location_uid,
+    MIN(dt) AS min_dt,
+    MAX(dt) AS max_dt
+FROM inserted
 WHERE
-    x.division_id = ud.division_id
-    AND x.vdsconfig_uid = ud.vdsconfig_uid
-    AND x.entity_location_uid = ud.entity_location_uid;
+    division_id + vdsconfig_uid + entity_location_uid IS NOT NULL
+GROUP BY
+    division_id,
+    vdsconfig_uid,
+    entity_location_uid
+ON CONFLICT (vdsconfig_uid, entity_location_uid, division_id)
+DO UPDATE SET
+first_active = LEAST(MIN(inserted.dt), existing.first_active),
+last_active = GREATEST(MAX(inserted.dt), existing.last_active);
