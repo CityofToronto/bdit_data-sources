c6f03a98c827c57a48a139eb0ec59f7f406fe994
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-09-09 20:58:31 +0000

    #1065 add check constraints to intersection_movements

--- volumes/miovision/sql/table/create-table-intersection_movements.sql
+++ volumes/miovision/sql/table/create-table-intersection_movements.sql
@@ -26,3 +26,11 @@ CREATE OR REPLACE TRIGGER intersection_movements_denylist_exclusion
 BEFORE INSERT ON miovision_api.intersection_movements
 FOR EACH ROW
 EXECUTE FUNCTION miovision_api.intersection_movements_exclude();
+
+ALTER TABLE IF EXISTS miovision_api.intersection_movements
+ADD CONSTRAINT intersecton_movements_exclude_bike_exits
+CHECK (NOT (classification_uid = 10 AND movement_uid = 8));
+
+ALTER TABLE IF EXISTS miovision_api.intersection_movements
+ADD CONSTRAINT intersecton_movements_exclude_xwalk_bikes
+CHECK (NOT (classification_uid = 7));
