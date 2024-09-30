5403a055027ed43c2d6f05363ecf64359e91696b
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-09-30 21:25:28 +0000

    #774 improved method of matching arterydata to traffic_signals

--- volumes/short_term_counting_program/sql/artery_locations_px.sql
+++ volumes/short_term_counting_program/sql/artery_locations_px.sql
@@ -1,9 +1,23 @@
-CREATE VIEW traffic.artery_locations_px AS
+CREATE VIEW traffic.artery_locations_px AS (
 
-	SELECT      arterycode,
-                (regexp_matches(location::text, 'PX (\d+)'::text))[1]::integer AS px,
-                regexp_replace(location::text, E'[\\n\\r]+', ' ', 'g') as location
-    FROM        traffic.arterydata;
-	
-COMMENT ON VIEW traffic.artery_locations_px IS 'Lookup between artery codes and px numbers (intersections)';
+
+    SELECT
+        arterydata.arterycode,
+        traffic_signal.px,
+        arterydata.location,
+        signals.geom
+    FROM traffic.arterydata
+    JOIN traffic.arteries_centreline USING (arterycode)
+    JOIN traffic.traffic_signal
+        ON traffic_signal."centrelineId" = arteries_centreline.centreline_id
+    JOIN gis.traffic_signal AS signals
+        ON trim(leading '0' FROM signals.px)::int = traffic_signal.px
+    ORDER BY px
+
+);
+
+COMMENT ON VIEW traffic.artery_locations_px IS
+'Lookup between artery codes and px numbers (intersections). '
+'Note that multiple arterycodes can be mapped to a single PX. ';
+
 GRANT SELECT ON TABLE traffic.artery_locations_px TO bdit_humans;
