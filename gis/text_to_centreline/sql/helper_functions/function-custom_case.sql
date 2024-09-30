6305660b5d05130cf43ce8f858c07188a4e62dc8
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-09-30 18:21:16 +0000

    #628 move direction cleaning to custom_case

--- gis/text_to_centreline/sql/helper_functions/function-custom_case.sql
+++ gis/text_to_centreline/sql/helper_functions/function-custom_case.sql
@@ -21,6 +21,11 @@ txt := regexp_REPLACE(txt, ' Point ', ' point ', 'g');
 txt := regexp_REPLACE(txt, ' Between ', ' between ', 'g');
 txt := regexp_REPLACE(txt, ' Thereof ', ' thereof ', 'g');
 txt := regexp_REPLACE(txt, ' The ', ' the ', 'g');
+--clean directions
+txt := regexp_REPLACE(txt, '( north/west )|( north west )|( west/north )', ' Northwest ', 'gi');
+txt := regexp_REPLACE(txt, '( north/east )|( north east )|( east/north )', ' Northeast ', 'gi');
+txt := regexp_REPLACE(txt, '( south/east )|( south east )|( east/south )', ' Southeast ', 'gi');
+txt := regexp_REPLACE(txt, '( south/west )|( south west )|( west/south )', ' Southwest ', 'gi');
 RETURN txt;
 END;
 $BODY$;
