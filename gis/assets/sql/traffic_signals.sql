38563be9a2b19177749a6f588412b57ea78e6cac
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-10-09 19:03:10 +0000

    #1079 lint+update traffic signals

--- gis/assets/sql/traffic_signals.sql
+++ gis/assets/sql/traffic_signals.sql
@@ -1,36 +1,87 @@
-﻿DROP TABLE IF EXISTS gis.traffic_signals;
-CREATE TABLE gis.traffic_signals (
-PX int primary key ,
-Main text ,
-Mid_Block text ,
-Side_1 text ,
-Side_2 text ,
-Private_Access text ,
-Additional_info text ,
-Geo_id bigint not null,
-Node_id bigint not null,
-X numeric,
-Y numeric ,
-Latitude text ,
-Longitude text ,
-Activation_date DATE,
-Signal_System text ,
-Non_system text ,
-Mode_of_Control text ,
-Pedestrian_Walk_speed text ,
-APS_Signal smallint,
-APS_Operation text ,
-APS_Activation_date DATe,
-Transit_Preempt BOOLEAN,
-Fire_preempt BOOLEAN,
-Rail_preempt BOOLEAN,
-Signalized_Approaches smallint ,
-UPS BOOLEAN,
-LED_Blankout BOOLEAN,
-lpi BOOLEAN ,
-Bicycle_Signal BOOLEAN
-);
-GRANT SELECT ON gis.traffic_signals TO bdit_humans;
+﻿-- Table: gis.traffic_signal
+-- DROP TABLE IF EXISTS gis.traffic_signal;
 
-ALTER TABLE gis.traffic_signals ADD COLUMN geom geometry(Point, 4326);
-UPDATE gis.traffic_signals SET geom = ST_SetSRID(ST_MakePoint(latitude::numeric, longitude::numeric), 4326);
+CREATE TABLE IF NOT EXISTS gis.traffic_signal
+(
+    px text COLLATE pg_catalog."default" NOT NULL,
+    main_street text COLLATE pg_catalog."default",
+    midblock_route text COLLATE pg_catalog."default",
+    side1_street text COLLATE pg_catalog."default",
+    side2_street text COLLATE pg_catalog."default",
+    private_access text COLLATE pg_catalog."default",
+    additional_info text COLLATE pg_catalog."default",
+    x numeric,
+    y numeric,
+    latitude numeric,
+    longitude numeric,
+    activationdate timestamp without time zone,
+    signalsystem text COLLATE pg_catalog."default",
+    non_system text COLLATE pg_catalog."default",
+    control_mode text COLLATE pg_catalog."default",
+    pedwalkspeed text COLLATE pg_catalog."default",
+    aps_operation text COLLATE pg_catalog."default",
+    numberofapproaches text COLLATE pg_catalog."default",
+    objectid integer,
+    geo_id integer,
+    node_id integer,
+    audiblepedsignal numeric,
+    transit_preempt numeric,
+    fire_preempt numeric,
+    rail_preempt numeric,
+    mi_prinx integer,
+    geom geometry,
+    bicycle_signal numeric,
+    ups numeric,
+    led_blankout_sign numeric,
+    lpi_north_implementation_date timestamp without time zone,
+    lpi_south_implementation_date timestamp without time zone,
+    lpi_east_implementation_date timestamp without time zone,
+    lpi_west_implementation_date timestamp without time zone,
+    lpi_comment text COLLATE pg_catalog."default",
+    aps_activation_date timestamp without time zone,
+    leading_pedestrian_intervals integer,
+    CONSTRAINT _traffic_signal_pkey PRIMARY KEY (px)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS gis.traffic_signal OWNER TO gis_admins;
+
+REVOKE ALL ON TABLE gis.traffic_signal FROM bdit_humans;
+
+GRANT SELECT ON TABLE gis.traffic_signal TO bdit_humans;
+
+GRANT ALL ON TABLE gis.traffic_signal TO gis_admins;
+
+GRANT ALL ON TABLE gis.traffic_signal TO vz_api_bot;
+
+COMMENT ON TABLE gis.traffic_signal
+IS 'Updated daily by the assets_pull dag https://github.com/CityofToronto/bdit_data-sources/tree/master/gis/assets';
+-- Index: traffic_signal_gix
+
+-- DROP INDEX IF EXISTS gis.traffic_signal_gix;
+
+CREATE INDEX IF NOT EXISTS traffic_signal_gix
+ON gis.traffic_signal USING gist
+(geom)
+TABLESPACE pg_default;
+
+-- Trigger: audit_trigger_row
+
+-- DROP TRIGGER IF EXISTS audit_trigger_row ON gis.traffic_signal;
+
+CREATE OR REPLACE TRIGGER audit_trigger_row
+AFTER INSERT OR DELETE OR UPDATE
+ON gis.traffic_signal
+FOR EACH ROW
+EXECUTE FUNCTION gis.if_modified_func('true');
+
+-- Trigger: audit_trigger_stm
+
+-- DROP TRIGGER IF EXISTS audit_trigger_stm ON gis.traffic_signal;
+
+CREATE OR REPLACE TRIGGER audit_trigger_stm
+AFTER TRUNCATE
+ON gis.traffic_signal
+FOR EACH STATEMENT
+EXECUTE FUNCTION gis.if_modified_func('true');
