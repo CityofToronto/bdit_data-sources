59c352031397fd6680b3104ece3b26ebe8bac427
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-10-22 21:02:59 +0000

    #1085 open data daily view

---
+++ volumes/ecocounter/views/create-view-open_data_daily_counts.sql
@@ -0,0 +1,31 @@
+DROP VIEW IF EXISTS ecocounter.open_data_daily_counts;
+CREATE VIEW ecocounter.open_data_daily_counts AS
+
+SELECT
+    s.site_id,
+    s.site_description,
+    cc.flow_id,
+    initcap(f.flow_direction) AS flow_direction,
+    cc.datetime_bin::date AS dt,
+    SUM(cc.raw_volume) AS raw_volume,
+    cc.correction_factor,
+    cc.validation_date,
+    ROUND(COALESCE(cc.correction_factor, 1) * SUM(cc.raw_volume)) AS corrected_volume
+--this view excludes anomalous ranges
+FROM ecocounter.counts_corrected AS cc
+JOIN ecocounter.flows AS f USING (flow_id)
+JOIN ecocounter.sites AS s USING (site_id)
+GROUP BY
+    s.site_id,
+    s.site_description,
+    cc.flow_id,
+    f.flow_direction,
+    cc.datetime_bin::date,
+    cc.correction_factor,
+    cc.validation_date,
+    cc.corrected_volume;
+
+COMMENT ON VIEW ecocounter.open_data_daily_counts IS
+'(In development) daily data scaled based on Spectrum studies for Open Data.';
+
+SELECT * FROM ecocounter.open_data_daily_counts WHERE dt >= '2024-10-01';
