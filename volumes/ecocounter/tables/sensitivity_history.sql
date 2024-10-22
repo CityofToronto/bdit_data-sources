5adb3fed54bfcd3ddccc4a1cc41c00e8f7e2eb12
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-10-22 21:00:20 +0000

    #1085 script used to populate sensitivity history

--- volumes/ecocounter/tables/sensitivity_history.sql
+++ volumes/ecocounter/tables/sensitivity_history.sql
@@ -22,3 +22,23 @@ REVOKE ALL ON TABLE ecocounter.sensitivity_history FROM bdit_humans;
 GRANT SELECT ON TABLE ecocounter.sensitivity_history TO bdit_humans;
 
 GRANT ALL ON TABLE ecocounter.sensitivity_history TO ecocounter_admins;
+
+/*
+--initial code used to populate
+
+INSERT INTO ecocounter.sensitivity_history (flow_id, sensitivity, date_range)
+
+SELECT
+    flow_id,
+    regexp_substr(reverse(setting), '[0-9]', 1)::int AS sensitivity, #the second number
+    daterange(date_of_change, null)
+FROM ecocounter.sensitivity_changes
+WHERE uid IN (16,17,18,19,20,21,22) --the ones that were like #->#
+UNION
+SELECT
+    flow_id,
+    regexp_substr(setting, '[0-9]', 1)::int AS sensitivity, #the first number
+    daterange(null, date_of_change)
+FROM ecocounter.sensitivity_changes
+WHERE uid IN (16,17,18,19,20,21,22)
+*/
