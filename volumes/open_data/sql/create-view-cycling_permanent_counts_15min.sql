b0b7c3f123d1527545a86a3ae49a89273ed4ac05
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2024-11-29 16:55:19 -0500

    #1085 add location_dir_id, extra columns to summary table

--- volumes/open_data/sql/create-view-cycling_permanent_counts_15min.sql
+++ volumes/open_data/sql/create-view-cycling_permanent_counts_15min.sql
@@ -1,14 +1,18 @@
 -- View: open_data.cycling_permanent_counts_15min
 
--- DROP TABLE IF EXISTS open_data.cycling_permanent_counts_15min;
+-- DROP VIEW IF EXISTS open_data.cycling_permanent_counts_15min;
 
 CREATE VIEW open_data.cycling_permanent_counts_15min AS
 SELECT
-    site_description::text AS location_name,
-    direction::text AS direction,
-    datetime_bin,
-    bin_volume::integer
-FROM ecocounter.open_data_15min_counts;
+    od.location_dir_id,
+    eco.site_description::text AS location_name,
+    eco.direction::text AS direction,
+    eco.datetime_bin,
+    eco.bin_volume::integer
+FROM ecocounter.open_data_15min_counts AS eco
+JOIN open_data.cycling_permanent_counts_locations AS od
+    ON eco.site_description::text = od.location_name
+    AND eco.direction::text = od.direction;
 
 ALTER TABLE IF EXISTS open_data.cycling_permanent_counts_15min OWNER TO od_admins;
 
