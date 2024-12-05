7fdca5bcf829c3309f339ef3c7621e9142b03c74
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2024-12-05 16:58:35 +0000

    #1108 sql for camera_details, config updates

--- volumes/miovision/sql/table/create-table-camera_details.sql
+++ volumes/miovision/sql/table/create-table-camera_details.sql
@@ -4,28 +4,26 @@
 
 CREATE TABLE IF NOT EXISTS miovision_api.camera_details
 (
-    intersection_id text COLLATE pg_catalog."default",
-    intersection_lat numeric,
-    intersection_long numeric,
-    intersection_name text COLLATE pg_catalog."default",
-    intersection_customid text COLLATE pg_catalog."default",
-    camera_id text COLLATE pg_catalog."default",
-    camera_type text COLLATE pg_catalog."default",
+    intersection_id text COLLATE pg_catalog."default" NOT NULL,
+    api_name text COLLATE pg_catalog."default",
+    camera_id text COLLATE pg_catalog."default" NOT NULL,
     camera_label text COLLATE pg_catalog."default",
-    camera_streamurl text COLLATE pg_catalog."default"
+    last_seen date NOT NULL,
+    CONSTRAINT camera_details_pkey PRIMARY KEY (intersection_id, camera_id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS miovision_api.camera_details OWNER TO
-miovision_api_bot;
+ALTER TABLE IF EXISTS miovision_api.camera_details
+    OWNER to miovision_api_bot;
 
 REVOKE ALL ON TABLE miovision_api.camera_details FROM bdit_humans;
+
 GRANT SELECT ON TABLE miovision_api.camera_details TO bdit_humans;
 
 GRANT ALL ON TABLE miovision_api.camera_details TO miovision_admins;
+
 GRANT ALL ON TABLE miovision_api.camera_details TO miovision_api_bot;
 
-COMMENT ON TABLE miovision_api.camera_details IS
-'Miovision camera details, excluding decommissioned intersections. '
-'Update using `get_camera_info.py` script.';
+COMMENT ON TABLE miovision_api.camera_details
+IS 'Miovision camera details. Updated by miovision_hardware Airflow DAG.';
