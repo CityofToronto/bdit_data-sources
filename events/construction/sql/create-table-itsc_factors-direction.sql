d7c3f90f709811ce247a318e7acf827d11e77bf0
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2024-12-06 17:42:50 -0500

    #1088 lookup tables

---
+++ events/construction/sql/create-table-itsc_factors-direction.sql
@@ -0,0 +1,15 @@
+-- Table: itsc_factors.direction
+
+-- DROP TABLE IF EXISTS itsc_factors.direction;
+
+CREATE TABLE IF NOT EXISTS itsc_factors.direction
+(
+    code integer NOT NULL,
+    direction text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT itsc_factors_direction_pkey PRIMARY KEY (code)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS itsc_factors.direction
+OWNER TO congestion_admins;
