2d402a21db45d4b8fa8d2dbcf420eb70dcc5616e
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2024-12-12 13:50:11 -0500

    #1088 add on insert delete trigger to locations

---
+++ events/construction/sql/create-function-delete_old_itsc_issue_locations.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION congestion_events.delete_old_itsc_issue_locations()
+RETURNS TRIGGER AS $$
+BEGIN
+
+    WITH latest AS (
+        SELECT
+            divisionid,
+            issueid,
+            MAX(timestamputc) AS max_timestamputc
+        FROM congestion_events.itsc_issue_locations
+        GROUP BY
+            divisionid,
+            issueid
+    )
+
+  -- Delete records older than the current one for the same primary keys
+  DELETE FROM congestion_events.itsc_issue_locations AS iil
+  USING latest
+  WHERE
+      iil.divisionid = latest.divisionid
+      AND iil.issueid = latest.issueid
+      AND iil.timestamputc < latest.max_timestamputc;
+
+END;
+$$ LANGUAGE plpgsql;
+
+GRANT EXECUTE ON congestion_events.delete_old_itsc_issue_locations TO congestion_admins;
+GRANT EXECUTE ON congestion_events.delete_old_itsc_issue_locations TO events_bot;
+
+COMMENT ON FUNCTION congestion_events.delete_old_itsc_issue_locations IS
+'Deletes old records from congestion_events.itsc_issue_locations on insert (trigger).';
