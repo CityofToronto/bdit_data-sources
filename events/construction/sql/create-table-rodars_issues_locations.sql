88a78dc517dfb9e8e5ecff8ad87288c892f353de
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2024-12-20 16:32:28 -0500

    #1088 rename itsc->rodars (harder)

--- events/construction/sql/create-table-rodars_issues_locations.sql
+++ events/construction/sql/create-table-rodars_issues_locations.sql
@@ -1,8 +1,8 @@
--- Table: congestion_events.itsc_issue_locations
+-- Table: congestion_events.rodars_issue_locations
 
--- DROP TABLE IF EXISTS congestion_events.itsc_issue_locations;
+-- DROP TABLE IF EXISTS congestion_events.rodars_issue_locations;
 
-CREATE TABLE IF NOT EXISTS congestion_events.itsc_issue_locations
+CREATE TABLE IF NOT EXISTS congestion_events.rodars_issue_locations
 (
     divisionid smallint NOT NULL,
     issueid integer NOT NULL,
@@ -30,18 +30,18 @@ CREATE TABLE IF NOT EXISTS congestion_events.itsc_issue_locations
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS congestion_events.itsc_issue_locations OWNER TO dbadmin;
+ALTER TABLE IF EXISTS congestion_events.rodars_issue_locations OWNER TO dbadmin;
 
-REVOKE ALL ON TABLE congestion_events.itsc_issue_locations FROM bdit_humans;
-GRANT SELECT ON TABLE congestion_events.itsc_issue_locations TO bdit_humans;
+REVOKE ALL ON TABLE congestion_events.rodars_issue_locations FROM bdit_humans;
+GRANT SELECT ON TABLE congestion_events.rodars_issue_locations TO bdit_humans;
 
-GRANT ALL ON TABLE congestion_events.itsc_issue_locations TO dbadmin;
+GRANT ALL ON TABLE congestion_events.rodars_issue_locations TO dbadmin;
 
-GRANT ALL ON TABLE congestion_events.itsc_issue_locations TO rds_superuser WITH GRANT OPTION;
+GRANT ALL ON TABLE congestion_events.rodars_issue_locations TO rds_superuser WITH GRANT OPTION;
 
-GRANT ALL ON TABLE congestion_events.itsc_issue_locations TO events_bot;
+GRANT ALL ON TABLE congestion_events.rodars_issue_locations TO events_bot;
 
 CREATE TRIGGER on_insert_delete_old
-AFTER INSERT ON congestion_events.itsc_issue_locations
+AFTER INSERT ON congestion_events.rodars_issue_locations
 FOR EACH STATEMENT
-EXECUTE FUNCTION congestion_events.delete_old_itsc_issue_locations();
+EXECUTE FUNCTION congestion_events.delete_old_rodars_issue_locations();
