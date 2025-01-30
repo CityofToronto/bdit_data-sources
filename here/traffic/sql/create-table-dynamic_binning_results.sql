7d3a0552c78554aa164436cf80af7ebca52ce3c0
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-01-30 23:15:49 +0000

    #1132 update dynamic bin results table; add uri

--- here/traffic/sql/create-table-dynamic_binning_results.sql
+++ here/traffic/sql/create-table-dynamic_binning_results.sql
@@ -2,7 +2,8 @@
 
 -- DROP TABLE IF EXISTS gwolofs.dynamic_binning_results;
 
-CREATE TABLE IF NOT EXISTS gwolofs.dynamic_binning_results (
+CREATE TABLE IF NOT EXISTS gwolofs.dynamic_binning_results
+(
     time_grp tsrange NOT NULL,
     bin_range tsrange NOT NULL,
     dt_start timestamp without time zone,
@@ -13,17 +14,20 @@ CREATE TABLE IF NOT EXISTS gwolofs.dynamic_binning_results (
     length_w_data numeric,
     num_obs integer,
     segment_uid smallint,
+    uri_string text COLLATE pg_catalog."default",
     CONSTRAINT dynamic_bins_unique_temp EXCLUDE USING gist (
         bin_range WITH &&,
         time_grp WITH =,
-        segment_uid WITH =
+        segment_uid WITH =,
+        uri_string WITH =
     )
+
 )
 
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS gwolofs.dynamic_binning_results
-OWNER TO gwolofs;
+    OWNER TO gwolofs;
 
 REVOKE ALL ON TABLE gwolofs.dynamic_binning_results FROM bdit_humans;
 
@@ -35,10 +39,10 @@ GRANT ALL ON TABLE gwolofs.dynamic_binning_results TO gwolofs;
 -- DROP INDEX IF EXISTS gwolofs.dynamic_binning_results_time_grp_segment_uid_idx;
 
 CREATE INDEX IF NOT EXISTS dynamic_binning_results_time_grp_segment_uid_idx
-ON gwolofs.dynamic_binning_results USING btree
-(
-    time_grp ASC NULLS LAST,
-    segment_uid ASC NULLS LAST
-)
-WITH (deduplicate_items=True)
-TABLESPACE pg_default;
+    ON gwolofs.dynamic_binning_results USING btree
+    (
+        time_grp ASC NULLS LAST,
+        segment_uid ASC NULLS LAST
+    )
+    WITH (deduplicate_items=True)
+    TABLESPACE pg_default;
