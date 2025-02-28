27c16b426b4166654ca0014e030fd0bf3ee75d8c
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2025-02-28 09:53:23 -0500

    #1156 minor column, index, pkey changes

--- bluetooth/itsc/sql/select-itsc-tt_raw.sql
+++ bluetooth/itsc/sql/select-itsc-tt_raw.sql
@@ -1,17 +1,17 @@
 SELECT
-    divisionid,
-    pathid,
-    rawdatatype,
-    timezone('UTC', timestamputc) AT TIME ZONE 'Canada/Eastern' AS dt, --convert timestamp (without timezone) at UTC to EDT/EST
-    round(traveltimems / 1000, 1) AS traveltime_s,
-    qualitymetric,
-    numsamples,
-    congestionstartmeters,
-    congestionendmeters,
-    minspeedkmh,
-    round(fifthpercentiletraveltimems / 1000, 1) AS fifthpercentiletraveltime_s,
-    round(nintyfifthpercentiletraveltimems / 1000, 1) AS nintyfifthpercentiletraveltime_s,
-    unmatched
+    divisionid AS division_id,
+    pathid AS path_id,
+    rawdatatype AS raw_data_type,
+    timezone('UTC', timestamputc) AT TIME ZONE 'Canada/Eastern' AS dt, --convert timestamp (without timezone) at UTC to EDT/ES AS dtT
+    round(traveltimems / 1000, 1) AS traveltime_s AS travel_time_s,
+    qualitymetric AS quality_metric,
+    numsamples AS num_samples,
+    congestionstartmeters AS congestion_start_meters,
+    congestionendmeters AS congestion_end_meters,
+    minspeedkmh AS min_speed_kmh,
+    round(fifthpercentiletraveltimems / 1000, 1) AS fifth_percentile_tt_s,
+    round(nintyfifthpercentiletraveltimems / 1000, 1) AS nintyfifth_percentile_tt_s,
+    unmatched AS unmatched
 FROM public.traveltimepathrawdata
 WHERE
     --this table only has divisionid = 2 data
