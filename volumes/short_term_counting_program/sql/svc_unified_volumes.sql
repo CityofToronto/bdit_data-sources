6e1796a939562864d210c0a707c4813ce87f8de7
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-03-04 20:55:39 +0000

    #1150 split into svc_daily_totals, svc_unified_volumes

---
+++ volumes/short_term_counting_program/sql/svc_unified_volumes.sql
@@ -0,0 +1,86 @@
+CREATE OR REPLACE VIEW traffic.svc_unified_volumes AS
+
+-- speed studies
+SELECT
+    study_id,
+    time_start::date AS count_date,
+    time_start,
+    time_end,
+    direction,
+    SUM(
+        vol_1_19_kph
+        + vol_20_25_kph
+        + vol_26_30_kph
+        + vol_31_35_kph
+        + vol_36_40_kph
+        + vol_41_45_kph
+        + vol_46_50_kph
+        + vol_51_55_kph
+        + vol_56_60_kph
+        + vol_61_65_kph
+        + vol_66_70_kph
+        + vol_71_75_kph
+        + vol_76_80_kph
+        + vol_81_160_kph
+    ) AS volume
+FROM traffic.svc_study_speed
+GROUP BY
+    study_id,
+    time_start::date,
+    time_start,
+    time_end,
+    direction
+ --15 minute bins
+
+UNION
+
+-- volume studies
+SELECT
+    study_id,
+    count_date,
+    time_start,
+    time_end,
+    direction,
+    SUM(volume) AS volume
+FROM traffic.svc_study_volume
+GROUP BY
+    study_id,
+    count_date,
+    time_start,
+    time_end,
+    direction
+
+UNION
+
+-- classification studies
+SELECT
+    study_id,
+    count_date,
+    time_start,
+    time_end,
+    direction,
+    SUM(
+        -- check that these are mutually exclusive
+        motorcycle
+        + cars
+        + "2a_4t"
+        + buses
+        + "2a_su"
+        + "3a_su"
+        + "4a_su"
+        + "4a_st"
+        + "5a_st"
+        + "6a_st"
+        + "5a_mt"
+        + "6a_mt"
+    ) AS volume
+FROM traffic.svc_study_class
+GROUP BY
+    study_id,
+    count_date,
+    time_start,
+    time_end,
+    direction;
+
+COMMENT ON VIEW traffic.svc_unified_volumes IS
+'A unified view of Speed, Volume, and Classification study volumes by 15 minute bin.';
