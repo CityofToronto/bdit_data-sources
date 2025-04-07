9e17dc6e8d74611b2104dec99bbab7627d530f94
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2025-04-07 17:14:15 -0400

    #1156 fix geoms to use all centreline partitions

--- bluetooth/itsc/sql/function-add_bluetooth_path_geom.sql
+++ bluetooth/itsc/sql/function-add_bluetooth_path_geom.sql
@@ -2,10 +2,15 @@ CREATE OR REPLACE FUNCTION bluetooth.itsc_add_bluetooth_path_geom()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Add geom by getting the matching paths from centreline_latest
-    SELECT st_union(centreline_latest.geom) INTO NEW.geom
-    FROM gis_core.centreline_latest
-    WHERE centreline_latest.centreline_id = ANY(NEW.centreline_ids); --array of centreline_ids
-
+    WITH cent AS (
+        SELECT DISTINCT ON (centreline_id) centreline_id, geom
+        FROM gis_core.centreline
+        WHERE centreline_id = ANY(NEW.centreline_ids) --array of centreline_ids
+        ORDER BY centreline_id, version_date DESC
+    )
+    SELECT st_union(geom) INTO NEW.geom
+    FROM cent;
+    
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
