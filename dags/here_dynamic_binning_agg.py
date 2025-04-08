3ea3d8ad63d91c33cff307c13ed96cde9a243686
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-04-08 18:27:17 +0000

    #1132 add here_dynamic_binning_agg DAG

---
+++ dags/here_dynamic_binning_agg.py
@@ -0,0 +1,61 @@
+import sys
+import os
+import logging
+import pendulum
+from datetime import timedelta
+
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.models import Variable
+from airflow.decorators import dag, task
+
+try:
+    repo_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    sys.path.insert(0, repo_path)
+    from dags.dag_functions import task_fail_slack_alert
+except:
+    raise ImportError("Cannot import slack alert functions")
+    
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+doc_md = "This DAG is running off the `1132-here-aggregation-proposal` branch to test dynamic binning aggregation."
+DAG_NAME = 'here_dynamic_binning_agg'
+DAG_OWNERS = Variable.get('dag_owners', deserialize_json=True).get(DAG_NAME, ["Unknown"]) 
+
+# Slack alert
+SLACK_CONN_ID = 'slack_data_pipeline'
+
+default_args = {
+    'owner': ','.join(DAG_OWNERS),
+    'depends_on_past':False,
+    'start_date': pendulum.datetime(2025, 4, 1, tz="America/Toronto"),
+    'email_on_failure': False,
+    'email_on_success': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+    'on_failure_callback': task_fail_slack_alert
+}
+
+@dag(
+    DAG_NAME, 
+    default_args=default_args, 
+    schedule=None, # triggered by `pull_here_path` DAG
+    doc_md = doc_md,
+    tags=["HERE", "aggregation"],
+    catchup=False
+)
+
+#to add: catchup, one task at a time, depends on past.
+
+def here_dynamic_binning_agg():
+
+    aggregate_daily = SQLExecuteQueryOperator(
+        #sql="SELECT covid.generate_citywide_tti( '{{macros.ds_add(ds, -1)}}' )",
+        task_id='aggregate_daily',
+        conn_id='congestion_bot',
+        autocommit=True,
+        retries = 0
+    )
+    aggregate_daily
+
+here_dynamic_binning_agg()
