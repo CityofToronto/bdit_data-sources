bb9a87fb6f15d73373b6c60b973122648bcef872
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-04-09 16:46:50 +0000

    #1132 remove time_grp, add bin_start, add temp table to reduce constraint size

--- here/traffic/sql/dynamic_bins/function-congestion_network_segment_agg.sql
+++ here/traffic/sql/dynamic_bins/function-congestion_network_segment_agg.sql
@@ -18,6 +18,20 @@ DECLARE
 
 BEGIN
 
+--using a temp table to aply the exclusion constraint should prevent the
+--insert from getting bogged down by large constraint on main table over time
+CREATE TEMPORARY TABLE congestion_raw_segments_temp (
+    segment_id integer NOT NULL,
+    bin_start timestamp without time zone NOT NULL,
+    bin_range tsrange NOT NULL,
+    tt numeric,
+    num_obs integer,
+    CONSTRAINT congestion_raw_segments_exclude_temp EXCLUDE USING gist (
+        bin_range WITH &&,
+        segment_id WITH =
+    )
+);
+
 EXECUTE FORMAT(
     $$
     WITH segments AS (
@@ -32,7 +46,6 @@ EXECUTE FORMAT(
     segment_5min_bins AS (
         SELECT
             links.segment_id,
-            timerange(tg.start_tod, tg.end_tod, '[)') AS time_grp,
             ta.tx,
             RANK() OVER w AS bin_rank,
             links.total_length,
@@ -44,21 +57,16 @@ EXECUTE FORMAT(
             ARRAY_AGG(links.length / ta.mean * 3.6 ORDER BY link_dir) AS tts,
             ARRAY_AGG(links.length ORDER BY link_dir) AS lengths
         FROM here.ta_path AS ta
-        JOIN gwolofs.congestion_time_grps AS tg ON
-            ta.tx >= %1$L::date + tg.start_tod
-            AND ta.tx < %1$L::date + tg.end_tod
         JOIN segments AS links USING (link_dir)
         WHERE
             ta.dt >= %1$L::date
             AND ta.dt < %1$L::date + interval '1 day'
         GROUP BY
             links.segment_id,
-            tg.start_tod,
-            tg.end_tod,
             ta.tx,
             links.total_length
        WINDOW w AS (
-            PARTITION BY links.segment_id, tg.start_tod, tg.end_tod
+            PARTITION BY links.segment_id
             ORDER BY ta.tx
        )
     ),
@@ -69,7 +77,6 @@ EXECUTE FORMAT(
         --also don't generate options past the next bin with 80%% length
         SELECT
             tx,
-            time_grp,
             segment_id,
             bin_rank AS start_bin,
             --generate all the options for the end bin within the group.
@@ -89,7 +96,7 @@ EXECUTE FORMAT(
             ) AS end_bin
         FROM segment_5min_bins
         WINDOW w AS (
-            PARTITION BY time_grp, segment_id
+            PARTITION BY segment_id
             ORDER BY tx
             --look only forward for end_bin options
             RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING
@@ -98,7 +105,6 @@ EXECUTE FORMAT(
     
     unnested_db_options AS (
         SELECT
-            dbo.time_grp,
             dbo.segment_id,
             s5b.total_length,
             dbo.tx AS dt_start,
@@ -110,62 +116,66 @@ EXECUTE FORMAT(
             SUM(s5b.num_obs) AS num_obs --sum of here.ta_path sample_size for each link_dir
         FROM dynamic_bin_options AS dbo
         LEFT JOIN segment_5min_bins AS s5b
-            ON s5b.time_grp = dbo.time_grp
-            AND s5b.segment_id = dbo.segment_id
+            ON s5b.segment_id = dbo.segment_id
             AND s5b.bin_rank >= dbo.start_bin
             AND s5b.bin_rank <= dbo.end_bin
         --this join is used to get the tx info about the last bin only
         LEFT JOIN segment_5min_bins AS s5b_end
-            ON s5b_end.time_grp = dbo.time_grp
-            AND s5b_end.segment_id = dbo.segment_id
+            ON s5b_end.segment_id = dbo.segment_id
             AND s5b_end.bin_rank = dbo.end_bin,
         --unnest all the observations from individual link_dirs to reaggregate them within new dynamic bin
         UNNEST(s5b.link_dirs, s5b.lengths, s5b.tts) AS unnested(link_dir, len, tt)
-        --dynamic bins should not exceed one hour (dt_end <= dt_start + 1 hr)
-        WHERE s5b_end.tx + interval '5 minutes' <= dbo.tx + interval '1 hour'
+        --dynamic bins should not exceed 15 minutes (dt_end <= dt_start + 15 min)
+        WHERE s5b_end.tx + interval '5 minutes' <= dbo.tx + interval '15 minutes'
         GROUP BY
-            dbo.time_grp,
             dbo.segment_id,
             s5b.total_length,
             dbo.tx, --stard_bin
             s5b_end.tx, --end_bin
             unnested.link_dir,
             unnested.len
+    ),
+
+    inserted AS (    
+        --this query contains overlapping values which get eliminated
+        --via on conflict with the exclusion constraint on congestion_raw_segments table.
+        INSERT INTO congestion_raw_segments_temp AS inserted (
+            bin_start, segment_id, bin_range, tt, num_obs
+        )
+        --distinct on ensures only the shortest option gets proposed for insert
+        SELECT DISTINCT ON (segment_id, dt_start)
+            dt_start AS bin_start,
+            segment_id,
+            tsrange(dt_start, dt_end, '[)') AS bin_range,
+            total_length / SUM(len) * SUM(tt) AS tt,
+            SUM(num_obs) AS num_obs --sum of here.ta_path sample_size for each segment
+        FROM unnested_db_options
+        GROUP BY
+            segment_id,
+            dt_start,
+            dt_end,
+            total_length
+        HAVING SUM(len) >= 0.8 * total_length
+        ORDER BY
+            segment_id,
+            dt_start,
+            dt_end --uses the option that ends first
+        --exclusion constraint + ordered insert to prevent overlapping bins
+        ON CONFLICT ON CONSTRAINT congestion_raw_segments_exclude_temp
+        DO NOTHING
+        RETURNING inserted.bin_start, inserted.segment_id, inserted.bin_range, inserted.tt, inserted.num_obs
     )
+
+    INSERT INTO gwolofs.congestion_raw_segments (dt, bin_start, segment_id, bin_range, tt, num_obs)
+    SELECT bin_start::date AS dt, bin_start, segment_id, bin_range, tt, num_obs
+    FROM inserted;
     
-    --this query contains overlapping values which get eliminated
-    --via on conflict with the exclusion constraint on congestion_raw_segments table.
-    INSERT INTO gwolofs.congestion_raw_segments (
-        dt, time_grp, segment_id, bin_range, tt, num_obs
-    )
-    --distinct on ensures only the shortest option gets proposed for insert
-    SELECT DISTINCT ON (time_grp, segment_id, dt_start)
-        dt_start::date AS dt,
-        time_grp,
-        segment_id,
-        tsrange(dt_start, dt_end, '[)') AS bin_range,
-        total_length / SUM(len) * SUM(tt) AS tt,
-        SUM(num_obs) AS num_obs --sum of here.ta_path sample_size for each segment
-    FROM unnested_db_options
-    GROUP BY
-        time_grp,
-        segment_id,
-        dt_start,
-        dt_end,
-        total_length
-    HAVING SUM(len) >= 0.8 * total_length
-    ORDER BY
-        time_grp,
-        segment_id,
-        dt_start,
-        dt_end --uses the option that ends first
-    --exclusion constraint + ordered insert to prevent overlapping bins
-    ON CONFLICT ON CONSTRAINT congestion_raw_segments_exclude
-    DO NOTHING;
     $$, 
     start_date,
     congestion_network_table
     );
+
+    DROP TABLE congestion_raw_segments_temp;
 
 END;
 $BODY$;
