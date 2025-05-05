789cd1e1e05f051e96e61442aeab355ddf9e9186
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-05-05 20:15:53 +0000

    #1080 add active_intersections view, add sequence+unique constraint to intersections

--- volumes/miovision/sql/table/create-table-intersections.sql
+++ volumes/miovision/sql/table/create-table-intersections.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE miovision_api.intersections
 (
-    intersection_uid integer PRIMARY KEY,
+    intersection_uid integer NOT NULL DEFAULT nextval('miovision_api.miovision_intersection_uids'::regclass),
     id text COLLATE pg_catalog."default",
     intersection_name text COLLATE pg_catalog."default",
     date_installed date,
@@ -16,12 +16,24 @@
     e_leg_restricted boolean,
     s_leg_restricted boolean,
     w_leg_restricted boolean,
-    api_name text COLLATE pg_catalog."default"
+    api_name text COLLATE pg_catalog."default",
+    CONSTRAINT intersections_pkey PRIMARY KEY (intersection_uid),
+    CONSTRAINT miovision_intersections_id_uniq UNIQUE (id)
 )
 WITH (
     oids = FALSE
 )
 TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS miovision_api.intersections
+OWNER TO miovision_admins;
+
+REVOKE ALL ON TABLE miovision_api.intersections FROM bdit_humans;
+GRANT SELECT ON TABLE miovision_api.intersections TO bdit_humans;
+
+GRANT ALL ON TABLE miovision_api.intersections TO miovision_admins;
+
+GRANT ALL ON TABLE miovision_api.intersections TO miovision_api_bot;
 
 COMMENT ON COLUMN miovision_api.intersections.intersection_name
 IS 'A short name for the intersection, following the convention
