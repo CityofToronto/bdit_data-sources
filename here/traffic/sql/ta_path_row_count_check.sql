af0cc16baa17cd0d996de3bb8bb93a0bf14d17eb
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-05-09 17:16:51 +0000

    #1159 fluff

--- here/traffic/sql/ta_path_row_count_check.sql
+++ here/traffic/sql/ta_path_row_count_check.sql
@@ -1,12 +1,12 @@
 WITH lookback AS ( --noqa: L045
     SELECT
         dt,
-        SUM({{ params.col_to_sum }}) AS count,
+        SUM({{ params.col_to_sum }}) AS count, --noqa: TMP
         ref.is_weekend_or_holiday(dt::date) AS is_weekend_or_holiday
     FROM here.ta_path_daily_summary
     WHERE
-        dt >= '{{macros.ds_add(ds, -1)}}'::date - interval '{{ params.lookback }}'
-        AND dt < '{{ ds }}'::date
+        dt >= '{{ macros.ds_add(ds, -1) }}'::date - interval '{{ params.lookback }}' --noqa: TMP
+        AND dt < '{{ ds }}'::date --noqa: TMP
     GROUP BY dt
 )
 
@@ -15,32 +15,32 @@ SELECT
     'Daily count: ' || to_char(
         today.count, 'FM9,999,999,999'
     ) AS ds_count,
-    initcap('{{ params.lookback }}') || ' Lookback Avg: ' || to_char(
+    initcap('{{ params.lookback }}') || ' Lookback Avg: ' || to_char( --noqa: TMP
         AVG(lb.count), 'FM9,999,999,999'
     ) AS lookback_avg,
     'Pass threshold: ' || to_char(
         FLOOR(thr.threshold * AVG(lb.count)),
         'FM9,999,999,999'
     ) AS passing_value,
-    weather.airport_weather_summary('{{macros.ds_add(ds, -1)}}'::date) --noqa: RF01
+    weather.airport_weather_summary('{{ macros.ds_add(ds, -1) }}'::date) --noqa: RF01, TMP
     AS weather_summary
 FROM lookback AS today
 JOIN lookback AS lb USING (is_weekend_or_holiday),
     LATERAL (
         --change threshold if holiday is not null
         SELECT
-            CASE(
+            CASE (
                 SELECT hol.holiday
                 FROM ref.holiday AS hol
                 WHERE hol.dt = today.dt
             ) IS NOT NULL
-        WHEN False THEN '{{ params.threshold }}'::numeric
-        WHEN True THEN 0.5::numeric --50% of weekend volumes for holidays is acceptable
-        END
+                WHEN FALSE THEN '{{ params.threshold }}'::numeric --noqa: TMP
+                WHEN TRUE THEN 0.5::numeric --50% of weekend volumes for holidays is acceptable
+            END
     ) AS thr (threshold)
 WHERE
-    today.dt = '{{macros.ds_add(ds, -1)}}'::date
-    AND lb.dt != '{{macros.ds_add(ds, -1)}}'::date
+    today.dt = '{{ macros.ds_add(ds, -1) }}'::date --noqa: TMP
+    AND lb.dt != '{{ macros.ds_add(ds, -1) }}'::date --noqa: TMP
 GROUP BY
     today.count,
     thr.threshold
