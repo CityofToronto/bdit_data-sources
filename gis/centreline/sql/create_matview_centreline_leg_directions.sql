e5571bb1052a75c06de78d5da45420d8033293b8
Nate Wessel <nate.wessel@toronto.ca>
2025-05-14 17:08:08 -0400

    remove certain types of intersections from consideration

--- gis/centreline/sql/create_matview_centreline_leg_directions.sql
+++ gis/centreline/sql/create_matview_centreline_leg_directions.sql
@@ -32,6 +32,12 @@ nodes AS (
     -- i.e. a legit intersection with three or more legs
     SELECT node_id
     FROM node_edges
+    JOIN gis_core.centreline_intersection_point_latest AS p
+        ON node_edges.node_id = p.intersection_id
+    WHERE p.classification NOT IN (
+        'SEUML', -- "Pseudo-Intersection-Overpass/Underpass"
+        'XICSL' -- "Expressway Interchange, Single-Level"
+    )
     GROUP BY node_id
     HAVING COUNT(DISTINCT edge_id) > 2
 ),
