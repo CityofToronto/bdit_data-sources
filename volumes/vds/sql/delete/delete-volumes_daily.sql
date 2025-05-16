9a51620899a36f769639594541d002b1e2ea90dd
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2025-05-16 13:54:14 -0400

    #1215 fix delete sql

--- volumes/vds/sql/delete/delete-volumes_daily.sql
+++ volumes/vds/sql/delete/delete-volumes_daily.sql
@@ -1,4 +1,2 @@
 DELETE FROM vds.volumes_daily
-WHERE
-    datetime_15min >= '{{ ds }} 00:00:00'::timestamp -- noqa: TMP
-    AND datetime_15min < '{{ ds }} 00:00:00'::timestamp + interval '1 DAY'; -- noqa: TMP
+WHERE dt = '{{ ds }}'::date -- noqa: TMP
