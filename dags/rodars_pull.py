7ed7f2365a99db061a59c79404c81d2074d8dda5
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-05-16 18:58:04 +0000

    #1211 make start_date timezone aware w/pendulum

--- dags/rodars_pull.py
+++ dags/rodars_pull.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pendulum
 from functools import partial
 from datetime import datetime, timedelta
 
@@ -24,7 +25,7 @@ DOC_MD = get_readme_docmd(README_PATH, DAG_NAME)
 default_args = {
     'owner': ','.join(DAG_OWNERS),
     'depends_on_past': False,
-    'start_date': datetime(2024, 11, 27),
+    'start_date': pendulum.datetime(2024, 11, 27, tz="America/Toronto"),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
