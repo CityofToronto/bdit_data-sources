f0adc8e6e5b5dc200f7764b3c2c95e1023e95351
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-05-27 20:32:27 +0000

    #1211 update dag_functions path

--- dags/open_data_checks.py
+++ dags/open_data_checks.py
@@ -28,7 +28,7 @@ DAG_OWNERS = Variable.get('dag_owners', deserialize_json=True).get(DAG_NAME, ['U
 
 repo_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.insert(0, repo_path)
-from dags.dag_functions import task_fail_slack_alert, send_slack_msg
+from bdit_dag_utils.utils.dag_functions import task_fail_slack_alert, send_slack_msg
 
 default_args = {
     'owner': ','.join(DAG_OWNERS),
