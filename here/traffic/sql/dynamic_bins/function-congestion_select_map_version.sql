bbf677954cad2d1f92e4a1e0e7d77ae3742ac5d8
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-06-06 19:34:29 +0000

    #1132 account for streets_valid_range_path

--- here/traffic/sql/dynamic_bins/function-congestion_select_map_version.sql
+++ here/traffic/sql/dynamic_bins/function-congestion_select_map_version.sql
@@ -4,31 +4,43 @@
 
 CREATE OR REPLACE FUNCTION gwolofs.congestion_select_map_version(
     start_date date,
-    end_date date
+    end_date date,
+    agg_type text default null, --null or 'path'
+    OUT selected_version text
 )
 RETURNS text
-LANGUAGE sql
+LANGUAGE plpgsql
 COST 100
 STABLE PARALLEL SAFE
 AS $BODY$
 
-SELECT street_version
-FROM here.street_valid_range AS svr,
-LATERAL (
-    SELECT svr.valid_range * daterange(
-        congestion_select_map_version.start_date,
-        congestion_select_map_version.end_date, '[)') AS overlap
-) AS lat
-WHERE UPPER(lat.overlap) - LOWER(lat.overlap) IS NOT NULL
-ORDER BY UPPER(lat.overlap) - LOWER(lat.overlap) DESC NULLS LAST
-LIMIT 1;
+DECLARE
+    svr text := 'street_valid_range' || CASE agg_type WHEN 'path' THEN '_path' ELSE '' END;
 
+BEGIN
+EXECUTE FORMAT(
+    $$
+    SELECT street_version
+    FROM here.%I AS svr,
+    LATERAL (
+        SELECT svr.valid_range * daterange(%L, %L, '[)') AS overlap
+    ) AS lat
+    WHERE UPPER(lat.overlap) - LOWER(lat.overlap) IS NOT NULL
+    ORDER BY UPPER(lat.overlap) - LOWER(lat.overlap) DESC NULLS LAST
+    LIMIT 1;
+    $$, svr, congestion_select_map_version.start_date, congestion_select_map_version.end_date
+) INTO selected_version;
+END;
 $BODY$;
 
-ALTER FUNCTION gwolofs.congestion_select_map_version(date, date)
+ALTER FUNCTION gwolofs.congestion_select_map_version(date, date, text)
 OWNER TO gwolofs;
 
-GRANT EXECUTE ON FUNCTION gwolofs.congestion_select_map_version(date, date) TO congestion_bot;
+GRANT EXECUTE ON FUNCTION gwolofs.congestion_select_map_version(date, date, text) TO congestion_bot;
 
 COMMENT ON FUNCTION gwolofs.congestion_select_map_version IS
 'Implement TT App selectMapVersion.py';
+
+--test cases
+SELECT * FROM gwolofs.congestion_select_map_version('2022-01-01', '2023-01-01');
+SELECT * FROM gwolofs.congestion_select_map_version('2022-01-01', '2023-01-01', 'path');
