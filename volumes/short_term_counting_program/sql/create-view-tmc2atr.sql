404c4473278f653449ebf311c2c7fbf766e42882
Nate Wessel <nate.wessel@toronto.ca>
2025-06-23 16:49:28 -0400

    simplify away first (wide) CTE

--- volumes/short_term_counting_program/sql/create-view-tmc2atr.sql
+++ volumes/short_term_counting_program/sql/create-view-tmc2atr.sql
@@ -1,116 +1,74 @@
+/*
+if zero bikes enter the intersection, then zero bikes leave
+but if there is a non-zero bike count entering, we can't say
+where those bikes exit (yet), so the volume is NULL
+*/
+
 CREATE OR REPLACE VIEW traffic.tmc2atr AS
 
-WITH tmc2atr_wide AS (
+WITH json_bundled AS (
     SELECT
         id,
         count_id,
         time_start,
         time_end,
-        -- North leg traffic
-        n_bike AS n_bike_sb,
-        -- if zero bikes enter the intersection, then zero bikes leave
-        -- but if there is a non-zero bike count entering, we can't say
-        -- where those bikes exit (yet), so the volume is NULL
-        CASE
-            WHEN w_bike + e_bike + s_bike = 0 THEN 0
-        END AS n_bike_nb,
-        n_cars_l + n_cars_t + n_cars_r AS n_cars_sb,
-        s_cars_t + e_cars_r + w_cars_l AS n_cars_nb,
-        n_truck_l + n_truck_t + n_truck_r AS n_truck_sb,
-        s_truck_t + e_truck_r + w_truck_l AS n_truck_nb,
-        -- South leg traffic
-        s_bike AS s_bike_nb,
-        CASE
-            WHEN w_bike + e_bike + n_bike = 0 THEN 0
-        END AS s_bike_sb,
-        s_cars_t + s_cars_r + s_cars_t AS s_cars_nb,
-        n_cars_t + e_cars_l + w_cars_r AS s_cars_sb,
-        s_truck_t + s_truck_r + s_truck_t AS s_truck_nb,
-        n_truck_t + e_truck_l + w_truck_r AS s_truck_sb,
-        -- East leg traffic
-        e_bike AS e_bike_wb,
-        CASE
-            WHEN w_bike + s_bike + n_bike = 0 THEN 0
-        END AS e_bike_eb,
-        e_cars_t + e_cars_r + e_cars_l AS e_cars_wb,
-        w_cars_t + n_cars_l + s_cars_r AS e_cars_eb,
-        e_truck_t + e_truck_r + e_truck_l AS e_truck_wb,
-        w_truck_t + n_truck_l + s_truck_r AS e_truck_eb,
-        -- West leg traffic
-        w_bike AS w_bike_eb,
-        CASE
-            WHEN e_bike + s_bike + n_bike = 0 THEN 0
-        END AS w_bike_wb,
-        w_cars_t + w_cars_r + w_cars_l AS w_cars_eb,
-        e_cars_t + s_cars_l + n_cars_r AS w_cars_wb,
-        w_truck_t + w_truck_r + w_truck_l AS w_truck_eb,
-        e_truck_t + s_truck_l + n_truck_r AS w_truck_wb
-    FROM traffic.tmc_study_data
-),
-
-json_bundled AS (
-    SELECT
-        id,
-        count_id,
-        time_start,
-        time_end,
-        json_build_object(
-            'bike', json_build_object(
-                'N', json_build_object(
-                    'NB', n_bike_nb,
-                    'SB', n_bike_sb
+        json_build_object( -- classification level
+            'bike', json_build_object( -- approach level
+                'N', json_build_object( -- direction level
+                    'NB', CASE WHEN w_bike + e_bike + s_bike = 0 THEN 0 END,
+                    'SB', n_bike
                 ),
                 'E', json_build_object(
-                    'EB', e_bike_eb,
-                    'WB', e_bike_wb
+                    'EB', CASE WHEN w_bike + s_bike + n_bike = 0 THEN 0 END,
+                    'WB', e_bike
                 ),
                 'S', json_build_object(
-                    'NB', s_bike_nb,
-                    'SB', s_bike_sb
+                    'NB', s_bike,
+                    'SB', CASE WHEN w_bike + e_bike + n_bike = 0 THEN 0 END
                 ),
                 'W', json_build_object(
-                    'EB', w_bike_eb,
-                    'WB', w_bike_wb
+                    'EB', w_bike,
+                    'WB', CASE WHEN e_bike + s_bike + n_bike = 0 THEN 0 END
                 )
             ),
             'car', json_build_object(
                 'N', json_build_object(
-                    'NB', n_cars_nb,
-                    'SB', n_cars_sb
+                    'NB', s_cars_t + e_cars_r + w_cars_l,
+                    'SB', n_cars_l + n_cars_t + n_cars_r
                 ),
                 'E', json_build_object(
-                    'EB', e_cars_eb,
-                    'WB', e_cars_wb
+                    'EB', w_cars_t + n_cars_l + s_cars_r,
+                    'WB', e_cars_t + e_cars_r + e_cars_l
                 ),
                 'S', json_build_object(
-                    'NB', s_cars_nb,
-                    'SB', s_cars_sb
+                    'NB', s_cars_t + s_cars_r + s_cars_t,
+                    'SB', n_cars_t + e_cars_l + w_cars_r
                 ),
                 'W', json_build_object(
-                    'EB', w_cars_eb,
-                    'WB', w_cars_wb
+                    'EB', w_cars_t + w_cars_r + w_cars_l,
+                    'WB', e_cars_t + s_cars_l + n_cars_r
                 )
             ),
             'truck', json_build_object(
                 'N', json_build_object(
-                    'NB', n_truck_nb,
-                    'SB', n_truck_sb
+                    'NB', s_truck_t + e_truck_r + w_truck_l,
+                    'SB', n_truck_l + n_truck_t + n_truck_r
                 ),
                 'E', json_build_object(
-                    'EB', e_truck_eb,
-                    'WB', e_truck_wb
+                    'EB', w_truck_t + n_truck_l + s_truck_r,
+                    'WB', e_truck_t + e_truck_r + e_truck_l
                 ),
                 'S', json_build_object(
-                    'NB', s_truck_nb,
-                    'SB', s_truck_sb
+                    'NB', s_truck_t + s_truck_r + s_truck_t,
+                    'SB', n_truck_t + e_truck_l + w_truck_r
                 ),
                 'W', json_build_object(
-                    'EB', w_truck_eb,
-                    'WB', w_truck_wb
+                    'EB', w_truck_t + w_truck_r + w_truck_l,
+                    'WB', e_truck_t + s_truck_l + n_truck_r
                 )
             )
         ) AS counts
-    FROM tmc2atr_wide
+    FROM traffic.tmc_study_data
 ),
 
 classified AS (
