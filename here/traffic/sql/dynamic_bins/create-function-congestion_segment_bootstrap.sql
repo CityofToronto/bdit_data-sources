065747123930d0b463cd4b3943a96199bbe40adf
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-08-29 14:14:30 +0000

    #1132 bootstrapping method - eliminate another cte

--- here/traffic/sql/dynamic_bins/create-function-congestion_segment_bootstrap.sql
+++ here/traffic/sql/dynamic_bins/create-function-congestion_segment_bootstrap.sql
@@ -6,12 +6,14 @@ CROSS JOIN generate_series(1,100,1)
 */
 
 
+
 CREATE OR REPLACE FUNCTION gwolofs.congestion_segment_bootstrap(
 	mnth date,
 	segment_ids bigint[],
     n_resamples int)
     RETURNS TABLE(
-        segment_id integer, mnth date, is_wkdy boolean, hr numeric, avg_tt real, n bigint, ci_lower real, ci_upper real
+        segment_id integer, mnth date, is_wkdy boolean, hr numeric,
+        avg_tt real, n bigint, ci_lower real, ci_upper real
     ) 
     LANGUAGE SQL
     COST 100
@@ -21,7 +23,7 @@ AS $BODY$
 WITH raw_obs AS (
     SELECT
         segment_id,
-        date_trunc('month', dt)::date AS mnth,
+        congestion_segment_bootstrap.mnth AS mnth,
         EXTRACT('isodow' FROM dt) IN (1, 2, 3, 4, 5) AS is_wkdy,
         EXTRACT('hour' FROM hr) AS hr,
         ARRAY_AGG(tt::real) AS tt_array,
@@ -34,7 +36,6 @@ WITH raw_obs AS (
         AND segment_id = ANY(congestion_segment_bootstrap.segment_ids)
     GROUP BY
         segment_id,
-        mnth,
         is_wkdy,
         EXTRACT('hour' FROM hr)
 ),
@@ -42,55 +43,45 @@ WITH raw_obs AS (
 random_selections AS (
     SELECT
         raw_obs.segment_id,
-        raw_obs.mnth,
         raw_obs.is_wkdy,
         raw_obs.hr,
-        sample_group.group_id,
         raw_obs.avg_tt,
         raw_obs.n,
+        raw_obs.mnth,
+        sample_group.group_id,
         --get a random observation from the array of tts
-        raw_obs.tt_array[ceiling(random() * raw_obs.n)] AS rnd_tt
+        AVG(raw_obs.tt_array[ceiling(random() * raw_obs.n)]) AS rnd_avg_tt
     FROM raw_obs
     CROSS JOIN generate_series(1, n)
     -- 200 resamples (could be any number)
     CROSS JOIN generate_series(1, congestion_segment_bootstrap.n_resamples) AS sample_group(group_id)
-),
-
-resampled_averages AS (
-    SELECT
-        segment_id,
-        mnth,
-        is_wkdy,
-        hr,
-        group_id,
-        AVG(rnd_tt) AS rnd_avg_tt
-    FROM random_selections
     GROUP BY
-        segment_id,
-        mnth,
-        is_wkdy,
-        hr,
-        group_id
+        raw_obs.segment_id,
+        raw_obs.is_wkdy,
+        raw_obs.hr,
+        raw_obs.avg_tt,
+        raw_obs.n,
+        raw_obs.mnth,
+        sample_group.group_id
 )
 
 SELECT
-    ra.segment_id,
-    ra.mnth,
-    ra.is_wkdy,
-    ra.hr,
-    raw_obs.avg_tt::real,
-    raw_obs.n,
-    percentile_disc(0.025) WITHIN GROUP (ORDER BY ra.rnd_avg_tt)::real AS ci_lower,
-    percentile_disc(0.975) WITHIN GROUP (ORDER BY ra.rnd_avg_tt)::real AS ci_upper
-FROM resampled_averages AS ra
-JOIN raw_obs USING (segment_id, mnth, is_wkdy, hr)
+    segment_id,
+    mnth,
+    is_wkdy,
+    hr,
+    avg_tt::real,
+    n,
+    percentile_disc(0.025) WITHIN GROUP (ORDER BY rnd_avg_tt)::real AS ci_lower,
+    percentile_disc(0.975) WITHIN GROUP (ORDER BY rnd_avg_tt)::real AS ci_upper
+FROM random_selections
 GROUP BY
-    ra.segment_id,
-    ra.mnth,
-    ra.is_wkdy,
-    ra.hr,
-    raw_obs.avg_tt,
-    raw_obs.n;
+    segment_id,
+    mnth,
+    is_wkdy,
+    hr,
+    avg_tt,
+    n;
 
     $BODY$;
 
