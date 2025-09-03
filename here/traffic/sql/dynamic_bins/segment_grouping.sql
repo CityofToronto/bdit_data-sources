31cd8e777275dd922b8086790be129a600f911c3
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-09-03 17:24:59 +0000

    #1132 update segment_grouping to work for all map versions

--- here/traffic/sql/dynamic_bins/segment_grouping.sql
+++ here/traffic/sql/dynamic_bins/segment_grouping.sql
@@ -1,22 +1,38 @@
-WITH summary AS (
-    --find the number of groups required to have no more than 100 per group
+WITH segments AS (
+    --segments active in relevant month
+    SELECT DISTINCT segment_id
+    FROM gwolofs.congestion_raw_segments
+    WHERE
+        dt >= {{ ds }}::date
+        AND dt < {{ ds }}::date + '1 month'::interval 
+),
+
+group_size AS (
+    --find the number of groups required to have no more than `max_group_size` per group
     SELECT
-        FLOOR(COUNT(DISTINCT segment_id)
-        / CEIL((COUNT(DISTINCT segment_id)) / 100.0)) AS num_per_group
-    FROM congestion.network_segments_24_4
+        FLOOR(COUNT(*)
+        / CEIL((COUNT(*)) / {{ params.max_group_size }}::numeric)) AS num_per_group
+    FROM segments
 ),
 
 groups AS (
     SELECT
-        CEIL(ROW_NUMBER() OVER (ORDER BY segment_id) / summary.num_per_group) AS group_id,
+        --assign group_ids using row number
+        CEIL(ROW_NUMBER() OVER (ORDER BY segment_id) / group_size.num_per_group) AS group_id,
         segment_id
-    FROM congestion.network_segments_24_4, summary
+    FROM segments, group_size
+),
+
+groups_summarized AS (
+    SELECT
+        group_id,
+        array_agg(segment_id) AS segment_ids,
+        COUNT(*)
+    FROM groups
+    GROUP BY group_id
+    ORDER BY group_id
 )
 
-SELECT
-    group_id,
-    array_agg(segment_id),
-    COUNT(*)
-FROM groups
-GROUP BY group_id
-ORDER BY group_id
+--return list of lists for xcom
+SELECT array_agg(segment_ids::text)
+FROM groups_summarized
