380b5981fc77695cd4917f4beff3e982df5b02ae
Wael Halabi <wael.halabi@toronto.ca>
2025-09-17 17:21:16 +0000

    #1268 updating files based on comments by reviewer, merged the leg_restrictions files into one file

--- volumes/miovision/sql/updates/update-n_leg_restricted.sql
+++
@@ -1,8 +0,0 @@
-UPDATE miovision_api.intersections
-SET n_leg_restricted= CASE
-	WHEN api_name = 'Overture Road and Lawrence Avenue East' THEN true
-
-END
-WHERE api_name IN (
-'Overture Road and Lawrence Avenue East'
-);
