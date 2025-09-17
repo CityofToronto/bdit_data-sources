4a6fe4554cf07f5f023cfd9545f3f9c45e70f83c
Wael Halabi <wael.halabi@toronto.ca>
2025-09-17 17:21:16 +0000

    #1268 updating files based on comments by reviewer, merged the leg_restrictions files into one file

--- volumes/miovision/sql/updates/update-date.sql
+++ volumes/miovision/sql/updates/update-date.sql
@@ -3,13 +3,15 @@ DROP TABLE IF EXISTS temp_min_dates;
 
 -- Recreate temp table with restriction to specific intersection_uids
 CREATE TEMP TABLE temp_min_dates AS
-SELECT intersection_uid, MIN(datetime_bin)::date AS min_datetime
+SELECT
+    intersection_uid,
+    MIN(datetime_bin)::date AS min_datetime
 FROM miovision_api.volumes
-WHERE intersection_uid IN (4440, 4441, 4442, 4443, 4444, 4445, 4446, 4447,4439)
+WHERE intersection_uid IN (4440, 4441, 4442, 4443, 4444, 4445, 4446, 4447, 4439)
 GROUP BY intersection_uid;
 
 -- Create index
-CREATE INDEX idx_temp_intersection_uid ON temp_min_dates(intersection_uid);
+CREATE INDEX idx_temp_intersection_uid ON temp_min_dates (intersection_uid);
 
 -- Perform the update
 UPDATE miovision_api.intersections AS i
--- volumes/miovision/sql/updates/update-int_id-px-geom.sql
+++ volumes/miovision/sql/updates/update-int_id-px-geom.sql
@@ -1,34 +1,36 @@
-WITH intersections(id, intersection_name_api) AS (
+WITH intersections (id, intersection_name_api) AS (
     VALUES
-        ('3a6c4b1b-91cc-4600-8b9e-f3416839397b', 'The Queensway and The East Mall'),
-        ('ae52ca3c-9a25-4c2c-a726-24388616fe54', 'Lawrence Ave E and Fortune Gate'),
-        ('c85d49f2-bd4a-4649-b967-ce24e39ac102', 'Lawrence Ave E and Galloway Rd'),
-        ('0f37f5c0-9eb0-45d8-80cf-bd3d3718c2d4', 'Lawrence Ave E and Mossbank Dr'),
-        ('0b4e1e39-a9e9-46a3-895f-0f21924c0e54', 'Lawrence Ave E and Scarborough Golf Club Road'),
-        ('01150688-deb1-4565-a0da-236ed77cad12', 'Orton Park Rd and Lawrence Ave E'),
-        ('0726384f-e753-4826-9ef2-72a817776ca5', 'Overture Rd and Lawrence Ave E'),
-        ('843d1d1f-5350-4aaf-a764-ec8ebf847673', 'Greenholm Circuit and Lawrence Ave E'),
-        ('5de86391-7559-48e3-aa46-7cfe0b28bd05', 'The Queensway and The West Mall')
+    ('3a6c4b1b-91cc-4600-8b9e-f3416839397b', 'The Queensway and The East Mall'),
+    ('ae52ca3c-9a25-4c2c-a726-24388616fe54', 'Lawrence Ave E and Fortune Gate'),
+    ('c85d49f2-bd4a-4649-b967-ce24e39ac102', 'Lawrence Ave E and Galloway Rd'),
+    ('0f37f5c0-9eb0-45d8-80cf-bd3d3718c2d4', 'Lawrence Ave E and Mossbank Dr'),
+    ('0b4e1e39-a9e9-46a3-895f-0f21924c0e54', 'Lawrence Ave E and Scarborough Golf Club Road'),
+    ('01150688-deb1-4565-a0da-236ed77cad12', 'Orton Park Rd and Lawrence Ave E'),
+    ('0726384f-e753-4826-9ef2-72a817776ca5', 'Overture Rd and Lawrence Ave E'),
+    ('843d1d1f-5350-4aaf-a764-ec8ebf847673', 'Greenholm Circuit and Lawrence Ave E'),
+    ('5de86391-7559-48e3-aa46-7cfe0b28bd05', 'The Queensway and The West Mall')
 ),
+
 enriched_data AS (
-    SELECT 
+    SELECT
         i.id,
         SPLIT_PART(i.intersection_name_api, ' and ', 1) AS street_main,
         SPLIT_PART(i.intersection_name_api, ' and ', 2) AS street_cross,
-        g[3] AS int_id,
+        g.int_result[3] AS int_id,
         ts.px::int AS px,
         ts.geom
-    FROM intersections i
+    FROM intersections AS i
     JOIN gis._get_intersection_id(
         SPLIT_PART(i.intersection_name_api, ' and ', 1),
         SPLIT_PART(i.intersection_name_api, ' and ', 2),
         0
-    ) AS g ON TRUE
-    LEFT JOIN gis.traffic_signal ts ON ts.node_id = g[3]
+    ) AS g (int_result) ON TRUE
+    LEFT JOIN gis.traffic_signal AS ts ON ts.node_id = g.int_result[3]
 )
 
 UPDATE miovision_api.intersections AS target
-SET px = enriched_data.px,
+SET
+    px = enriched_data.px,
     geom = enriched_data.geom,
     int_id = enriched_data.int_id,
     street_main = enriched_data.street_main,
