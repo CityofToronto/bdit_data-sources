8abd94186f3bdb7dec74ecdfcc8e6599050c5d1f
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2025-11-05 11:11:33 -0500

    #1156 fix for EDT->EST transition

--- bluetooth/itsc/bluetooth_itsc.py
+++ bluetooth/itsc/bluetooth_itsc.py
@@ -4,6 +4,7 @@ import pandas as pd
 from numpy import nan
 from psycopg2 import sql, Error
 from psycopg2.extras import execute_values
+from datetime import datetime, timedelta
 
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.exceptions import AirflowFailException, AirflowSkipException
@@ -12,6 +13,16 @@ SQL_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'sql')
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
+def check_dst(start_date):
+    start = datetime.strptime(start_date, '%Y-%m-%d')
+    tz_1 = start.astimezone().tzname()
+    tz_2 = (start + timedelta(days = 1)).astimezone().tzname()
+    if tz_1 == 'EDT' and tz_2 == 'EST':
+        LOGGER.info(f"EDT -> EST time change occured today.")
+    else:
+        LOGGER.info(f"Normal day.")
+    return tz_1 == 'EDT' and tz_2 == 'EST'
 
 def fetch_and_insert_raw_tt_data(
     start_date,
@@ -24,7 +35,12 @@ def fetch_and_insert_raw_tt_data(
     - Inserts into RDS `bluetooth.itsc_tt_raw` table.
     '''
     
-    select_fpath = os.path.join(SQL_DIR, 'select-itsc-tt_raw.sql')
+    # Pull raw data from Postgres database
+    if not check_dst(start_date):
+        select_fpath = os.path.join(SQL_DIR, 'select-itsc-tt_raw.sql')
+    else:
+        select_fpath = os.path.join(SQL_DIR, 'select-itsc-tt_raw_dst_safe.sql')
+    
     with open(select_fpath, 'r', encoding="utf-8") as file:
         select_query = sql.SQL(file.read()).format(
             start = sql.Literal(start_date + " 00:00:00 Canada/Eastern")
@@ -62,8 +78,11 @@ def fetch_and_insert_raw_tt_pathdata(
     - Fetches data from ITS Central `traveltimepathdata` table.  
     - Inserts into RDS `bluetooth.itsc_tt_raw_pathdata` table.
     '''
-    
-    select_fpath = os.path.join(SQL_DIR, 'select-itsc-tt_raw_pathdata.sql')
+    # Pull raw data from Postgres database
+    if not check_dst(start_date):
+        select_fpath = os.path.join(SQL_DIR, 'select-itsc-tt_raw_pathdata.sql')
+    else:
+        select_fpath = os.path.join(SQL_DIR, 'select-itsc-tt_raw_pathdata_dst_safe.sql')
     with open(select_fpath, 'r', encoding="utf-8") as file:
         select_query = sql.SQL(file.read()).format(
             start = sql.Literal(start_date + " 00:00:00 Canada/Eastern")
