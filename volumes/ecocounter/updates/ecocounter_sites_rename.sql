1fbc6246c442ff2011cabcf6ce353e08cb5a868d
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2025-12-04 14:23:09 -0500

    #1288 fix names based on consensus

--- volumes/ecocounter/updates/ecocounter_sites_rename.sql
+++ volumes/ecocounter/updates/ecocounter_sites_rename.sql
@@ -26,7 +26,7 @@ VALUES
     ('Bloor St W between Huron St & Spadina Ave (retired)', 'Bloor St W, west of Huron St (retired)'),
     ('Bloor St W, between Palmerston & Markham', 'Bloor St W, east of Palmerston Blvd'),
     ('Bloor St W, between Palmerston & Markham (retired)', 'Bloor St W, east of Palmerston Blvd (retired)'),
-    ('Bloor St W, East of Old Mill Trail', 'Bloor St W, east of Old Mill Tr'),
+    ('Bloor St W, East of Old Mill Trail', 'Bloor St W, east of Old Mill Trl'),
     ('YorkU - Keele St, North of Four Winds Dr', 'Keele St, north of Four Winds Dr (multi-use path)'),
     ('YorkU - Murray Ross Pkwy, North of Shoreham Dr', 'Murray Ross Pkwy, north of Shoreham Dr (multi-use path)'),
     ('YorkU - Murray Ross Pkwy, West of Evelyn Wiggins Dr', 'Murray Ross Pkwy, west of Evelyn Wiggins Dr (multi-use path)'),
@@ -43,13 +43,13 @@ VALUES
     ('Sherbourne St, North of Wellesley St E (retired)', 'Sherbourne St, north of Wellesley St E (retired)'),
     ('YorkU - Evelyn Wiggins Dr, North of Murray Ross Pkwy', 'Evelyn Wiggins Dr, north of Murray Ross Pkwy (two-way cycle track)'),
     ('Bloor St W at Oakmount Rd, Display Counter', 'Bloor St W, west of Oakmount Rd'),
-    ('Steeles Ave E and McCowan Rd WB', 'Steeles Ave E, east of McCowan Rd'),
+    ('Steeles Ave E and McCowan Rd WB', 'Steeles Ave E, at McCowan Rd'),
     ('Steeles Ave E and Midland Ave EB', 'Steeles Ave E, east of Midland Ave'),
     ('Steeles Ave E and Midland Ave WB', 'Steeles Ave E, east of Midland Ave'),
     ('Multi-use path south-east of Keele & Sheppard', 'Keele St, south of Sheppard Ave W (multi-use path)'),
     ('Multi-use path south of Sheppard Ave at Sentinel Rd', 'Sheppard Ave W, west of Sentinel Rd (multi-use path)'),
-    ('Millwood - NE of Donlands', 'Millwood Rd, north of Donlands Ave'),
-    ('Millwood - NW of Pape', 'Millwood Rd, north of Pape Ave')
+    ('Millwood - NE of Donlands', 'Millwood Rd, north of Pape Ave/Donlands Ave'),
+    ('Millwood - NW of Pape', 'Millwood Rd, north of Pape Ave/Donlands Ave')
 );
 
 UPDATE ecocounter.open_data_15min_counts AS og
