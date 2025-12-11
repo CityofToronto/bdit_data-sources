96fc149311c63ddeda51ad33f9e9794167f2f457
A-DUYVESTYN <a.duyvestyn@gmail.com>
2025-12-11 15:16:01 +0000

    #1292 add decommisioned date to empty flows

--- volumes/ecocounter/updates/ecocounter_flow_updates_bloor_castle_frank_plus_decomm.sql
+++ volumes/ecocounter/updates/ecocounter_flow_updates_bloor_castle_frank_plus_decomm.sql
@@ -1,5 +1,10 @@
+--these flow_ids have never produced data
+UPDATE ecocounter.flows_unfiltered
+SET date_decommissioned = '2025-12-11'
+WHERE flow_id IN (353455984, 353629056, 353629057, 353629035, 353629036);
+
 -- CAUTION:
--- Two Flows at Bloor St E, west of Castle Frank Rd include contraflow and are incorrect as of 2025-12-09:
+-- Two Flows at Bloor St E, west of Castle Frank Rd are incorrect as of 2025-12-09:
 -- * 353354428 was labelled Westbound, it is actually Eastbound
 -- * 353517542 was labelled Eastbound, it is actually Westbound
 
