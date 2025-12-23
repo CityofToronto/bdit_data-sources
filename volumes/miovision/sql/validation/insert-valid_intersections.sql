62cd053c6b4381d62ef232e047f4c798a3da9b65
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-12-23 21:55:32 +0000

    #1284 add separate upserts for ranges ended by invalid range

--- volumes/miovision/sql/validation/insert-valid_intersections.sql
+++ volumes/miovision/sql/validation/insert-valid_intersections.sql
@@ -1,3 +1,4 @@
+--
 INSERT INTO miovision_validation.valid_intersections (
     intersection_uid, intersection_name, start_date,
     end_date, classification, classification_uids
@@ -15,10 +16,16 @@ LEFT JOIN miovision_validation.valid_intersections AS valid2
     USING (intersection_uid, start_date, classification)
 WHERE
     valid1.all_pass
-    AND (
-        valid2.intersection_uid IS NULL --anti join
-        OR coalesce(valid2.end_date, '1990-01-01') != coalesce(valid1.end_date, '1990-01-01')
-    )
+    AND valid2.intersection_uid IS NULL --anti join existing
 ON CONFLICT ON CONSTRAINT valid_intersections_start_date_class_pkey
-DO UPDATE
-SET end_date = EXCLUDED.end_date;
+DO NOTHING;
+
+--update end dates
+UPDATE miovision_validation.valid_intersections AS vi
+SET end_date = viv.end_date
+FROM miovision_validation.valid_intersections_view AS viv
+WHERE
+    vi.intersection_uid = viv.intersection_uid
+    AND vi.start_date = viv.start_date
+    AND vi.classification = viv.classification
+    AND coalesce(vi.end_date, '1990-01-01') != coalesce(viv.end_date, '1990-01-01');
--- volumes/miovision/sql/validation/insert-valid_legs.sql
+++ volumes/miovision/sql/validation/insert-valid_legs.sql
@@ -16,10 +16,17 @@ LEFT JOIN miovision_validation.valid_legs AS valid2
     USING (intersection_uid, start_date, classification, leg)
 WHERE
     valid1.all_pass
-    AND (
-        valid2.intersection_uid IS NULL --anti join
-        OR coalesce(valid2.end_date, '1990-01-01') != coalesce(valid1.end_date, '1990-01-01')
-    )
+    AND valid2.intersection_uid IS NULL --anti join
 ON CONFLICT ON CONSTRAINT valid_legs_start_date_class_pkey
-DO UPDATE
-SET end_date = EXCLUDED.end_date;
+DO NOTHING;
+
+--update end dates
+UPDATE miovision_validation.valid_legs AS vl
+SET end_date = vlv.end_date
+FROM miovision_validation.valid_legs_view AS vlv
+WHERE
+    vl.intersection_uid = vlv.intersection_uid
+    AND vl.start_date = vlv.start_date
+    AND vl.classification  = vlv.classification
+    AND vl.leg = vlv.leg
+    AND coalesce(vl.end_date, '1990-01-01') != coalesce(vlv.end_date, '1990-01-01');
