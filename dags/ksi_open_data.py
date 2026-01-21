fc8c4e4d6830214c53943cc0fb20f493338b0838
chmnata <chmnata@gmail.com>
2026-01-21 22:56:24 +0000

    #1332 update dag to add human in the loop

--- dags/ksi_open_data.py
+++ dags/ksi_open_data.py
@@ -9,8 +9,11 @@ from functools import partial
 from datetime import datetime, timedelta
 
 from airflow.sdk import dag, task, task_group
-from airflow.models import Variable
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.standard.operators.hitl import ApprovalOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.sdk import Variable
 
 try:
     repo_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
@@ -47,36 +50,59 @@ default_args = {'owner': ','.join(DAG_OWNERS),
 )
 
 def ksi_opan_data():
-        
     @task_group()
     def data_checks():
 
-        check_deleted_collision = SQLCheckOperatorWithReturnValue(
+        check_deleted_collision = SQLExecuteQueryOperator(
             task_id="check_deleted_collision",
             sql='''
                     SELECT COUNT(*) = 0 AS _check, 'There are '|| count(*) ||' deleted collision_id comparing to last updated. collision_id: '||
                     ARRAY_TO_STRING(array_agg(collision_id), ', ') AS missing
                     FROM(
-                    SELECT collision_id FROM open_data_staging.ksi
+                    SELECT DISTINCT collision_id FROM open_data.ksi
                     except all
-                    SELECT collision_id FROM open_data.ksi) AS diff
+                    SELECT DISTINCT collision_id FROM natalie.ksi_open_data_Test) AS diff;
                     ''',
-            conn_id="collisions_bot"
+            conn_id="collisions_bot",
+            do_xcom_push=True,
         )
-        check_dup_collision_person = SQLCheckOperatorWithReturnValue(
+        check_dup_collision = SQLExecuteQueryOperator(
             task_id="check_dup_collision",
             sql='''
                     SELECT COUNT(*) = 0 AS _check, 'There are '|| count(*) ||' deleted collision person pair comparing to last updated. collision_id: '||
                     ARRAY_TO_STRING(array_agg(collision_id), ', ') AS missing
                     FROM(
-                    SELECT DISTINCT collision_id, per_no FROM open_data_staging.ksi
+                    SELECT DISTINCT collision_id, per_no FROM open_data.ksi
                     except all
-                    SELECT DISTINCT collision_id, per_no FROM open_data.ksi) AS diff
+                    SELECT DISTINCT collision_id, per_no FROM natalie.ksi_open_data_Test) AS diff;
                     ''',
-            conn_id="collisions_bot"
+            conn_id="collisions_bot",
+            do_xcom_push=True,
         )
-        check_deleted_collision >> check_dup_collision_person
+        return check_deleted_collision, check_dup_collision
     
+    check_deleted_collision, check_dup_collision = data_checks()
+
+    @task
+    def summarize_checks(*check_results):
+        errors = []
+
+        for results in check_results:
+            ok, missing = results[0]
+            # grab list of errored collisions
+            if ok is False: 
+                errors.append(missing)
+
+        return {
+            "has_errors": bool(errors),
+            "details": "\n".join(errors) if errors else "No issues found."
+        }
+
+    checks_summary = summarize_checks(
+        check_deleted_collision.output,
+        check_dup_collision.output,
+    )
+
     refresh_ksi_staging = SQLExecuteQueryOperator(
         sql='''
             REFRESH MATERIALIZED VIEW CONCURRENTLY open_data_staging.ksi;
@@ -96,8 +122,44 @@ def ksi_opan_data():
         task_id='truncate_and_copy',
         conn_id='collisions_bot',
         autocommit=True,
-        retries = 0
+        retries = 0,
+        # so it doesnt get skipped
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS,
     )
+
+    @task()
+    def checks_failed_message(summary: dict, **context):
+        slack_ids = Variable.get("slack_member_id", deserialize_json=True)
+        owners = context.get('dag').owner.split(',')
+        list_names = " ".join([slack_ids.get(name, name) for name in owners])
+        send_slack_msg(
+            context=context,
+            msg=(
+                f"{list_names}:cat_yell: KSI open data checks failed, refresh paused for approval. Table will not be refreshed until manual approval.\n"
+                f"{summary['details']}"
+            ),
+            channel="slack_data_pipeline_dev",
+        )
+    @task.branch
+    def decide_approval(summary: dict):
+        return "checks_failed_message" if summary["has_errors"] else "skip_approval"
+
+    skip_approval = EmptyOperator(task_id="skip_approval")
+
+    approve_refresh = ApprovalOperator(
+        task_id="approve_refresh",
+        subject="Refresh has been paused because of the following errors:",
+        body=(
+            "The following data checks failed:\n\n"
+            "{{ ti.xcom_pull(task_ids='summarize_checks')['details'] }}"
+            ),
+        defaults="Approve",
+        assigned_users=[
+                {"id": "1", "name": "admin"},
+                {"id": "3", "name": "nchan"},
+                {"id": "6", "name": "gwolofs"}, #gotta add david
+            ],
+    )   
 
     @task()
     def status_message(**context):
@@ -105,10 +167,20 @@ def ksi_opan_data():
             context=context,
             msg=f"KSI table successfully refreshed :white_check_mark:. ",
             channel='slack_data_pipeline_dev'
-            #use_proxy=True
             )
+        
+    branch = decide_approval(checks_summary)
+    refresh_ksi_staging >> data_checks() >> checks_summary >> branch
 
-    data_checks() >> refresh_ksi_staging >> truncate_and_copy >> status_message()
+    # if no data checks failed
+    # continue with refresh
+    branch >> skip_approval >> truncate_and_copy
+    # If data checks failed, prompt for approval
+    # Then only refresh when approved
+    branch >> checks_failed_message(checks_summary) >> approve_refresh >> truncate_and_copy
+
+    truncate_and_copy >> status_message()
+
 
 ksi_opan_data()
 
