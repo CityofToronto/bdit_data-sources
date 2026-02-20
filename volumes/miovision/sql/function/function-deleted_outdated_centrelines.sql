591f6eb3cf2118f6be110bdd6b75c331a13d615a
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2026-02-20 10:21:22 -0500

    #1168 check gis_core.centreline_latest is not null before using for delete anti-join

--- volumes/miovision/sql/function/function-deleted_outdated_centrelines.sql
+++ volumes/miovision/sql/function/function-deleted_outdated_centrelines.sql
@@ -1,5 +1,10 @@
 CREATE OR REPLACE FUNCTION miovision_api.delete_outdated_centreline_ids()
 RETURNS void AS $$
+BEGIN
+
+    IF NOT EXISTS (SELECT 1 FROM gis_core.centreline_latest) THEN
+        RAISE EXCEPTION 'gis_core.centreline_latest is empty — aborting delete to avoid data loss.';
+    END IF;
 
     WITH outdated AS (
         SELECT
@@ -10,18 +15,17 @@ RETURNS void AS $$
         LEFT JOIN gis_core.centreline_latest AS latest USING (centreline_id)
         WHERE
             cm.centreline_id IS NOT NULL
-            --entry exists, but is no longer valid
             AND latest.centreline_id IS NULL
     )
-    
     DELETE FROM miovision_api.centreline_miovision AS cm
     USING outdated AS od
     WHERE od.centreline_id = cm.centreline_id
     AND od.intersection_uid = cm.intersection_uid
     AND od.leg = cm.leg;
 
+END;
 $$
-LANGUAGE sql
+LANGUAGE plpgsql
 SECURITY DEFINER;
 
 COMMENT ON FUNCTION miovision_api.delete_outdated_centreline_ids
