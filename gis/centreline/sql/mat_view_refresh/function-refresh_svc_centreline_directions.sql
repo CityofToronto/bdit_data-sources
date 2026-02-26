4a05cd0a08b141212460073b464f3af8b6d8c0d7
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2026-02-26 19:11:58 +0000

    #1353 adjust traffic.svc_centreline_directions to truncate/insert

--- gis/centreline/sql/mat_view_refresh/function-refresh_svc_centreline_directions.sql
+++ gis/centreline/sql/mat_view_refresh/function-refresh_svc_centreline_directions.sql
@@ -6,7 +6,89 @@ LANGUAGE sql
 COST 100
 VOLATILE SECURITY DEFINER PARALLEL UNSAFE
 AS $BODY$
-REFRESH MATERIALIZED VIEW CONCURRENTLY traffic.svc_centreline_directions WITH DATA;
+
+/*
+A mid-block SVC/ATR count is assigned two of the four cardinal directions.
+These will be opposites (N/S or E/W), but a given centreline segment isn't
+necessarily defined as N/S or E/W per se. A diagonal segment could be
+assigned either pair while still being clear about which direction on the
+segment is meant.
+
+This view assigns directons, relative to the centreline segment, to each
+of the four cardinal directions. Some of these, like N/S on Bloor are
+extremely unlikely to show up in the data, and are excluded, but many other
+unlikely combinations are kept just in case.
+
+As such, this view should be joined on the actual directions assigned to SVCs
+rather than used on its own. Such a join *should* filter out most silly values.
+*/
+
+TRUNCATE traffic.svc_centreline_directions;
+
+WITH to_cardinal (bearing, direction) AS (
+    -- define cardinal directions in degrees, but rotated
+    -- by -17 degrees to match the orientation of the street grid
+    VALUES
+    (radians(360 - 17), 'NB'),
+    (radians(90 - 17), 'EB'),
+    (radians(180 - 17), 'SB'),
+    (radians(270 - 17), 'WB')
+)
+
+INSERT INTO traffic.svc_centreline_directions (centreline_id, from_node, to_node, direction, centreline_geom_directed, absolute_angular_distance)
+SELECT
+    cl.centreline_id,
+    CASE
+        WHEN r.reversed THEN cl.to_intersection_id
+        ELSE cl.from_intersection_id
+    END AS from_node,
+    CASE
+        WHEN r.reversed THEN cl.from_intersection_id
+        ELSE cl.to_intersection_id
+    END AS to_node,
+    ad.direction,
+    CASE
+        WHEN r.reversed THEN ST_Reverse(cl.geom)
+        ELSE cl.geom
+    END AS centreline_geom_directed,
+    CASE
+        WHEN r.reversed THEN (180 - degrees(ad.angular_distance))::real
+        ELSE degrees(ad.angular_distance)::real
+    END AS absolute_angular_distance
+FROM gis_core.centreline_latest AS cl
+CROSS JOIN
+    LATERAL (
+        SELECT
+            to_cardinal.direction,
+            -- get the minimum angular distance between the compass bearing and centreline's azimuth
+            LEAST( -- math is done in radians
+                ABS(
+                    ST_Azimuth(ST_PointN(cl.geom, 1)::geography, ST_PointN(cl.geom, -1)::geography)
+                    - to_cardinal.bearing
+                ),
+                (2 * PI()) - ABS(
+                    ST_Azimuth(ST_PointN(cl.geom, 1)::geography, ST_PointN(cl.geom, -1)::geography)
+                    - to_cardinal.bearing
+                )
+            ) AS angular_distance
+        FROM to_cardinal
+    ) AS ad, -- ad for angular distance
+    LATERAL (
+        -- edge geometry should be reversed where such a reversal
+        -- would reduce the angular distance
+        SELECT ad.angular_distance > radians(90) AS reversed
+    ) AS r
+-- exclude results where the cardinal direction is orthogonal, +/- 10 degrees
+WHERE
+    ad.angular_distance < radians(80)
+    OR ad.angular_distance > radians(100);
+
+COMMENT ON TABLE traffic.svc_centreline_directions
+IS 'Maps the four cardinal directions (NB, SB, EB, & WB) referenced by SVCs onto '
+'specific directions of travel along edges of the `gis_core.centreline_latest` network. '
+'Refreshed automatically by `gcc_layers_pull_bigdata` DAG after inserts into '
+'`gis_core.centreline_latest`.' || ' Last updated: ' || CURRENT_DATE;
+
 $BODY$;
 
 ALTER FUNCTION traffic.refresh_svc_centreline_directions()
--- volumes/short_term_counting_program/sql/create-view-svc_centreline_directions.sql
+++ volumes/short_term_counting_program/sql/create-view-svc_centreline_directions.sql
@@ -14,74 +14,24 @@ As such, this view should be joined on the actual directions assigned to SVCs
 rather than used on its own. Such a join *should* filter out most silly values.
 */
 
-CREATE MATERIALIZED VIEW traffic.svc_centreline_directions AS
+CREATE TABLE traffic.svc_centreline_directions (
+    centreline_id integer,
+    from_node integer,
+    to_node integer,
+    direction text,
+    centreline_geom_directed geometry,
+    absolute_angular_distance real
+);
 
-WITH to_cardinal (bearing, direction) AS (
-    -- define cardinal directions in degrees, but rotated
-    -- by -17 degrees to match the orientation of the street grid
-    VALUES
-    (radians(360 - 17), 'NB'),
-    (radians(90 - 17), 'EB'),
-    (radians(180 - 17), 'SB'),
-    (radians(270 - 17), 'WB')
-)
-
-SELECT
-    cl.centreline_id,
-    CASE
-        WHEN r.reversed THEN cl.to_intersection_id
-        ELSE cl.from_intersection_id
-    END AS from_node,
-    CASE
-        WHEN r.reversed THEN cl.from_intersection_id
-        ELSE cl.to_intersection_id
-    END AS to_node,
-    ad.direction,
-    CASE
-        WHEN r.reversed THEN ST_Reverse(cl.geom)
-        ELSE cl.geom
-    END AS centreline_geom_directed,
-    CASE
-        WHEN r.reversed THEN (180 - degrees(ad.angular_distance))::real
-        ELSE degrees(ad.angular_distance)::real
-    END AS absolute_angular_distance
-FROM gis_core.centreline_latest AS cl
-CROSS JOIN
-    LATERAL (
-        SELECT
-            to_cardinal.direction,
-            -- get the minimum angular distance between the compass bearing and centreline's azimuth
-            LEAST( -- math is done in radians
-                ABS(
-                    ST_Azimuth(ST_PointN(cl.geom, 1)::geography, ST_PointN(cl.geom, -1)::geography)
-                    - to_cardinal.bearing
-                ),
-                (2 * PI()) - ABS(
-                    ST_Azimuth(ST_PointN(cl.geom, 1)::geography, ST_PointN(cl.geom, -1)::geography)
-                    - to_cardinal.bearing
-                )
-            ) AS angular_distance
-        FROM to_cardinal
-    ) AS ad, -- ad for angular distance
-    LATERAL (
-        -- edge geometry should be reversed where such a reversal
-        -- would reduce the angular distance
-        SELECT ad.angular_distance > radians(90) AS reversed
-    ) AS r
--- exclude results where the cardinal direction is orthogonal, +/- 10 degrees
-WHERE
-    ad.angular_distance < radians(80)
-    OR ad.angular_distance > radians(100);
-
-CREATE UNIQUE INDEX ON traffic.svc_centreline_directions (centreline_id, direction);
-
-ALTER MATERIALIZED VIEW traffic.svc_centreline_directions OWNER TO gis_admins;
-
-COMMENT ON MATERIALIZED VIEW traffic.svc_centreline_directions
+COMMENT ON TABLE traffic.svc_centreline_directions
 IS 'Maps the four cardinal directions (NB, SB, EB, & WB) referenced by SVCs onto '
 'specific directions of travel along edges of the `gis_core.centreline_latest` network. '
 'Refreshed automatically by `gcc_layers_pull_bigdata` DAG after inserts into '
 '`gis_core.centreline_latest`.';
+
+CREATE UNIQUE INDEX ON traffic.svc_centreline_directions (centreline_id, direction);
+
+ALTER TABLE traffic.svc_centreline_directions OWNER TO gis_admins;
 
 COMMENT ON COLUMN traffic.svc_centreline_directions.geom_directed
 IS 'centreline segment geom drawn in the direction of `direction`';
