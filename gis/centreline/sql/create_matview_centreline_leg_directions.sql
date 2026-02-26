be7a5c606aed76e88eebdfbba8be9272aabe68ba
Nate Wessel <nate.wessel@toronto.ca>
2026-02-26 13:57:29 -0500

    Expose absolute angular offset for easier downstream QC of leg assignment (#1361)
    * #1190 expose angular offset for easier downstream QC
    
    * #1190 reduce precision
    
    * qualify column comment

--- gis/centreline/sql/create_matview_centreline_leg_directions.sql
+++ gis/centreline/sql/create_matview_centreline_leg_directions.sql
@@ -266,6 +266,7 @@ SELECT DISTINCT ON (
     -- last node of the leg is the intersection
     ST_PointN(unified_legs.stub_geom, -1) AS intersection_geom,
     edges.linear_name_full_legal AS street_name,
+    unified_legs.angular_distance::real AS angular_offset_from_cardinal_direction,
     unified_legs.stub_geom AS leg_stub_geom,
     unified_legs.full_geom AS leg_full_geom
 FROM unified_legs
@@ -292,6 +293,9 @@ IS 'Automated mapping of centreline intersection legs onto the four cardinal dir
 
 COMMENT ON COLUMN gis_core.centreline_leg_directions.leg
 IS 'cardinal direction, one of (north, east, south, west)';
+
+COMMENT ON COLUMN gis_core.centreline_leg_directions.angular_offset_from_cardinal_direction
+IS 'absolute degrees difference from ideal cardinal direction (oriented to Toronto grid)';
 
 COMMENT ON COLUMN gis_core.centreline_leg_directions.leg_stub_geom
 IS 'first (up to) 30m of the centreline segment geometry pointing *inbound* toward the reference intersection';
