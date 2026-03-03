02589de0a86933cf6bd3e79421919b5900cb27f4
Natalie <46324452+chmnata@users.noreply.github.com>
2026-03-03 14:35:16 -0500

    KSI Open Data pipeline DAG (#1343)
    * #1332 add dag and sql
    
    * #1332 update dag to add human in the loop
    
    * #1332 update readme with diagram and column def
    
    * #1332 fix approval url
    
    * #1332 add check dup collision task and update readme
    
    * #1332 update readme
    
    * #1332 sql concat stname, create geom first, deal with accdate
    
    * #1343 update table and mat view owner
    
    * #1343 remove streetype and dir from readme
    
    * #1343 sqlfluff
    
    * #1343 reduce summary task to decide approval task and simply somethings
    
    * #1343 update data flow png
    
    * #1343 fluff again
    
    * #1343 added another check
    
    * #1343 make: into []
    
    * #1343 add david as dag_owner to alert in slack
    
    * #1343 one last sqlfluff
    
    * #1343 change boolean fields to text
    
    * #1343 change dag execution time to be after collision replicator
    
    * #1343 update to >=2006 and remove uid
    
    * #1343 update mat view index, grant permission to human
    
    * #1343 remove uid from readme
    
    * #1343 make failtorem text from boolean
    
    * #1343 get rid of city_damage
    
    * #1343 update readme columns
    
    * #1343 remove city_damage in data check
    
    * #1343 change ward name to use area_desc
    
    * #1343 change spelling to manoeuvre
    
    * #1343 remove changed field
    
    * #1343 update readme and dataflow png

---
+++ collisions/ksi_open_data/sql/create_table_ksiopendata.sql
@@ -0,0 +1,64 @@
+CREATE TABLE IF NOT EXISTS open_data.ksi
+(
+    collision_id text NOT NULL,
+    accdate timestamp without time zone NOT NULL,
+    stname1 text,
+    stname2 text,
+    stname3 text,
+    per_inv integer,
+    acclass text,
+    accloc text,
+    traffictl text,
+    impactype text,
+    visible text,
+    light text,
+    rdsfcond text,
+    road_class text,
+    failtorem text,
+    longitude double precision,
+    latitude double precision,
+    veh_no smallint,
+    vehtype text,
+    initdir text,
+    per_no smallint,
+    invage smallint,
+    injury text,
+    safequip text,
+    drivact text,
+    drivcond text,
+    pedact text,
+    pedcond text,
+    manoeuvre text,
+    pedtype text,
+    cyclistype text,
+    cycact text,
+    cyccond text,
+    road_user text,
+    fatal_no smallint,
+    wardname text,
+    division character varying,
+    neighbourhood text,
+    aggressive text,
+    distracted text,
+    cyclist text,
+    motorcyclist text,
+    other_micromobility text,
+    older_adult text,
+    pedestrian text,
+    red_light text,
+    school_child text,
+    heavy_truck text,
+    CONSTRAINT collision_id_per_no UNIQUE (collision_id, per_no)
+);
+
+ALTER TABLE IF EXISTS open_data.ksi
+OWNER TO od_admins;
+
+GRANT SELECT ON TABLE open_data.ksi TO od_extract_svc;
+
+GRANT ALL ON TABLE open_data.ksi TO collisions_bot;
+
+REVOKE ALL ON TABLE open_data.ksi FROM bdit_humans;
+
+COMMENT ON TABLE open_data.ksi
+IS 'Table for KSI open data, link: ';
