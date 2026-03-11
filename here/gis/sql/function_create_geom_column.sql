722092ba6b3c80845db7997ee9250dc0379d125c
chmnata <chmnata@gmail.com>
2026-03-11 19:13:08 +0000

    #1355 make sure to also deal with multipoint and not just lines

--- here/gis/sql/function_create_geom_column.sql
+++ here/gis/sql/function_create_geom_column.sql
@@ -22,8 +22,34 @@ IF wkbgeom_exists THEN
 	RAISE NOTICE 'No wkb_geometry column.';
 END IF;
 
-EXECUTE FORMAT('ALTER TABLE here_gis.%I ALTER COLUMN geom SET DATA TYPE geometry(LineString,4326) USING ST_GeometryN(geom, 1);', tablename);
 
+-- see if geom is point or line
+EXECUTE FORMAT(
+    'SELECT ST_GeometryType(geom) FROM here_gis.%I WHERE geom IS NOT NULL LIMIT 1',
+    tablename
+) INTO geom_type;
+
+-- set geometry for point
+IF geom_type = 'ST_MultiPoint' THEN
+    EXECUTE FORMAT(
+        'ALTER TABLE here_gis.%I
+         ALTER COLUMN geom
+         SET DATA TYPE geometry(Point,4326)
+         USING ST_GeometryN(geom,1);',
+        tablename
+    );
+-- set geometry for line
+ELSIF geom_type = 'ST_MultiLineString' THEN
+    EXECUTE FORMAT(
+        'ALTER TABLE here_gis.%I
+         ALTER COLUMN geom
+         SET DATA TYPE geometry(LineString,4326)
+         USING ST_GeometryN(geom,1);',
+        tablename
+    );
+ELSE
+        RAISE NOTICE 'Geometry type % not handled.', geom_type;
+    END IF;
 RETURN 1;
 END;
 $BODY$;
