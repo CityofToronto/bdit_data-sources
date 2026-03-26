11137a3fda1fc0a99c874d6dc0dbe82f2624ce56
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2026-03-26 20:54:05 +0000

    #1340 enforce auto_flagged range notes are not altered

--- volumes/miovision/sql/table/create_anomalous_ranges.sql
+++ volumes/miovision/sql/table/create_anomalous_ranges.sql
@@ -29,7 +29,10 @@ CREATE TABLE miovision_api.anomalous_ranges (
     ON DELETE NO ACTION,
     CONSTRAINT miovision_anomalous_ranges_range_order CHECK (
         range_start < range_end
-    )
+    ),
+    CONSTRAINT anomalous_ranges_check CHECK (
+    CASE WHEN investigation_level = 'auto_flagged'::text THEN notes = 'Zero counts, identified by a daily airflow process running function miovision_api.identify_zero_counts'::text
+    ELSE NULL::boolean END)
 );
 
 ALTER TABLE IF EXISTS miovision_api.anomalous_ranges
