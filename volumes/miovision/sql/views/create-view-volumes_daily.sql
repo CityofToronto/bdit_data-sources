e8ba28c437f8ddc78159478eb1f900bb0ebda739
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2026-03-26 20:45:28 +0000

    #1340 filter out all vehicles if lights absent

--- volumes/miovision/sql/views/create-view-volumes_daily.sql
+++ volumes/miovision/sql/views/create-view-volumes_daily.sql
@@ -22,6 +22,9 @@ CREATE VIEW miovision_api.volumes_daily AS (
             OR ar.intersection_uid IS NULL
         ) AND (
             ar.classification_uid = v.classification_uid
+            --don't use any vehicle modes if lights are anomalous
+            OR (ar.classification_uid = 1 AND v.classification_uid IN (1,3,4,5,9))
+            --issue affects all modes
             OR ar.classification_uid IS NULL
         )
         --omitting join to ar.leg implies that a single leg outage is
