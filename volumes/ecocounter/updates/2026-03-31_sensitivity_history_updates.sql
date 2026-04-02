1d974fd468037f268aab0e726fbfa3fb3ee11356
A-DUYVESTYN <a.duyvestyn@gmail.com>
2026-04-02 15:41:55 +0000

    #1369 address comments

--- volumes/ecocounter/updates/2026-03-31_sensitivity_history_updates.sql
+++ volumes/ecocounter/updates/2026-03-31_sensitivity_history_updates.sql
@@ -8,7 +8,7 @@ SET
     date_range = daterange(lower(date_range), '2025-05-14', '[)')
 WHERE flow_id IN (353324722, 353324720) AND date_range = '[2022-07-25,)';
 
-INSERT INTO ecocounter.sensitivity_history (
+INSERT INTO ecocounter.sensitivity_history(flow_id, date_range, setting) (
 VALUES 
     (353324722, daterange('2025-05-14', NULL, '[)'), '2 (01C0)'),
     (353324720, daterange('2025-05-14', NULL, '[)'), '2 (01C0)')
@@ -20,7 +20,7 @@ SET
     date_range = daterange(lower(date_range), '2025-05-14', '[)')
 WHERE flow_id IN (353341348) AND date_range = '[2024-09-01,)';
 
-INSERT INTO ecocounter.sensitivity_history (
+INSERT INTO ecocounter.sensitivity_history(flow_id, date_range, setting) (
 VALUES 
     (353341348, daterange('2025-05-14', NULL, '[)'), '2')
 );
@@ -28,17 +28,17 @@ VALUES
 -- Newly repaired or reconfigured sensors
 
 -- Bloor St E, west of Castle Frank Rd
--- re-installed sensors started producing good data on ~2025-03-01. after ~2 yr gap.
-INSERT INTO ecocounter.sensitivity_history (
+-- re-installed sensors started producing good data on 2025-02-27. after ~2 yr gap.
+INSERT INTO ecocounter.sensitivity_history(flow_id, date_range, setting) (
 VALUES 
-    (353517542, daterange('2025-03-01', NULL, '[)'), 'initial config after repair'),
-    (353354428, daterange('2025-03-01', NULL, '[)'), 'initial config after repair')
+    (353517542, daterange('2025-02-27', NULL, '[)'), 'initial config after repair'),
+    (353354428, daterange('2025-02-27', NULL, '[)'), 'initial config after repair')
 );
 
 -- Bloor St W, west of Huron St (site_id = 300028396) - 
 -- reconfigured sensors (8 in total for bike,scooter,main-flow,contra-flow,2 sides-of-the-road)
 -- started producing good data on 2025-06-27.
-INSERT INTO ecocounter.sensitivity_history (
+INSERT INTO ecocounter.sensitivity_history(flow_id, date_range, setting) (
 VALUES 
     (353341333, daterange('2025-06-27', NULL, '[)'), 'initial config after reinstallation'),
     (353554896, daterange('2025-06-27', NULL, '[)'), 'initial config after install'),
