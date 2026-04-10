c9d2d884ef4a74f5847758bea450a1531e176331
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2026-04-10 19:12:04 +0000

    #1383 use generate series to ensure no missing days

--- congestion/function/function-agg_overnight_tt.sql
+++ congestion/function/function-agg_overnight_tt.sql
@@ -5,50 +5,53 @@ RETURNS void
 SECURITY DEFINER
 AS $$
 
-    WITH windowed AS (
-        SELECT DISTINCT
-            segment_id,
-            date_part('isodow', dt) <= 5 AS is_wkdy,
-            dt,
-            AVG(tt) OVER (PARTITION BY segment_id, date_part('isodow', dt) <= 5 ORDER BY dt RANGE BETWEEN '6 months' PRECEDING AND '1 day' PRECEDING) AS overnight_avg_tt,
-            --alternative: weighted avg
-            --SUM(tt * num_obs) / SUM(num_obs) OVER (PARTITION BY segment_id, date_part('isodow', dt) <= 5 ORDER BY dt RANGE BETWEEN '6 months' PRECEDING AND '1 day' PRECEDING) AS overnight_avg_tt
-            COUNT(*) OVER (PARTITION BY segment_id, date_part('isodow', dt) <= 5 ORDER BY dt RANGE BETWEEN '6 months' PRECEDING AND '1 day' PRECEDING) AS rolling_6month_quasi_obs
-        FROM here_agg.raw_segments
-        WHERE
-            dt >= agg_overnight_tt.mnth - interval '6 months'
-            AND dt < agg_overnight_tt.mnth + interval '1 month'
-            AND (
-                (
-                    dt < '2024-01-01'::date
-                    AND hr BETWEEN 0 AND 3
-                ) OR (
-                    dt >= '2024-01-01'::date
-                    AND hr BETWEEN 1 AND 4
-                )
-            )
-        ORDER BY
-            is_wkdy DESC,
-            segment_id,
-            dt
-    )
-
     INSERT INTO here_agg.segment_overnight_tts (
-        segment_id, dt, is_wkdy, overnight_avg_tt, rolling_6month_quasi_obs
+        segment_id, dt, is_wkdy, overnight_avg_tt, overnight_avg_tt_weighted, rolling_6month_quasi_obs
     )
     SELECT
-        segment_id,
-        dt,
-        is_wkdy,
-        overnight_avg_tt,
-        rolling_6month_quasi_obs
-    FROM windowed
+        rs.segment_id,
+        gs.dt::date AS dt,
+        date_part('isodow', gs.dt) <= 5 AS is_wkdy,
+        AVG(tt) AS overnight_avg_tt,
+        --alternative: weighted avg
+        SUM(tt * num_obs) / SUM(num_obs) AS overnight_avg_tt_weighted,
+        COUNT(*) AS rolling_6month_quasi_obs
+    FROM generate_series(
+        agg_overnight_tt.mnth,
+        agg_overnight_tt.mnth::date + interval '1 month' - interval '1 day',
+        interval '1 day'
+    ) AS gs(dt)
+    LEFT JOIN here_agg.raw_segments AS rs ON
+        --range between
+        rs.dt >= gs.dt - interval '6 months' --6 months before
+        AND rs.dt < gs.dt --1 day preceding
+        AND (date_part('isodow', gs.dt) <= 5) = (date_part('isodow', rs.dt) <= 5 ) --is_wkdy = is_wkdy
+        AND (
+            (
+                rs.dt < '2024-01-01'::date
+                AND rs.hr BETWEEN 0 AND 3
+            ) OR (
+                rs.dt >= '2024-01-01'::date
+                AND rs.hr BETWEEN 1 AND 4
+            )
+        )
     WHERE
-        dt >= agg_overnight_tt.mnth
+        --partition selection
+        rs.dt >= agg_overnight_tt.mnth::date - interval '6 months'
+        AND rs.dt < agg_overnight_tt.mnth::date + interval '1 month'
+    GROUP BY
+        rs.segment_id,
+        date_part('isodow', gs.dt) <= 5,
+        gs.dt
+    ORDER BY
+        gs.dt,
+        is_wkdy DESC,
+        rs.segment_id
     ON CONFLICT ON CONSTRAINT segment_overnight_tts_pkey
     DO UPDATE
     SET
         overnight_avg_tt = EXCLUDED.overnight_avg_tt,
+        overnight_avg_tt_weighted = EXCLUDED.overnight_avg_tt_weighted,
         rolling_6month_quasi_obs = EXCLUDED.rolling_6month_quasi_obs;
        
 $$ LANGUAGE sql;
