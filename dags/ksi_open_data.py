5921fd7f705441fd1842b8b985f4b966b8abab48
Natalie <46324452+chmnata@users.noreply.github.com>
2026-04-29 17:09:42 -0400

    KSI DAG update slack noti channel (#1373)
    * #1362 update slack noti channel
    
    * #1364 also add open data link to readme
    
    * #1364 remove partial for slack alert
    
    * #1364 remove channel in send slack msg too

--- dags/ksi_open_data.py
+++ dags/ksi_open_data.py
@@ -37,7 +37,7 @@ default_args = {'owner': ','.join(DAG_OWNERS),
                 'email_on_success': False,
                 'retries': 0,
                 'retry_delay': timedelta(minutes=5),
-                'on_failure_callback': partial(task_fail_slack_alert, channel="slack_data_pipeline_dev")
+                'on_failure_callback': task_fail_slack_alert,
                 }
 
 @dag(
@@ -167,7 +167,6 @@ def ksi_opan_data():
                 f"{details}\n"
                 f"Approve/reject *<{url}|here>*."
             ),
-            channel="slack_data_pipeline_dev",
         )
 
     @task.branch
@@ -211,7 +210,6 @@ def ksi_opan_data():
         send_slack_msg(
             context=context,
             msg=f"KSI table successfully refreshed :white_check_mark:.",
-            channel='slack_data_pipeline_dev'
             )
     
     checks = data_checks()
