0fe862034c4090e70e50f5f378f0453583d4845a
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2026-05-14 16:56:37 -0400

    #1156 fix ordering in centreline inferred geoms

--- bluetooth/itsc/sql/function-add_bluetooth_path_geom.sql
+++ bluetooth/itsc/sql/function-add_bluetooth_path_geom.sql
@@ -1,16 +1,28 @@
 CREATE OR REPLACE FUNCTION bluetooth.itsc_add_bluetooth_path_geom()
 RETURNS trigger AS $$
 BEGIN
-    -- Add geom by getting the matching paths from centreline_latest
-    WITH cent AS (
-        SELECT DISTINCT ON (centreline_id) centreline_id, geom
-        FROM gis_core.centreline
-        WHERE centreline_id = ANY(NEW.centreline_ids) --array of centreline_ids
-        ORDER BY centreline_id, version_date DESC
-    )
-    SELECT st_union(geom) INTO NEW.geom
-    FROM cent;
+    WITH ordered_ids AS (
+        SELECT ordinality, centreline_id
+        -- Unnest the array of centreline_ids with ordinality to preserve order
+        FROM unnest(NEW.centreline_ids) WITH ORDINALITY AS t (centreline_id, ordinality)
+    ),
     
+    cent AS (
+        -- Add geom by getting the paths from centreline (sort by version_id)
+        SELECT DISTINCT ON (c.centreline_id)
+            c.centreline_id,
+            c.geom,
+            o.ordinality
+        FROM gis_core.centreline AS c
+        JOIN ordered_ids AS o USING (centreline_id)
+        ORDER BY
+            c.centreline_id,
+            c.version_date DESC
+    )
+    
+    SELECT ST_LineMerge(ST_Collect(geom ORDER BY ordinality)) INTO NEW.geom
+    FROM cent;
+
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
