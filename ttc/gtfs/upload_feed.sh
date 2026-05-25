d7fb1d304f960902f6277ef6578ab763b35caea6
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2026-05-25 18:46:56 +0000

    #1404 make calendar_dates.txt conditional on file existance

--- ttc/gtfs/upload_feed.sh
+++ ttc/gtfs/upload_feed.sh
@@ -2,7 +2,9 @@
 set -e  # Exit on any error
 cd $1
 /usr/bin/psql -h $HOST -U $LOGIN -d bigdata -c "\COPY gtfs.calendar(service_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday, start_date, end_date) FROM 'calendar.txt' WITH (FORMAT 'csv', HEADER TRUE) ;"
-/usr/bin/psql -h $HOST -U $LOGIN -d bigdata -c "\COPY gtfs.calendar_dates(service_id, date_, exception_type) FROM 'calendar_dates.txt' WITH (FORMAT 'csv', HEADER TRUE) ;"
+if [ -f "calendar_dates.txt" ]; then
+    /usr/bin/psql -h $HOST -U $LOGIN -d bigdata -c "\COPY gtfs.calendar_dates(service_id, date_, exception_type) FROM 'calendar_dates.txt' WITH (FORMAT 'csv', HEADER TRUE) ;"
+fi
 /usr/bin/psql -h $HOST -U $LOGIN -d bigdata -c "\COPY gtfs.routes(route_id, agency_id, route_short_name, route_long_name, route_desc, route_type, route_url, route_color, route_text_color) FROM 'routes.txt' WITH (FORMAT 'csv', HEADER TRUE) ;"
 /usr/bin/psql -h $HOST -U $LOGIN -d bigdata -c "\COPY gtfs.shapes(shape_id, shape_pt_lat, shape_pt_lon, shape_pt_sequence, shape_dist_traveled) FROM 'shapes.txt' WITH (FORMAT 'csv', HEADER TRUE) ;"
 /usr/bin/psql -h $HOST -U $LOGIN -d bigdata -c "\COPY gtfs.stop_times(trip_id, arrival_time, departure_time, stop_id, stop_sequence, stop_headsign, pickup_type, drop_off_type, shape_dist_traveled) FROM 'stop_times.txt' WITH (FORMAT 'csv', HEADER TRUE) ;"
