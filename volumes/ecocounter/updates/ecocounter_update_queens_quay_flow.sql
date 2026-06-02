a84857afc35b1996ad6e7ff2e53eda19508b06e3
Wael Halabi <wael.halabi@toronto.ca>
2026-06-02 16:15:31 +0000

    #1410 updated includes contraflow field

--- volumes/ecocounter/updates/ecocounter_update_queens_quay_flow.sql
+++ volumes/ecocounter/updates/ecocounter_update_queens_quay_flow.sql
@@ -2,18 +2,18 @@ UPDATE ecocounter.flows_unfiltered AS fu
 SET
     flow_direction = vals.flow_direction,
     direction_main = vals.direction_main,
-    includes_contraflow = vals.includes_contraflow,
-    validated = vals.validated,
+    includes_contraflow = FALSE,
+    validated = FALSE,
     mode_counted = vals.mode_counted
 FROM (
     VALUES
-    (353706300, 'Westbound', 'Eastbound'::gwolofs.travel_directions, TRUE,  FALSE, 'bike'),
-    (353706299, 'Eastbound', 'Eastbound'::gwolofs.travel_directions, FALSE, FALSE, 'bike'),
-    (353706301, 'Eastbound', 'Eastbound'::gwolofs.travel_directions, FALSE, FALSE, 'scooter'),
-    (353706302, 'Westbound', 'Eastbound'::gwolofs.travel_directions, TRUE,  FALSE, 'scooter'),
-    (353706303, 'Westbound', 'Westbound'::gwolofs.travel_directions, FALSE, FALSE, 'bike'),
-    (353706304, 'Eastbound', 'Westbound'::gwolofs.travel_directions, TRUE,  FALSE, 'bike'),
-    (353706305, 'Westbound', 'Westbound'::gwolofs.travel_directions, FALSE, FALSE, 'scooter'),
-    (353706306, 'Eastbound', 'Westbound'::gwolofs.travel_directions, TRUE,  FALSE, 'scooter')
-) AS vals(flow_id, flow_direction, direction_main, includes_contraflow, validated, mode_counted)
+    (353706300, 'Westbound', 'Eastbound'::gwolofs.travel_directions, 'bike'),
+    (353706299, 'Eastbound', 'Eastbound'::gwolofs.travel_directions, 'bike'),
+    (353706301, 'Eastbound', 'Eastbound'::gwolofs.travel_directions, 'scooter'),
+    (353706302, 'Westbound', 'Eastbound'::gwolofs.travel_directions, 'scooter'),
+    (353706303, 'Westbound', 'Westbound'::gwolofs.travel_directions, 'bike'),
+    (353706304, 'Eastbound', 'Westbound'::gwolofs.travel_directions, 'bike'),
+    (353706305, 'Westbound', 'Westbound'::gwolofs.travel_directions, 'scooter'),
+    (353706306, 'Eastbound', 'Westbound'::gwolofs.travel_directions, 'scooter')
+) AS vals(flow_id, flow_direction, direction_main, mode_counted)
 WHERE vals.flow_id = fu.flow_id;
