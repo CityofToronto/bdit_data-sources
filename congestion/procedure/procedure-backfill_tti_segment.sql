bb0148a7aa25cbbe589a36f85b4ac53c775730d7
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2026-06-04 21:01:14 +0000

    #1383 fix to backfill procedure

--- congestion/procedure/procedure-backfill_tti_segment.sql
+++ congestion/procedure/procedure-backfill_tti_segment.sql
@@ -13,6 +13,8 @@ BEGIN
         RAISE NOTICE 'Processing date: %', v_current_date;
         RAISE NOTICE '[%] Running dynamic binning', v_current_date;
 
+        DELETE FROM here_agg.raw_segments WHERE segment_id = ANY(p_segments) AND dt = v_current_date::date;
+        
         --segment dynamic binning
         PERFORM here_agg.network_segment_agg(
             v_current_date,
@@ -21,7 +23,10 @@ BEGIN
 
         -- On the first day of the month, aggregate the previous months lookback stats (overnight, pkt)
         IF EXTRACT(DAY FROM v_current_date) = 1 THEN
-
+            
+            --this would have to be for the previous month, and have a delete statement preceding.
+            --PERFORM here_agg.monthly_bootstrap(date_trunc('month', v_current_date::date)::date, p_segments);
+            
             RAISE NOTICE '[%] Aggregating the previous months lookback stats (overnight, pkt)', v_current_date;
 
             --add pkt/overnight speeds for the segments
@@ -31,12 +36,18 @@ BEGIN
             );
 
         END IF;
+        
+        --this would have to be for the previous week, and have a delete statement preceding.
+        -- Weekly aggregation run every Saturdays
+        /*IF EXTRACT(ISODOW FROM v_current_date) = 6 THEN
+            PERFORM here_agg.weekly_bootstrap(date_trunc('month', v_current_date::date)::date, p_segments);
+        END IF;*/
 
         -- Every day
         RAISE NOTICE '[%] Running daily aggregations', v_current_date;
 
         --store hourly avg speeds
-        PERFORM here_agg.segment_travel_times_hrly_avg_agg(
+        PERFORM here_agg.segment_travel_times_hrly_agg(
             v_current_date,
             p_segments
         );
