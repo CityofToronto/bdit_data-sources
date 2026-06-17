12b856bc7518500cfab9b2eedeecbbc4b07dc609
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2026-06-17 19:15:02 +0000

    #1429 extract tpana xml

---
+++ bluetooth/tpana/tpana_detectors.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS bluetooth.tpana_detectors (
+    detector_id varchar primary key,
+    detector_name varchar,
+    short_name varchar,
+    equipment_type varchar,
+    latitude float,
+    longitude float,
+    altitude float,
+    utc_offset int,
+    allowed_silence_s int,
+    gapout_time_s int,
+    real_time_stats int,
+    battery_alarm_threshold_v float,
+    additional_info text,
+    comments text
+);
+
+COMMENT ON TABLE bluetooth.tpana_detectors
+IS 'Detector details extracted from TPANA''s Equipment.xml received on 2026-06-02.';
+
+ALTER TABLE bluetooth.tpana_detectors OWNER TO bt_admins;
+
+GRANT SELECT ON TABLE bluetooth.tpana_detectors TO bdit_humans;
+
+ALTER TABLE bluetooth.tpana_detectors ADD COLUMN geom geometry;
+UPDATE bluetooth.tpana_detectors SET geom = st_setsrid(st_makepoint(longitude, latitude), 4326);
---
+++ bluetooth/tpana/tpana_links.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS bluetooth.tpana_links (
+    link_id varchar primary key,
+    link_name varchar,
+    short_name varchar,
+    additional_info text,
+    src_detector_id varchar references bluetooth.tpana_detectors(detector_id),
+    dest_detector_id varchar references bluetooth.tpana_detectors(detector_id),
+    line_distance_m float,
+    path_distance_m float,
+    route_direction_name varchar,
+    speed_limit_kmh int,
+    max_travel_time_s int,
+    detection_rules varchar,
+    real_time_stats varchar,
+    comments text,
+    extra_info text
+);
+
+COMMENT ON TABLE bluetooth.tpana_links
+IS 'Link details extracted from TPANA''s Equipment.xml received on 2026-06-02.';
+
+ALTER TABLE bluetooth.tpana_links OWNER TO bt_admins;
+
+GRANT SELECT ON TABLE bluetooth.tpana_links TO bdit_humans;
