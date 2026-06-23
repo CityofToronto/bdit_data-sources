ced1760e9c017aec8496486a470015ef105636e2
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2026-06-23 19:43:41 +0000

    #1431 switch to pulling sites instead of flows

--- volumes/ecocounter/pull_data_from_api.py
+++ volumes/ecocounter/pull_data_from_api.py
@@ -6,6 +6,7 @@ from psycopg2 import connect
 from psycopg2.extras import execute_values
 from datetime import datetime, timedelta
 from airflow.exceptions import AirflowFailException
+import time
 
 LOGGER = logging.getLogger(__name__)
 
@@ -19,7 +20,11 @@ def getSites(password_getter: Callable[[], str], sites: any = ()):
     pw = password_getter() # Retrieve only when needed
     response = requests.get(
         f'{URL}/sites',
-        headers={'X-API-KEY': f'{pw}'}
+        headers={'X-API-KEY': f'{pw}'},
+        params={
+            'include': 'flows',
+            'pageSize': 500
+        }
     )
     if response.status_code!=200:
         raise AirflowFailException(f"{response.status_code}: {response.reason}")
@@ -33,31 +38,38 @@ def getSites(password_getter: Callable[[], str], sites: any = ()):
             result.append(site)
     return result
 
-# get all of a flows ("channel") data from the API
-def getFlowData(password_getter: Callable[[], str], flow_id: int, startDate: datetime, endDate: datetime):
+# get all of a site's data from the API
+def getSiteData(password_getter: Callable[[], str], site_id: int, startDate: datetime, endDate: datetime):
     pw = password_getter() # Retrieve only when needed
-    requestChunkSize = timedelta(days=100)
+    requestChunkSize = timedelta(days=30)
     requestStart = startDate
     data = []
     while requestStart < endDate:
         requestEnd = min(requestStart + requestChunkSize, endDate)
-        response = requests.get(
-            f'{URL}/history/traffic/raw/site/{flow_id}',
-            headers={'X-API-KEY': pw},
-            params={
-                'begin': requestStart.isoformat(timespec='seconds'),
-                'end':  requestEnd.isoformat(timespec='seconds'),
-                'complete': 'false',
-                'step': '15m'
-            }
-        )
-        if response.status_code==200:
-            data += response.json()
-            requestStart += requestChunkSize
-        elif response.status_code==401:
-            raise AirflowFailException(f"{response.status_code}: {response.reason}")
-        else:
-            raise AirflowFailException(f"{response.status_code}: {response.reason}")
+        for _ in range(3):
+            response = requests.get(
+                f'{URL}/history/traffic/raw',
+                headers={'X-API-KEY': pw},
+                params={
+                    'siteId': site_id,
+                    'startDate': requestStart.strftime('%Y-%m-%d'),
+                    'endDate':  requestEnd.strftime('%Y-%m-%d'),
+                    'gapFilling': 'false',
+                    'validatedDataOnly': 'false',
+                    'rawDataOnly': 'true'
+                }
+            )
+            if response.status_code==200:
+                data += response.json()
+                requestStart += requestChunkSize
+                break
+            elif response.status_code==429: #too many requests
+                LOGGER.warning('Retrying in 2 minutes if tries remain.')
+                time.sleep(120)
+            elif response.status_code==401:
+                raise AirflowFailException(f"{response.status_code}: {response.reason}")
+            else:
+                raise AirflowFailException(f"{response.status_code}: {response.reason}")
     return data
 
 def getKnownSites(conn: any):
@@ -65,14 +77,6 @@ def getKnownSites(conn: any):
         cur.execute('SELECT site_id FROM ecocounter.sites_unfiltered WHERE date_decommissioned IS NULL;')
         sites = cur.fetchall()
         return [site[0] for site in sites]
-
-def getKnownFlows(conn: any, site: int):
-    with conn.cursor() as cur:
-        cur.execute('SELECT flow_id FROM ecocounter.flows_unfiltered WHERE date_decommissioned IS NULL AND site_id = %s;',
-                    (site, )
-        )
-        flows = cur.fetchall()
-        return [flow[0] for flow in flows]
 
 # do we have a record of this site in the database?
 def siteIsKnownToUs(site_id: int, conn: any):
@@ -92,67 +96,72 @@ def flowIsKnownToUs(flow_id: int, conn: any):
         )
         return cursor.rowcount > 0
 
-# DANGER delete all count data for a given flow
-def truncateFlowSince(flow_id: int, conn: any, startDate: datetime, endDate: datetime):
+# DANGER delete all count data for a given site
+def truncateSiteSince(site_id: int, conn: any, startDate: datetime, endDate: datetime):
     with conn.cursor() as cursor:
         cursor.execute(
-            """DELETE FROM ecocounter.counts_unfiltered
-            WHERE flow_id = %s
-            AND datetime_bin >= %s
-            AND datetime_bin < %s""",
-            (flow_id, startDate, endDate)
+            """DELETE FROM ecocounter.counts_unfiltered AS c
+            USING ecocounter.flows_unfiltered AS f
+            WHERE
+                f.site_id = %s
+                AND c.flow_id = f.flow_id
+                AND c.datetime_bin >= %s
+                AND c.datetime_bin < %s""",
+            (site_id, startDate, endDate)
         )
 
 # insert records
-def insertFlowCounts(conn: any, volume: any):
+def insertSiteCounts(conn: any, volume: any):
     insert_query="INSERT INTO ecocounter.counts_unfiltered (flow_id, datetime_bin, volume) VALUES %s"
     with conn.cursor() as cur:
         execute_values(cur, insert_query, volume)
     return cur.query
 
 # insert new site record
-def insertSite(conn: any, site_id: int, site_name: str, counter: str, lon: float, lat: float):
+def insertSite(conn: any, site_id: int, site_name: str, lon: float, lat: float):
     insert_query="""
-    INSERT INTO ecocounter.sites_unfiltered (site_id, site_description, counter, geom, validated)
+    INSERT INTO ecocounter.sites_unfiltered (site_id, site_description, geom, validated)
     VALUES (
         %s::numeric,
-        %s::text,
         %s::text,
         ST_SetSRID(ST_MakePoint(%s, %s), 4326),
         null::boolean --not validated by default
     )
     """
     with conn.cursor() as cur:
-        cur.execute(insert_query, (site_id, site_name, counter, lon, lat))
+        cur.execute(insert_query, (site_id, site_name, lon, lat))
 
 # insert new flow record
-def insertFlow(conn: any, flow_id: int, site_id: int, flow_name: str, bin_size: int):
+def insertFlow(conn: any, flow_id: int, site_id: int, flow_name: str, granularity: str):
     insert_query="""
     INSERT INTO ecocounter.flows_unfiltered (flow_id, site_id, flow_direction, bin_size, validated)
     VALUES (
         %s::numeric,
         %s::numeric,
         coalesce(lower(substring(%s::text, '(West|East|North|South)'))||'bound', 'unknown'),
-        (%s::text || ' minutes')::interval,
+        CASE %s WHEN 'PT1H' THEN '60 minutes'::interval WHEN 'PT15M' THEN '15 minutes'::interval ELSE NULL END,
         null::boolean --not validated
     )
     """
     with conn.cursor() as cur:
-        cur.execute(insert_query, (flow_id, site_id, flow_name, bin_size))
+        cur.execute(insert_query, (flow_id, site_id, flow_name, granularity))
 
-def truncate_and_insert(conn, token, flow_id, start_date, end_date):
-    LOGGER.info(f'Attempting to fetch data for flow {flow_id} from {start_date} to {end_date}.')
+def truncate_and_insert(conn, token, site_id, start_date, end_date):
+    LOGGER.info(f'Attempting to fetch data for site {site_id} from {start_date} to {end_date}.')
     # empty the count table for this flow
-    truncateFlowSince(flow_id, conn, start_date, end_date)          
+    truncateSiteSince(site_id, conn, start_date, end_date)
     # and fill it back up!
-    counts = getFlowData(token, flow_id, start_date, end_date)
+    counts = getSiteData(token, site_id, start_date, end_date)
+    
     #convert response into a tuple for inserting
-    volume=[]
-    for count in counts:
-        row=(flow_id, count['date'], count['counts'])
-        volume.append(row)
-    LOGGER.info(f'{len(volume)} rows fetched for flow {flow_id} from {start_date} to {end_date}.')
-    insertFlowCounts(conn, volume)
+    for flow in counts:
+        volume=[]
+        for dt in flow['data']:
+            row=(flow['flowID'], dt['timestamp'], dt['counts'])
+            volume.append(row)
+        #log and insert one flow at a time
+        LOGGER.info(f"{len(volume)} rows fetched for flow {flow['flowID']} from {start_date} to {end_date}.")
+        insertSiteCounts(conn, volume)
 
 #for testing/pulling data without use of airflow.
 def run_api(
@@ -172,10 +181,4 @@ def run_api(
         if not siteIsKnownToUs(site['id'], conn):
             print('unknown site', site['id'], site['name'])
             continue
-        #"flow" == "channel"
-        for flow in site['channels']:
-            flow_id = flow['id']
-            if not flowIsKnownToUs(flow_id, conn):
-                print('unknown flow', flow_id)
-                continue
-            truncate_and_insert(conn, password_getter, flow_id, start_date, end_date)
+        truncate_and_insert(conn, password_getter, site['id'], start_date, end_date)
