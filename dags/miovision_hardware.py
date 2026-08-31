301fd5c527572e5c64dc3e558ed695825a872091
DavidB <david.boucaud@toronto.ca>
2026-08-31 17:51:17 +0000

    #1452 create_slack_message

--- dags/miovision_hardware.py
+++ dags/miovision_hardware.py
@@ -10,7 +10,8 @@ try:
     repo_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
     sys.path.insert(0, repo_path)
     from dags.dag_owners import owners
-    from bdit_dag_utils.utils.dag_functions import task_fail_slack_alert, get_readme_docmd
+    from bdit_dag_utils.utils.dag_functions import task_fail_slack_alert, send_slack_msg, get_readme_docmd
+    from bdit_dag_utils.utils.common_tasks import check_1st_of_month
     from volumes.miovision.api.configuration_info import (
         get_cameras, get_configuration_dates
     )
@@ -56,7 +57,28 @@ def pull_miovision_dag():
         with mio_postgres.get_conn() as conn:
             get_cameras(conn)
 
-    pull_config_dates()
+    @task(pre_execute=check_1st_of_month)
+    def create_slack_message(response, **context):
+        failure_count = len(response)
+        msg_str = f"Failure to pull_config_dates on {failure_count} intersections"
+
+        extra_msg = ""
+
+        if failure_count > 0:
+
+            for l in response:
+                intersection_id = l['intersection_id']
+                status_code = l['status_code']
+                response =  l['reason']
+                extra_msg += f"Intersection {intersection_id} recieved {status_code} error: {response} \n"
+
+        send_slack_msg(context=context,
+                       msg=msg_str,
+                       attachments=[{"text": extra_msg}],
+                       channel='slack_data_pipeline_data_quality')
+
     pull_camera_details()
+    failure_responses = pull_config_dates()
+    create_slack_message(response=failure_responses)
 
 pull_miovision_dag()
