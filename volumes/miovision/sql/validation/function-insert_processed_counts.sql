99ae15d68bef0e0abcbe021e7b3a261e172783ca
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2026-09-01 20:52:55 +0000

    #1284 use volumes and denylist to fix up which legs are being qced

--- volumes/miovision/sql/validation/function-insert_processed_counts.sql
+++ volumes/miovision/sql/validation/function-insert_processed_counts.sql
@@ -20,23 +20,36 @@ RETURNS void AS $$
         JOIN traffic.tmc_study_data AS dat USING (count_id)
         --cross join to get every movement
         CROSS JOIN miovision_validation.spec_class_move_map AS mmp,
+        --anti join denylist to exclude invalid (wrong way) movements
+        LATERAL (
+            SELECT COUNT(imd.*) AS invalid_movements
+            FROM miovision_api.intersection_movements_denylist AS imd
+            WHERE 
+                imd.intersection_uid = studies.intersection_uid
+                AND imd.classification_uid = ANY(mmp.classification_uids)
+                AND imd.leg = mmp.mio_leg
+                AND imd.movement_uid = ANY(mmp.movement_uids)
+        ) AS imd,
         --sum the right element of the spectrum study json based on spec_movement
         LATERAL (
             SELECT SUM(NULLIF(to_jsonb(dat.*) ->> unnested.spec_movement, ''::text)::numeric) AS spectrum_count
             FROM UNNEST(mmp.spec_movements) AS unnested(spec_movement)
-        ) AS lat(spectrum_count),
+        ) AS lat (spectrum_count),
         --lateral select Miovision volumes
         LATERAL (
-            SELECT COALESCE(SUM(v15.volume), 0) AS miovision_api_volume
-            FROM miovision_api.volumes_15min_mvt_unfiltered AS v15
+            SELECT COALESCE(SUM(v.volume), 0) AS miovision_api_volume
+            FROM miovision_api.volumes AS v
             WHERE
-                v15.datetime_bin = dat.time_start
-                AND v15.intersection_uid = studies.intersection_uid
-                AND v15.leg = mmp.mio_leg
-                AND v15.classification_uid = ANY(mmp.classification_uids)
-                AND v15.movement_uid = ANY(mmp.movement_uids)
-        ) AS mv(miovision_api_volume)
-        WHERE studies.processed = False
+                v.datetime_bin >= dat.time_start
+                AND v.datetime_bin < dat.time_end
+                AND v.intersection_uid = studies.intersection_uid
+                AND v.leg = mmp.mio_leg
+                AND v.classification_uid = ANY(mmp.classification_uids)
+                AND v.movement_uid = ANY(mmp.movement_uids)
+        ) AS mv (miovision_api_volume)
+        WHERE
+            studies.processed = False
+            AND imd.invalid_movements = 0 --movements not in denylist
     ),
     
     --only insert results with both non-zero spectrum and miovision api results.
@@ -48,7 +61,9 @@ RETURNS void AS $$
         GROUP BY
             intersection_uid,
             count_date
-        HAVING SUM(spec_count) > 0 AND SUM(miovision_api_volume) > 0
+        HAVING
+            SUM(spec_count) > 0
+            AND SUM(miovision_api_volume) > 0
     ),
 
     inserted AS (
